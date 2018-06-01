523567fdce58c7e66da0c31faa992a4d1add8ca5
Ferdinand <fexcalo@gmail.com>
2018-06-01 21:28:21 +0200

    Container Blocks shouldn't drop their ItemBlock.

--- src/main/java/net/fexcraft/mod/fvtm/blocks/ContainerBlock.java
+++ src/main/java/net/fexcraft/mod/fvtm/blocks/ContainerBlock.java
@@ -1,6 +1,7 @@
 package net.fexcraft.mod.fvtm.blocks;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import net.fexcraft.mod.fvtm.FVTM;
 import net.fexcraft.mod.fvtm.api.Container.ContainerData;
@@ -23,6 +24,8 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -228,7 +231,7 @@ public class ContainerBlock extends BlockContainer {
 
     @Override
     public ItemStack getPickBlock(IBlockState state, RayTraceResult target, World world, BlockPos pos, EntityPlayer player){
-        /*if(world.getTileEntity(pos) != null){
+        if(world.getTileEntity(pos) != null){
             try{
                 ContainerTileEntity tile = (ContainerTileEntity)world.getTileEntity(pos);
                 return tile.getContainerData().getContainer().getItemStack(tile.getContainerData());
@@ -236,8 +239,23 @@ public class ContainerBlock extends BlockContainer {
             catch(Exception e){
                 e.printStackTrace();
             }
-        }*/
+        }
         return ItemStack.EMPTY;
+    }
+    
+    @Override
+    public int quantityDropped(Random random){
+        return 0;
+    }
+    
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune){
+        return Items.AIR;
+    }
+    
+    @Override
+    public ItemStack getItem(World worldIn, BlockPos pos, IBlockState state){
+        return new ItemStack(this);
     }
 
 }

b3073de98dec569fa385feb614fb9cd2a676475c
Ferdinand <fexcalo@gmail.com>
2018-08-04 01:04:31 +0200

    (FVP) C9 Vehicle and a few Parts.

---
+++ src/main/java/net/fexcraft/mod/addons/fvp/models/part/c9/ModelC9ST.java
@@ -0,0 +1,79 @@
+package net.fexcraft.mod.addons.fvp.models.part.c9;
+
+import net.fexcraft.mod.fvtm.model.part.PartModel;
+import net.fexcraft.mod.lib.tmt.ModelRendererTurbo;
+
+public class ModelC9ST extends PartModel {
+
+    public ModelC9ST(){
+    	super(); textureX = 512; textureY = 512;
+        this.addToCreators("01e4af9b-2a30-471e-addf-f6338ffce04b");
+        steering = new ModelRendererTurbo[12];
+		steering[0] = new ModelRendererTurbo(this, 161, 137, textureX, textureY); // Box 446
+		steering[1] = new ModelRendererTurbo(this, 273, 137, textureX, textureY); // Box 447
+		steering[2] = new ModelRendererTurbo(this, 489, 9, textureX, textureY); // Box 448
+		steering[3] = new ModelRendererTurbo(this, 97, 41, textureX, textureY); // Box 449
+		steering[4] = new ModelRendererTurbo(this, 73, 129, textureX, textureY); // Box 450
+		steering[5] = new ModelRendererTurbo(this, 345, 129, textureX, textureY); // Box 451
+		steering[6] = new ModelRendererTurbo(this, 241, 113, textureX, textureY); // Box 452
+		steering[7] = new ModelRendererTurbo(this, 297, 113, textureX, textureY); // Box 453
+		steering[8] = new ModelRendererTurbo(this, 329, 113, textureX, textureY); // Box 454
+		steering[9] = new ModelRendererTurbo(this, 353, 113, textureX, textureY); // Box 455
+		steering[10] = new ModelRendererTurbo(this, 409, 129, textureX, textureY); // Box 456
+		steering[11] = new ModelRendererTurbo(this, 433, 145, textureX, textureY); // Box 457
+
+		steering[0].addShapeBox(0F, -1F, -1F, 4, 2, 2, 0F,0F, -0.4F, -0.4F, 0F, -0.4F, -0.4F, 0F, -0.4F, -0.4F, 0F, -0.4F, -0.4F, 0F, -0.4F, -0.4F, 0F, -0.4F, -0.4F, 0F, -0.4F, -0.4F, 0F, -0.4F, -0.4F); // Box 446
+		steering[0].setRotationPoint(6.5F, -2.5F, 8F);
+		steering[0].rotateAngleZ = -0.20943951F;
+
+		steering[1].addShapeBox(-1F, -1F, -1F, 1, 3, 2, 0F,-0.4F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, -0.4F, 0F, 0F, -0.4F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, -0.4F, 0F, 0F); // Box 447
+		steering[1].setRotationPoint(6.5F, -2.5F, 8F);
+		steering[1].rotateAngleZ = -0.20943951F;
+
+		steering[2].addBox(-0.8F, 2F, -1.5F, 1, 1, 3, 0F); // Box 448
+		steering[2].setRotationPoint(6.5F, -2.5F, 8F);
+		steering[2].rotateAngleZ = -0.20943951F;
+
+		steering[3].addBox(-0.8F, -3F, -1.5F, 1, 1, 3, 0F); // Box 449
+		steering[3].setRotationPoint(6.5F, -2.5F, 8F);
+		steering[3].rotateAngleZ = -0.20943951F;
+
+		steering[4].addBox(-0.8F, -1.5F, 2F, 1, 3, 1, 0F); // Box 450
+		steering[4].setRotationPoint(6.5F, -2.5F, 8F);
+		steering[4].rotateAngleZ = -0.20943951F;
+
+		steering[5].addBox(-0.8F, -1.5F, -3F, 1, 3, 1, 0F); // Box 451
+		steering[5].setRotationPoint(6.5F, -2.5F, 8F);
+		steering[5].rotateAngleZ = -0.20943951F;
+
+		steering[6].addShapeBox(-0.8F, -3F, -2.5F, 1, 1, 1, 0F,0F, -1.5F, 0.5F, 0F, -1.5F, 0.5F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0.5F, -0.5F, 0F, 0.5F, -0.5F, 0F, 0F, 0F, 0F, 0F, 0F); // Box 452
+		steering[6].setRotationPoint(6.5F, -2.5F, 8F);
+		steering[6].rotateAngleZ = -0.20943951F;
+
+		steering[7].addShapeBox(-0.8F, 2F, -2.5F, 1, 1, 1, 0F,0F, 0.5F, -0.5F, 0F, 0.5F, -0.5F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, -1.5F, 0.5F, 0F, -1.5F, 0.5F, 0F, 0F, 0F, 0F, 0F, 0F); // Box 453
+		steering[7].setRotationPoint(6.5F, -2.5F, 8F);
+		steering[7].rotateAngleZ = -0.20943951F;
+
+		steering[8].addShapeBox(-0.8F, 2F, 1.5F, 1, 1, 1, 0F,0F, 0F, 0F, 0F, 0F, 0F, 0F, 0.5F, -0.5F, 0F, 0.5F, -0.5F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, -1.5F, 0.5F, 0F, -1.5F, 0.5F); // Box 454
+		steering[8].setRotationPoint(6.5F, -2.5F, 8F);
+		steering[8].rotateAngleZ = -0.20943951F;
+
+		steering[9].addShapeBox(-0.8F, -3F, 1.5F, 1, 1, 1, 0F,0F, 0F, 0F, 0F, 0F, 0F, 0F, -1.5F, 0.5F, 0F, -1.5F, 0.5F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0.5F, -0.5F, 0F, 0.5F, -0.5F); // Box 455
+		steering[9].setRotationPoint(6.5F, -2.5F, 8F);
+		steering[9].rotateAngleZ = -0.20943951F;
+
+		steering[10].addShapeBox(-1F, 0F, -0.5F, 1, 3, 1, 0F,-0.4F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, -0.4F, 0F, 0F, -0.3F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, -0.3F, 0F, 0F); // Box 456
+		steering[10].setRotationPoint(6.5F, -2.5F, 8F);
+		steering[10].rotateAngleX = 2.26892803F;
+		steering[10].rotateAngleZ = -0.20943951F;
+
+		steering[11].addShapeBox(-1F, 0F, -0.5F, 1, 3, 1, 0F,-0.4F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, -0.4F, 0F, 0F, -0.3F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, -0.3F, 0F, 0F); // Box 457
+		steering[11].setRotationPoint(6.5F, -2.5F, 8F);
+		steering[11].rotateAngleX = -2.26892803F;
+		steering[11].rotateAngleZ = -0.20943951F;
+
+        translateAll(0F, 0F, 0F);
+        flipAll();
+    }
+
+}
---
+++ src/main/java/net/fexcraft/mod/addons/fvp/models/part/c9/ModelC9Seats.java
@@ -0,0 +1,68 @@
+package net.fexcraft.mod.addons.fvp.models.part.c9;
+
+import net.fexcraft.mod.fvtm.model.part.PartModel;
+import net.fexcraft.mod.lib.tmt.ModelRendererTurbo;
+
+public class ModelC9Seats extends PartModel {
+
+    public ModelC9Seats(){
+    	super(); textureX = 512; textureY = 512;
+        this.addToCreators("01e4af9b-2a30-471e-addf-f6338ffce04b");
+		body = new ModelRendererTurbo[13];
+		body[0] = new ModelRendererTurbo(this, 65, 169, textureX, textureY); // Box 381
+		body[1] = new ModelRendererTurbo(this, 249, 177, textureX, textureY); // Box 427
+		body[2] = new ModelRendererTurbo(this, 393, 193, textureX, textureY); // Box 428
+		body[3] = new ModelRendererTurbo(this, 441, 193, textureX, textureY); // Box 429
+		body[4] = new ModelRendererTurbo(this, 145, 201, textureX, textureY); // Box 430
+		body[5] = new ModelRendererTurbo(this, 1, 177, textureX, textureY); // Box 431
+		body[6] = new ModelRendererTurbo(this, 393, 169, textureX, textureY); // Box 432
+		body[7] = new ModelRendererTurbo(this, 49, 193, textureX, textureY); // Box 433
+		body[8] = new ModelRendererTurbo(this, 409, 169, textureX, textureY); // Box 434
+		body[9] = new ModelRendererTurbo(this, 193, 201, textureX, textureY); // Box 442
+		body[10] = new ModelRendererTurbo(this, 321, 201, textureX, textureY); // Box 443
+		body[11] = new ModelRendererTurbo(this, 353, 201, textureX, textureY); // Box 444
+		body[12] = new ModelRendererTurbo(this, 9, 209, textureX, textureY); // Box 445
+
+		body[0].addShapeBox(0F, 0F, 0F, 11, 4, 34, 0F, 0F, 0F, 0.4F, -0.3F, 0F, -0.4F, -0.3F, 0F, -0.4F, 0F, 0F, 0.4F, 0F, 0F, 0.6F, 0.4F, 0F, 0F, 0.4F, 0F, 0F, 0F, 0F, 0.6F); // Box 381
+		body[0].setRotationPoint(-21.5F, 7.8F, -17F);
+
+		body[1].addShapeBox(0F, 0F, 0F, 3, 15, 32, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, -5F, 0F, 2F, 4F, 0F, 0F, 4F, 0F, 0F, -5F, 0F, 2F); // Box 427
+		body[1].setRotationPoint(-27F, -6.6F, -16F);
+
+		body[2].addShapeBox(0F, 0F, 0F, 11, 1, 12, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F); // Box 428
+		body[2].setRotationPoint(-37F, -6.5F, -6F);
+
+		body[3].addShapeBox(0F, 0F, 0F, 11, 1, 11, 0F, -0.8F, -0.85F, -0.5F, 0F, -0.2F, -0.5F, 0F, 0F, 0F, 0F, 0F, 0F, -0.9F, 0.8F, 0F, 0F, 0F, -0.5F, 0F, 0F, 0F, 0F, 0F, 0F); // Box 429
+		body[3].setRotationPoint(-37F, -6.5F, -17F);
+
+		body[4].addShapeBox(0F, 0F, 0F, 11, 1, 11, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, -0.2F, -0.5F, -0.8F, -0.85F, -0.5F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, -0.5F, -0.9F, 0.8F, 0F); // Box 430
+		body[4].setRotationPoint(-37F, -6.5F, 6F);
+
+		body[5].addShapeBox(0F, 0F, 0F, 2, 17, 2, 0F, 7.5F, 0F, 0F, 0F, 0F, 0F, -1.5F, -0.5F, -1F, 2F, -0.1F, -0.5F, 1F, -9.5F, 0F, 5F, 0F, 0F, 4F, 0F, 1F, 0F, -8F, 0F); // Box 431
+		body[5].setRotationPoint(-27F, -6.6F, 16F);
+
+		body[6].addShapeBox(0F, 0F, 0F, 3, 9, 1, 0F, -0.4F, 0.5F, -0.5F, 0F, 0F, 0F, -1.2F, -6.2F, 0.8F, 0.1F, -1.1F, 0.3F, 0F, -2F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, -2F, 0F); // Box 432
+		body[6].setRotationPoint(-29.5F, -6.1F, 17F);
+
+		body[7].addShapeBox(0F, 0F, 0F, 2, 17, 2, 0F, 2F, -0.1F, -0.5F, -1.5F, -0.5F, -1F, 0F, 0F, 0F, 7.5F, 0F, 0F, 0F, -8F, 0F, 4F, 0F, 1F, 5F, 0F, 0F, 1F, -9.5F, 0F); // Box 433
+		body[7].setRotationPoint(-27F, -6.6F, -18F);
+
+		body[8].addShapeBox(0F, 0F, 0F, 3, 9, 1, 0F, 0.1F, -1.1F, 0.3F, -1.2F, -6.2F, 0.8F, 0F, 0F, 0F, -0.4F, 0.5F, -0.5F, 0F, -2F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, -2F, 0F); // Box 434
+		body[8].setRotationPoint(-29.5F, -6.1F, -18F);
+
+		body[9].addShapeBox(0F, 0F, 0F, 3, 15, 12, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, -1F, 0F, 0F, 1F, 0F, 0F, 1F, 0F, 0F, -1F, 0F, 0F); // Box 442
+		body[9].setRotationPoint(-7.5F, -6F, -15.5F);
+
+		body[10].addShapeBox(0F, 0F, 0F, 3, 15, 12, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, -1F, 0F, 0F, 1F, 0F, 0F, 1F, 0F, 0F, -1F, 0F, 0F); // Box 443
+		body[10].setRotationPoint(-7.5F, -6F, 3.5F);
+
+		body[11].addShapeBox(0F, 0F, 0F, 12, 3, 12, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F); // Box 444
+		body[11].setRotationPoint(-4.5F, 7.8F, -15.5F);
+
+		body[12].addShapeBox(0F, 0F, 0F, 12, 3, 12, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F, 0F); // Box 445
+		body[12].setRotationPoint(-4.5F, 7.8F, 3.5F);
+
+        flipAll();
+    }
+
+}

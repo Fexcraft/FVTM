d60c1475da47c0b0276ca567bb03d5520fb00849
Ferdinand <fexcalo@gmail.com>
2018-08-22 22:32:43 +0200

    FMT-

--- src/main/java/net/fexcraft/mod/addons/hcp/HeavyCargoPack.java
+++ src/main/java/net/fexcraft/mod/addons/hcp/HeavyCargoPack.java
@@ -3,18 +3,12 @@ package net.fexcraft.mod.addons.hcp;
 import java.io.File;
 
 import net.fexcraft.mod.fvtm.impl.HybridAddon;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 public class HeavyCargoPack extends HybridAddon {
 
     public HeavyCargoPack(File file){
-		super(file);
+		super(file); //MinecraftForge.EVENT_BUS.register(this);
 	}
-    
-    public void onPreInit(FMLPreInitializationEvent event){
-    	MinecraftForge.EVENT_BUS.register(this);
-    }
 
     //@SubscribeEvent public void register(Register<Block> event){ /*//*/ }
 

45d184a8e68754f6f07a9fd53d19f18540d15611
Ferdinand <fercalo96@yahoo.de>
2019-10-13 21:02:02 +0200

    updated version

--- src/main/java/net/fexcraft/mod/addons/gep/GeneralEssentialsPack.java
+++ src/main/java/net/fexcraft/mod/addons/gep/GeneralEssentialsPack.java
@@ -4,5 +4,5 @@ import net.fexcraft.mod.fvtm.data.addon.AddonClass;
 import net.minecraftforge.fml.common.Mod;
 
 @AddonClass(registryname = "fvtm:gep")
-@Mod(modid = "gep", name = "General Essentials Pack", version = "2.0", useMetadata = true, dependencies = "required-after:fcl;required-after:fvtm")
+@Mod(modid = "gep", name = "General Essentials Pack", version = "2.0.1", useMetadata = true, dependencies = "required-after:fcl;required-after:fvtm")
 public class GeneralEssentialsPack {}
--- src/main/java/net/fexcraft/mod/addons/hcp/HeavyCargoPack.java
+++ src/main/java/net/fexcraft/mod/addons/hcp/HeavyCargoPack.java
@@ -4,5 +4,5 @@ import net.fexcraft.mod.fvtm.data.addon.AddonClass;
 import net.minecraftforge.fml.common.Mod;
 
 @AddonClass(registryname = "fvtm:hcp")
-@Mod(modid = "hcp", name = "Heavy Cargo Pack", version = "2.0", useMetadata = true, dependencies = "required-after:fcl;required-after:fvtm")
+@Mod(modid = "hcp", name = "Heavy Cargo Pack", version = "2.0.1", useMetadata = true, dependencies = "required-after:fcl;required-after:fvtm")
 public class HeavyCargoPack {}

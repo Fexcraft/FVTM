89dd488f7bb1bfa7d694568e26c0f1ce0922cfb8
Ferdinand <fercalo96@yahoo.de>
2020-05-29 20:01:17 +0200

    Added Item dropping for MultiBlocks

--- src/main/java/net/fexcraft/mod/fvtm/block/generated/M_4ROT.java
+++ src/main/java/net/fexcraft/mod/fvtm/block/generated/M_4ROT.java
@@ -2,9 +2,12 @@ package net.fexcraft.mod.fvtm.block.generated;
 
 import static net.fexcraft.mod.fvtm.util.Properties.FACING;
 
+import java.util.Random;
+
 import javax.annotation.Nullable;
 
 import net.fexcraft.lib.mc.utils.Print;
+import net.fexcraft.mod.fvtm.block.generated.M_4ROT_TE.TileEntity;
 import net.fexcraft.mod.fvtm.data.Capabilities;
 import net.fexcraft.mod.fvtm.data.block.Block;
 import net.fexcraft.mod.fvtm.data.block.MultiBlockData;
@@ -12,12 +15,15 @@ import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemDye;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
@@ -97,6 +103,28 @@ public class M_4ROT extends PlainBase {
     public void breakBlock(World world, BlockPos pos, IBlockState state){
     	if(!world.isRemote) M_4ROT_TE.processBreak(world, pos, state);
         super.breakBlock(world, pos, state);
+    }
+
+    @Override
+    public ItemStack getPickBlock(IBlockState state, RayTraceResult target, World world, BlockPos pos, EntityPlayer player){
+        TileEntity tile = (TileEntity)world.getTileEntity(world.getCapability(Capabilities.MULTIBLOCKS, null).getMultiBlockCore(pos));
+        return tile == null ? ItemStack.EMPTY : tile.getBlockData().newItemStack();
+    }
+    
+    @Override
+    public int quantityDropped(Random random){
+        return 0;
+    }
+    
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune){
+        return Items.AIR;
+    }
+    
+    @Override
+    public ItemStack getItem(World world, BlockPos pos, IBlockState state){
+        TileEntity tile = (TileEntity)world.getTileEntity(world.getCapability(Capabilities.MULTIBLOCKS, null).getMultiBlockCore(pos));
+        return tile == null ? ItemStack.EMPTY : tile.getBlockData().newItemStack();
     }
 
 }
--- src/main/java/net/fexcraft/mod/fvtm/block/generated/M_4ROT_TE.java
+++ src/main/java/net/fexcraft/mod/fvtm/block/generated/M_4ROT_TE.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 
 import javax.annotation.Nullable;
 
@@ -23,8 +24,11 @@ import net.fexcraft.mod.fvtm.data.block.MultiBlockData;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemDye;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -33,6 +37,7 @@ import net.minecraft.util.EnumHand;
 import net.minecraft.util.ITickable;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
@@ -167,9 +172,11 @@ public class M_4ROT_TE extends BlockBase {
 			if(posstate.getBlock() instanceof M_4ROT_TE || posstate.getBlock() instanceof M_4ROT){
 				TileEntity tile = (TileEntity)world.getTileEntity(blkpos);
 				if(tile != null && tile.iscore){
-					if(tile.iscore){
-						//TODO empty out inventories (drop)
-					}
+					//TODO empty out inventories (drop)
+					EntityItem item = new EntityItem(world);
+					item.setPosition(tile.getPos().getX() + 0.5, tile.getPos().getY() + 0.5, tile.getPos().getZ() + 0.5);
+					item.setItem(tile.getBlockData().newItemStack());
+					world.spawnEntity(item);
 				}
 	            world.setBlockState(blkpos, Blocks.AIR.getDefaultState());
 			}
@@ -180,6 +187,30 @@ public class M_4ROT_TE extends BlockBase {
 	public net.minecraft.tileentity.TileEntity createNewTileEntity(World world, int meta){
 		return type.getMultiBlock() != null && type.getMultiBlock().isTickable() ? new M_4ROT_TE.TickableTE(this) : new M_4ROT_TE.TileEntity(this);
 	}
+
+    @Override
+    public ItemStack getPickBlock(IBlockState state, RayTraceResult target, World world, BlockPos pos, EntityPlayer player){
+    	TileEntity our = (TileEntity)world.getTileEntity(pos);
+        TileEntity tile = our.iscore ? our : our.reference;
+        return tile == null ? ItemStack.EMPTY : tile.getBlockData().newItemStack();
+    }
+    
+    @Override
+    public int quantityDropped(Random random){
+        return 0;
+    }
+    
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune){
+        return Items.AIR;
+    }
+    
+    @Override
+    public ItemStack getItem(World world, BlockPos pos, IBlockState state){
+    	TileEntity our = (TileEntity)world.getTileEntity(pos);
+        TileEntity tile = our.iscore ? our : our.reference;
+        return tile == null ? ItemStack.EMPTY : tile.getBlockData().newItemStack();
+    }
 	
 	public static class TileEntity extends BlockBase.TileEntity {
 		

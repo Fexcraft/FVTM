70d8d8910ca06468de3a31fee9739fe77e40bb5a
Ferdinand <fercalo96@yahoo.de>
2020-08-13 22:36:52 +0200

    Update VecUtil.java

--- src/main/java/net/fexcraft/mod/fvtm/util/VecUtil.java
+++ src/main/java/net/fexcraft/mod/fvtm/util/VecUtil.java
@@ -12,7 +12,8 @@ import net.fexcraft.lib.common.math.Vec3f;
 public class VecUtil {
 	
 	public static final Vec3f rotByRad(float rad, Vec3f vec){
-        double co = Math.cos(rad), si = Math.sin(rad); return new Vec3f(co * vec.xCoord - si * vec.zCoord, vec.yCoord, si * vec.xCoord + co * vec.zCoord);
+        double co = Math.cos(rad), si = Math.sin(rad);
+        return new Vec3f(co * vec.xCoord - si * vec.zCoord, vec.yCoord, si * vec.xCoord + co * vec.zCoord);
 	}
 
 }

5b99f2834004b1dc31eace10ecb60747e380da2b
Ferdinand <fercalo96@yahoo.de>
2020-09-16 18:31:04 +0200

    Update PresetTab.java

--- src/main/java/net/fexcraft/mod/fvtm/util/PresetTab.java
+++ src/main/java/net/fexcraft/mod/fvtm/util/PresetTab.java
@@ -8,36 +8,48 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class PresetTab extends CreativeTab {
-	
+
 	public static final PresetTab INSTANCE = new PresetTab();
 	private NonNullList<ItemStack> list, collection = NonNullList.create();
 	public NonNullList<ItemStack> ITEMS;
-    private int icon, sec;
+	private int icon, sec;
 
 	public PresetTab(){
 		super("fvtm:presets");
 	}
 
-    @Override
-    public ItemStack createIcon(){
-        return null;
-    }
+	@Override
+	public ItemStack createIcon(){
+		return null;
+	}
 
-    @SideOnly(Side.CLIENT)
-    public String getTranslatedTabLabel(){
-        return "FVTM - (Vehicle) Presets";
-    }
+	@SideOnly(Side.CLIENT)
+	public String getTranslatedTabLabel(){
+		return "FVTM - (Vehicle) Presets";
+	}
 
-    @Override
-    public ItemStack getIcon(){
-        if(list == null){ list = NonNullList.create(); displayAllRelevantItems(list); }
-        if(sec != Time.getSecond()){
-        	sec = Time.getSecond(); this.icon++; if(icon >= list.size()){ icon = 0; }
-        } return icon >= list.size() ? ItemStack.EMPTY : list.get(icon);
-    }
+	@Override
+	public ItemStack getIcon(){
+		if(list == null){
+			list = NonNullList.create();
+			displayAllRelevantItems(list);
+		}
+		if(sec != Time.getSecond()){
+			sec = Time.getSecond();
+			this.icon++;
+			if(icon >= list.size()){
+				icon = 0;
+			}
+		}
+		return icon >= list.size() ? ItemStack.EMPTY : list.get(icon);
+	}
 
 	public void add(ItemStack stack){
-        collection.add(stack); if(ITEMS != null){ ITEMS.add(stack); list = ITEMS; }
+		collection.add(stack);
+		if(ITEMS != null){
+			ITEMS.add(stack);
+			list = ITEMS;
+		}
 	}
 
 	public NonNullList<ItemStack> get(){

a99b136eb566951dfa57887d42f947772df5b244
Ferdinand <fercalo96@yahoo.de>
2020-10-26 21:03:16 +0100

    Update Axle.java

--- src/main/java/net/fexcraft/mod/fvtm/sys/uni12/Axle.java
+++ src/main/java/net/fexcraft/mod/fvtm/sys/uni12/Axle.java
@@ -11,6 +11,7 @@ public class Axle {
 	public Vec3d pos;
 	public final ArrayList<String> wheels = new ArrayList<>();
 	public double weight_ratio;
+	public double weight_on;
 	
 	public Axle(int id, Vec3d pos){
 		this.id = id;
@@ -20,6 +21,11 @@ public class Axle {
 	public void initCenter(TreeMap<String, Vec3d> map){
 		wheels.forEach(str -> pos = pos.add(0, 0, map.get(str).z));
 		pos = new Vec3d(pos.x, pos.y, pos.z / wheels.size());
+	}
+
+	public void calcWeight(double mass, Vec3d acc, double height, double base){
+		double ahb = 0.2 * acc.x * height / base;
+		weight_on = mass * (weight_ratio * ULandVehicle.GRAVITY + (pos.x > 0 ? -ahb : ahb));
 	}
 
 }

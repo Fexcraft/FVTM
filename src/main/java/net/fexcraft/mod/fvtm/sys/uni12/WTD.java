00606c6976514a8b860aabd23a1b65452ae1b11f
Ferdinand <fercalo96@yahoo.de>
2020-11-11 21:14:08 +0100

    Create WTD.java

---
+++ src/main/java/net/fexcraft/mod/fvtm/sys/uni12/WTD.java
@@ -0,0 +1,16 @@
+package net.fexcraft.mod.fvtm.sys.uni12;
+
+import net.minecraft.util.math.Vec3d;
+
+public class WTD {
+
+	public final String id;
+	public Vec3d pos;
+	public Axle axle;
+	public double drag;
+
+	public WTD(String key){
+		this.id = key;
+	}
+
+}

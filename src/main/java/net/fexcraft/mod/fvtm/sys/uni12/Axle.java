90c1a12c90b68be57aaa649e007002f05d11e53c
Ferdinand <fercalo96@yahoo.de>
2020-11-11 21:19:29 +0100

    Update Axle.java

--- src/main/java/net/fexcraft/mod/fvtm/sys/uni12/Axle.java
+++ src/main/java/net/fexcraft/mod/fvtm/sys/uni12/Axle.java
@@ -1,26 +1,25 @@
 package net.fexcraft.mod.fvtm.sys.uni12;
 
 import java.util.ArrayList;
-import java.util.TreeMap;
 
 import net.minecraft.util.math.Vec3d;
 
 public class Axle {
-	
+
+	public final ArrayList<WTD> wheels = new ArrayList<>();
 	public final int id;
-	public Vec3d pos;
-	public final ArrayList<String> wheels = new ArrayList<>();
 	public double weight_ratio;
 	public double weight_on;
 	public double yaw_speed;
+	public Vec3d pos;
 	
 	public Axle(int id, Vec3d pos){
 		this.id = id;
 		this.pos = pos;
 	}
 
-	public void initCenter(TreeMap<String, Vec3d> map){
-		wheels.forEach(str -> pos = pos.add(0, 0, map.get(str).z));
+	public void initCenter(){
+		wheels.forEach(wtd -> pos = pos.add(0, 0, wtd.pos.z));
 		pos = new Vec3d(pos.x, pos.y, pos.z / wheels.size());
 	}
 

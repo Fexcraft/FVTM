b46f21bcd56351ff49cb6425d680ac730f39f8ba
Ferdinand <fercalo96@yahoo.de>
2020-11-13 11:20:22 +0100

    Update TireFunction.java

--- src/main/java/net/fexcraft/mod/fvtm/util/function/TireFunction.java
+++ src/main/java/net/fexcraft/mod/fvtm/util/function/TireFunction.java
@@ -1,18 +1,25 @@
 package net.fexcraft.mod.fvtm.util.function;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.annotation.Nullable;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
+import net.fexcraft.lib.common.json.JsonUtil;
 import net.fexcraft.lib.mc.utils.Formatter;
 import net.fexcraft.mod.fvtm.data.WheelSlot;
+import net.fexcraft.mod.fvtm.data.block.BlockUtil;
 import net.fexcraft.mod.fvtm.data.part.Function;
 import net.fexcraft.mod.fvtm.data.part.Part;
 import net.fexcraft.mod.fvtm.data.part.PartData;
 import net.fexcraft.mod.fvtm.data.vehicle.VehicleData;
 import net.fexcraft.mod.fvtm.util.handler.TireInstallationHandler.TireData;
+import net.minecraft.block.material.Material;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
@@ -20,11 +27,30 @@ import net.minecraft.world.World;
 
 public class TireFunction extends Function {
 	
+	private static HashMap<Part, TireAttr> TIRES = new HashMap<>();
 	private String inst_on;
 	private WheelSlot wheel;
 
 	public TireFunction(Part part, JsonObject obj){
 		super(part, obj);
+		TireAttr attr = new TireAttr();
+		attr.general_grip = JsonUtil.getIfExists(obj, "general_grip", 1f).floatValue();
+		attr.corner_stiffness = JsonUtil.getIfExists(obj, "stiffness", 5f).floatValue();
+		attr.corner_stiffness_steering = JsonUtil.getIfExists(obj, "steering_stiffness", 5.2f).floatValue();
+		if(obj.has("material_table")){
+			JsonObject table = obj.get("material_table").getAsJsonObject();
+			for(Map.Entry<String, JsonElement> entry : table.entrySet()){
+				Material mat = BlockUtil.getMaterial(entry.getKey(), true);
+				if(mat == null) continue;
+				JsonArray array = entry.getValue().getAsJsonArray();
+				float g = array.size() > 0 ? array.get(0).getAsFloat() : 1f;
+				float r = array.size() > 1 ? array.get(1).getAsFloat() : 0.9f;
+				float cs = array.size() > 2 ? array.get(2).getAsFloat() : attr.corner_stiffness;
+				float css = array.size() > 3 ? array.get(3).getAsFloat() : attr.corner_stiffness_steering;
+				attr.table.put(mat, new MatTireAttr(g, r, cs, css));
+			}
+		}
+		TIRES.put(part, attr);
 	}
 
 	@Override
@@ -69,6 +95,45 @@ public class TireFunction extends Function {
         tooltip.add(Formatter.format("&9Tire Outer Radius: &7" + tiredata.getOuterRadius()));
         tooltip.add(Formatter.format("&9Tire Inner Radius: &7" + tiredata.getInnerRadius()));
         tooltip.add(Formatter.format("&9Tire Width: &7" + tiredata.getWidth()));
+    }
+    
+    public static class TireAttr {
+    	
+    	private HashMap<Material, MatTireAttr> table = new HashMap<>();
+    	private float general_grip, corner_stiffness, corner_stiffness_steering;
+
+        public float getGripFor(Material mat, boolean rainfall){
+        	if(table.containsKey(mat)){
+        		return rainfall ? table.get(mat).rainfall : table.get(mat).general;
+        	}
+    		return general_grip * (rainfall ? DEFAULT_TABLE.get(mat).rainfall : DEFAULT_TABLE.get(mat).general);
+        }
+        
+        public float getCornerStiffnessFor(Material mat, boolean steering){
+        	if(table.containsKey(mat)){
+        		return steering ? table.get(mat).corner_stiffness_steering : table.get(mat).corner_stiffness;
+        	}
+    		return steering ? DEFAULT_TABLE.get(mat).corner_stiffness_steering : DEFAULT_TABLE.get(mat).corner_stiffness;
+        }
+        
+    }
+    
+    public static class MatTireAttr {
+    	
+    	public final float general, rainfall, corner_stiffness, corner_stiffness_steering;
+
+		public MatTireAttr(float g, float r, float cs, float css){
+			general = g;
+			rainfall = r;
+			corner_stiffness = cs;
+			corner_stiffness_steering = css;
+		}
+    	
+    }
+    
+    private static HashMap<Material, MatTireAttr> DEFAULT_TABLE = new HashMap<>();
+    static {
+    	//TODO
     }
 
 }

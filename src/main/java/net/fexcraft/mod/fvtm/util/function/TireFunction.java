996a3776115f13549637b520eb883cc737090ba3
Ferdinand <fercalo96@yahoo.de>
2020-11-13 11:37:37 +0100

    Update TireFunction.java

--- src/main/java/net/fexcraft/mod/fvtm/util/function/TireFunction.java
+++ src/main/java/net/fexcraft/mod/fvtm/util/function/TireFunction.java
@@ -133,7 +133,41 @@ public class TireFunction extends Function {
     
     private static HashMap<Material, MatTireAttr> DEFAULT_TABLE = new HashMap<>();
     static {
-    	//TODO
+    	float dc = 5f, ds = 5.2f;
+    	DEFAULT_TABLE.put(Material.AIR, new MatTireAttr(0.1f, 0.1f, dc, ds));
+    	DEFAULT_TABLE.put(Material.GRASS, new MatTireAttr(0.7f, 0.4f, dc, ds));
+    	DEFAULT_TABLE.put(Material.GROUND, new MatTireAttr(0.9f, 0.75f, dc, ds));
+    	DEFAULT_TABLE.put(Material.WOOD, new MatTireAttr(1.2f, 0.9f, dc, ds));
+    	DEFAULT_TABLE.put(Material.ROCK, new MatTireAttr(1f, 0.9f, dc, ds));
+    	DEFAULT_TABLE.put(Material.IRON, new MatTireAttr(1.2f, 0.9f, dc, ds));
+    	DEFAULT_TABLE.put(Material.ANVIL, new MatTireAttr(1.5f, 1f, dc, ds));
+    	DEFAULT_TABLE.put(Material.WATER, new MatTireAttr(0.1f, 0.1f, dc, ds));
+    	DEFAULT_TABLE.put(Material.LAVA, new MatTireAttr(0.1f, 0.2f, dc, ds));
+    	DEFAULT_TABLE.put(Material.LEAVES, new MatTireAttr(0.8f, 0.4f, dc, ds));
+    	DEFAULT_TABLE.put(Material.PLANTS, new MatTireAttr(0.8f, 0.4f, dc, ds));
+    	DEFAULT_TABLE.put(Material.VINE, new MatTireAttr(0.8f, 0.4f, dc, ds));
+    	DEFAULT_TABLE.put(Material.SPONGE, new MatTireAttr(0.75f, 0.6f, dc, ds));
+    	DEFAULT_TABLE.put(Material.CLOTH, new MatTireAttr(0.8f, 0.8f, dc, ds));
+    	DEFAULT_TABLE.put(Material.FIRE, new MatTireAttr(1f, 1.2f, dc, ds));
+    	DEFAULT_TABLE.put(Material.SAND, new MatTireAttr(0.5f, 0.2f, dc, ds));
+    	DEFAULT_TABLE.put(Material.CIRCUITS, new MatTireAttr(1.25f, 1f, dc, ds));
+    	DEFAULT_TABLE.put(Material.CARPET, new MatTireAttr(1.1f, 0.9f, dc, ds));
+    	DEFAULT_TABLE.put(Material.GLASS, new MatTireAttr(1.1f, 0.8f, dc, ds));
+    	DEFAULT_TABLE.put(Material.REDSTONE_LIGHT, new MatTireAttr(1f, 1f, dc, ds));
+    	DEFAULT_TABLE.put(Material.TNT, new MatTireAttr(1.5f, 1.2f, dc, ds));
+    	DEFAULT_TABLE.put(Material.CORAL, new MatTireAttr(0.5f, 0.5f, dc, ds));
+    	DEFAULT_TABLE.put(Material.ICE, new MatTireAttr(0.3f, 0.2f, dc, ds));
+    	DEFAULT_TABLE.put(Material.PACKED_ICE, new MatTireAttr(0.5f, 0.3f, dc, ds));
+    	DEFAULT_TABLE.put(Material.SNOW, new MatTireAttr(0.75f, 0.5f, dc, ds));
+    	DEFAULT_TABLE.put(Material.CRAFTED_SNOW, new MatTireAttr(1f, 0.8f, dc, ds));
+    	DEFAULT_TABLE.put(Material.CACTUS, new MatTireAttr(0.7f, 0.9f, dc, ds));
+    	DEFAULT_TABLE.put(Material.CLAY, new MatTireAttr(0.8f, 0.5f, dc, ds));
+    	DEFAULT_TABLE.put(Material.GOURD, new MatTireAttr(0.7f, 0.4f, dc, ds));
+    	DEFAULT_TABLE.put(Material.PORTAL, new MatTireAttr(2f, 2f, dc, ds));
+    	DEFAULT_TABLE.put(Material.CAKE, new MatTireAttr(0.45f, 0.1f, dc, ds));
+    	DEFAULT_TABLE.put(Material.WEB, new MatTireAttr(1f, 0.8f, dc, ds));
+    	DEFAULT_TABLE.put(Material.PISTON, new MatTireAttr(1f, 0.9f, dc, ds));
+    	DEFAULT_TABLE.put(Material.BARRIER, new MatTireAttr(1.2f, 1.1f, dc, ds));
     }
 
 }

2a393db8a7cd3ae55dcf1405467a15ce85b4c461
Ferdinand <fercalo96@yahoo.de>
2020-11-13 11:48:28 +0100

    Update WTD.java

--- src/main/java/net/fexcraft/mod/fvtm/sys/uni12/WTD.java
+++ src/main/java/net/fexcraft/mod/fvtm/sys/uni12/WTD.java
@@ -1,5 +1,6 @@
 package net.fexcraft.mod.fvtm.sys.uni12;
 
+import net.fexcraft.mod.fvtm.util.function.TireFunction.TireAttr;
 import net.minecraft.util.math.Vec3d;
 
 public class WTD {
@@ -7,6 +8,7 @@ public class WTD {
 	public final String id;
 	public Vec3d pos;
 	public Axle axle;
+	public TireAttr function;
 
 	public WTD(String key){
 		this.id = key;

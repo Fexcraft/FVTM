a3212cd2995bd3209aa87c05d0c9315a6c162b27
Ferdinand <fercalo96@yahoo.de>
2020-11-17 23:55:13 +0100

    Update TransmissionFunction.java

--- src/main/java/net/fexcraft/mod/fvtm/util/function/TransmissionFunction.java
+++ src/main/java/net/fexcraft/mod/fvtm/util/function/TransmissionFunction.java
@@ -20,22 +20,55 @@ import net.minecraft.world.World;
 public class TransmissionFunction extends StaticFuntion {
 	
 	private ArrayList<Float> ratios = new ArrayList<>();
+	private float u_low, u_mid, u_high;
+	private float d_low, d_mid, d_high;
+	private int fgears, rgears;
 	private boolean automatic;
 
 	public TransmissionFunction(Part part, JsonObject obj){
 		super(part, obj);
-		JsonArray rats = obj.has("ratios") ? obj.get("ratios").getAsJsonArray() : null;
+		if(obj == null) obj = new JsonObject();
+		JsonArray rats = obj.has("gear_ratios") ? obj.get("gear_ratios").getAsJsonArray() : null;
 		if(rats == null){
 			ratios.add(3f);
 			ratios.add(2f);
 			ratios.add(1f);
 			ratios.add(0.5f);
+			ratios.add(0f);
+			ratios.add(-3f);
 		}
 		else for(JsonElement elm : rats){
 			ratios.add(elm.getAsFloat());
 		}
-		automatic = JsonUtil.getIfExists(obj, "automatic", false);
+		if(!ratios.contains(0f)) ratios.add(0f);
+		for(float f : ratios){
+			if(f > 0) fgears++;
+			if(f < 0) rgears++;
+		}
 		Collections.sort(ratios);
+		automatic = JsonUtil.getIfExists(obj, "automatic", false);
+		rats = obj.has("throttle_ratios_up") ? obj.get("throttle_ratios_up").getAsJsonArray() : null;
+		if(rats == null){
+			u_low = 0.35f;
+			u_mid = 0.5f;
+			u_high = 0.75f;
+		}
+		else{
+			u_low = rats.get(0).getAsFloat();
+			u_mid = rats.get(1).getAsFloat();
+			u_high = rats.get(2).getAsFloat();
+		}
+		rats = obj.has("throttle_ratios_down") ? obj.get("throttle_ratios_down").getAsJsonArray() : null;
+		if(rats == null){
+			d_low = 0.25f;
+			d_mid = 0.4f;
+			d_high = 0.6f;
+		}
+		else{
+			d_low = rats.get(0).getAsFloat();
+			d_mid = rats.get(1).getAsFloat();
+			d_high = rats.get(2).getAsFloat();
+		}
 	}
 
 	@Override
@@ -45,8 +78,8 @@ public class TransmissionFunction extends StaticFuntion {
 
     @Override
     public void addInformation(ItemStack stack, World world, PartData data, List<String> tooltip, ITooltipFlag flag){
-        tooltip.add(Formatter.format("&9Gears: &7" + ratios.size()));
-        tooltip.add(Formatter.format("&9Range: &7" + ratios.get(0) + "-" + ratios.get(ratios.size() - 1)));
+        tooltip.add(Formatter.format("&9Gears: &7" + fgears + " / N / " + (rgears == 1 ? "R" : rgears)));
+        tooltip.add(Formatter.format("&9Range: &7" + (ratios.get(rgears + 1) + "-" + ratios.get(ratios.size() - 1)) + " / " + (rgears == 1 ? ratios.get(0) : ratios.get(0) + "-" + ratios.get(rgears))));
         tooltip.add(Formatter.format("&9Type: &7" + (automatic ? "automatic" : "manual")));
     }
     
@@ -62,8 +95,32 @@ public class TransmissionFunction extends StaticFuntion {
     	return !automatic;
     }
 	
+	public int getFGearAmount(){
+		return fgears;
+	}
+	
+	public int getRGearAmount(){
+		return fgears;
+	}
+	
 	public float getRatio(int gear){
 		return ratios.get(gear);
+	}
+	
+	/** To be called from the vehicle vehicle when this is an automatic transmission, to check if it should change gears. */
+	public int processAutoShift(int gear, int rpm, int rpm_max, float throttle, boolean forward){
+		float max = rpm_max * (throttle < 0.3 ? u_low : throttle < 0.7 ? u_mid : u_high);
+		float min = rpm_max * (throttle < 0.3 ? d_low : throttle < 0.7 ? d_mid : d_high);
+		if(rpm < min){
+			if(gear <= 1) return 1;
+			else return gear - 1;
+		}
+		else if(rpm > max){
+			int gears = (forward ? fgears : rgears);
+			if(gear >= gears) return gears;
+			else return gear + 1;
+		}
+		return gear;
 	}
 
 }

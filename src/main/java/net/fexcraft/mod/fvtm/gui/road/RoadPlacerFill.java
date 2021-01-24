f26c16dd8647acd5fbb00a4d21c46e066bda6feb
Ferdinand <fercalo96@yahoo.de>
2021-01-24 21:23:32 +0100

    Update RoadPlacerFill.java

--- src/main/java/net/fexcraft/mod/fvtm/gui/road/RoadPlacerFill.java
+++ src/main/java/net/fexcraft/mod/fvtm/gui/road/RoadPlacerFill.java
@@ -2,7 +2,7 @@ package net.fexcraft.mod.fvtm.gui.road;
 
 import static net.fexcraft.lib.mc.utils.Formatter.PARAGRAPH_SIGN;
 import static net.fexcraft.mod.fvtm.gui.GuiHandler.LISTENERID;
-import static net.fexcraft.mod.fvtm.gui.GuiHandler.ROADTOOL;
+import static net.fexcraft.mod.fvtm.gui.GuiHandler.ROADTOOLCUSTOMFILL;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -10,17 +10,24 @@ import java.util.ArrayList;
 import net.fexcraft.lib.mc.gui.GenericGui;
 import net.fexcraft.mod.fvtm.util.Perms;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
 
 public class RoadPlacerFill extends GenericGui<RoadPlacerFillContainer> {
 	
 	private static final ResourceLocation texture = new ResourceLocation("fvtm:textures/gui/road_gen_sel.png");
+	private static int minheight = 3;
 	//
 	private ArrayList<String> ttip = new ArrayList<String>();
+	private int[] size = new int[]{ 1, 0, 0, 0, 0};
 	
 	public RoadPlacerFill(EntityPlayer player, int x, int y, int z){
 		super(texture, new RoadPlacerFillContainer(player, x, y, z), player);
 		if(!Perms.ROAD_PLACER_GUI.has(player)) player.closeScreen();
+		if(!container.stack.getTagCompound().hasKey("Layers")){
+			container.stack.getTagCompound().setIntArray("Layers", size);
+		}
 		this.defbackground = true;
 		this.deftexrect = true;
 		container.gui = this;
@@ -36,12 +43,27 @@ public class RoadPlacerFill extends GenericGui<RoadPlacerFillContainer> {
 			int j = i * 20;
 			this.buttons.put("a" + i, new BasicButton("a" + i, guiLeft + 27, guiTop + 7 + j, 27, 7 + j, 8, 8, true));
 			this.buttons.put("s" + i, new BasicButton("s" + i, guiLeft + 27, guiTop + 17 + j, 27, 17 + j, 8, 8, true));
+			this.texts.put("tu" + i, new BasicText(guiLeft + 39, guiTop + 8 + j, 128, 0x6a6a6a, null));
+			this.texts.put("td" + i, new BasicText(guiLeft + 39, guiTop + 16 + j, 128, 0x6a6a6a, null));
 		}
 	}
 
 	@Override
 	protected void predraw(float pticks, int mouseX, int mouseY){
+		texts.get("tu0").string = container.cr ? "Custom Road Fill" : container.roadinv.getStackInSlot(0).isEmpty() ? "No Fill Block" : container.roadinv.getStackInSlot(0).getDisplayName();
+		texts.get("td0").string = (size[0] + " block width");
 		//
+		texts.get("tu1").string = container.roadinv.getStackInSlot(1).isEmpty() ? "No Fill Block" : container.roadinv.getStackInSlot(1).getDisplayName();
+		texts.get("td1").string = "ground fill " + (size[1] > 0 ? "enabled" : "disabled");
+		//
+		texts.get("tu2").string = container.roadinv.getStackInSlot(2).isEmpty() ? "No Fill Block" : container.roadinv.getStackInSlot(2).getDisplayName();
+		texts.get("td2").string = "L side " + (size[2] > 0 ? size[2] + " blocks high" : "disabled");
+		//
+		texts.get("tu3").string = container.roadinv.getStackInSlot(3).isEmpty() ? "No Fill Block" : container.roadinv.getStackInSlot(3).getDisplayName();
+		texts.get("td3").string = "R side " + (size[3] > 0 ? size[3] + " blocks high" : "disabled");
+		//
+		texts.get("tu4").string = container.ct ? "Custom Top Fill" : container.roadinv.getStackInSlot(4).isEmpty() ? "No Fill Block" : container.roadinv.getStackInSlot(4).getDisplayName();
+		texts.get("td4").string = "top fill " + (size[4] > 0 ? "enabled" : "disabled");
 	}
 
 	@Override
@@ -74,20 +96,50 @@ public class RoadPlacerFill extends GenericGui<RoadPlacerFillContainer> {
 	@Override
 	protected boolean buttonClicked(int mouseX, int mouseY, int mouseButton, String key, BasicButton button){
 		if(button.name.equals("road")){
-			openGui(ROADTOOL, new int[]{ 0, 0, 0 }, LISTENERID);
+			openGui(ROADTOOLCUSTOMFILL, new int[]{ 0, 0, 0 }, LISTENERID);
 			return true;
 		}
 		else if(button.name.equals("roof")){
-			openGui(ROADTOOL, new int[]{ 4, 0, 0 }, LISTENERID);
+			openGui(ROADTOOLCUSTOMFILL, new int[]{ 4, 0, 0 }, LISTENERID);
 			return true;
+		}
+		else if(button.name.startsWith("a")){
+			int idx = Integer.parseInt(button.name.substring(1));
+			size(idx, 1);
+		}
+		else if(button.name.startsWith("s")){
+			int idx = Integer.parseInt(button.name.substring(1));
+			size(idx, -1);
 		}
 		return false;
 	}
 	
+	private void size(int idx, int am){
+		switch(idx){
+			case 0: if(size[idx] + am >= 0 && size[idx] + am < 64) size[idx] += am; break;
+			case 1: if(size[idx] + am >= 0 && size[idx] + am < 2) size[idx] += am; break;
+			case 2:
+			case 3:
+				if(size[2] + am > 0 && size[2] < minheight){
+					if(size[2] + am < minheight) size[2] = minheight;
+					else size[2] += am;
+				}
+				else if(size[2] + am < minheight) size[2] = 0;
+				else if(size[2] + am < 64) size[2] += am;
+				else;
+				size[3] = size[2];
+				break;
+			case 4: if(size[idx] + am >= 0 && size[idx] + am < 2) size[idx] += am; break;
+		}
+	}
+
 	@Override
     public void keyTyped(char typedChar, int keyCode) throws IOException{
         if(keyCode == 1){
-			openGui(ROADTOOL, null, LISTENERID);
+			NBTTagCompound compound = new NBTTagCompound();
+			compound.setIntArray("sizes", size);
+			compound.setString("cargo", "save");
+			container.send(Side.SERVER, compound);
             return;
         }
         super.keyTyped(typedChar, keyCode);

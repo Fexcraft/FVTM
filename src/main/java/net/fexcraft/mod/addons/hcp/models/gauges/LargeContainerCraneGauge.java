cb3f28be5882282272cd4277cc537cadaac107c5
Ferdinand <fercalo96@yahoo.de>
2021-04-10 00:09:04 +0200

    Update LargeContainerCraneGauge.java

--- src/main/java/net/fexcraft/mod/addons/hcp/models/gauges/LargeContainerCraneGauge.java
+++ src/main/java/net/fexcraft/mod/addons/hcp/models/gauges/LargeContainerCraneGauge.java
@@ -4,10 +4,8 @@ package net.fexcraft.mod.addons.hcp.models.gauges;
 import net.fexcraft.lib.common.Static;
 import net.fexcraft.lib.common.math.Vec3f;
 import net.fexcraft.lib.mc.api.registry.fModel;
-import net.fexcraft.mod.fvtm.entity.JunctionSwitchEntity;
 import net.fexcraft.mod.fvtm.model.RailGaugeModel;
 import net.fexcraft.mod.fvtm.model.TurboList;
-import net.fexcraft.mod.fvtm.sys.rail.Junction;
 
 /** This file was exported via the FVTM Exporter V1.3 of<br>
  *  FMT (Fex's Modelling Toolbox) v.1.2.9 &copy; 2019 - Fexcraft.net<br>
@@ -27,11 +25,6 @@ public class LargeContainerCraneGauge extends RailGaugeModel {
 		this.addRailRectShape(Static.sixteenth, -183, 2, 2, 2, tr, tl, bl, br, true);
 		this.addRailRectShape(Static.sixteenth, -175, 2, 2, 2, tr, tl, bl, br, true);
 		this.groups.add(new TurboList("ties"));
-	}
-	
-	@Override
-	public void renderSwitch(JunctionSwitchEntity entity, Junction junction){
-		return;
 	}
 
 }

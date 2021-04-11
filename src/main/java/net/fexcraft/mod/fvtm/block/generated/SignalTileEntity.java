cbe4c9b9947fd060fcce57477cdbe4d072597b18
Ferdinand <fercalo96@yahoo.de>
2021-04-11 18:12:27 +0200

    Update SignalTileEntity.java

--- src/main/java/net/fexcraft/mod/fvtm/block/generated/SignalTileEntity.java
+++ src/main/java/net/fexcraft/mod/fvtm/block/generated/SignalTileEntity.java
@@ -1,6 +1,8 @@
 package net.fexcraft.mod.fvtm.block.generated;
 
 public class SignalTileEntity extends BlockTileEntity {
+	
+	public SignalTileEntity(){}
 
 	public SignalTileEntity(BlockBase type){
 		super(type);

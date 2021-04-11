fa5052b1770ed18eb5800c9b047015b434d7c92e
Ferdinand <fercalo96@yahoo.de>
2021-04-11 19:52:18 +0200

    Update SignalItem0.java

--- src/main/java/net/fexcraft/mod/fvtm/item/SignalItem0.java
+++ src/main/java/net/fexcraft/mod/fvtm/item/SignalItem0.java
@@ -34,77 +34,85 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 @fItem(modid = "fvtm", name = "simple_signal")
 public class SignalItem0 extends Item implements JunctionGridItem {
 
-    public static SignalItem0 INSTANCE;
-    private static float[][] gridcolour;
-    static {
-    	RGB cyan = new RGB(java.awt.Color.CYAN.getRGB()); gridcolour = new float[][]{ cyan.toFloatArray(), cyan.toFloatArray() };
-    }
+	public static SignalItem0 INSTANCE;
+	private static float[][] gridcolour;
+	static{
+		RGB cyan = new RGB(java.awt.Color.CYAN.getRGB());
+		gridcolour = new float[][] { cyan.toFloatArray(), cyan.toFloatArray() };
+	}
 
 	public SignalItem0(){
-		this.setHasSubtypes(true); this.setMaxStackSize(1); INSTANCE = this;
-    }
+		this.setHasSubtypes(true);
+		this.setMaxStackSize(1);
+		INSTANCE = this;
+	}
 
-    @SideOnly(Side.CLIENT)
-    @Override
-    public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn){
-        tooltip.add(Formatter.format("&9Rightclick on a Junction to apply."));
-    }
-	
+	@SideOnly(Side.CLIENT)
 	@Override
-    public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ){
-        if(world.isRemote){ return EnumActionResult.PASS; }
-        RailSys syscap = world.getCapability(Capabilities.RAILSYSTEM, null).get();
-        if(syscap == null){
+	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn){
+		tooltip.add(Formatter.format("&9Rightclick on a Junction to apply."));
+	}
+
+	@Override
+	public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ){
+		if(world.isRemote){ return EnumActionResult.PASS; }
+		RailSys syscap = world.getCapability(Capabilities.RAILSYSTEM, null).get();
+		if(syscap == null){
 			Print.chat(player, "&cWorld Capability not found.");
-	        return EnumActionResult.FAIL;
-        }
-        ItemStack stack = player.getHeldItem(hand);
-        Vec316f vector = new Vec316f(world, new Vec3d(pos).add(hitX, hitY, hitZ), Config.RAIL_PLACING_GRID);
+			return EnumActionResult.FAIL;
+		}
+		ItemStack stack = player.getHeldItem(hand);
+		Vec316f vector = new Vec316f(world, new Vec3d(pos).add(hitX, hitY, hitZ), Config.RAIL_PLACING_GRID);
 		if(stack.getTagCompound() == null) stack.setTagCompound(new NBTTagCompound());
 		Junction junction = syscap.getJunction(vector, true);
 		if(junction == null){ return EnumActionResult.PASS; }
-        if(player.isSneaking()){
-        	junction.setSignal(null, null); Print.chat(player, "&bJunction Signal &creset&b.");
-        	junction.checkTrackSectionConsistency(); junction.pollSignal(null);
+		if(player.isSneaking()){
+			junction.setSignal(null, null);
+			Print.bar(player, "&bJunction Signal &creset&b.");
+			junction.checkTrackSectionConsistency();
+			junction.pollSignal(null);
 			return EnumActionResult.SUCCESS;
 		}
-        if(junction.signal != null){
-        	if(junction.signal.type == SignalType.Kind.BLOCK){
-        		EntryDirection dir = EntryDirection.FORWARD;
-        		if(junction.signal.oneway && junction.signal_dir == EntryDirection.FORWARD){
-        			junction.setSignal(SignalType.ONE_WAY_BLOCK, EntryDirection.BACKWARD);
-        			dir = EntryDirection.BACKWARD;
-        		}
-        		else if(junction.signal.oneway && junction.signal_dir == EntryDirection.BACKWARD){
-        			junction.setSignal(SignalType.TWO_WAY_BLOCK, EntryDirection.BOTH);
-        			dir = EntryDirection.BOTH;
-        		}
-        		else if(!junction.signal.oneway){
-        			junction.setSignal(SignalType.ONE_WAY_BLOCK, EntryDirection.FORWARD);
-        			dir = EntryDirection.FORWARD;
-        		}
-        		else{
-        			Print.chat(player, "A strange error happened, see location in log.");
-        			Static.exception(null, false);
-        		} junction.pollSignal(null);
-        		Print.chat(player, "&bJunction Signal &aupdated&b. [" + dir.getSaveByte() + "/" + dir.name().toLowerCase() + "]");
-    	        return EnumActionResult.SUCCESS;
-        	}
-        	else{
-            	Print.chat(player, "&cThis Junction has already a Signal.");
-    			return EnumActionResult.FAIL;
-        	}
-        }
-        if(junction.size() != 2){
-        	Print.chat(player, "&cOnly Junctions with 2 Tracks can have Signals.");
+		if(junction.signal != null){
+			if(junction.signal.type == SignalType.Kind.BLOCK){
+				EntryDirection dir = EntryDirection.FORWARD;
+				if(junction.signal.oneway && junction.signal_dir == EntryDirection.FORWARD){
+					junction.setSignal(SignalType.ONE_WAY_BLOCK, EntryDirection.BACKWARD);
+					dir = EntryDirection.BACKWARD;
+				}
+				else if(junction.signal.oneway && junction.signal_dir == EntryDirection.BACKWARD){
+					junction.setSignal(SignalType.TWO_WAY_BLOCK, EntryDirection.BOTH);
+					dir = EntryDirection.BOTH;
+				}
+				else if(!junction.signal.oneway){
+					junction.setSignal(SignalType.ONE_WAY_BLOCK, EntryDirection.FORWARD);
+					dir = EntryDirection.FORWARD;
+				}
+				else{
+					Print.bar(player, "A strange error happened, see location in log.");
+					Static.exception(null, false);
+				}
+				junction.pollSignal(null);
+				Print.bar(player, "&bJunction Signal &aupdated&b. [" + dir.getSaveByte() + "/" + dir.name().toLowerCase() + "]");
+				return EnumActionResult.SUCCESS;
+			}
+			else{
+				Print.bar(player, "&cThis Junction has already a Signal.");
+				return EnumActionResult.FAIL;
+			}
+		}
+		if(junction.size() != 2){
+			Print.bar(player, "&cOnly Junctions with 2 Tracks can have Signals.");
 			return EnumActionResult.FAIL;
-        }
-		junction.setSignal(SignalType.TWO_WAY_BLOCK, EntryDirection.BOTH); Print.chat(player, "&bJunction Signal &7set&b.");
-		junction.checkTrackSectionConsistency(); junction.pollSignal(null);
+		}
+		junction.setSignal(SignalType.TWO_WAY_BLOCK, EntryDirection.BOTH);
+		Print.chat(player, "&bJunction Signal &7set&b.");
+		junction.checkTrackSectionConsistency();
+		junction.pollSignal(null);
 		if(!player.capabilities.isCreativeMode) stack.shrink(1);
-        return EnumActionResult.SUCCESS;
-    }
-	
+		return EnumActionResult.SUCCESS;
+	}
+
 	@Override
 	public float[][] getGridColours(){
 		return gridcolour;

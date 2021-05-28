146c7ba58856815f0fbefecbacdf5aeca809e678
Ferdinand <fercalo96@yahoo.de>
2021-05-28 18:56:48 +0200

    Update Perms.java

--- src/main/java/net/fexcraft/mod/fvtm/util/Perms.java
+++ src/main/java/net/fexcraft/mod/fvtm/util/Perms.java
@@ -13,11 +13,13 @@ public class Perms {
 	
 	public static final Perm RAIL_PLACER_GUI = new Perm("fvtm.gui.rail_placer");
 	public static final Perm ROAD_PLACER_GUI = new Perm("fvtm.gui.road_placer");
+	public static final Perm ROAD_PLACER_ITEM = new Perm("fvtm.item.road_placer");
 	//public static final Perm ROAD_PLACER_GUI_NOBLOCK = new Perm("fvtm.gui.road_placer.noblock");
 	
 	public static void register(){
 		PermissionAPI.registerNode(RAIL_PLACER_GUI.id(), ALL, "FVTM GUI for placing rails.");
 		PermissionAPI.registerNode(ROAD_PLACER_GUI.id(), OP, "FVTM GUI for placing (block) roads.");
+		PermissionAPI.registerNode(ROAD_PLACER_ITEM.id(), OP, "FVTM Item for placing (block) roads.");
 		//PermissionAPI.registerNode(ROAD_PLACER_GUI_NOBLOCK.id(), Config.NO_RAIL_BLOCKS ? ALL : OP, "Allows to place road blocks without inventory consumption via the RoadPlacer GUI.");
 	}
 	

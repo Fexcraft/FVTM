a30c910722cb4eae79a69f4ccc55cd6da9852fd4
Ferdinand <fercalo96@yahoo.de>
2021-06-29 19:49:07 +0200

    Update PartSlot.java

--- src/main/java/net/fexcraft/mod/fvtm/data/part/PartSlot.java
+++ src/main/java/net/fexcraft/mod/fvtm/data/part/PartSlot.java
@@ -1,10 +1,16 @@
 package net.fexcraft.mod.fvtm.data.part;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 import net.fexcraft.lib.mc.utils.Pos;
 import net.fexcraft.lib.mc.utils.Static;
 import net.fexcraft.mod.fvtm.util.Rot;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class PartSlot {
 	
@@ -20,6 +26,30 @@ public class PartSlot {
 		category = array.size() > 4 ? array.get(4).getAsString() : defcat + "_" + idx;
 		radius = array.size() > 5 ? array.get(5).getAsInt() * Static.sixteenth : 0.25f;
 		rotation = array.size() > 6 && array.get(6).isJsonArray() ? new Rot(array.get(6).getAsJsonArray()) : Rot.NULL;
+	}
+	
+	public static class PartSlots extends ArrayList<PartSlot> {
+
+		@SideOnly(Side.CLIENT)
+		public HashMap<String, Integer> count;
+		public final boolean copy_rot;
+
+		public PartSlots(Part part, JsonObject obj){
+			JsonArray jslots = obj.get("slots").getAsJsonArray();
+			for(int i = 0; i < jslots.size(); i++){
+				JsonArray array = jslots.get(i).getAsJsonArray();
+				add(new PartSlot(part.getCategory(), array, i));
+			}
+			copy_rot = obj.has("copy_rot") ? obj.get("copy_rot").getAsBoolean() : false;
+			if(Static.side().isClient()){
+				count = new HashMap<>();
+				for(PartSlot slot : this){
+					if(count.containsKey(slot.type)) count.put(slot.type, count.get(slot.type) + 1);
+					else count.put(slot.type, 1);
+				}
+			}
+		}
+		
 	}
 
 }

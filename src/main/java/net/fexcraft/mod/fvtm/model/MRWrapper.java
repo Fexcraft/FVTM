81a06ab75f411f1cee2a95d3b24ab260219105a9
Ferdinand Calo <fercalo96@yahoo.de>
2021-07-19 23:40:14 +0200

    Update MRWrapper.java

--- src/main/java/net/fexcraft/mod/fvtm/model/MRWrapper.java
+++ src/main/java/net/fexcraft/mod/fvtm/model/MRWrapper.java
@@ -21,23 +21,26 @@ public class MRWrapper extends ModelRenderer {
 	private ModelRenderer parent;
 	private ArrayList<ArrayList<String>> cloth_models = new ArrayList<>();
 	private ArrayList<ClothItem> cloth_items = new ArrayList<>();
+	private ArrayList<String> cloth_groups = new ArrayList<>();
 	private RenderPlayer renderer;
 	private EntityPlayer player;
+	private String id;
 
-	public MRWrapper(ModelBase base, ModelRenderer parent, RenderPlayer renderer){
+	public MRWrapper(ModelBase base, ModelRenderer parent, RenderPlayer renderer, String id){
 		super(base);
 		this.parent = parent;
 		this.renderer = renderer;
+		this.id = id;
 	}
 	
-	public static MRWrapper get(ModelBase base, ModelRenderer from, RenderPlayer renderer){
+	public static MRWrapper get(ModelBase base, ModelRenderer from, RenderPlayer renderer, String id){
 		if(from.childModels == null || from.childModels.isEmpty()){
-			from.addChild(new MRWrapper(base, from, renderer));
+			from.addChild(new MRWrapper(base, from, renderer, id));
 		}
 		for(ModelRenderer ren : from.childModels){
 			if(ren instanceof MRWrapper) return (MRWrapper)ren;
 		}
-		MRWrapper wrap = new MRWrapper(base, from, renderer);
+		MRWrapper wrap = new MRWrapper(base, from, renderer, id);
 		from.addChild(wrap);
 		return wrap;
 	}
@@ -49,20 +52,28 @@ public class MRWrapper extends ModelRenderer {
     	RenderCache cache = player.getCapability(Capabilities.RENDERCACHE, null);
 		for(int i = 0; i < cloth_items.size(); i++){
 			net.fexcraft.lib.tmt.ModelBase.bindTexture(cloth_items.get(i).getType().getTexture());
+			if(cloth_groups.get(i).startsWith("garb") && id.equals("right_leg")){
+				GL11.glTranslatef(0, 0, 0.25f);
+			}
 			cloth_items.get(i).getType().getModel().render(cloth_items.get(i), cloth_models.get(i), player, cache);
+			if(cloth_groups.get(i).startsWith("garb") && id.equals("right_leg")){
+				GL11.glTranslatef(0, 0, -0.25f);
+			}
 		}
 		cloth_items.clear();
 		cloth_models.clear();
+		cloth_groups.clear();
     	if(Command.OTHER) DebugModels.center.renderPlain();
     	RGB.glColorReset();
     	GL11.glRotatef(90, 0, -1, 0);
     	renderer.bindTexture(renderer.getEntityTexture((AbstractClientPlayer)player));
 	}
 
-	public void set(EntityPlayer player, ClothItem item, ArrayList<String> list){
+	public void set(EntityPlayer player, ClothItem item, ArrayList<String> list, String key){
 		this.player = player;
 		cloth_items.add(item);
 		cloth_models.add(list);
+		cloth_groups.add(key);
 	}
 
 	public ModelRenderer getParent(){

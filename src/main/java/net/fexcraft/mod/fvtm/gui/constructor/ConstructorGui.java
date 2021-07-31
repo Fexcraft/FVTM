ed84ce8641b31d1160fd262ef07870947c03bef0
Ferdinand Calo <fercalo96@yahoo.de>
2021-07-31 16:37:09 +0200

    Update ConstructorGui.java

--- src/main/java/net/fexcraft/mod/fvtm/gui/constructor/ConstructorGui.java
+++ src/main/java/net/fexcraft/mod/fvtm/gui/constructor/ConstructorGui.java
@@ -8,6 +8,7 @@ import org.lwjgl.input.Keyboard;
 
 import net.fexcraft.lib.common.math.RGB;
 import net.fexcraft.lib.mc.gui.GenericGui;
+import net.fexcraft.lib.mc.utils.Formatter;
 import net.fexcraft.mod.fvtm.FVTM;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.resources.I18n;
@@ -146,6 +147,7 @@ public abstract class ConstructorGui extends GenericGui<ConstructorContainer> {
 					string = I18n.format(split[0], (Object[])Arrays.copyOfRange(split, 1, split.length));
 				}
 			}
+			string = Formatter.format(string);
 			this.width = gui.mc.fontRenderer.getStringWidth(string);
 			if(color != null) this.color = color;
 		}

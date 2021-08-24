d8c15dd94aa50980206899986f8313cd2e53b05a
Ferdinand Calo <fercalo96@yahoo.de>
2021-08-24 21:34:12 +0200

    Update RVStore.java

--- src/main/java/net/fexcraft/mod/fvtm/util/handler/RVStore.java
+++ src/main/java/net/fexcraft/mod/fvtm/util/handler/RVStore.java
@@ -5,8 +5,10 @@ import java.util.UUID;
 
 import net.fexcraft.mod.fvtm.entity.RenderViewEntity;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerChangedDimensionEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedOutEvent;
+import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerRespawnEvent;
 
 /**
  * Render View Entity Storage/Handler
@@ -31,6 +33,24 @@ public class RVStore {
 		if(event.player.world.isRemote) return;
 		RenderViewEntity entity = ENTITIES.remove(event.player.getGameProfile().getId());
 		if(entity != null) entity.setDead();
+	}
+	
+	@SubscribeEvent
+	public void onRespawn(PlayerRespawnEvent event){
+		RenderViewEntity entity = ENTITIES.remove(event.player.getGameProfile().getId());
+		if(entity != null) entity.setDead();
+		entity = new RenderViewEntity(event.player);
+		ENTITIES.put(event.player.getGameProfile().getId(), entity);
+		event.player.world.spawnEntity(entity);
+	}
+	
+	@SubscribeEvent
+	public void onRespawn(PlayerChangedDimensionEvent event){
+		RenderViewEntity entity = ENTITIES.remove(event.player.getGameProfile().getId());
+		if(entity != null) entity.setDead();
+		entity = new RenderViewEntity(event.player);
+		ENTITIES.put(event.player.getGameProfile().getId(), entity);
+		event.player.world.spawnEntity(entity);
 	}
 
 }

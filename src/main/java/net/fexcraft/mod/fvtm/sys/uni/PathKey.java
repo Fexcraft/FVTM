a557bfbf79415c6352967ceceac1064814a1f789
Ferdinand Calo <fercalo96@yahoo.de>
2021-09-17 15:48:20 +0200

    Update PathKey.java

--- src/main/java/net/fexcraft/mod/fvtm/sys/uni/PathKey.java
+++ src/main/java/net/fexcraft/mod/fvtm/sys/uni/PathKey.java
@@ -10,9 +10,18 @@ public class PathKey implements Comparable<PathKey> {
 	protected byte[] xyz = new byte[6];
 	
 	public PathKey(Vec316f start, Vec316f end){
-		pos[0] = start.pos.getX(); pos[1] = start.pos.getY(); pos[2] = start.pos.getZ();
-		pos[3] = end.pos.getX(); pos[4] = end.pos.getY(); pos[5] = end.pos.getZ();
-		xyz[0] = start.x; xyz[1] = start.y; xyz[2] = start.z; xyz[3] = end.x; xyz[4] = end.y; xyz[5] = end.z;
+		pos[0] = start.pos.getX();
+		pos[1] = start.pos.getY();
+		pos[2] = start.pos.getZ();
+		pos[3] = end.pos.getX();
+		pos[4] = end.pos.getY();
+		pos[5] = end.pos.getZ();
+		xyz[0] = start.x;
+		xyz[1] = start.y;
+		xyz[2] = start.z;
+		xyz[3] = end.x;
+		xyz[4] = end.y;
+		xyz[5] = end.z;
 	}
 
 	public String toUnitId(boolean opposite){
@@ -32,29 +41,53 @@ public class PathKey implements Comparable<PathKey> {
 	}
 	
 	public NBTTagCompound write(NBTTagCompound compound){
-		compound.setIntArray("key_pos", pos); compound.setByteArray("key_xyz", xyz); return compound;
+		compound.setIntArray("key_pos", pos);
+		compound.setByteArray("key_xyz", xyz);
+		return compound;
 	}
 
 	public PathKey(PathKey key, boolean opposite){
 		if(opposite){
-			pos[0] = key.pos[3]; pos[1] = key.pos[4]; pos[2] = key.pos[5];
-			pos[3] = key.pos[0]; pos[4] = key.pos[1]; pos[5] = key.pos[2];
-			xyz[0] = key.xyz[3]; xyz[1] = key.xyz[4]; xyz[2] = key.xyz[5];
-			xyz[3] = key.xyz[0]; xyz[4] = key.xyz[1]; xyz[5] = key.xyz[2];
-		} else{ pos = key.pos; xyz = key.xyz; }
+			pos[0] = key.pos[3];
+			pos[1] = key.pos[4];
+			pos[2] = key.pos[5];
+			pos[3] = key.pos[0];
+			pos[4] = key.pos[1];
+			pos[5] = key.pos[2];
+			xyz[0] = key.xyz[3];
+			xyz[1] = key.xyz[4];
+			xyz[2] = key.xyz[5];
+			xyz[3] = key.xyz[0];
+			xyz[4] = key.xyz[1];
+			xyz[5] = key.xyz[2];
+		}
+		else{
+			pos = key.pos;
+			xyz = key.xyz;
+		}
 	}
 
 	@Override
 	public int compareTo(PathKey o){
-		for(int i = 0; i < 6; i++){ if(o.pos[order[i]] > pos[order[i]]) return 1; if(o.pos[order[i]] < pos[order[i]]) return -1; }
-		for(int i = 0; i < 6; i++){ if(o.xyz[order[i]] > xyz[order[i]]) return 1; if(o.xyz[order[i]] < xyz[order[i]]) return -1; }
+		for(int i = 0; i < 6; i++){
+			if(o.pos[order[i]] > pos[order[i]]) return 1;
+			if(o.pos[order[i]] < pos[order[i]]) return -1;
+		}
+		for(int i = 0; i < 6; i++){
+			if(o.xyz[order[i]] > xyz[order[i]]) return 1;
+			if(o.xyz[order[i]] < xyz[order[i]]) return -1;
+		}
 		return 0;
 	}
 	
 	@Override
 	public boolean equals(Object obj){
-		if(obj instanceof PathKey == false) return false; PathKey key = (PathKey)obj;
-		for(int i = 0; i < 6; i++){ if(key.pos[i] != pos[i] || key.xyz[i] != xyz[i]) return false; } return true;
+		if(obj instanceof PathKey == false) return false;
+		PathKey key = (PathKey)obj;
+		for(int i = 0; i < 6; i++){
+			if(key.pos[i] != pos[i] || key.xyz[i] != xyz[i]) return false;
+		}
+		return true;
 	}
 
 	public Vec316f toVec3f(int i){

32a22f07f72805bc575c1690839d1c4cfc74af73
Ferdinand Calo <fercalo96@yahoo.de>
2021-09-20 19:36:20 +0200

    Update WireModel.java

--- src/main/java/net/fexcraft/mod/fvtm/model/WireModel.java
+++ src/main/java/net/fexcraft/mod/fvtm/model/WireModel.java
@@ -24,7 +24,8 @@ public class WireModel extends GenericModel<BlockData, TileEntity> {
 	
 	public static final HashMap<String, WireModel> DECOS = new HashMap<>();
 	public static final WireModel EMPTY = new WireModel();
-	public ArrayList<Vec3f[]> wire_model = new ArrayList<>();
+	public HashMap<Integer, ArrayList<Vec3f[]>> wire_model = new HashMap<>();
+	public HashMap<Integer, Object[]> wire_data = new HashMap<>();
 	public boolean wire_tempcull = false;
 	protected ResourceLocation texture = Resources.NULL_TEXTURE;
 	protected ArrayList<String> accepts = new ArrayList<>();
@@ -41,7 +42,8 @@ public class WireModel extends GenericModel<BlockData, TileEntity> {
 		wire_tempcull = Boolean.parseBoolean(ObjParser.getCommentValue(data, "WireCulling:"));
 		List<String[]> wires = ObjParser.getCommentValues(data, new String[]{ "Wire:" }, null, null);
 		if(wires.isEmpty()) return;
-		for(String[] args : wires){
+		for(int i = 0; i < wires.size(); i++){
+			String[] args = wires.get(i);
 			boolean rect = args[0].equals("rect") || args[0].equals("flat");
 			float scale = Float.parseFloat(args[1]);
 			float sx = Float.parseFloat(args[2]);
@@ -50,12 +52,28 @@ public class WireModel extends GenericModel<BlockData, TileEntity> {
 			float h = Float.parseFloat(args[5]);
 			boolean m = Boolean.parseBoolean(args[6]);
 			if(rect){
-				this.addWireRect(scale, sx, sy, w, h, m);
+				this.addWireRect(i, scale, sx, sy, w, h, m);
 			}
 			else{
 				Vec3f tl = new Vec3f(args, 7), tr = new Vec3f(args, 10), bl = new Vec3f(args, 13), br = new Vec3f(args, 16);
-				this.addWireRectShape(scale, sx, sy, w, h, tl, tr, bl, br, m);
+				this.addWireRectShape(i, scale, sx, sy, w, h, tl, tr, bl, br, m);
 			}
+		}
+		wires = ObjParser.getCommentValues(data, new String[]{ "WireData:" }, null, null);
+		for(String[] args : wires){
+			int idx = Integer.parseInt(args[0]);
+			if(args.length < 2) continue;
+			Object[] odata = new Object[1];
+			for(int i = 1; i < args.length; i++){
+				String[] split = args[i].split(":");
+				switch(split[0]){
+					case "noslack":{
+						odata[0] = true;
+						break;
+					}
+				}
+			}
+			wire_data.put(idx, odata);
 		}
 	}
 
@@ -74,36 +92,38 @@ public class WireModel extends GenericModel<BlockData, TileEntity> {
 		transforms.deapply();
 	}
 	
-	public void addWireRect(float scale, float start_x, float start_y, float width, float height, boolean mirror){
-		wire_model.add(new Vec3f[]{ new Vec3f(start_x, start_y, 0).scale(scale), new Vec3f(start_x + width, start_y, 0).scale(scale) });
+	public void addWireRect(int idx, float scale, float start_x, float start_y, float width, float height, boolean mirror){
+		if(!wire_model.containsKey(idx)) wire_model.put(idx, new ArrayList<>());
+		wire_model.get(idx).add(new Vec3f[]{ new Vec3f(start_x, start_y, 0).scale(scale), new Vec3f(start_x + width, start_y, 0).scale(scale) });
 		if(height > 0){
-			wire_model.add(new Vec3f[]{ new Vec3f(start_x, start_y - height, 0).scale(scale), new Vec3f(start_x, start_y, 0).scale(scale) });
-			wire_model.add(new Vec3f[]{ new Vec3f(start_x + width, start_y, 0).scale(scale), new Vec3f(start_x + width, start_y - height, 0).scale(scale) });
-			wire_model.add(new Vec3f[]{ new Vec3f(start_x, start_y - height, 0).scale(scale), new Vec3f(start_x + width, start_y - height, 0).scale(scale) });
+			wire_model.get(idx).add(new Vec3f[]{ new Vec3f(start_x, start_y - height, 0).scale(scale), new Vec3f(start_x, start_y, 0).scale(scale) });
+			wire_model.get(idx).add(new Vec3f[]{ new Vec3f(start_x + width, start_y, 0).scale(scale), new Vec3f(start_x + width, start_y - height, 0).scale(scale) });
+			wire_model.get(idx).add(new Vec3f[]{ new Vec3f(start_x, start_y - height, 0).scale(scale), new Vec3f(start_x + width, start_y - height, 0).scale(scale) });
 		}
 		else{
 			float h = 0.01f / scale;
-			wire_model.add(new Vec3f[]{ new Vec3f(start_x, start_y - h, 0).scale(scale), new Vec3f(start_x + width, start_y - h, 0).scale(scale) });
+			wire_model.get(idx).add(new Vec3f[]{ new Vec3f(start_x, start_y - h, 0).scale(scale), new Vec3f(start_x + width, start_y - h, 0).scale(scale) });
 		}
-		if(mirror) addWireRect(scale, -start_x - width, start_y, width, height, false);
+		if(mirror) addWireRect(idx, scale, -start_x - width, start_y, width, height, false);
 	}
 	
-	public void addWireRectShape(float scale, float start_x, float start_y, float width, float height, Vec3f tl, Vec3f tr, Vec3f bl, Vec3f br, boolean mirror){
+	public void addWireRectShape(int idx, float scale, float start_x, float start_y, float width, float height, Vec3f tl, Vec3f tr, Vec3f bl, Vec3f br, boolean mirror){
 		if(tl == null) tl = new Vec3f();
 		if(tr == null) tr = new Vec3f();
 		if(bl == null) bl = new Vec3f();
 		if(br == null) br = new Vec3f();
-		wire_model.add(new Vec3f[]{ new Vec3f(start_x + bl.x, start_y + tl.y, 0).scale(scale), new Vec3f(start_x + width + br.x, start_y + tr.y, 0).scale(scale) });
+		if(!wire_model.containsKey(idx)) wire_model.put(idx, new ArrayList<>());
+		wire_model.get(idx).add(new Vec3f[]{ new Vec3f(start_x + bl.x, start_y + tl.y, 0).scale(scale), new Vec3f(start_x + width + br.x, start_y + tr.y, 0).scale(scale) });
 		if(height > 0){
-			wire_model.add(new Vec3f[]{ new Vec3f(start_x + tl.x, start_y - height + bl.y, 0).scale(scale), new Vec3f(start_x + bl.x, start_y + tl.y, 0).scale(scale) });
-			wire_model.add(new Vec3f[]{ new Vec3f(start_x + width + br.x, start_y + tr.y, 0).scale(scale), new Vec3f(start_x + width + tr.x, start_y - height + br.y, 0).scale(scale) });
-			wire_model.add(new Vec3f[]{ new Vec3f(start_x + tl.x, start_y - height + bl.y, 0).scale(scale), new Vec3f(start_x + width + tr.x, start_y - height + br.y, 0).scale(scale) });
+			wire_model.get(idx).add(new Vec3f[]{ new Vec3f(start_x + tl.x, start_y - height + bl.y, 0).scale(scale), new Vec3f(start_x + bl.x, start_y + tl.y, 0).scale(scale) });
+			wire_model.get(idx).add(new Vec3f[]{ new Vec3f(start_x + width + br.x, start_y + tr.y, 0).scale(scale), new Vec3f(start_x + width + tr.x, start_y - height + br.y, 0).scale(scale) });
+			wire_model.get(idx).add(new Vec3f[]{ new Vec3f(start_x + tl.x, start_y - height + bl.y, 0).scale(scale), new Vec3f(start_x + width + tr.x, start_y - height + br.y, 0).scale(scale) });
 		}
 		else{
 			float h = 0.01f / scale;
-			wire_model.add(new Vec3f[]{ new Vec3f(start_x + tl.x, start_y - h + bl.y, 0).scale(scale), new Vec3f(start_x + width + tr.x, start_y - h + br.y, 0).scale(scale) });
+			wire_model.get(idx).add(new Vec3f[]{ new Vec3f(start_x + tl.x, start_y - h + bl.y, 0).scale(scale), new Vec3f(start_x + width + tr.x, start_y - h + br.y, 0).scale(scale) });
 		}
-		if(mirror) addWireRectShape(scale, -start_x - width, start_y, width, height, tl, tr, bl, br, false);
+		if(mirror) addWireRectShape(idx, scale, -start_x - width, start_y, width, height, tl, tr, bl, br, false);
 	}
 
 	public void texture(ResourceLocation resloc){

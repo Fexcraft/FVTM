52134f684ffbe16007ca8c92cb9f8bc1f46c78a5
Ferdinand Calo <fercalo96@yahoo.de>
2021-09-20 13:43:12 +0200

    Update FMFParser.java

--- src/main/java/net/fexcraft/mod/fvtm/model/FMFParser.java
+++ src/main/java/net/fexcraft/mod/fvtm/model/FMFParser.java
@@ -11,8 +11,8 @@ import net.fexcraft.lib.common.math.RGB;
 import net.fexcraft.lib.common.math.TexturedPolygon;
 import net.fexcraft.lib.common.math.TexturedVertex;
 import net.fexcraft.lib.common.math.Vec3f;
-import net.fexcraft.lib.mc.utils.Print;
 import net.fexcraft.lib.tmt.BoxBuilder;
+import net.fexcraft.lib.tmt.CustomUVBuilder;
 import net.fexcraft.lib.tmt.CylinderBuilder;
 import net.fexcraft.lib.tmt.ModelRendererTurbo;
 import net.fexcraft.lib.tmt.RotationOrder;
@@ -40,7 +40,6 @@ public class FMFParser {
 		int r = -1;
 		String larray = null;
 		while((r = stream.read()) > -1){
-			Print.debug("#S " + r);
 			switch(r){
 				case N:{
 					model.name = readString(stream);
@@ -59,10 +58,8 @@ public class FMFParser {
 				}
 				case G:{
 					TurboList list = new TurboList(readString(stream));
-					Print.debug(list.name + " parsing");
 					readPolygons(stream, list, model.textureX, model.textureY);
 					model.groups.add(list);
-					Print.debug(list.name + " added");
 					break;
 				}
 				case V:{
@@ -78,9 +75,8 @@ public class FMFParser {
 					((ArrayList<String>)data.get(larray)).add(readString(stream));
 					break;
 				}
-				default: continue;
+				default: break;
 			}
-			Print.debug("#E " + r);
 		}
 		return data;
 	}
@@ -102,278 +98,201 @@ public class FMFParser {
 	}
 	
 	private static void readPolygons(InputStream stream, TurboList list, int tx, int ty) throws IOException {
-		int r = -1;
-		boolean end = false;
-		while(!end){
+		int r = -1, tv = 0;
+		ArrayList<TexturedVertex> verts = null;
+		ArrayList<Vec3f> norms = null;
+		ModelRendererTurbo mrt = null;
+		BoxBuilder box = null;
+		CylinderBuilder cyl = null;
+		CustomUVBuilder cuv = null;
+		while(true){
 			if((r = stream.read()) == -1) break;
-			Print.debug("p " + r);
-			switch(r){
-				case PE:{
-					end = true;
-					break;
+			if(r == PE || r > PO) break;
+			int type = r;
+			mrt = new ModelRendererTurbo(list, 0, 0, tx, ty);
+			if(r == PB) cuv = box = new BoxBuilder(mrt);
+			else if(r == PC) cuv = cyl = new CylinderBuilder(mrt);
+			else if(r == PO){
+				verts = new ArrayList<>();
+				norms = new ArrayList<>();
+				tv = 0;
+			}
+			boolean done = false;
+			while(!done){
+				if((r = stream.read()) == -1) break;
+				if(r == PE){
+					if(type == PB) list.add(box.build());
+					else if(type == PC) list.add(cyl.build());
+					else if(type == PO) list.add(mrt);
+					done = true;
 				}
-				case PB:{
-					BoxBuilder box = new BoxBuilder(new ModelRendererTurbo(list, 0, 0, tx, ty));
-					boolean done = false;
-					Print.debug(box);
-					while(!done){
-						if((r = stream.read()) == -1) break;
-						Print.debug("b " + r);
-						switch(r){
-							case PP:{
-								float[] fl = readFloats(stream, 3);
-								box.getRoot().setPosition(fl[0], fl[1], fl[2]);
-								break;
-							}
-							case PR:{
-								float[] fl = readFloats(stream, 3);
-								box.getRoot().setRotationAngle(fl[0], fl[1], fl[2]);
-								break;
-							}
-							case PF:{
-								float[] fl = readFloats(stream, 3);
-								box.setOffset(fl[0], fl[1], fl[2]);
-								break;
-							}
-							case PT:{
-								int[] in = readIntegers(stream, 2);
-								box.getRoot().setTextureOffset(in[0], in[1]);
-								break;
-							}
-							case PL:{
-								box.getRoot().setColor(new RGB(readIntegers(stream, 1)[0]));
-								break;
-							}
-							case PBS:{
-								float[] fl = readFloats(stream, 3);
-								box.setSize(fl[0], fl[1], fl[2]);
-								break;
-							}
-							case PBC:{
-								int in = stream.read();
-								float[] fl = readFloats(stream, 3);
-								box.setCorner(in, fl[0], fl[1], fl[2]);
-								break;
-							}
-							case PBE:{
-								box.setExpansion(readFloats(stream, 1)[0]);
-								break;
-							}
-							case PDF:{
-								box.removePolygons(readBooleans(stream, 6));
-								break;
-							}
-							case PDU:{
-								box.setDetachedUV(readBooleans(stream, 6));
-								break;
-							}
-							case PCU:{
-								int in = stream.read();
-								float[] fl = readFloats(stream, stream.read());
-								box.setPolygonUV(in, fl);
-								break;
-							}
-							case PM:{
-								box.getRoot().boxName = readString(stream);
-								break;
-							}
-							case PRO:{
-								int[] ro = new int[]{ stream.read(), stream.read(), stream.read() };
-								box.getRoot().setRotationOrder(getRotationOrder(ro));
-								break;
-							}
-							case PTM:{
-								box.getRoot().texName = readString(stream);
-								break;
-							}
-							case PE:{
-								list.add(box.build());
-								done = true;
-								break;
-							}
-						}
+				switch(r){
+					case PP:{
+						float[] fl = readFloats(stream, 3);
+						mrt.setPosition(fl[0], fl[1], fl[2]);
+						break;
 					}
-					break;
-				}
-				case PC:{
-					CylinderBuilder cyl = new CylinderBuilder(new ModelRendererTurbo(list, 0, 0, tx, ty));
-					boolean done = false;
-					Print.debug(cyl);
-					while(!done){
-						if((r = stream.read()) == -1) break;
-						Print.debug("c " + r);
-						switch(r){
-							case PP:{
-								float[] fl = readFloats(stream, 3);
-								cyl.getRoot().setPosition(fl[0], fl[1], fl[2]);
-								break;
-							}
-							case PR:{
-								float[] fl = readFloats(stream, 3);
-								cyl.getRoot().setRotationAngle(fl[0], fl[1], fl[2]);
-								break;
-							}
-							case PF:{
-								float[] fl = readFloats(stream, 3);
-								cyl.setPosition(fl[0], fl[1], fl[2]);
-								break;
-							}
-							case PT:{
-								int[] in = readIntegers(stream, 2);
-								cyl.getRoot().setTextureOffset(in[0], in[1]);
-								break;
-							}
-							case PL:{
-								cyl.getRoot().setColor(new RGB(readIntegers(stream, 1)[0]));
-								break;
-							}
-							case PCRL:{
-								float[] fl = readFloats(stream, 3);
-								cyl.setRadius(fl[0], fl[1]);
-								cyl.setLength(fl[2]);
-								break;
-							}
-							case PCD:{
-								cyl.setDirection(readIntegers(stream, 1)[0]);
-								break;
-							}
-							case PCSG:{
-								int[] in = readIntegers(stream, 2);
-								cyl.setSegments(in[0], in[1]);
-								break;
-							}
-							case PCSL:{
-								float[] fl = readFloats(stream, 2);
-								cyl.setScale(fl[0], fl[1]);
-								break;
-							}
-							case PCTO:{
-								float[] fl = readFloats(stream, 3);
-								cyl.setTopOffset(fl[0], fl[1], fl[2]);
-								break;
-							}
-							case PCTR:{
-								float[] fl = readFloats(stream, 3);
-								cyl.setTopRotation(fl[0], fl[1], fl[2]);
-								break;
-							}
-							case PCRT:{
-								float[] fl = readFloats(stream, 2);
-								cyl.setRadialTexture(fl[0], fl[1]);
-								break;
-							}
-							case PDF:{
-								cyl.removePolygons(readBooleans(stream, 6));
-								break;
-							}
-							case PDU:{
-								cyl.setDetachedUV(readBooleans(stream, 6));
-								break;
-							}
-							case PCU:{
-								int in = stream.read();
-								float[] fl = readFloats(stream, stream.read());
-								cyl.setPolygonUV(in, fl);
-								break;
-							}
-							case PM:{
-								cyl.getRoot().boxName = readString(stream);
-								break;
-							}
-							case PRO:{
-								int[] ro = new int[]{ stream.read(), stream.read(), stream.read() };
-								cyl.getRoot().setRotationOrder(getRotationOrder(ro));
-								break;
-							}
-							case PTM:{
-								cyl.getRoot().texName = readString(stream);
-								break;
-							}
-							case PE:{
-								list.add(cyl.build());
-								done = true;
-								break;
-							}
-						}
+					case PR:{
+						float[] fl = readFloats(stream, 3);
+						mrt.setRotationAngle(fl[0], fl[1], fl[2]);
+						break;
 					}
-					break;
-				}
-				case PO:{
-					boolean done = false;
-					ModelRendererTurbo turbo = new ModelRendererTurbo(list, 0, 0, tx, ty);
-					ArrayList<TexturedVertex> verts = new ArrayList<>();
-					ArrayList<Vec3f> norms = new ArrayList<>();
-					int tv = 0;
-					while(!done){
-						if((r = stream.read()) == -1) break;
-						switch(r){
-							case PP:{
-								float[] fl = readFloats(stream, 3);
-								turbo.setPosition(fl[0], fl[1], fl[2]);
-								break;
+					case PT:{
+						if(type == PO){
+							float[] fl = readFloats(stream, 2);
+							verts.get(tv++).setTexturePosition(fl[0], fl[1]);
+							break;
+						}
+						int[] in = readIntegers(stream, 2);
+						mrt.setTextureOffset(in[0], in[1]);
+						break;
+					}
+					case PL:{
+						mrt.setColor(new RGB(readIntegers(stream, 1)[0]));
+						break;
+					}
+					case PDF:{
+						if(type == PO){
+							int a = readIntegers(stream, 1)[0];
+							int[] idx = readIntegers(stream, a);
+							TexturedVertex[] vrts = new TexturedVertex[a];
+							for(int i = 0; i < idx.length; i++){
+								vrts[i] = verts.get(idx[i]);
 							}
-							case PR:{
-								float[] fl = readFloats(stream, 3);
-								turbo.setRotationAngle(fl[0], fl[1], fl[2]);
-								break;
-							}
-							case PF:{
-								float[] fl = readFloats(stream, 3);
-								verts.add(new TexturedVertex(fl[0], fl[1], fl[2], 0, 0));
-								break;
-							}
-							case PT:{
-								float[] fl = readFloats(stream, 2);
-								verts.get(tv++).setTexturePosition(fl[0], fl[1]);
-								break;
-							}
-							case PN:{
-								float[] fl = readFloats(stream, 3);
-								norms.add(new Vec3f(fl[0], fl[1], fl[2]));
-								break;
-							}
-							case PL:{
-								turbo.setColor(new RGB(readIntegers(stream, 1)[0]));
-								break;
-							}
-							case PDF:{
-								int a = readIntegers(stream, 1)[0];
-								int[] idx = readIntegers(stream, a);
-								TexturedVertex[] vrts = new TexturedVertex[a];
+							TexturedPolygon poly = new TexturedPolygon(vrts);
+							if(norms.size() > 0){
+								idx = readIntegers(stream, a);
 								for(int i = 0; i < idx.length; i++){
-									vrts[i] = verts.get(idx[i]);
+									poly.getNormalVerts().add(norms.get(idx[i]));
 								}
-								TexturedPolygon poly = new TexturedPolygon(vrts);
-								if(norms.size() > 0){
-									idx = readIntegers(stream, a);
-									for(int i = 0; i < idx.length; i++){
-										poly.getNormalVerts().add(norms.get(idx[i]));
-									}
-								}
-								turbo.copyTo(poly);
-								break;
 							}
-							case PM:{
-								turbo.boxName = readString(stream);
-								break;
-							}
-							case PRO:{
-								int[] ro = new int[]{ stream.read(), stream.read(), stream.read() };
-								turbo.setRotationOrder(getRotationOrder(ro));
-								break;
-							}
-							case PTM:{
-								turbo.texName = readString(stream);
-								break;
-							}
-							case PE:{
-								list.add(turbo);
-								done = true;
-								break;
-							}
+							mrt.copyTo(poly);
+							break;
+						}
+						boolean[] arr = new boolean[6];
+						arr[0] = stream.read() > 0;
+						arr[1] = stream.read() > 0;
+						arr[2] = stream.read() > 0;
+						arr[3] = stream.read() > 0;
+						arr[4] = stream.read() > 0;
+						arr[5] = stream.read() > 0;
+						cuv.removePolygons(arr);
+						break;
+					}
+					case PDU:{
+						boolean[] arr = new boolean[6];
+						arr[0] = stream.read() > 0;
+						arr[1] = stream.read() > 0;
+						arr[2] = stream.read() > 0;
+						arr[3] = stream.read() > 0;
+						arr[4] = stream.read() > 0;
+						arr[5] = stream.read() > 0;
+						cuv.setDetachedUV(arr);
+						break;
+					}
+					case PCU:{
+						int in = stream.read();
+						cuv.setPolygonUV(in, readFloats(stream, stream.read()));
+						break;
+					}
+					case PM:{
+						mrt.boxName = readString(stream);
+						break;
+					}
+					case PRO:{
+						int[] ro = new int[]{ stream.read(), stream.read(), stream.read() };
+						mrt.setRotationOrder(getRotationOrder(ro));
+						break;
+					}
+					case PTM:{
+						mrt.texName = readString(stream);
+						break;
+					}
+					default: break;
+				}
+				if(type == PB){
+					switch(r){
+						case PF:{
+							float[] fl = readFloats(stream, 3);
+							box.setOffset(fl[0], fl[1], fl[2]);
+							break;
+						}
+						case PBS:{
+							float[] fl = readFloats(stream, 3);
+							box.setSize(fl[0], fl[1], fl[2]);
+							break;
+						}
+						case PBC:{
+							int in = stream.read();
+							float[] fl = readFloats(stream, 3);
+							box.setCorner(in, fl[0], fl[1], fl[2]);
+							break;
+						}
+						case PBE:{
+							box.setExpansion(readFloats(stream, 1)[0]);
+							break;
+						}
+						default: break;
+					}
+				}
+				else if(type == PC){
+					switch(r){
+						case PF:{
+							float[] fl = readFloats(stream, 3);
+							cyl.setPosition(fl[0], fl[1], fl[2]);
+							break;
+						}
+						case PCRL:{
+							float[] fl = readFloats(stream, 3);
+							cyl.setRadius(fl[0], fl[1]);
+							cyl.setLength(fl[2]);
+							break;
+						}
+						case PCD:{
+							cyl.setDirection(readIntegers(stream, 1)[0]);
+							break;
+						}
+						case PCSG:{
+							int[] in = readIntegers(stream, 2);
+							cyl.setSegments(in[0], in[1]);
+							break;
+						}
+						case PCSL:{
+							float[] fl = readFloats(stream, 2);
+							cyl.setScale(fl[0], fl[1]);
+							break;
+						}
+						case PCTO:{
+							float[] fl = readFloats(stream, 3);
+							cyl.setTopOffset(fl[0], fl[1], fl[2]);
+							break;
+						}
+						case PCTR:{
+							float[] fl = readFloats(stream, 3);
+							cyl.setTopRotation(fl[0], fl[1], fl[2]);
+							break;
+						}
+						case PCRT:{
+							float[] fl = readFloats(stream, 2);
+							cyl.setRadialTexture(fl[0], fl[1]);
+							break;
+						}
+						default: break;
+					}
+				}
+				else if(type == PO){
+					switch(r){
+						case PF:{
+							float[] fl = readFloats(stream, 3);
+							verts.add(new TexturedVertex(fl[0], fl[1], fl[2], 0, 0));
+							break;
+						}
+						case PN:{
+							float[] fl = readFloats(stream, 3);
+							norms.add(new Vec3f(fl[0], fl[1], fl[2]));
+							break;
 						}
 					}
-					break;
 				}
 			}
 		}
@@ -384,7 +303,7 @@ public class FMFParser {
 		for(int i = 0; i < t; i++){
 			byte[] bit = new byte[4];
 			int r = stream.read(bit);
-			if(r < 4) return arr;//error
+			if(r < 0) return arr;//error
 			arr[i] = ByteBuffer.wrap(bit).getFloat();
 		}
 		return arr;
@@ -395,18 +314,8 @@ public class FMFParser {
 		for(int i = 0; i < t; i++){
 			byte[] bit = new byte[4];
 			int r = stream.read(bit);
-			if(r < 4) return arr;//error
+			if(r < 0) return arr;//error
 			arr[i] = ByteBuffer.wrap(bit).getInt();
-		}
-		return arr;
-	}
-
-	private static boolean[] readBooleans(InputStream stream, int t) throws IOException {
-		boolean[] arr = new boolean[t];
-		for(int i = 0; i < t; i++){
-			int r = stream.read();
-			if(r < 1) return arr;//error
-			arr[i] = r == 1;
 		}
 		return arr;
 	}

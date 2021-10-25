775af76b3f5efb28b0d8d8d7f7b67eb7aa87d7c6
Ferdinand Calo <fercalo96@yahoo.de>
2021-10-25 23:45:40 +0200

    Update ParticleEntity.java

--- src/main/java/net/fexcraft/mod/fvtm/sys/particle/ParticleEntity.java
+++ src/main/java/net/fexcraft/mod/fvtm/sys/particle/ParticleEntity.java
@@ -2,6 +2,7 @@ package net.fexcraft.mod.fvtm.sys.particle;
 
 import org.lwjgl.opengl.GL11;
 
+import net.fexcraft.lib.common.math.RGB;
 import net.fexcraft.lib.common.math.Vec3f;
 
 public class ParticleEntity {
@@ -9,6 +10,8 @@ public class ParticleEntity {
 	private Particle particle;
 	protected Vec3f pos, prev, temp;
 	protected Vec3f dir, speed;
+	protected float percent, scale;
+	public RGB color;
 	public int passed;
 	
 	public ParticleEntity(Particle part, Vec3f pos){
@@ -18,6 +21,9 @@ public class ParticleEntity {
 		temp = new Vec3f();
 		dir = particle.dir;
 		speed = particle.speed;
+		percent = 0;
+		scale = particle.scale;
+		color = new RGB(particle.color);
 	}
 	
 	public void setDirSpeed(Vec3f dir, Vec3f speed){
@@ -29,9 +35,13 @@ public class ParticleEntity {
 		temp.x = prev.x + (pos.x - prev.x) * ticks;
 		temp.y = prev.y + (pos.y - prev.y) * ticks;
 		temp.z = prev.z + (pos.z - prev.z) * ticks;
+		GL11.glPushMatrix();
 		GL11.glTranslatef(temp.x, temp.y, temp.z);
-		particle.model.render();
-		GL11.glTranslatef(-temp.x, -temp.y, -temp.z);
+		color.glColorApply();
+		GL11.glScalef(scale, scale, scale);
+		particle.model.render(1f);
+		RGB.glColorReset();
+		GL11.glPopMatrix();
 	}
 
 	public void update(){
@@ -41,6 +51,10 @@ public class ParticleEntity {
 		pos.y = prev.y += dir.y * speed.y;
 		pos.z = prev.z += dir.z * speed.z;
 		passed++;
+	}
+
+	public boolean expired(){
+		return passed >= particle.persistence;
 	}
 
 }

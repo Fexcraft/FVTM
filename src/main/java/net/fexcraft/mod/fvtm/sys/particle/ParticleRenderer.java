3c23b4b92b421bae3c191ed0307e9c2f40ff90b9
Ferdinand Calo <fercalo96@yahoo.de>
2021-10-26 19:41:38 +0200

    Update ParticleRenderer.java

--- src/main/java/net/fexcraft/mod/fvtm/sys/particle/ParticleRenderer.java
+++ src/main/java/net/fexcraft/mod/fvtm/sys/particle/ParticleRenderer.java
@@ -8,6 +8,7 @@ import net.fexcraft.mod.fvtm.render.RailRenderer;
 import net.fexcraft.mod.fvtm.sys.uni.EntitySystem;
 import net.fexcraft.mod.fvtm.sys.uni.SystemManager;
 import net.fexcraft.mod.fvtm.sys.uni.SystemManager.Systems;
+import net.fexcraft.mod.fvtm.util.Resources;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.OpenGlHelper;
 import net.minecraft.world.World;
@@ -20,10 +21,11 @@ public class ParticleRenderer {
         GL11.glPushMatrix();
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
         GL11.glTranslated(-cx, -cy, -cz);
-        Minecraft.getMinecraft().renderEngine.bindTexture(RailRenderer.WOOLTEX);
+        Minecraft.getMinecraft().renderEngine.bindTexture(Resources.WHITE_TEXTURE);
         EntitySystem sys = SystemManager.get(Systems.ENTITY, world);
         ents.addAll(sys.particles);
         for(ParticleEntity part : ents){
+        	if(part == null || part.pos == null) return;
 	        int z = RailRenderer.getBrightness(part.pos), x = z % 65536, y = z / 65536;
 	        OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float)x / 1.0F, (float)y / 1.0F);
             part.render(ticks);

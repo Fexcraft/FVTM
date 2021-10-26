5c8073f46ca05cd944c5a79aa84277f0d012e81d
Ferdinand Calo <fercalo96@yahoo.de>
2021-10-26 14:00:37 +0200

    Update ParticleEntity.java

--- src/main/java/net/fexcraft/mod/fvtm/sys/particle/ParticleEntity.java
+++ src/main/java/net/fexcraft/mod/fvtm/sys/particle/ParticleEntity.java
@@ -2,6 +2,7 @@ package net.fexcraft.mod.fvtm.sys.particle;
 
 import org.lwjgl.opengl.GL11;
 
+import net.fexcraft.lib.common.Static;
 import net.fexcraft.lib.common.math.RGB;
 import net.fexcraft.lib.common.math.Vec3f;
 
@@ -47,10 +48,27 @@ public class ParticleEntity {
 	public void update(){
 		prev = pos;
 		pos = new Vec3f();
-		pos.x = prev.x += dir.x * speed.x;
-		pos.y = prev.y += dir.y * speed.y;
-		pos.z = prev.z += dir.z * speed.z;
+		float x = (Static.random.nextFloat() - 0.5f) / 50f, z = (Static.random.nextFloat() - 0.5f) / 50f;
+		pos.x = prev.x + dir.x * speed.x + x;
+		pos.y = prev.y + dir.y * speed.y;
+		pos.z = prev.z + dir.z * speed.z + z;
+		percent = passed / (float)particle.persistence;
+		if(particle.scale_end > 0){
+			scale = particle.scale + (particle.scale_end - particle.scale) * percent;
+		}
+		if(particle.end_color != null){
+			color.packed = mix(particle.color, particle.end_color, percent);
+		}
 		passed++;
+	}
+	
+	public int mix(RGB base, RGB mix, float percent){
+		float[] fl = base.toFloatArray();
+		float[] f1 = mix.toFloatArray();
+		int r = (int)(fl[0] + (f1[0] - fl[0]) * percent * 256);
+		int g = (int)(fl[1] + (f1[1] - fl[1]) * percent * 256);
+		int b = (int)(fl[2] + (f1[2] - fl[2]) * percent * 256);
+		return (65536 * r) + (256 * g) + b;
 	}
 
 	public boolean expired(){

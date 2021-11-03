8c86ca681b17cd94ff8359d1e2357a73063ccb5c
Ferdinand Calo <fercalo96@yahoo.de>
2021-11-03 22:50:13 +0100

    Create ConditionEvent.java

---
+++ src/main/java/net/fexcraft/mod/fvtm/event/ConditionEvent.java
@@ -0,0 +1,37 @@
+package net.fexcraft.mod.fvtm.event;
+
+import net.fexcraft.mod.fvtm.sys.condition.Condition;
+import net.fexcraft.mod.fvtm.sys.condition.Condition.Conditional;
+import net.minecraftforge.fml.common.eventhandler.Event;
+
+public class ConditionEvent extends Event {
+
+	private Condition condition;
+
+	public ConditionEvent(Condition con){
+		this.condition = con;
+	}
+
+	public Condition getCondition(){
+		return condition;
+	}
+	
+	public static class ConditionalCreate extends ConditionEvent {
+		
+		private Conditional result;
+
+		public ConditionalCreate(Condition con){
+			super(con);
+		}
+		
+		public void setConditional(Conditional cond){
+			result = cond;
+		}
+		
+		public Conditional getConditional(){
+			return result;
+		}
+		
+	}
+
+}

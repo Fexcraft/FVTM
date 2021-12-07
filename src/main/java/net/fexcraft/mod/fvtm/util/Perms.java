f6db915e9bfbbdfdea88fbb505e4c4c30d84bb6f
Ferdinand Calo <fercalo96@yahoo.de>
2021-12-07 21:46:17 +0100

    Update ServerReceiver.java

--- src/main/java/net/fexcraft/mod/fvtm/util/Perms.java
+++ src/main/java/net/fexcraft/mod/fvtm/util/Perms.java
@@ -15,7 +15,8 @@ public class Perms {
 	public static final Perm RAIL_PLACER_GUI = new Perm("fvtm.gui.rail_placer");
 	public static final Perm ROAD_PLACER_GUI = new Perm("fvtm.gui.road_placer");
 	public static final Perm ROAD_PLACER_ITEM = new Perm("fvtm.item.road_placer");
-	public static final Perm EDIT_ATTRIBUTES = new Perm("fvtm.attribute.edit");
+	public static final Perm EDIT_INTERNAL_ATTRIBUTES = new Perm("fvtm.attribute.edit_internal");
+	public static final Perm EDIT_NON_EDITABLE_ATTRIBUTES = new Perm("fvtm.attribute.edit_non_editable");
 	//public static final Perm ROAD_PLACER_GUI_NOBLOCK = new Perm("fvtm.gui.road_placer.noblock");
 	
 	public static void register(){
@@ -23,7 +24,8 @@ public class Perms {
 		PermissionAPI.registerNode(ROAD_PLACER_GUI.id(), OP, "FVTM GUI for placing (block) roads.");
 		PermissionAPI.registerNode(ROAD_PLACER_ITEM.id(), Config.ROADTOOL_FOR_ALL ? ALL : OP, "FVTM Item for placing (block) roads.");
 		//PermissionAPI.registerNode(ROAD_PLACER_GUI_NOBLOCK.id(), Config.NO_RAIL_BLOCKS ? ALL : OP, "Allows to place road blocks without inventory consumption via the RoadPlacer GUI.");
-		PermissionAPI.registerNode(EDIT_ATTRIBUTES.id(), OP, "Permission to edit non editable attributes or when outside of a vehicle.");
+		PermissionAPI.registerNode(EDIT_INTERNAL_ATTRIBUTES.id(), OP, "Permission to edit internal attributes (which are not marked as external) when being outside a vehicle.");
+		PermissionAPI.registerNode(EDIT_NON_EDITABLE_ATTRIBUTES.id(), OP, "Permission to edit non editable attributes.");
 	}
 	
 	public static class Perm {

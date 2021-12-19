37fa405fd27baf87556985918da969383b37d7b4
Ferdinand Calo <fercalo96@yahoo.de>
2021-12-19 22:54:27 +0100

    HCP moved to own repository.
    https://github.com/Fexcraft/HCP
    https://github.com/Fexcraft/HCP/commit/c1cd46007ce9b3866f3dbb901cb0f2a63e788be6

--- src/main/java/net/fexcraft/mod/addon/hcp/HeavyCargoPack.java
+++
@@ -1,8 +0,0 @@
-package net.fexcraft.mod.addon.hcp;
-
-import net.fexcraft.mod.fvtm.data.addon.AddonClass;
-import net.minecraftforge.fml.common.Mod;
-
-@AddonClass(registryname = "fvtm:hcp")
-@Mod(modid = "hcp", name = "Heavy Cargo Pack", version = "2.1.3", useMetadata = true, dependencies = "required-after:fcl;required-after:fvtm")
-public class HeavyCargoPack {}

b9999fba93d6e8951e56b3815418506faa46f65a
Ferdinand Calo <fercalo96@yahoo.de>
2021-12-19 22:45:30 +0100

    GEP moved to own repository.
    https://github.com/Fexcraft/GEP
    https://github.com/Fexcraft/GEP/commit/270e9e93f5235f0cdb80845dfbdaa199b29f3cab

--- src/main/java/net/fexcraft/mod/addon/gep/GeneralEssentialsPack.java
+++
@@ -1,8 +0,0 @@
-package net.fexcraft.mod.addon.gep;
-
-import net.fexcraft.mod.fvtm.data.addon.AddonClass;
-import net.minecraftforge.fml.common.Mod;
-
-@AddonClass(registryname = "fvtm:gep")
-@Mod(modid = "gep", name = "General Essentials Pack", version = "2.3.0", useMetadata = true, dependencies = "required-after:fcl;required-after:fvtm")
-public class GeneralEssentialsPack {}

bf4c2d9e1a5d3608418d0a576179edc58d5f0702
Ferdinand Calo <fercalo96@yahoo.de>
2021-12-25 18:09:07 +0100

    Update WireModel.java

--- src/main/java/net/fexcraft/mod/fvtm/model/WireModel.java
+++ src/main/java/net/fexcraft/mod/fvtm/model/WireModel.java
@@ -35,7 +35,10 @@ public class WireModel extends GenericModel<BlockData, TileEntity> {
 	
 	public WireModel(JsonObject obj){ super(obj); }
 	
-	public WireModel(Object[] stream, String type){ super(stream, type); }
+	@Override
+	public WireModel parse(Object[] stream, String type){
+		return super.parse(stream, type);
+	}
 	
 	public WireModel(ResourceLocation loc, ObjModel data, ArrayList<String> objgroups, boolean exclude){
 		super(loc, data, objgroups, exclude);

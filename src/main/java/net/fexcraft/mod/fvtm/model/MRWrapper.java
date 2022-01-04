4b0c62b94776717c8775a1a9718ee2b51e61e9da
Ferdinand Calo <fercalo96@yahoo.de>
2022-01-04 22:39:46 +0100

    Update MRWrapper.java

--- src/main/java/net/fexcraft/mod/fvtm/model/MRWrapper.java
+++ src/main/java/net/fexcraft/mod/fvtm/model/MRWrapper.java
@@ -9,11 +9,11 @@ import net.fexcraft.mod.fvtm.data.Capabilities;
 import net.fexcraft.mod.fvtm.data.root.RenderCache;
 import net.fexcraft.mod.fvtm.item.ClothItem;
 import net.fexcraft.mod.fvtm.util.Command;
-import net.minecraft.client.entity.AbstractClientPlayer;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
-import net.minecraft.client.renderer.entity.RenderPlayer;
-import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.entity.RenderLivingBase;
+import net.minecraft.entity.EntityLivingBase;
 
 /** @author Ferdinand Calo' (FEX___96) */
 public class MRWrapper extends ModelRenderer {
@@ -22,18 +22,18 @@ public class MRWrapper extends ModelRenderer {
 	private ArrayList<ArrayList<String>> cloth_models = new ArrayList<>();
 	private ArrayList<ClothItem> cloth_items = new ArrayList<>();
 	private ArrayList<String> cloth_groups = new ArrayList<>();
-	private RenderPlayer renderer;
-	private EntityPlayer player;
+	private RenderLivingBase<?> renderer;
+	private EntityLivingBase entity;
 	private String id;
 
-	public MRWrapper(ModelBase base, ModelRenderer parent, RenderPlayer renderer, String id){
+	public MRWrapper(ModelBase base, ModelRenderer parent, RenderLivingBase<?> renderer, String id){
 		super(base);
 		this.parent = parent;
 		this.renderer = renderer;
 		this.id = id;
 	}
 	
-	public static MRWrapper get(ModelBase base, ModelRenderer from, RenderPlayer renderer, String id){
+	public static MRWrapper get(ModelBase base, ModelRenderer from, RenderLivingBase<?> renderer, String id){
 		if(from.childModels == null || from.childModels.isEmpty()){
 			from.addChild(new MRWrapper(base, from, renderer, id));
 		}
@@ -49,7 +49,8 @@ public class MRWrapper extends ModelRenderer {
 	public void render(float scale){
 		if(cloth_models.isEmpty()) return;
     	GL11.glRotatef(90, 0, 1, 0);
-    	RenderCache cache = player.getCapability(Capabilities.RENDERCACHE, null);
+    	int deftex = GL11.glGetInteger(GL11.GL_TEXTURE_BINDING_2D);
+    	RenderCache cache = entity.getCapability(Capabilities.RENDERCACHE, null);
 		for(int i = 0; i < cloth_items.size(); i++){
 			net.fexcraft.lib.tmt.ModelBase.bindTexture(cloth_items.get(i).getType().getTexture());
 			if(cloth_groups.get(i).startsWith("skirt")){
@@ -57,7 +58,7 @@ public class MRWrapper extends ModelRenderer {
 				if(id.contains("right")) GL11.glTranslatef(0, 0, 0.25f);
 				GL11.glRotated(parent.rotateAngleX * 5, 0, 0, 1);
 			}
-			cloth_items.get(i).getType().getModel().render(cloth_items.get(i), cloth_models.get(i), player, cache);
+			cloth_items.get(i).getType().getModel().render(cloth_items.get(i), cloth_models.get(i), entity, cache);
 			if(cloth_groups.get(i).startsWith("skirt")) GL11.glPopMatrix();
 		}
 		cloth_items.clear();
@@ -66,11 +67,11 @@ public class MRWrapper extends ModelRenderer {
     	if(Command.OTHER) DebugModels.center.renderPlain();
     	RGB.glColorReset();
     	GL11.glRotatef(90, 0, -1, 0);
-    	renderer.bindTexture(renderer.getEntityTexture((AbstractClientPlayer)player));
+    	GlStateManager.bindTexture(deftex);
 	}
 
-	public void set(EntityPlayer player, ClothItem item, ArrayList<String> list, String key){
-		this.player = player;
+	public void set(EntityLivingBase entity, ClothItem item, ArrayList<String> list, String key){
+		this.entity = entity;
 		cloth_items.add(item);
 		cloth_models.add(list);
 		cloth_groups.add(key);

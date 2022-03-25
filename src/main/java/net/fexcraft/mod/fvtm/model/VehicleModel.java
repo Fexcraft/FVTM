e6d313ea3c9847079367433522f13eadc4959148
Ferdinand Calo <fercalo96@yahoo.de>
2022-03-25 11:57:33 +0100

    Update VehicleModel.java

--- src/main/java/net/fexcraft/mod/fvtm/model/VehicleModel.java
+++ src/main/java/net/fexcraft/mod/fvtm/model/VehicleModel.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
+import org.apache.commons.lang3.math.NumberUtils;
 import org.lwjgl.opengl.GL11;
 
 import com.google.gson.JsonObject;
@@ -52,11 +54,47 @@ public class VehicleModel extends GenericModel<VehicleData, Object> implements F
 		try{
 			HashMap<String, Object> data = FMFParser.parse(this, (InputStream)stream[0]);
 			if(data.containsKey("ItemScale")){
-				try{
-					item_scale.setAll(Float.parseFloat(data.get("ItemScale").toString()));
+				Object obj = data.get("ItemScale");
+				if(obj instanceof ArrayList){
+					ArrayList<String> list = (ArrayList<String>)obj;
+					for(String str : list){
+						try{
+							parseItemTransform(0, str);
+						}
+						catch(Exception e){
+							e.printStackTrace();
+						}
+					}
 				}
-				catch(Exception e){
-					e.printStackTrace();
+				else{
+					try{
+						item_scale.setAll(Float.parseFloat(obj.toString()));
+					}
+					catch(Exception e){
+						e.printStackTrace();
+					}
+				}
+			}
+			if(data.containsKey("ItemTranslate")){
+				ArrayList<String> list = (ArrayList<String>)data.get("ItemTranslate");
+				for(String str : list){
+					try{
+						parseItemTransform(1, str);
+					}
+					catch(Exception e){
+						e.printStackTrace();
+					}
+				}
+			}
+			if(data.containsKey("ItemRotate")){
+				ArrayList<String> list = (ArrayList<String>)data.get("ItemRotate");
+				for(String str : list){
+					try{
+						parseItemTransform(2, str);
+					}
+					catch(Exception e){
+						e.printStackTrace();
+					}
 				}
 			}
 			stream[0] = data;
@@ -67,12 +105,107 @@ public class VehicleModel extends GenericModel<VehicleData, Object> implements F
 		return super.parse(stream, type);
 	}
 	
+	private void parseItemTransform(int type, String str) throws Exception {
+		String[] split = str.split(" ");
+		split[1] = split[1].toLowerCase();
+		float[] val = new float[3];
+		boolean set = split[0].toLowerCase().equals("set");
+		if(split.length == 3){
+			val[0] = val[1] = val[2] = Float.parseFloat(split[1]);
+		}
+		else{
+			val[0] = Float.parseFloat(split[1]);
+			val[1] = Float.parseFloat(split[2]);
+			val[2] = Float.parseFloat(split[3]);
+		}
+		if(split[1].equals("all")){
+			switch(type){
+				case 0:
+					if(set) item_scale.setAll(val[0], val[1], val[2]);
+					else item_scale.addAll(val[0], val[1], val[2]);
+					return;
+				case 1:
+					if(set) item_translate.setAll(val[0], val[1], val[2]);
+					else item_translate.addAll(val[0], val[1], val[2]);
+					return;
+				case 2:
+					if(set) item_rotate.setAll(val[0], val[1], val[2]);
+					else item_rotate.addAll(val[0], val[1], val[2]);
+					return;
+			}
+		}
+		else{
+			TransformType trype = null;
+			switch(split[1]){
+				case "none": trype = TransformType.NONE; break;
+				case "third_person_left_hand": trype = TransformType.THIRD_PERSON_LEFT_HAND; break;
+				case "third_person_right_hand": trype = TransformType.THIRD_PERSON_RIGHT_HAND; break;
+				case "first_person_left_hand": trype = TransformType.FIRST_PERSON_LEFT_HAND; break;
+				case "first_person_right_hand": trype = TransformType.THIRD_PERSON_RIGHT_HAND; break;
+				case "head": trype = TransformType.HEAD; break;
+				case "gui": trype = TransformType.GUI; break;
+				case "ground": trype = TransformType.GROUND; break;
+				case "fixed": trype = TransformType.FIXED; break;
+			}
+			switch(type){
+				case 0:
+					if(set) item_scale.set(trype, val[0], val[1], val[2]);
+					else item_scale.add(trype, val[0], val[1], val[2]);
+					return;
+				case 1:
+					if(set) item_translate.set(trype, val[0], val[1], val[2]);
+					else item_translate.add(trype, val[0], val[1], val[2]);
+					return;
+				case 2:
+					if(set) item_rotate.set(trype, val[0], val[1], val[2]);
+					else item_rotate.add(trype, val[0], val[1], val[2]);
+					return;
+			}
+		}
+	}
+
 	public VehicleModel(ResourceLocation loc, ObjModel data, ArrayList<String> objgroups, boolean exclude){
 		super(loc, data, objgroups, exclude);
 		try{
 			String val = ObjParser.getCommentValue(data, "ItemScale:");
 			if(val != null){
 				item_scale.setAll(Float.parseFloat(val));
+			}
+			List<String> iscal = ObjParser.getCommentValues(data, new String[]{ "ItemScale:" }, 0);
+			if(iscal.size() > 0){
+				for(String str : iscal){
+					try{
+						if(NumberUtils.isCreatable(str)){
+							item_scale.setAll(Float.parseFloat(str));
+						}
+						else parseItemTransform(0, str);
+					}
+					catch(Exception e){
+						e.printStackTrace();
+					}
+				}
+			}
+			List<String> itrans = ObjParser.getCommentValues(data, new String[]{ "ItemTranslate:" }, 0);
+			if(itrans.size() > 0){
+				for(String str : itrans){
+					try{
+						parseItemTransform(1, str);
+					}
+					catch(Exception e){
+						e.printStackTrace();
+					}
+				}
+			}
+			List<String> irotat = ObjParser.getCommentValues(data, new String[]{ "ItemRotate:" }, 0);
+			if(irotat.size() > 0){
+				for(String str : irotat){
+					try{
+						parseItemTransform(2, str);
+					}
+					catch(Exception e){
+						e.printStackTrace();
+					}
+				}
 			}
 		}
 		catch(Exception e){

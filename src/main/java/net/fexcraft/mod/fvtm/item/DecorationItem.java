dda2bafa1ec4e493654ff990cc12a8aeada21e25
Ferdinand Calo <fercalo96@yahoo.de>
2022-04-23 21:48:48 +0200

    Update DecorationItem.java

--- src/main/java/net/fexcraft/mod/fvtm/item/DecorationItem.java
+++ src/main/java/net/fexcraft/mod/fvtm/item/DecorationItem.java
@@ -1,5 +1,7 @@
 package net.fexcraft.mod.fvtm.item;
 
+import static net.fexcraft.mod.fvtm.gui.GuiHandler.DECORATION_EDITOR;
+
 import java.util.List;
 
 import javax.annotation.Nullable;
@@ -7,6 +9,8 @@ import javax.annotation.Nullable;
 import net.fexcraft.lib.common.math.RGB;
 import net.fexcraft.lib.mc.api.registry.fItem;
 import net.fexcraft.lib.mc.utils.Formatter;
+import net.fexcraft.lib.mc.utils.Print;
+import net.fexcraft.mod.fvtm.FVTM;
 import net.fexcraft.mod.fvtm.data.JunctionGridItem;
 import net.fexcraft.mod.fvtm.entity.Decoration;
 import net.fexcraft.mod.fvtm.util.Vec316f;
@@ -49,17 +53,24 @@ public class DecorationItem extends Item implements JunctionGridItem {
 	public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ){
 		if(world.isRemote) return EnumActionResult.PASS;
 		ItemStack stack = player.getHeldItem(hand);
+		Print.debug(hitX, hitY, hitZ);
 		Vec316f vector = new Vec316f(world, new Vec3d(pos).add(hitX, hitY, hitZ), 16);
 		Decoration decoen = new Decoration(world);
 		decoen.setPosition(vector.vector.x, vector.vector.y, vector.vector.z);
 		world.spawnEntity(decoen);
 		if(!player.capabilities.isCreativeMode) stack.shrink(1);
+    	player.openGui(FVTM.getInstance(), DECORATION_EDITOR, world, decoen.getEntityId(), 0, 0);
 		return EnumActionResult.SUCCESS;
 	}
 
 	@Override
 	public float[][] getGridColours(){
 		return gridcolour;
+	}
+
+	@Override
+	public int getPlacingGrid(){
+		return 16;
 	}
 
 }

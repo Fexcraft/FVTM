c77ecd6d0bc27e927fd1a73663c5fe54188615a3
Ferdinand Calo <fercalo96@yahoo.de>
2022-04-23 21:24:32 +0200

    Create Decoration.java

---
+++ src/main/java/net/fexcraft/mod/fvtm/entity/Decoration.java
@@ -0,0 +1,178 @@
+package net.fexcraft.mod.fvtm.entity;
+
+import static net.fexcraft.lib.common.Static.sixteenth;
+import static net.fexcraft.mod.fvtm.gui.GuiHandler.DECORATION_EDITOR;
+
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+
+import io.netty.buffer.ByteBuf;
+import net.fexcraft.lib.mc.utils.Print;
+import net.fexcraft.mod.fvtm.FVTM;
+import net.fexcraft.mod.fvtm.data.Capabilities;
+import net.fexcraft.mod.fvtm.item.DecorationItem;
+import net.fexcraft.mod.fvtm.item.MaterialItem;
+import net.fexcraft.mod.fvtm.sys.tsign.TrafficSigns;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.nbt.NBTTagString;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.RayTraceResult;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.common.registry.IEntityAdditionalSpawnData;
+
+public class Decoration extends Entity implements IEntityAdditionalSpawnData {
+	
+	private ArrayList<String> decos = new ArrayList<>();
+	private boolean locked;
+	private int size = 4;
+
+    public Decoration(World world){
+        super(world);
+        stepHeight = 0;
+        setSize(size * sixteenth, size * sixteenth);
+    }
+
+    @Override
+    public void writeSpawnData(ByteBuf buffer){
+    	try{
+    		buffer.writeBoolean(locked);
+    		buffer.writeInt(size);
+    		buffer.writeInt(decos.size());
+    		for(String str : decos){
+    			buffer.writeInt(str.length());
+    			buffer.writeCharSequence(str, StandardCharsets.UTF_8);
+    		}
+    	}
+    	catch(Exception e){
+			e.printStackTrace();
+		}
+    }
+
+    @Override
+    public void readSpawnData(ByteBuf buffer){
+    	try{
+    		locked = buffer.readBoolean();
+        	size = buffer.readInt();
+        	decos.clear();
+        	int amount = buffer.readInt();
+        	for(int i = 0; i < amount; i++){
+        		decos.add(buffer.readCharSequence(buffer.readInt(), StandardCharsets.UTF_8).toString());
+        	}
+    	}
+    	catch(Exception e){
+			e.printStackTrace();
+		}
+    }
+
+	@Override
+    protected void readEntityFromNBT(NBTTagCompound compound){
+        size = compound.getInteger("size");
+        decos.clear();
+        if(compound.hasKey("decorations")){
+        	NBTTagList list = (NBTTagList)compound.getTag("decorations");
+        	for(int i = 0; i < list.tagCount(); i++) decos.add(list.getStringTagAt(i));
+        }
+    }
+
+    @Override
+    protected void writeEntityToNBT(NBTTagCompound compound){
+    	compound.setInteger("size", size);
+    	NBTTagList list = new NBTTagList();
+    	for(String str : decos) list.appendTag(new NBTTagString(str));
+    	if(list.tagCount() > 0) compound.setTag("decorations", list);
+    }
+    
+	@Override
+	public void setDead(){
+		super.setDead();
+	}
+
+    @Override
+    public void fall(float k, float l){
+        //
+    }
+
+    @Override
+    protected void entityInit(){
+        //
+    }
+
+    @Override
+    public void onUpdate(){
+    	//
+    }
+
+    @Override
+    public void setPositionAndRotationDirect(double d, double d1, double d2, float f, float f1, int i, boolean b){
+        //
+    }
+
+    @Override
+    public boolean canBePushed(){
+        return false;
+    }
+
+    @Override
+    public boolean canBeCollidedWith(){
+        return !isDead;
+    }
+
+    @Override
+    public void applyEntityCollision(Entity entity){
+        return;
+    }
+    
+    @Override
+    public boolean processInitialInteract(EntityPlayer player, EnumHand hand){
+        if(isDead || world.isRemote || hand == EnumHand.OFF_HAND){
+            return false;
+        }
+        ItemStack stack = player.getHeldItem(hand);
+        if(!stack.isEmpty() && stack.getItem() instanceof MaterialItem && ((MaterialItem)stack.getItem()).getType().isVehicleKey()){
+            locked = !locked;
+            Print.chat(player, "Toggled sign status.");
+            return true;
+        }
+        if(locked){
+            Print.chat(player, "Sign is locked.");
+            return true;
+        }
+        if(stack.isEmpty()){
+        	player.openGui(FVTM.getInstance(), DECORATION_EDITOR, world, getEntityId(), 0, 0);
+        	return true;
+        }
+        return false;
+    }
+    
+    @Override
+    public boolean attackEntityFrom(DamageSource damagesource, float i){
+        if(world.isRemote || isDead){
+            return true;
+        }
+        if(damagesource.damageType.equals("player")){
+            if(locked){
+                Print.chat(damagesource.getImmediateSource(), "Deco is locked.");
+                return true;
+            }
+        	ItemStack stack = this.getPickedResult(null);
+            entityDropItem(stack, 0.5F);
+            setDead();
+            BlockPos pos = getPosition();
+            TrafficSigns cap = world.getChunk(pos).getCapability(Capabilities.TRAFFIC_SIGNS, null);
+            if(cap != null) cap.remove(pos);
+        }
+        return true;
+    }
+    
+    @Override
+    public ItemStack getPickedResult(RayTraceResult target){
+        return new ItemStack(DecorationItem.INSTANCE);
+    }
+
+}

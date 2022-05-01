50d57bb7aa4683a119dd59b06262b636d3f93b9e
Ferdinand Calo <fercalo96@yahoo.de>
2022-05-01 18:00:33 +0200

    Update DecoEditorContainer.java

--- src/main/java/net/fexcraft/mod/fvtm/gui/deco/DecoEditorContainer.java
+++ src/main/java/net/fexcraft/mod/fvtm/gui/deco/DecoEditorContainer.java
@@ -1,26 +1,57 @@
 package net.fexcraft.mod.fvtm.gui.deco;
 
 import net.fexcraft.lib.mc.gui.GenericContainer;
+import net.fexcraft.lib.mc.utils.Print;
 import net.fexcraft.mod.fvtm.data.DecorationData;
 import net.fexcraft.mod.fvtm.entity.Decoration;
+import net.fexcraft.mod.fvtm.util.Resources;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class DecoEditorContainer extends GenericContainer {
 	
-	protected Decoration deco;
+	protected Decoration entity;
 	protected DecorationData selected;
+	@SideOnly(Side.CLIENT)
+	protected DecoEditor gui;
 
 	public DecoEditorContainer(EntityPlayer player, World world, int entid){
 		super(player);
-		deco = (Decoration)world.getEntityByID(entid);
+		entity = (Decoration)world.getEntityByID(entid);
+		if(Resources.DECORATION_CATEGORIES.isEmpty()){
+			player.closeScreen();
+			Print.chat(player, "error, no decorations installed");
+		}
 	}
 
 	@Override
 	protected void packet(Side side, NBTTagCompound packet, EntityPlayer player){
-		//
+		if(!packet.hasKey("cargo")) return;
+		String cargo = packet.getString("cargo");
+		switch(cargo){
+			case "add":{
+				DecorationData deco = Resources.DECORATIONS.get(packet.getString("key"));
+				entity.decos.add(deco.copy());
+				if(side.isServer()) send(Side.CLIENT, packet);
+				else{
+					entity.decos.get(entity.decos.size() - 1).copy(deco);
+					gui.updateEntries();
+				}
+				break;
+			}
+			case "rem":{
+				entity.decos.remove(packet.getInteger("idx"));
+				if(side.isServer()){
+					send(Side.CLIENT, packet);
+				}
+				else{
+					gui.updateEntries();
+				}
+			}
+		}
 	}
 
 }

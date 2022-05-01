2e3339a53722c95db1ea7c895e670119d334eca8
Ferdinand Calo <fercalo96@yahoo.de>
2022-05-01 19:37:52 +0200

    Update DecoEditorContainer.java

--- src/main/java/net/fexcraft/mod/fvtm/gui/deco/DecoEditorContainer.java
+++ src/main/java/net/fexcraft/mod/fvtm/gui/deco/DecoEditorContainer.java
@@ -1,6 +1,7 @@
 package net.fexcraft.mod.fvtm.gui.deco;
 
 import net.fexcraft.lib.mc.gui.GenericContainer;
+import net.fexcraft.lib.mc.utils.Pos;
 import net.fexcraft.lib.mc.utils.Print;
 import net.fexcraft.mod.fvtm.data.DecorationData;
 import net.fexcraft.mod.fvtm.entity.Decoration;
@@ -49,9 +50,66 @@ public class DecoEditorContainer extends GenericContainer {
 				}
 				else{
 					gui.updateEntries();
+					gui.select(-1);
 				}
+				break;
+			}
+			case "pos":{
+				DecorationData deco = entity.decos.get(packet.getInteger("idx"));
+				Pos pos = null;
+				switch(packet.getInteger("axis")){
+					case 0: pos = new Pos(packet.getFloat("value"), deco.offset.y, deco.offset.z); break;
+					case 1: pos = new Pos(deco.offset.x, packet.getFloat("value"), deco.offset.z); break;
+					case 2: pos = new Pos(deco.offset.x, deco.offset.y, packet.getFloat("value")); break;
+					default: return;
+				}
+				deco.offset = pos;
+				if(side.isServer()) send(Side.CLIENT, packet);
+				break;
+			}
+			case "rot":{
+				DecorationData deco = entity.decos.get(packet.getInteger("idx"));
+				switch(packet.getInteger("axis")){
+					case 0: deco.rotx = packet.getFloat("value"); break;
+					case 1: deco.roty = packet.getFloat("value"); break;
+					case 2: deco.rotz = packet.getFloat("value"); break;
+					default: return;
+				}
+				if(side.isServer()) send(Side.CLIENT, packet);
+				break;
+			}
+			case "scale":{
+				DecorationData deco = entity.decos.get(packet.getInteger("idx"));
+				switch(packet.getInteger("axis")){
+					case 0: deco.sclx = packet.getFloat("value"); break;
+					case 1: deco.scly = packet.getFloat("value"); break;
+					case 2: deco.sclz = packet.getFloat("value"); break;
+					default: return;
+				}
+				if(side.isServer()) send(Side.CLIENT, packet);
+				break;
+			}
+			case "tex":{
+				DecorationData deco = entity.decos.get(packet.getInteger("idx"));
+				int sel = packet.getInteger("sel");
+				if(sel >= 0 && sel < deco.textures.size()){
+					deco.seltex = sel;
+					if(side.isServer()) send(Side.CLIENT, packet);
+					else{
+						gui.select(gui.selected);
+					}
+				}
+				break;
 			}
 		}
 	}
+
+	@Override
+    public void onContainerClosed(EntityPlayer player){
+        super.onContainerClosed(player);
+        if(!player.world.isRemote && entity != null){
+        	entity.updateClient();
+        }
+    }
 
 }

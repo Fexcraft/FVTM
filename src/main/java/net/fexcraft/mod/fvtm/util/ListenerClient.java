abb239c7e55f4abc5e2f0801d4905e4481802330
Ferdinand Calo <fercalo96@yahoo.de>
2022-05-02 10:28:39 +0200

    Update ListenerClient.java

--- src/main/java/net/fexcraft/mod/fvtm/util/ListenerClient.java
+++ src/main/java/net/fexcraft/mod/fvtm/util/ListenerClient.java
@@ -4,6 +4,7 @@ import net.fexcraft.lib.mc.api.packet.IPacketListener;
 import net.fexcraft.lib.mc.network.packet.PacketNBTTagCompound;
 import net.fexcraft.mod.fvtm.data.Capabilities;
 import net.fexcraft.mod.fvtm.data.vehicle.VehicleEntity;
+import net.fexcraft.mod.fvtm.entity.Decoration;
 import net.fexcraft.mod.fvtm.sys.tsign.TrafficSignData;
 import net.fexcraft.mod.fvtm.sys.tsign.TrafficSigns;
 import net.fexcraft.mod.fvtm.sys.uni12.ULandVehicle;
@@ -67,6 +68,11 @@ public class ListenerClient implements IPacketListener<PacketNBTTagCompound> {
 				BlockPos pos = BlockPos.fromLong(packet.nbt.getLong("pos"));
 				TrafficSigns signs = player.world.getChunk(pos).getCapability(Capabilities.TRAFFIC_SIGNS, null);
 				if(signs != null) signs.addSignAt(pos, packet.nbt.getFloat("rotation"), packet.nbt.getFloat("offset"), true);
+				return;
+			}
+			case "deco_update":{
+				Decoration ent = (Decoration)player.world.getEntityByID(packet.nbt.getInteger("entid"));
+				if(ent != null) ent.readEntityFromNBT(packet.nbt);
 				return;
 			}
 			default: return;

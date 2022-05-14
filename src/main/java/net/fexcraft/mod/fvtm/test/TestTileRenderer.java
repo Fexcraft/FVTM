1313286bc428971070191540a140eb345518a7d8
Ferdinand Calo <fercalo96@yahoo.de>
2022-05-14 14:51:42 +0200

    Update TestTileRenderer.java

--- src/main/java/net/fexcraft/mod/fvtm/test/TestTileRenderer.java
+++ src/main/java/net/fexcraft/mod/fvtm/test/TestTileRenderer.java
@@ -1,13 +1,22 @@
 package net.fexcraft.mod.fvtm.test;
 
 import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Vector3f;
+import com.mojang.math.Vector4f;
 
-import net.fexcraft.mod.fvtm.FVTM;
+import net.fexcraft.lib.frl.Polygon;
+import net.fexcraft.lib.frl.Polyhedron;
+import net.fexcraft.lib.frl.Vertex;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider.Context;
+import net.minecraft.resources.ResourceLocation;
 
 public class TestTileRenderer implements BlockEntityRenderer<TestTile> {
+	
+	public static ResourceLocation resloc = new ResourceLocation("minecraft:textures/block/stone.png");
 
 	public TestTileRenderer(Context context){
 		//
@@ -15,8 +24,26 @@ public class TestTileRenderer implements BlockEntityRenderer<TestTile> {
 
 	@Override
 	public void render(TestTile tile, float ticks, PoseStack stack, MultiBufferSource buffers, int light, int overlay){
-		//
-		FVTM.getLogger().info("test " + ticks);
+        stack.pushPose();
+        stack.translate(0, 1.2, 0);
+        //stack.mulPose(Vector3f.YP.rotationDegrees(1));
+        VertexConsumer consumer = buffers.getBuffer(RenderType.entityCutout(resloc));
+        for(Polyhedron<?> poli : T1PModel.MODEL.hedrons){
+        	stack.pushPose();
+        	stack.translate(poli.posX * 0.0625f, poli.posY * 0.0625f, poli.posZ * 0.0625f);
+            PoseStack.Pose pose = stack.last();
+            for(Polygon poly : poli.polygons){
+               for(Vertex vertex : poly.vertices){
+                  Vector4f pos = new Vector4f(vertex.vector.x, vertex.vector.y, vertex.vector.z, 1.0F);
+                  pos.transform(pose.pose());
+                  Vector3f norm = new Vector3f(vertex.norm.x, vertex.norm.y, vertex.norm.z);
+                  norm.transform(pose.normal());
+                  consumer.vertex(pos.x(), pos.y(), pos.z(), 1f, 1f, 1f, 1f, vertex.u, vertex.v, overlay, light, norm.x(), norm.y(), norm.z());
+               }
+            }
+            stack.popPose();
+        }
+        stack.popPose();
 	}
 
 }

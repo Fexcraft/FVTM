82cb0acb3f1d067af40632cefc27256552f91ddd
Ferdinand Calo <fercalo96@yahoo.de>
2022-05-14 14:51:37 +0200

    Update TestBlock.java

--- src/main/java/net/fexcraft/mod/fvtm/test/TestBlock.java
+++ src/main/java/net/fexcraft/mod/fvtm/test/TestBlock.java
@@ -5,16 +5,17 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Material;
 
 public class TestBlock extends Block implements EntityBlock {
 
-	public TestBlock(Properties p_49795_){
-		super(p_49795_);
+	public TestBlock(){
+		super(Properties.of(Material.STONE).noOcclusion());
 	}
 
 	@Override
-	public BlockEntity newBlockEntity(BlockPos p_153215_, BlockState p_153216_) {
-		return new TestTile(p_153215_, p_153216_);
+	public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
+		return new TestTile(pos, state);
 	}
 
 }

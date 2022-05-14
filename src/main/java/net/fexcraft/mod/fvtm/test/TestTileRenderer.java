fe13a1657a0dedc9189996fa4730c6d6687bd8eb
Ferdinand Calo <fercalo96@yahoo.de>
2022-05-14 15:16:55 +0200

    Update TestTileRenderer.java

--- src/main/java/net/fexcraft/mod/fvtm/test/TestTileRenderer.java
+++ src/main/java/net/fexcraft/mod/fvtm/test/TestTileRenderer.java
@@ -19,15 +19,16 @@ public class TestTileRenderer implements BlockEntityRenderer<TestTile> {
 	public static ResourceLocation resloc = new ResourceLocation("minecraft:textures/block/stone.png");
 
 	public TestTileRenderer(Context context){
-		//
 	}
 
 	@Override
 	public void render(TestTile tile, float ticks, PoseStack stack, MultiBufferSource buffers, int light, int overlay){
         stack.pushPose();
         stack.translate(0, 1.2, 0);
+    	stack.mulPose(Vector3f.YP.rotationDegrees(180));
+    	stack.mulPose(Vector3f.ZP.rotationDegrees(180));
         //stack.mulPose(Vector3f.YP.rotationDegrees(1));
-        VertexConsumer consumer = buffers.getBuffer(RenderType.entityCutout(resloc));
+        VertexConsumer consumer = buffers.getBuffer(RenderType.entityTranslucent(resloc));
         for(Polyhedron<?> poli : T1PModel.MODEL.hedrons){
         	stack.pushPose();
         	stack.translate(poli.posX * 0.0625f, poli.posY * 0.0625f, poli.posZ * 0.0625f);
@@ -38,7 +39,7 @@ public class TestTileRenderer implements BlockEntityRenderer<TestTile> {
                   pos.transform(pose.pose());
                   Vector3f norm = new Vector3f(vertex.norm.x, vertex.norm.y, vertex.norm.z);
                   norm.transform(pose.normal());
-                  consumer.vertex(pos.x(), pos.y(), pos.z(), 1f, 1f, 1f, 1f, vertex.u, vertex.v, overlay, light, norm.x(), norm.y(), norm.z());
+                  consumer.vertex(pos.x(), pos.y(), pos.z(), 1f, 1f, 1f, 1f, vertex.u, vertex.v, overlay, light, 1, 1, 1);
                }
             }
             stack.popPose();

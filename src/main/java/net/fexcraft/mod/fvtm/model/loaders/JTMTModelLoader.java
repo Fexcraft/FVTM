c81dae5b84caabd628878cee576dccb5358dfc7c
Ferdinand Calo <fercalo96@yahoo.de>
2022-05-20 17:07:35 +0200

    Create JTMTModelLoader.java

---
+++ src/main/java/net/fexcraft/mod/fvtm/model/loaders/JTMTModelLoader.java
@@ -0,0 +1,68 @@
+package net.fexcraft.mod.fvtm.model.loaders;
+
+import java.util.ArrayList;
+import java.util.Map.Entry;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import net.fexcraft.lib.common.json.JsonToTMT;
+import net.fexcraft.lib.common.json.JsonUtil;
+import net.fexcraft.lib.mc.utils.Static;
+import net.fexcraft.mod.fvtm.data.root.Model.ModelData;
+import net.fexcraft.mod.fvtm.data.root.Model.ModelLoader;
+import net.fexcraft.mod.fvtm.model.GenericModel;
+import net.fexcraft.mod.fvtm.model.TurboList;
+import net.fexcraft.mod.fvtm.util.Resources;
+import net.minecraft.util.ResourceLocation;
+
+public class JTMTModelLoader implements ModelLoader {
+
+	@Override
+	public boolean accepts(String name, String suffix){
+		return suffix.equals(".jtmt");
+	}
+
+	@Override
+	public Object[] load(String name, ModelData confdata) throws Exception {
+		GenericModel model = new GenericModel();
+		JsonObject obj = JsonUtil.getObjectFromInputStream(Resources.getModelInputStream(new ResourceLocation(name), true));
+		if(obj.has("creators")){
+			obj.get("creators").getAsJsonArray().forEach(elm -> {
+				confdata.creators().add(elm.getAsString());
+			});
+		}
+		int tx = confdata.values.set("TextureWidth", obj.get("texture_size_x").getAsInt());
+		int ty = confdata.values.set("TextureHeight", obj.get("texture_size_y").getAsInt());
+        try{
+            if(JsonUtil.getIfExists(obj, "format", 2).intValue() == 1){
+                JsonObject modelobj = obj.get("model").getAsJsonObject();
+                for(Entry<String, JsonElement> entry : modelobj.entrySet()){
+                	model.groups.add(new TurboList(entry.getKey(), JsonToTMT.parse(null, entry.getValue().getAsJsonArray(), tx, ty)));
+                }
+            }
+            else{
+            	JsonObject modelobj = obj.get("groups").getAsJsonObject();
+                for(Entry<String, JsonElement> entry : modelobj.entrySet()){
+                	JsonObject group = entry.getValue().getAsJsonObject();
+                	model.groups.add(new TurboList(entry.getKey(), JsonToTMT.parse(null, group.get("polygons").getAsJsonArray(), tx, ty)));
+                	if(group.has("fvtm:programs")){
+                		ArrayList<Object> arr = confdata.values.get("Programs", () -> new ArrayList<>());
+                		JsonArray array = group.get("fvtm:programs").getAsJsonArray();
+                		for(JsonElement elm : array){
+                			if(elm.isJsonPrimitive()) arr.add(entry.getKey() + " " + elm.getAsString());
+                			else arr.add(new Object[]{ entry.getKey(), elm });
+                		}
+                	}
+                }
+            }
+        }
+        catch(Throwable thr){
+        	thr.printStackTrace();
+        	Static.stop();
+        }
+		return new Object[]{ model, confdata };
+	}
+
+}

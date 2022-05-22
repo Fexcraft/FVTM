037c1704eacb523662cf98ba8013a400bcc4ef5c
Ferdinand Calo <fercalo96@yahoo.de>
2022-05-23 01:20:14 +0200

    Update BlockModel.java

--- src/main/java/net/fexcraft/mod/fvtm/model/BlockModel.java
+++ src/main/java/net/fexcraft/mod/fvtm/model/BlockModel.java
@@ -1,39 +1,28 @@
 package net.fexcraft.mod.fvtm.model;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.HashMap;
 import java.util.Map;
 
 import org.lwjgl.opengl.GL11;
 
-import com.google.gson.JsonObject;
-
-import net.fexcraft.lib.common.utils.ObjParser;
-import net.fexcraft.lib.common.utils.ObjParser.ObjModel;
 import net.fexcraft.lib.mc.render.FCLBlockModel;
 import net.fexcraft.lib.mc.render.FCLItemModel;
 import net.fexcraft.lib.mc.utils.Print;
 import net.fexcraft.lib.tmt.ModelRendererTurbo;
 import net.fexcraft.mod.fvtm.data.Capabilities;
 import net.fexcraft.mod.fvtm.data.block.BlockData;
-import net.fexcraft.mod.fvtm.data.root.RenderCache;
 import net.fexcraft.mod.fvtm.item.BlockItem;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms.TransformType;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
-import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
-import net.minecraft.util.ResourceLocation;
 
 public class BlockModel extends GenericModel implements FCLItemModel, FCLBlockModel {
 
 	public static final BlockModel EMPTY = new BlockModel();
-    //public static boolean PREVIEW = false;
+    public static final ModelRenderData RENDERDATA = new ModelRenderData();
 	
     public float gui_translate_x = 0;
     public float gui_translate_y = 0;
@@ -46,67 +35,29 @@ public class BlockModel extends GenericModel implements FCLItemModel, FCLBlockMo
 		super();
 	}
 	
-	public BlockModel(JsonObject obj){
-		super(obj);
-	}
-	
 	@Override
-	public BlockModel parse(Object[] stream, String type){
-		try{
-			HashMap<String, Object> data = FMFParser.parse(this, (InputStream)stream[0]);
-			if(data.containsKey("ItemScale")){
-				try{
-					float scale = Float.parseFloat(data.get("ItemScale").toString());
-					gui_scale_x = scale;
-					gui_scale_y = scale;
-					gui_scale_z = scale;
-				}
-				catch(Exception e){
-					e.printStackTrace();
-				}
+	public BlockModel parse(ModelData data){
+		super.parse(data);
+		if(data.containsKey("ItemScale")){
+			try{
+				float scale = Float.parseFloat(data.get("ItemScale").toString());
+				gui_scale_x = scale;
+				gui_scale_y = scale;
+				gui_scale_z = scale;
 			}
-			stream[0] = data;
+			catch(Exception e){
+				e.printStackTrace();
+			}
 		}
-		catch(IOException e){
-			e.printStackTrace();
-		}
-		return super.parse(stream, type);
-	}
-	
-	public BlockModel(ResourceLocation loc, ObjModel data, ArrayList<String> objgroups, boolean exclude){
-		super(loc, data, objgroups, exclude);
-		try{
-			String val = ObjParser.getCommentValue(data, "ItemScale:");
-			float scale = val == null ? 0.75f : Float.parseFloat(val);
-			gui_scale_x = scale;
-			gui_scale_y = scale;
-			gui_scale_z = scale;
-		}
-		catch(Exception e){
-			e.printStackTrace();
-		}
-	}
-
-	@Override
-	public void render(BlockData data, TileEntity tile){
-		transforms.apply();
-		for(ModelGroup list : groups) list.renderBlock(tile, data, null);
-		transforms.deapply();
-	}
-
-	@Override
-	public void render(BlockData data, TileEntity tile, Entity ent, RenderCache cache){
-		transforms.apply();
-		GL11.glShadeModel(smooth_shading ? GL11.GL_FLAT : GL11.GL_SMOOTH);
-		for(ModelGroup list : groups) list.renderBlock(tile, data, cache);
-		transforms.deapply();
+		return this;
 	}
 	
 	@Override
 	public void renderItem(TransformType type, ItemStack item, EntityLivingBase entity){
 		if(item.getItem() instanceof BlockItem == false){ Print.debug("wrong_instance"); return; }
         BlockData data = item.getCapability(Capabilities.VAPDATA, null).getBlockData(); if(data == null){ Print.debug("no_data"); return; }
-        BlockModel model = (BlockModel)data.getType().getModel(); if(model == null){ Print.debug("no_model"); return; }
+        BlockModel model = (BlockModel)data.getType().getModel();
+        if(model == null){ Print.debug("no_model"); return; }
         float[] scal = new float[]{ model.gui_scale_x, model.gui_scale_y, model.gui_scale_z };
         //
         GL11.glPushMatrix();
@@ -156,7 +107,7 @@ public class BlockModel extends GenericModel implements FCLItemModel, FCLBlockMo
                 GL11.glPushMatrix();
                 GL11.glRotatef(180f, 1, 0, 0); GL11.glRotatef(180f, 0, 1, 0);
                 super.bindTexture(data.getCurrentTexture());
-                model.render(data, null, null, null);
+                model.render(RENDERDATA.set(data, null, null));
                 GL11.glPopMatrix();
             }
             GL11.glScalef(-scal[0], -scal[1], -scal[2]);

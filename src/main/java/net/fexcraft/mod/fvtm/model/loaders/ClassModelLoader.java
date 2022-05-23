0e0bbd20609fd2836928ab07efaf566ddfff7d47
Ferdinand Calo <fercalo96@yahoo.de>
2022-05-24 00:20:40 +0200

    Update ClassModelLoader.java

--- src/main/java/net/fexcraft/mod/fvtm/model/loaders/ClassModelLoader.java
+++ src/main/java/net/fexcraft/mod/fvtm/model/loaders/ClassModelLoader.java
@@ -7,7 +7,7 @@ public class ClassModelLoader implements ModelLoader {
 
 	@Override
 	public boolean accepts(String name, String suffix){
-		return suffix.equals(suffix);
+		return suffix.equals("class");
 	}
 
 	@Override

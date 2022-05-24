672f72df25bf7130eda1826dae76eaf9a4c9fc75
Ferdinand Calo <fercalo96@yahoo.de>
2022-05-24 22:34:13 +0200

    Update Model.java

--- src/main/java/net/fexcraft/mod/fvtm/data/root/Model.java
+++ src/main/java/net/fexcraft/mod/fvtm/data/root/Model.java
@@ -223,11 +223,12 @@ public interface Model {
 		public boolean accepts(String name, String suffix);
 		
 		/**
-		 * @param name the model adress/resourcelocation
+		 * @param name the model address/resourcelocation
 		 * @param confdata existing model data from config
+		 * @param model new model instance if necessary (usually only unused by the class model loader)
 		 * @return the model, with optionally a (updated or overridden) ModelData object on 2nd index
 		 */
-		public Object[] load(String name, ModelData confdata) throws Exception;
+		public Object[] load(String name, ModelData confdata, Supplier<Model> model) throws Exception;
 		
 	}
 	

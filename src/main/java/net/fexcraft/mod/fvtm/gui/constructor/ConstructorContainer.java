6039ed262ed7a3844e3028fb9b23642e258d105e
Ferdinand Calo <fercalo96@yahoo.de>
2022-06-26 23:17:29 +0200

    Update ConstructorContainer.java

--- src/main/java/net/fexcraft/mod/fvtm/gui/constructor/ConstructorContainer.java
+++ src/main/java/net/fexcraft/mod/fvtm/gui/constructor/ConstructorContainer.java
@@ -1,15 +1,18 @@
 package net.fexcraft.mod.fvtm.gui.constructor;
 
+import net.fexcraft.lib.common.math.RGB;
 import net.fexcraft.lib.mc.gui.GenericContainer;
 import net.fexcraft.mod.fvtm.block.ConstructorEntity;
 import net.fexcraft.mod.fvtm.gui.ConstructorCommandSender;
+import net.fexcraft.mod.fvtm.gui.construct.ConstConInterface;
+import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 
-public class ConstructorContainer extends GenericContainer {
+public class ConstructorContainer extends GenericContainer implements ConstConInterface {
 	
 	protected ConstructorCommandSender sender;
 	protected ConstructorEntity entity;
@@ -28,11 +31,16 @@ public class ConstructorContainer extends GenericContainer {
 			this.gui.titletext.update(packet.getString("titletext"), color);
 			this.gui.onTitleTextUpdate();
 		}
-		//entity.processGUIPacket(side, packet, player, this);
+		entity.processGUIPacket(side, packet, player, this);
 	}
 
 	public void setGUI(ConstructorGui congui){
 		this.gui = congui;
+	}
+	
+	/** To be used from server side. */
+	public final void setTitleText(String string, RGB color){
+		setTitleText(string, color.packed);
 	}
 	
 	/** To be used from server side. */
@@ -54,7 +62,7 @@ public class ConstructorContainer extends GenericContainer {
 		return entity;
 	}
 
-	public ConstructorCommandSender getCommandSender(){
+	public ICommandSender getCommandSender(){
 		return sender;
 	}
 

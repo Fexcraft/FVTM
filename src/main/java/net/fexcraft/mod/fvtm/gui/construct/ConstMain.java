81110505c5cd4cc9770dde65d6af86b64baf8454
Ferdinand Calo <fercalo96@yahoo.de>
2022-07-09 00:30:32 +0200

    Update ConstMain.java

--- src/main/java/net/fexcraft/mod/fvtm/gui/construct/ConstMain.java
+++ src/main/java/net/fexcraft/mod/fvtm/gui/construct/ConstMain.java
@@ -26,21 +26,23 @@ public class ConstMain extends ConstGui {
 		addTopButton(ConstGuiElement.HELP);
 		addTopButton(ConstGuiElement.SAVE);
 		addTopButton(ConstGuiElement.SPAWN);
-		addElement(GENERIC_SEG, "status", "gui.fvtm.constructor.main.status", button -> openGui(CONSTRUCTOR_STATUS, tilepos, LISTENERID));
+		addElement(GENERIC_SEG, "status", "gui.fvtm.constructor.main.status", (button, mb) -> openGui(CONSTRUCTOR_STATUS, tilepos, LISTENERID));
 		if(container.noContainer() && !container.isEmpty()){
-			addElement(GENERIC_SEG, "info", "gui.fvtm.constructor.main.data", button -> openGui(CONSTRUCTOR_CONTENTINFO, tilepos, LISTENERID));
+			addElement(GENERIC_SEG, "info", "gui.fvtm.constructor.main.data", (button, mb) -> openGui(CONSTRUCTOR_CONTENTINFO, tilepos, LISTENERID));
 		}
 		if(container.hasVehicle()){
 			addElement(EMPTY_SEG, "spacer0", null, null);
-			addElement(GENERIC_SEG, "part_cache", "gui.fvtm.constructor.main.part_cache", button -> openGui(CONSTRUCTOR_PARTINFO, tilepos, LISTENERID));
-			addElement(GENERIC_SEG, "part_manager", "gui.fvtm.constructor.main.part_manager", button -> openGui(CONSTRUCTOR_PARTMANAGER, tilepos, LISTENERID));
-			addElement(GENERIC_SEG, "part_install", "gui.fvtm.constructor.main.part_installer", button -> openGui(CONSTRUCTOR_PARTINSTALLER, tilepos, LISTENERID));
+			addElement(GENERIC_SEG, "part_cache", "gui.fvtm.constructor.main.part_cache", (button, mb) -> openGui(CONSTRUCTOR_PARTINFO, tilepos, LISTENERID));
+			addElement(GENERIC_SEG, "part_manager", "gui.fvtm.constructor.main.part_manager", (button, mb) -> openGui(CONSTRUCTOR_PARTMANAGER, tilepos, LISTENERID));
+			addElement(GENERIC_SEG, "part_install", "gui.fvtm.constructor.main.part_installer", (button, mb) -> openGui(CONSTRUCTOR_PARTINSTALLER, tilepos, LISTENERID));
 		}
-		addElement(EMPTY_SEG, "spacer1", null, null);
-		addElement(GENERIC_SEG, "texture", "gui.fvtm.constructor.main.texture", button -> openGui(CONSTRUCTOR_TEXTUREMANAGER, tilepos, LISTENERID));
-		addElement(GENERIC_SEG, "color", "gui.fvtm.constructor.main.color", button -> openGui(CONSTRUCTOR_PAINTER, tilepos, LISTENERID));
+		if(!container.isEmpty()){
+			addElement(EMPTY_SEG, "spacer1", null, null);
+			addElement(GENERIC_SEG, "texture", "gui.fvtm.constructor.main.texture", (button, mb) -> openGui(CONSTRUCTOR_TEXTUREMANAGER, tilepos, LISTENERID));
+			addElement(GENERIC_SEG, "color", "gui.fvtm.constructor.main.color", (button, mb) -> openGui(CONSTRUCTOR_PAINTER, tilepos, LISTENERID));
+		}
 		addElement(EMPTY_SEG, "spacer2", null, null);
-		addElement(GENERIC_SEG, "exit", "gui.fvtm.constructor.main.exit", button -> player.closeScreen());
+		addElement(GENERIC_SEG, "exit", "gui.fvtm.constructor.main.exit", (button, mb) -> player.closeScreen());
 		finish_init();
 	}
 

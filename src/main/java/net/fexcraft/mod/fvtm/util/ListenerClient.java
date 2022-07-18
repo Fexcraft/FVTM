bb7203c409808c1a5ce410d2c4b7b666883f2b4b
Ferdinand Calo <fercalo96@yahoo.de>
2022-07-18 23:32:43 +0200

    Update ListenerClient.java

--- src/main/java/net/fexcraft/mod/fvtm/util/ListenerClient.java
+++ src/main/java/net/fexcraft/mod/fvtm/util/ListenerClient.java
@@ -1,16 +1,21 @@
 package net.fexcraft.mod.fvtm.util;
 
+import java.util.UUID;
+
 import net.fexcraft.lib.mc.api.packet.IPacketListener;
 import net.fexcraft.lib.mc.network.packet.PacketNBTTagCompound;
 import net.fexcraft.mod.fvtm.data.Capabilities;
 import net.fexcraft.mod.fvtm.data.vehicle.VehicleEntity;
 import net.fexcraft.mod.fvtm.entity.Decoration;
+import net.fexcraft.mod.fvtm.sys.rail.RailPlacingUtil;
+import net.fexcraft.mod.fvtm.sys.rail.RailPlacingUtil.NewTrack;
 import net.fexcraft.mod.fvtm.sys.tsign.TrafficSignData;
 import net.fexcraft.mod.fvtm.sys.tsign.TrafficSigns;
 import net.fexcraft.mod.fvtm.sys.uni12.ULandVehicle;
 import net.fexcraft.mod.fvtm.util.config.Config;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
 
 public class ListenerClient implements IPacketListener<PacketNBTTagCompound> {
@@ -74,6 +79,21 @@ public class ListenerClient implements IPacketListener<PacketNBTTagCompound> {
 				Decoration ent = (Decoration)player.world.getEntityByID(packet.nbt.getInteger("entid"));
 				if(ent != null) ent.readEntityFromNBT(packet.nbt);
 				return;
+			}
+			case "rail_place_util":{
+				UUID uuid = new UUID(packet.nbt.getLong("uuid_m"), packet.nbt.getLong("uuid_l"));
+				if(packet.nbt.hasKey("new")){
+					RailPlacingUtil.CL_CURRENT = new NewTrack(new Vec316f(packet.nbt.getCompoundTag("vector")), Resources.RAILGAUGES.getValue(new ResourceLocation(packet.nbt.getString("gauge"))));
+					RailPlacingUtil.QUEUE.put(uuid, RailPlacingUtil.CL_CURRENT);
+				}
+				else if(packet.nbt.hasKey("reset")){
+					RailPlacingUtil.CL_CURRENT = null;
+					RailPlacingUtil.QUEUE.remove(uuid);
+				}
+				else{
+					Vec316f vector = new Vec316f(packet.nbt.getCompoundTag("vector"));
+					RailPlacingUtil.CL_CURRENT.add(vector);
+				}
 			}
 			default: return;
 		}

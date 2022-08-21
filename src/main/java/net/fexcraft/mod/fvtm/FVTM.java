9d5162e569d1933d72bfa5c2676ec868b518c195
Ferdinand Calo <fercalo96@yahoo.de>
2022-08-21 20:42:48 +0200

    Update FVTM.java

--- src/main/java/net/fexcraft/mod/fvtm/FVTM.java
+++ src/main/java/net/fexcraft/mod/fvtm/FVTM.java
@@ -98,7 +98,7 @@ public class FVTM {
 
 	public static final String MODID = "fvtm";
 	public static final String PREFIX = Formatter.format("&0[&9FVTM&0]&7 ");
-	public static final String VERSION = "3.8.68";
+	public static final String VERSION = "3.8.69";
 
 	@Mod.Instance(FVTM.MODID)
 	private static FVTM INSTANCE;

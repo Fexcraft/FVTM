55b9160e3550e2accc9fdfdd7790e176b0f63764
Ferdinand Calo <fercalo96@yahoo.de>
2022-09-23 14:01:04 +0200

    Update ServerReceiver.java

--- src/main/java/net/fexcraft/mod/fvtm/gui/ServerReceiver.java
+++ src/main/java/net/fexcraft/mod/fvtm/gui/ServerReceiver.java
@@ -7,23 +7,16 @@ import net.fexcraft.lib.mc.network.packet.PacketNBTTagCompound;
 import net.fexcraft.lib.mc.utils.Print;
 import net.fexcraft.mod.fvtm.FVTM;
 import net.fexcraft.mod.fvtm.data.Capabilities;
-import net.fexcraft.mod.fvtm.data.attribute.Attribute;
-import net.fexcraft.mod.fvtm.data.vehicle.VehicleEntity;
-import net.fexcraft.mod.fvtm.sys.rail.Compound;
-import net.fexcraft.mod.fvtm.sys.rail.RailEntity;
-import net.fexcraft.mod.fvtm.sys.rail.vis.RailVehicle;
 import net.fexcraft.mod.fvtm.sys.uni.GenericVehicle;
-import net.fexcraft.mod.fvtm.util.Perms;
-import net.fexcraft.mod.fvtm.util.Resources;
 import net.fexcraft.mod.fvtm.util.caps.ContainerHolderUtil;
 import net.fexcraft.mod.fvtm.util.caps.ContainerHolderUtil.Implementation;
+import net.fexcraft.mod.fvtm.util.handler.AttrReqHandler;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
-import net.minecraftforge.server.permission.PermissionAPI;
 
 public class ServerReceiver implements IPacketListener<PacketNBTTagCompound> {
 
@@ -45,87 +38,11 @@ public class ServerReceiver implements IPacketListener<PacketNBTTagCompound> {
 		World world = objs[0] == null ? (World)objs[1] : player.world;
 		switch(task){
 			case "attr_toggle":{
-				boolean bool = packet.nbt.getBoolean("bool");
-				VehicleEntity veh = (VehicleEntity)world.getEntityByID(packet.nbt.getInteger("entity"));
-				String attribute = packet.nbt.getString("attr");
-				final Attribute<?> attr = veh.getVehicleData().getAttribute(attribute);
-				if(!attr.editable() && !Perms.EDIT_INTERNAL_ATTRIBUTES.has(player) && (attr.hasPerm() ? !PermissionAPI.hasPermission(player, attr.perm()) : true)){
-					Print.chat(player, "No permission. [ED]");
-					return;
-				}
-				if(player.getRidingEntity() != veh.getEntity() && !attr.external() &&!Perms.EDIT_INTERNAL_ATTRIBUTES.has(player) && (attr.hasPerm() ? !PermissionAPI.hasPermission(player, attr.perm()) : true)){
-					Print.chat(player, "No permission. [EX]");
-					return;
-				}
-				Object old = attr.value();
-				toggleAttr(attr, bool, packet.nbt, false, null);
-				Object syncval = attr.value();
-				PacketHandler.getInstance().sendToAllAround(packet, Resources.getTargetPoint(veh.getEntity()));
-				veh.getVehicleData().getScripts().forEach(script -> {
-					script.onAttributeToggle(veh.getEntity(), attr, old, player);
-				});
-				if(!attr.sync()) return;
-				if(veh.getVehicleType().isRailVehicle()){
-					RailVehicle rail = (RailVehicle)veh;
-					Compound com = rail.rek.ent().getCompound();
-					if(com.isSingular() || !com.isHead(rail.rek.ent()) && !com.isEnd(rail.rek.ent())) return;
-					boolean mirror = attr.valuetype().isBoolean() && attr.group() != null && attr.group().contains("mirror_lr");
-					NBTTagCompound compound = packet.nbt.copy();
-					if(mirror){
-						com.forEachMirror(com.isHead(rail.rek.ent()), new String[]{ attribute }, flip -> {
-							if(flip[0].contains("left")){
-								flip[0] = flip[0].replace("left", "right");
-							}
-							else{
-								flip[0] = flip[0].replace("right", "left");
-							}
-						}, pass -> {}, (ent, val) -> {
-							compound.setString("attr", val[0]);
-							toggleAttrRailEnt(ent, val[0], bool, compound, true, syncval);
-						});
-					}
-					else{
-						for(RailEntity ent : com.getEntitites()){
-							if(ent == rail.rek.ent()) continue;
-							toggleAttrRailEnt(ent, attribute, bool, compound, true, syncval);
-						}
-					}
-				}
-				else{
-					if(veh.getFrontCoupledEntity() != null) return;
-					VehicleEntity trailer = veh.getRearCoupledEntity();
-					while(trailer != null){
-						Attribute<?> attr0 = trailer.getVehicleData().getAttribute(attribute);
-						if(attr0 != null){
-							NBTTagCompound compound = packet.nbt.copy();
-							toggleAttr(attr0, bool, compound, true, syncval);
-							compound.setInteger("entity", trailer.getEntity().getEntityId());
-							PacketHandler.getInstance().sendToAllAround(new PacketNBTTagCompound(compound), Resources.getTargetPoint(trailer.getEntity()));
-						}
-						trailer = trailer.getRearCoupledEntity();
-					}
-				}
+				AttrReqHandler.processToggleRequest(world, player, packet.nbt);
 				break;
 			}
 			case "attr_update":{
-				boolean reset = packet.nbt.hasKey("reset") && packet.nbt.getBoolean("reset");
-				VehicleEntity veh = (VehicleEntity)player.world.getEntityByID(packet.nbt.getInteger("entity"));
-				Attribute<?> attr = veh.getVehicleData().getAttribute(packet.nbt.getString("attr"));
-				if(!attr.editable() && !Perms.EDIT_INTERNAL_ATTRIBUTES.has(player) && (attr.hasPerm() ? !PermissionAPI.hasPermission(player, attr.perm()) : true)){
-					Print.chat(player, "No permission. [ED]");
-					return;
-				}
-				if(player.getRidingEntity() != veh.getEntity() && !attr.external() &&!Perms.EDIT_INTERNAL_ATTRIBUTES.has(player) && (attr.hasPerm() ? !PermissionAPI.hasPermission(player, attr.perm()) : true)){
-					Print.chat(player, "No permission. [EX]");
-					return;
-				}
-				if(reset){
-					attr.reset();
-				}
-				else{
-					attr.value(attr.parseValue(packet.nbt.getString("value")));
-				}
-				((GenericVehicle)veh).sendAttributeUpdate(attr);
+				AttrReqHandler.processUpdateRequest(world, player, packet.nbt);
 				break;
 			}
 			case "update_container_holder":{
@@ -162,40 +79,6 @@ public class ServerReceiver implements IPacketListener<PacketNBTTagCompound> {
 			}
 			default:
 				return;
-		}
-	}
-
-	private void toggleAttrRailEnt(RailEntity ent, String attribute, boolean bool, NBTTagCompound compound, boolean b, Object syncval){
-		Attribute<?> attr0 = ent.vehdata.getAttribute(attribute);
-		if(attr0 == null) return;
-		toggleAttr(attr0, bool, compound, true, syncval);
-		if(ent.entity != null){
-			compound.setLong("railid", ent.uid);
-			compound.setInteger("entity", ent.entity.getEntityId());
-			PacketHandler.getInstance().sendToAllAround(new PacketNBTTagCompound(compound), Resources.getTargetPoint(ent.entity));
-		}
-	}
-
-	private void toggleAttr(Attribute<?> attr, boolean bool, NBTTagCompound nbt, boolean check, Object syncval){
-		if(check && attr.sync()){
-			attr.value(syncval);
-			return;
-		}
-		if(attr.valuetype().isTristate()){
-			if(attr.valuetype().isBoolean() || !nbt.hasKey("reset")){
-				attr.value(bool);
-				nbt.setBoolean("bool", attr.boolean_value());
-			}
-			else{
-				attr.value(null);
-				nbt.setBoolean("reset", true);
-			}
-		}
-		else if(attr.valuetype().isNumber()){
-			attr.value(attr.valuetype().isInteger() ? nbt.getInteger("value") : nbt.getFloat("value"));
-		}
-		else{
-			Print.log("no code for toggling this attribute type yet");
 		}
 	}
 

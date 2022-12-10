b26bdec1cd9b1c4b435f8315ae0190aed43ac85c
Ferdinand Calo <fercalo96@yahoo.de>
2022-12-10 19:40:22 +0100

    Update BaseBlockRenderer.java

--- src/main/java/net/fexcraft/mod/fvtm/render/BaseBlockRenderer.java
+++ src/main/java/net/fexcraft/mod/fvtm/render/BaseBlockRenderer.java
@@ -7,6 +7,7 @@ import net.fexcraft.mod.fvtm.block.generated.BlockTileEntity;
 import net.fexcraft.mod.fvtm.data.Capabilities;
 import net.fexcraft.mod.fvtm.data.block.BlockData;
 import net.fexcraft.mod.fvtm.model.BlockModel;
+import net.fexcraft.mod.fvtm.util.Resources;
 import net.fexcraft.mod.fvtm.util.TexUtil;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 
@@ -21,8 +22,8 @@ public class BaseBlockRenderer extends TileEntitySpecialRenderer<BlockTileEntity
         GL11.glPushMatrix();
         GL11.glTranslated(posX + 0.5F, posY, posZ + 0.5F);
         data = tile.getBlockData();
-        model = (BlockModel) data.getType().getModel();
-        if(model.bindtex) TexUtil.bindTexture(data.getCurrentTexture());
+        model = (BlockModel)data.getType().getModel();
+        TexUtil.bindTexture(model.bindtex ? data.getCurrentTexture() : Resources.WHITE_TEXTURE);
         GL11.glPushMatrix();
         GL11.glRotatef(180F, 0.0F, 0.0F, 1.0F);
         GL11.glRotated(data.getType().getBlockType().getRotationForMeta(tile.getBlockMetadata()), 0, 1, 0);

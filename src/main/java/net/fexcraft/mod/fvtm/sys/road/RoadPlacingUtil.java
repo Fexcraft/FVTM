1bd0b50ae24e35aba3ace5b9b19f2bf92d00dee2
Ferdinand Calo <fercalo96@yahoo.de>
2022-12-23 19:36:08 +0100

    Update RoadPlacingUtil.java

--- src/main/java/net/fexcraft/mod/fvtm/sys/road/RoadPlacingUtil.java
+++ src/main/java/net/fexcraft/mod/fvtm/sys/road/RoadPlacingUtil.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
+import net.fexcraft.lib.common.math.Vec3f;
 import net.fexcraft.lib.mc.network.PacketHandler;
 import net.fexcraft.lib.mc.network.packet.PacketNBTTagCompound;
 import net.fexcraft.lib.mc.utils.Print;
+import net.fexcraft.lib.mc.utils.Static;
 import net.fexcraft.mod.fvtm.entity.RoadMarker;
 import net.fexcraft.mod.fvtm.item.RoadToolItem;
 import net.fexcraft.mod.fvtm.item.RoadToolItem.Road;
@@ -25,9 +27,10 @@ public class RoadPlacingUtil {
 
 	public static void place(World world, EntityPlayer player, ItemStack stack, Vec316f vector){
 		UUID roadid = CURRENT.get(player.getGameProfile().getId());
+		int width = stack.getTagCompound().getIntArray("RoadLayers")[0];
 		if(roadid == null){
 			UUID newid = genId();
-			QUEUE.put(newid, new NewRoad(newid, vector));
+			QUEUE.put(newid, new NewRoad(newid, vector, width));
 			CURRENT.put(player.getGameProfile().getId(), newid);
 			//
 			NBTTagCompound compound = new NBTTagCompound();
@@ -36,6 +39,7 @@ public class RoadPlacingUtil {
 			compound.setString("subtask", "new");
 			compound.setLong("uuid_l", newid.getMostSignificantBits());
 			compound.setLong("uuid_m", newid.getLeastSignificantBits());
+			compound.setInteger("width", width);
 			compound.setBoolean("new", true);
 			compound.setTag("vector", vector.write());
 			PacketHandler.getInstance().sendToAll(new PacketNBTTagCompound(compound));
@@ -48,7 +52,7 @@ public class RoadPlacingUtil {
 		}
 		NewRoad road = QUEUE.get(roadid);
 		if(road == null) CURRENT.remove(player.getGameProfile().getId());
-		road.add(vector);
+		road.add(vector, width);
 		//
 		NBTTagCompound compound = new NBTTagCompound();
 		compound.setString("target_listener", Resources.UTIL_LISTENER);
@@ -56,6 +60,7 @@ public class RoadPlacingUtil {
 		compound.setString("subtask", "add");
 		compound.setLong("uuid_l", roadid.getMostSignificantBits());
 		compound.setLong("uuid_m", roadid.getLeastSignificantBits());
+		compound.setInteger("width", width);
 		compound.setTag("vector", vector.write());
 		PacketHandler.getInstance().sendToAllAround(new PacketNBTTagCompound(compound), Resources.getTargetPoint(player.dimension, vector.pos));
 		//
@@ -74,17 +79,21 @@ public class RoadPlacingUtil {
 	public static class NewRoad {
 		
 		public ArrayList<Vec316f> points = new ArrayList<>();
+		public ArrayList<ArrayList<Vec3f>> preview;
 		public Road road;
-		public int selected = -1;
+		public int selected = -1, width;
 		public UUID id;
 
-		public NewRoad(UUID uuid, Vec316f vector){
+		public NewRoad(UUID uuid, Vec316f vector, int width){
 			points.add(vector);
+			this.width = width;
 			id = uuid;
 		}
 
-		public void add(Vec316f vector){
+		public void add(Vec316f vector, int width){
 			points.add(selected == -1 ? points.size() : ++selected, vector);
+			this.width = width;
+			preview = null;
 			genroad();
 		}
 
@@ -124,6 +133,7 @@ public class RoadPlacingUtil {
 			if(selected < -1) selected = -1;
 			points.remove(rem);
 			genroad();
+			preview = null;
 			//
 			if(points.size() == 0){
 				reset();
@@ -177,6 +187,21 @@ public class RoadPlacingUtil {
 			if(!tool.placeRoad(player, player.world, stack, vector, new Road(nroad.points.toArray(new Vec316f[0])), player)) return;
 			Print.chat(player, "&o> Road Created.");
 			nroad.reset();
+		}
+
+		public void genpreview(){
+			float angle, half = (width * 0.5f);
+			preview = new ArrayList<>();
+			for(int i = 0; i < width + 1; i++) preview.add(new ArrayList<>());
+			Vec3f last, vec = road.vecpath[0];
+			for(float pass = 0; pass < road.length + 0.125f; pass += 0.125f){
+				last = vec;
+				vec = road.getVectorPosition0(pass == 0 ? 0.001f : pass, false);
+				angle = (float)Math.atan2(last.z - vec.z, last.x - vec.x) + Static.rad90;
+				for(int w = 0; w < width + 1; w++){
+					preview.get(w).add(vec.add(RoadToolItem.grv(angle, new Vec3f(-half + w, 0, 0))));
+				}
+			}
 		}
 		
 	}

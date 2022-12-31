aacd8db861f4803318a2d96e0933801bc4af82a0
Ferdinand Calo <fercalo96@yahoo.de>
2022-12-31 23:41:28 +0100

    Create G_SIMPLE.java

---
+++ src/main/java/net/fexcraft/mod/fvtm/block/generated/G_SIMPLE.java
@@ -0,0 +1,80 @@
+package net.fexcraft.mod.fvtm.block.generated;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import net.fexcraft.mod.fvtm.data.block.Block;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+
+public class G_SIMPLE extends PlainBase {
+
+	public G_SIMPLE(Block type){
+		super(type);
+		this.setDefaultState(this.blockState.getBaseState());
+	}
+
+	@Override
+	public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos){
+		return type.getAABB("default", "")[0];
+	}
+
+	@Override
+	public AxisAlignedBB getSelectedBoundingBox(IBlockState state, World worldIn, BlockPos pos){
+		return type.getAABB("selection", "")[0].offset(pos);
+	}
+
+	@Nullable
+	@Override
+	public AxisAlignedBB getCollisionBoundingBox(IBlockState state, IBlockAccess world, BlockPos pos){
+		return type.getAABB("collision", "")[0];
+	}
+
+    @Override
+    public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer){
+        return this.getDefaultState();
+    }
+
+    @Override
+    public void onBlockPlacedBy(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase placer, ItemStack stack){
+        worldIn.setBlockState(pos, state, 2);
+    }
+
+	@Override
+	public IBlockState getStateFromMeta(int meta){
+		return this.getDefaultState();
+	}
+
+	@Override
+	public int getMetaFromState(IBlockState state){
+		return 0;
+	}
+
+	@Override
+	protected BlockStateContainer createBlockState(){
+		return new BlockStateContainer(this, new IProperty[0]);
+	}
+
+	@Override
+	public void breakBlock(World world, BlockPos pos, IBlockState state){
+		super.breakBlock(world, pos, state);
+	}
+
+	@Override
+	protected void addCollisionsToList(IBlockState state, World world, BlockPos pos, AxisAlignedBB entitybox, List<AxisAlignedBB> boxes){
+		for(AxisAlignedBB aabb : type.getAABB("collision", "")){
+			if(entitybox == null) boxes.add(aabb);
+			else addCollisionBoxToList(pos, entitybox, boxes, aabb);
+		}
+	}
+
+}

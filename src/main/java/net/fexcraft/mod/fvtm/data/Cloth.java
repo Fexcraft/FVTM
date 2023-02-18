52177065353b90151a4d1b1d04798b1824972eda
Ferdinand Calo <fercalo96@yahoo.de>
2023-02-18 17:25:31 +0100

    Update Cloth.java

--- src/main/java/net/fexcraft/mod/fvtm/data/Cloth.java
+++ src/main/java/net/fexcraft/mod/fvtm/data/Cloth.java
@@ -56,10 +56,10 @@ public class Cloth extends TypeCore<Cloth> implements Tabbed, ItemTextureable {
 
 	@Override
 	public Cloth parse(JsonObject obj){
-		this.registryname = DataUtil.getRegistryName(obj);
-		if(registryname == null) return null;
 		this.pack = DataUtil.getAddon(obj);
 		if(pack == null) return null;
+		this.registryname = DataUtil.getRegistryName(pack, obj);
+		if(registryname == null) return null;
 		//
 		this.name = JsonUtil.getIfExists(obj, "Name", "Unnamed Clothing");
 		this.description = DataUtil.getStringArray(obj, "Description", true, true);

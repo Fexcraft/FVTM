f04db40c77cd0a2db97dbafde8a33fcda0e5d6cc
Ferdinand Calo <fercalo96@yahoo.de>
2023-02-25 22:46:08 +0100

    Create GBCElm.java

---
+++ src/main/java/net/fexcraft/mod/fvtm/gui/block/GBCElm.java
@@ -0,0 +1,34 @@
+package net.fexcraft.mod.fvtm.gui.block;
+
+public enum GBCElm {
+
+    HEAD(false, 0, 16, true),
+    SPACER(false, 106, 14, true),
+    FOOTER(false, 120, 23, true),
+    ITEMVIEW(false, 86, 20, true),
+    //
+    ELM_FULL_TEXT(false, 16, 14, true),
+    ELM_LEFT_TEXT(false, 30, 14, false),
+    ELM_RIGHT_TEXT(true, 30, 14, false),
+    ELM_TWO_BUTTONS(false, 58, 14, true),
+    ELM_LEFT_BUTTON(false, 58, 14, false),
+    ELM_RIGHT_BUTTON(true, 58, 14, false),
+    ELM_FULL_CLEAR(false, 72, 14, true),
+    ELM_LEFT_CLEAR(false, 72, 14, false),
+    ELM_RIGHT_CLEAR(true, 72, 14, false),
+    ELM_TWO_PROGRESS(false, 44, 14, true),
+    ELM_LEFT_PROGRESS(false, 44, 14, false),
+    ELM_RIGHT_PROGRESS(true, 44, 14, false),
+    ;
+
+    public int x, y, h;
+    public boolean middle, full;
+
+    GBCElm(boolean central, int sy, int sh, boolean isfull){
+        x = (middle = central) ? 128 : 0;
+        y = sy;
+        h = sh;
+        full = isfull;
+    }
+
+}

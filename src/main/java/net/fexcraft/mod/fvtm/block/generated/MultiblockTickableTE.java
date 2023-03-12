1942f1683afb836b8161a1bad408018d139e60fa
Ferdinand Calo <fercalo96@yahoo.de>
2023-03-12 21:10:40 +0100

    Update MultiblockTickableTE.java

--- src/main/java/net/fexcraft/mod/fvtm/block/generated/MultiblockTickableTE.java
+++ src/main/java/net/fexcraft/mod/fvtm/block/generated/MultiblockTickableTE.java
@@ -1,21 +1,34 @@
 package net.fexcraft.mod.fvtm.block.generated;
 
+import java.util.Map;
+
 import net.fexcraft.lib.mc.api.packet.IPacketReceiver;
 import net.fexcraft.lib.mc.network.packet.PacketTileEntityUpdate;
+import net.fexcraft.mod.fvtm.data.inv.InvHandler;
 import net.minecraft.util.ITickable;
 
 public class MultiblockTickableTE extends MultiblockTileEntity implements IPacketReceiver<PacketTileEntityUpdate>, ITickable {
-	
+
 	public MultiblockTickableTE(BlockBase type){
 		super(type);
 	}
 	
 	public MultiblockTickableTE(){}
 
+	private int pass = 20;
+
 	@Override
 	public void update(){
 		if(data == null || mdata == null || mdata.getScript() == null) return;
 		mdata.getScript().onUpdate(this);
+		if(pass > 0){
+			pass--;
+			return;
+		}
+		pass = 20;
+		for(Map.Entry<String, InvHandler> entry : mdata.getInventories().entrySet()){
+			entry.getValue().update(this, entry.getKey());
+		}
 	}
 
 }

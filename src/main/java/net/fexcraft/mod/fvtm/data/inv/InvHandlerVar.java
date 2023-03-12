eca343efde39ad3ef4adb261e0cf94fbee540f8c
Ferdinand Calo <fercalo96@yahoo.de>
2023-03-12 18:00:55 +0100

    Update InvHandlerVar.java

--- src/main/java/net/fexcraft/mod/fvtm/data/inv/InvHandlerVar.java
+++ src/main/java/net/fexcraft/mod/fvtm/data/inv/InvHandlerVar.java
@@ -1,27 +1,37 @@
 package net.fexcraft.mod.fvtm.data.inv;
 
+import net.fexcraft.mod.fvtm.util.DataUtil;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.NonNullList;
 
 public class InvHandlerVar extends InvHandler {
 	
-	protected int value;
-	protected String category, conid;
+	protected int value, items;
+	protected String concat, conid;
+	protected NonNullList<ItemStack> stacks;
+	protected boolean in;
 
 	public InvHandlerVar(String arg, int cap){
 		super(InvType.VARIABLE);
 		String[] str = arg.split(":");
 		conid = str[0];
-		category = str.length > 1 ? str[1] : "fluid";
+		concat = str.length > 1 ? str[1] : "fluid";
+		items = str.length > 2 ? Integer.parseInt(str[2]) : 1;
+		in = str.length > 3 ? str[3].equals("in") : false;
+		stacks = NonNullList.<ItemStack>withSize(items, ItemStack.EMPTY);
 		capacity = cap;
 	}
 
 	public NBTTagCompound save(NBTTagCompound compound, String ctag){
 		compound.setInteger(ctag, value());
+		DataUtil.saveAllItems(compound, stacks, false, ctag + "-items");
 		return compound;
 	}
 
 	public void load(NBTTagCompound compound, String ctag){
 		value = compound.getInteger(ctag);
+		DataUtil.loadAllItems(compound, stacks, ctag + "-items");
 	}
 
 	@Override
@@ -63,4 +73,15 @@ public class InvHandlerVar extends InvHandler {
 		return value;
 	}
 
+	public int items(){
+		return items;
+	}
+
+	public String content_id(){
+		return conid;
+	}
+
+	public String container_cat(){
+		return concat;
+	}
 }

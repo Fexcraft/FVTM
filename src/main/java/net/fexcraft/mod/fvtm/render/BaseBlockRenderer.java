d80b236550e0b5e091ea48fb4063bcc9a6d69a7c
Ferdinand Calo <fercalo96@yahoo.de>
2023-03-28 11:30:22 +0200

    Update BaseBlockRenderer.java

--- src/main/java/net/fexcraft/mod/fvtm/render/BaseBlockRenderer.java
+++ src/main/java/net/fexcraft/mod/fvtm/render/BaseBlockRenderer.java
@@ -27,7 +27,7 @@ public class BaseBlockRenderer extends TileEntitySpecialRenderer<BlockTileEntity
         GL11.glPushMatrix();
         GL11.glRotatef(180F, 0.0F, 0.0F, 1.0F);
         GL11.glRotated(data.getType().getBlockType().getRotationForMeta(tile.getBlockMetadata()), 0, 1, 0);
-        model.render(BlockModel.RENDERDATA.set(data, tile, tile.getCapability(Capabilities.RENDERCACHE, null), false));
+        model.render(BlockModel.RENDERDATA.set(data, tile, tile.getCapability(Capabilities.RENDERCACHE, null), null, false));
         GL11.glPopMatrix();
         GL11.glPopMatrix();
     }

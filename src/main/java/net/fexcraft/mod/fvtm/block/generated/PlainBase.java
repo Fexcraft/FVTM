d636c36da8203195ad3cad6f214f8b8e83632790
Ferdinand Calo <fercalo96@yahoo.de>
2023-04-06 21:30:27 +0200

    Update PlainBase.java

--- src/main/java/net/fexcraft/mod/fvtm/block/generated/PlainBase.java
+++ src/main/java/net/fexcraft/mod/fvtm/block/generated/PlainBase.java
@@ -5,21 +5,23 @@ import java.util.List;
 
 import javax.annotation.Nullable;
 
-import net.fexcraft.lib.common.math.RGB;
-import net.fexcraft.lib.mc.utils.Print;
 import net.fexcraft.mod.fvtm.data.block.Block;
 import net.fexcraft.mod.fvtm.data.block.BlockFunction;
 import net.fexcraft.mod.fvtm.item.BlockItem;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemDye;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.*;
+import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.DamageSource;
+import net.minecraft.util.EnumBlockRenderType;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 public abstract class PlainBase extends net.minecraft.block.Block {
@@ -100,6 +102,11 @@ public abstract class PlainBase extends net.minecraft.block.Block {
 			}
 		}
 		return super.onBlockActivated(world, pos, state, player, hand, side, hitX, hitY, hitZ);
+	}
+
+	@Override
+	public boolean isLadder(IBlockState state, IBlockAccess world, BlockPos pos, EntityLivingBase entity){
+		return type.isLadder();
 	}
 
 }

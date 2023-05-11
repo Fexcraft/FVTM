09013772dcd4154a310e7f5acbd46f48236d6db7
Ferdinand Calo <fercalo96@yahoo.de>
2023-05-11 18:56:50 +0200

    Create ResLoc.java

---
+++ src/main/java/net/fexcraft/mod/uni/impl/ResLoc.java
@@ -0,0 +1,26 @@
+package net.fexcraft.mod.uni.impl;
+
+import net.fexcraft.mod.uni.IDL;
+import net.minecraft.util.ResourceLocation;
+
+public class ResLoc extends ResourceLocation implements IDL {
+
+    public ResLoc(String name){
+        super(name);
+    }
+
+    public ResLoc(String space, String path){
+        super(space, path);
+    }
+
+    @Override
+    public String space(){
+        return getNamespace();
+    }
+
+    @Override
+    public String id(){
+        return getPath();
+    }
+
+}

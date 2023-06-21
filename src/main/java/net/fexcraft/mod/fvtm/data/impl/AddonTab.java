9d8c64927efe9e0a9b579353172b5e9ce1f5cd28
Ferdinand Calo <fercalo96@yahoo.de>
2023-06-21 16:46:10 +0200

    Create AddonTab.java

---
+++ src/main/java/net/fexcraft/mod/fvtm/data/impl/AddonTab.java
@@ -0,0 +1,66 @@
+package net.fexcraft.mod.fvtm.data.impl;
+
+import java.util.Collection;
+
+import net.fexcraft.lib.common.math.Time;
+import net.fexcraft.lib.mc.registry.CreativeTab;
+import net.fexcraft.mod.fvtm.data.addon.Addon;
+import net.fexcraft.mod.fvtm.data.addon.AddonNew;
+import net.fexcraft.mod.uni.IDL;
+import net.fexcraft.mod.uni.client.CTab;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class AddonTab extends CreativeTab implements CTab {
+
+    private NonNullList<ItemStack> list;
+    private int icon, sec;
+	private Addon addon;
+
+	@Deprecated
+	public AddonTab(Addon addon, String string){
+		super(addon.getRegistryName().toString() + (string.equals(DEFAULT) ? "" : "." + string));
+		this.addon = addon;
+	}
+
+	public AddonTab(AddonNew addon, String string){
+		super(addon.getID().colon() + (string.equals(DEFAULT) ? "" : "." + string));
+		//this.addon = addon;
+	}
+
+    @Override
+    public ItemStack createIcon(){
+        return null;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public String getTranslatedTabLabel(){
+        return addon.getName();
+    }
+
+    public Addon getAddon(){
+        return addon;
+    }
+
+    @Override
+    public ItemStack getIcon(){
+        if(list == null){ list = NonNullList.create(); this.displayAllRelevantItems(list); }
+        if(sec != Time.getSecond()){
+        	sec = Time.getSecond(); this.icon++; if(icon >= list.size()){ icon = 0; }
+        } return icon >= list.size() ? ItemStack.EMPTY : list.get(icon);
+    }
+    
+    public static final AddonTab getTab(IDL addonid){
+    	return (AddonTab)TABS.get(addonid);
+    }
+
+	public static Collection<CTab> getTabs(){
+		return TABS.values();
+	}
+
+}

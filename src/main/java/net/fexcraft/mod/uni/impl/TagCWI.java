59af1419ead1214d1f0dee13ca38e0ca7e3eb541
Ferdinand Calo <fercalo96@yahoo.de>
2023-06-22 23:07:42 +0200

    Create TagCWI.java

---
+++ src/main/java/net/fexcraft/mod/uni/impl/TagCWI.java
@@ -0,0 +1,70 @@
+package net.fexcraft.mod.uni.impl;
+
+import net.fexcraft.mod.uni.tag.TagCW;
+import net.minecraft.nbt.NBTTagCompound;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class TagCWI implements TagCW {
+
+	private NBTTagCompound compound;
+
+	public TagCWI(){
+		compound = new NBTTagCompound();
+	}
+
+	public TagCWI(NBTTagCompound com){
+		compound = com;
+	}
+
+	@Override
+	public String getString(String key){
+		return compound.getString(key);
+	}
+
+	@Override
+	public float getFloat(String key){
+		return compound.getFloat(key);
+	}
+
+	@Override
+	public int getInteger(String key){
+		return compound.getInteger(key);
+	}
+
+	@Override
+	public boolean getBoolean(String key){
+		return compound.getBoolean(key);
+	}
+
+	@Override
+	public boolean has(String key){
+		return compound.hasKey(key);
+	}
+
+	@Override
+	public void set(String key, String val){
+		compound.setString(key, val);
+	}
+
+	@Override
+	public void set(String key, float val){
+		compound.setFloat(key, val);
+	}
+
+	@Override
+	public void set(String key, int val){
+		compound.setInteger(key, val);
+	}
+
+	@Override
+	public void set(String key, boolean val){
+		compound.setBoolean(key, val);
+	}
+
+	@Override
+	public <C> C cast(){
+		return (C)compound;
+	}
+}

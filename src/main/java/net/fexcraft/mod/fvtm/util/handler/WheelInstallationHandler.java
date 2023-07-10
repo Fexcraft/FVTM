8f0b258023489b95725395534d746ad039f5b55b
Ferdinand Calo <fercalo96@yahoo.de>
2023-07-10 22:11:26 +0200

    Update WheelInstallationHandler.java

--- src/main/java/net/fexcraft/mod/fvtm/util/handler/WheelInstallationHandler.java
+++ src/main/java/net/fexcraft/mod/fvtm/util/handler/WheelInstallationHandler.java
@@ -1,11 +1,13 @@
 package net.fexcraft.mod.fvtm.util.handler;
 
+import static net.fexcraft.mod.fvtm.util.AnotherUtil.toV3;
+
 import javax.annotation.Nullable;
 
 import com.google.gson.JsonObject;
 
 import net.fexcraft.lib.common.json.JsonUtil;
-import net.fexcraft.lib.mc.utils.Pos;
+import net.fexcraft.mod.uni.Pos;
 import net.fexcraft.lib.mc.utils.Print;
 import net.fexcraft.mod.fvtm.data.WheelSlot;
 import net.fexcraft.mod.fvtm.data.part.PartData;
@@ -78,7 +80,7 @@ public class WheelInstallationHandler extends PartInstallationHandler {
 		}
 		WheelData idata = part.getType().getInstallationHandlerData();
 		Pos partpos = part.getInstalledPos();
-		data.getWheelPositions().put(cat, new Pos(partpos.x, -partpos.y - idata.radius, -partpos.z + ((cat.contains("left") ? -idata.width : idata.width) * 0.5f)).to16Double());
+		data.getWheelPositions().put(cat, toV3(new Pos(partpos.x, -partpos.y - idata.radius, -partpos.z + ((cat.contains("left") ? -idata.width : idata.width) * 0.5f))));
 		// Print.debug("New WheelPos: " + data.getWheelPositions().get(cat));
 		Print.chatnn(sender, "handler.install.fvtm.wheel.success");
 		return true;

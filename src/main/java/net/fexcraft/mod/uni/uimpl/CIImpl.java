630b08af4cfcb432b4d49a09257ac2977432c615
Ferdinand Calo <fercalo96@yahoo.de>
2023-07-23 18:24:12 +0200

    Create CIImpl.java

---
+++ src/main/java/net/fexcraft/mod/uni/uimpl/CIImpl.java
@@ -0,0 +1,15 @@
+package net.fexcraft.mod.uni.uimpl;
+
+import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.mod.uni.ui.ContainerInterface;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class CIImpl extends ContainerInterface {
+
+	public CIImpl(JsonMap map){
+		super(map);
+	}
+
+}

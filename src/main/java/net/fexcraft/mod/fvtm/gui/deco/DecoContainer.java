52e2f60ea5844c93904d04f663157e76060740a1
Ferdinand Calo <fercalo96@yahoo.de>
2023-07-23 18:24:04 +0200

    Create DecoContainer.java

---
+++ src/main/java/net/fexcraft/mod/fvtm/gui/deco/DecoContainer.java
@@ -0,0 +1,15 @@
+package net.fexcraft.mod.fvtm.gui.deco;
+
+import net.fexcraft.mod.uni.ui.ContainerInterface;
+import net.fexcraft.mod.uni.uimpl.UniCon;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class DecoContainer extends UniCon {
+
+	public DecoContainer(ContainerInterface con){
+		super(con);
+	}
+
+}

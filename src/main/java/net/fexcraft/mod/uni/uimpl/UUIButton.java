b62649cc6e2772a8c52dc011b4bd01bad6789f96
Ferdinand Calo <fercalo96@yahoo.de>
2023-07-25 22:16:26 +0200

    Update UUIButton.java

--- src/main/java/net/fexcraft/mod/uni/uimpl/UUIButton.java
+++ src/main/java/net/fexcraft/mod/uni/uimpl/UUIButton.java
@@ -20,18 +20,18 @@ public class UUIButton extends UIButton {
 	@Override
 	public void draw(Object gui, float ticks, int gl, int gt, int mx, int my){
 		if(!visible) return;
-		if(texture != null) ui.bindTexture(texture);
+		UniUI uui = (UniUI)gui;
+		if(texture != null) uui.bindTexture(texture);
 		rgb = enabled ? hovered ? hcolor : ecolor : dcolor;
 		RGB.glColorReset();
 		int u = enabled ? hovered ? htx : tx : dtx;
 		int v = enabled ? hovered ? hty : ty : dty;
 		rgb.glColorApply();
 		if(absolute){
-			UniUI uui = (UniUI)gui;
 			uui.drawTexturedModalRect(x < 0 ? uui.width + x : x, y < 0 ? uui.height + y : y, u, v, width, height);
 		}
 		else{
-			((UniUI)gui).drawTexturedModalRect(gl + x, gt + y, u, v, width, height);
+			uui.drawTexturedModalRect(gl + x, gt + y, u, v, width, height);
 		}
 		RGB.glColorReset();
 	}

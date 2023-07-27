a46badde410e776760ccce88792c89507f6aea23
Ferdinand Calo <fercalo96@yahoo.de>
2023-07-27 17:35:33 +0200

    Update DecoContainer.java

--- src/main/java/net/fexcraft/mod/fvtm/gui/deco/DecoContainer.java
+++ src/main/java/net/fexcraft/mod/fvtm/gui/deco/DecoContainer.java
@@ -1,8 +1,12 @@
 package net.fexcraft.mod.fvtm.gui.deco;
 
+import static net.fexcraft.mod.fvtm.FvtmRegistry.DECORATIONS;
+
 import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.mod.fvtm.data.DecorationData;
 import net.fexcraft.mod.fvtm.entity.Decoration;
+import net.fexcraft.mod.fvtm.ui.DecoEditor;
+import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.ui.ContainerInterface;
 import net.minecraft.world.World;
 
@@ -33,6 +37,36 @@ public class DecoContainer extends ContainerInterface {
 			}
 		}
 		return null;
+	}
+
+	@Override
+	public void packet(TagCW com, boolean client){
+		String task = com.getString("task");
+		switch(task){
+			case "add":{
+				DecorationData deco = DECORATIONS.get(com.getString("key"));
+				entity.decos.add(deco.copy());
+				if(!client){
+					SEND_TO_CLIENT.accept(com);
+				}
+				else{
+					entity.decos.get(entity.decos.size() - 1).copy(deco);
+					((DecoEditor)ui).updateEntries();
+				}
+				return;
+			}
+			case "rem":{
+				entity.decos.remove(com.getInteger("idx"));
+				if(!client){
+					SEND_TO_CLIENT.accept(com);
+				}
+				else{
+					((DecoEditor)ui).updateEntries();
+					((DecoEditor)ui).select(-1, -1);
+				}
+				return;
+			}
+		}
 	}
 
 }

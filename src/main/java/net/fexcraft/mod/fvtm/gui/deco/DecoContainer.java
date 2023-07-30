dc1956d4b834b0988efb05bc350c54ac389f561f
Ferdinand Calo <fercalo96@yahoo.de>
2023-07-30 23:11:33 +0200

    Update DecoContainer.java

--- src/main/java/net/fexcraft/mod/fvtm/gui/deco/DecoContainer.java
+++ src/main/java/net/fexcraft/mod/fvtm/gui/deco/DecoContainer.java
@@ -6,6 +6,7 @@ import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.mod.fvtm.data.DecorationData;
 import net.fexcraft.mod.fvtm.entity.Decoration;
 import net.fexcraft.mod.fvtm.ui.DecoEditor;
+import net.fexcraft.mod.uni.Pos;
 import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.ui.ContainerInterface;
 import net.minecraft.world.World;
@@ -65,6 +66,41 @@ public class DecoContainer extends ContainerInterface {
 					((DecoEditor)ui).select(-1, -1);
 				}
 				return;
+			}
+			case "pos":{
+				DecorationData deco = entity.decos.get(com.getInteger("idx"));
+				Pos pos = null;
+				switch(com.getInteger("axis")){
+					case 0: pos = new Pos(com.getFloat("value"), deco.offset.y, deco.offset.z); break;
+					case 1: pos = new Pos(deco.offset.x, com.getFloat("value"), deco.offset.z); break;
+					case 2: pos = new Pos(deco.offset.x, deco.offset.y, com.getFloat("value")); break;
+					default: return;
+				}
+				deco.offset = pos;
+				if(!client) SEND_TO_CLIENT.accept(com);
+				break;
+			}
+			case "rot":{
+				DecorationData deco = entity.decos.get(com.getInteger("idx"));
+				switch(com.getInteger("axis")){
+					case 0: deco.rotx = com.getFloat("value"); break;
+					case 1: deco.roty = com.getFloat("value"); break;
+					case 2: deco.rotz = com.getFloat("value"); break;
+					default: return;
+				}
+				if(!client) SEND_TO_CLIENT.accept(com);
+				break;
+			}
+			case "scale":{
+				DecorationData deco = entity.decos.get(com.getInteger("idx"));
+				switch(com.getInteger("axis")){
+					case 0: deco.sclx = com.getFloat("value"); break;
+					case 1: deco.scly = com.getFloat("value"); break;
+					case 2: deco.sclz = com.getFloat("value"); break;
+					default: return;
+				}
+				if(!client) SEND_TO_CLIENT.accept(com);
+				break;
 			}
 		}
 	}

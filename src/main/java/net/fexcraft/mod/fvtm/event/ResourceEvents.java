b5cf207040e36e9ca88cfec91948bd2112622073
Ferdinand Calo <fercalo96@yahoo.de>
2023-09-04 22:32:47 +0200

    Update ResourceEvents.java

--- src/main/java/net/fexcraft/mod/fvtm/event/ResourceEvents.java
+++ src/main/java/net/fexcraft/mod/fvtm/event/ResourceEvents.java
@@ -2,7 +2,7 @@ package net.fexcraft.mod.fvtm.event;
 
 
 import net.fexcraft.mod.fvtm.data.block.BlockFunction;
-import net.fexcraft.mod.fvtm.data.part.Function;
+import net.fexcraft.mod.fvtm.data.part.PartFunction;
 import net.fexcraft.mod.fvtm.util.Resources;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.eventhandler.Event;
@@ -25,11 +25,11 @@ public class ResourceEvents extends Event {
 			super(resources);
 		}
 		
-		public void registerFunction(String regname, Class<? extends Function> function){
+		public void registerFunction(String regname, Class<? extends PartFunction> function){
 			Resources.registerFunction(regname, function, false);
 		}
 		
-		public void registerFunction(ResourceLocation regname, Class<? extends Function> function){
+		public void registerFunction(ResourceLocation regname, Class<? extends PartFunction> function){
 			Resources.registerFunction(regname, function, false);
 		}
 

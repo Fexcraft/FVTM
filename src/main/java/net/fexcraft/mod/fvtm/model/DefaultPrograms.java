d51c3e5cb052cd1f27741d4952436c23243f5ede
Ferdinand Calo <fercalo96@yahoo.de>
2023-09-07 19:14:03 +0200

    Update DefaultPrograms.java

--- src/main/java/net/fexcraft/mod/fvtm/model/DefaultPrograms.java
+++ src/main/java/net/fexcraft/mod/fvtm/model/DefaultPrograms.java
@@ -35,7 +35,7 @@ import net.fexcraft.mod.fvtm.data.vehicle.WheelSlot;
 import net.fexcraft.mod.fvtm.render.EffectRenderer;
 import net.fexcraft.mod.fvtm.sys.uni.GenericVehicle;
 import net.fexcraft.mod.fvtm.util.TexUtil;
-import net.fexcraft.mod.fvtm.util.function.EngineFunction;
+import net.fexcraft.mod.fvtm.function.EngineFunction;
 import net.fexcraft.mod.fvtm.function.WheelFunction;
 import net.fexcraft.mod.uni.IDL;
 import net.fexcraft.mod.uni.IDLManager;

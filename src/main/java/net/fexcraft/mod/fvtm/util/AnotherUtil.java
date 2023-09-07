b5caba7dc4d816f532e67ae18cb830845705b655
Ferdinand Calo <fercalo96@yahoo.de>
2023-09-07 23:03:34 +0200

    Update AnotherUtil.java

--- src/main/java/net/fexcraft/mod/fvtm/util/AnotherUtil.java
+++ src/main/java/net/fexcraft/mod/fvtm/util/AnotherUtil.java
@@ -1,7 +1,7 @@
 package net.fexcraft.mod.fvtm.util;
 
 import net.fexcraft.mod.uni.Pos;
-import net.minecraft.nbt.NBTTagCompound;
+import net.fexcraft.mod.uni.tag.TagCW;
 import net.minecraft.util.math.Vec3d;
 
 /**
@@ -18,15 +18,15 @@ public class AnotherUtil {
 		return prefix == null ? "" : prefix + "_";
 	}
 
-	public static void toNBT(Pos pos, String key, NBTTagCompound compound){
-		if(compound == null) compound = new NBTTagCompound();
+	public static void toTag(Pos pos, String key, TagCW compound){
+		if(compound == null) compound = TagCW.create();
 		key = prefix(key);
-		compound.setFloat(key + "x", pos.x);
-		compound.setFloat(key + "y", pos.y);
-		compound.setFloat(key + "z", pos.z);
+		compound.set(key + "x", pos.x);
+		compound.set(key + "y", pos.y);
+		compound.set(key + "z", pos.z);
 	}
 
-	public static Pos frNBT(String key, NBTTagCompound compound){
+	public static Pos frNBT(String key, TagCW compound){
 		return new Pos(compound.getFloat(key = prefix(key) + "x"), compound.getFloat(key + "y"), compound.getFloat(key + "z"));
 	}
 

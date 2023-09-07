3917e37193c5d66573b7b37873bb0f1317273db0
Ferdinand Calo <fercalo96@yahoo.de>
2023-09-07 23:03:11 +0200

    Update PartData.java

--- src/main/java/net/fexcraft/mod/fvtm/data/part/PartData.java
+++ src/main/java/net/fexcraft/mod/fvtm/data/part/PartData.java
@@ -1,110 +1,110 @@
 package net.fexcraft.mod.fvtm.data.part;
 
 import static net.fexcraft.mod.fvtm.util.AnotherUtil.frNBT;
-import static net.fexcraft.mod.fvtm.util.AnotherUtil.toNBT;
+import static net.fexcraft.mod.fvtm.util.AnotherUtil.toTag;
 
 import java.util.Map;
 import java.util.TreeMap;
 
-import com.google.gson.JsonObject;
-import net.fexcraft.mod.fvtm.data.root.DataCore;
+import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.mod.fvtm.data.ContentData;
 import net.fexcraft.mod.fvtm.data.root.Textureable;
 import net.fexcraft.mod.fvtm.data.root.Textureable.TextureHolder;
 import net.fexcraft.mod.fvtm.data.root.Textureable.TextureUser;
 import net.fexcraft.mod.fvtm.util.Rot;
 import net.fexcraft.mod.uni.Pos;
 import net.fexcraft.mod.uni.impl.TagCWI;
+import net.fexcraft.mod.uni.tag.TagCW;
+import net.fexcraft.mod.uni.tag.TagLW;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTBase;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.ResourceLocation;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
-public class PartData extends DataCore<Part, PartData> implements TextureUser {
+public class PartData extends ContentData<Part, PartData> implements TextureUser {
 	
 	protected TreeMap<String, PartFunction> functions = new TreeMap<>();
 	protected Textureable texture;
 	protected Pos currentpos = new Pos(0, 0, 0);
 	protected Rot currentrot = new Rot();
-	//protected Vec3f currentrot = new Vec3f();//TODO add this?
 	protected String rotpoint;
 
 	public PartData(Part type){
 		super(type);
 		texture = new Textureable(type);
 		for(PartFunction func : type.functions){
-			//TODO this.functions.put(func.getId(), func.copy(type));
+			functions.put(func.getId(), func.copy(type));
 		}
 	}
 
 	@Override
-	public NBTTagCompound write(NBTTagCompound compound){
-		if(compound == null) compound = new NBTTagCompound();
-		compound.setString("Part", type.getRegistryName().toString());
-		toNBT(currentpos, "CurrentPos", compound);
+	public TagCW write(TagCW compound){
+		if(compound == null) compound = TagCW.create();
+		compound.set("Part", type.getIDS());
+		toTag(currentpos, "CurrentPos", compound);
 		currentrot.toNBT("CurrentRot", compound);
-		if(rotpoint != null && !rotpoint.equals("vehicle")) compound.setString("SwivelPoint", rotpoint);
+		if(rotpoint != null && !rotpoint.equals("vehicle")) compound.set("SwivelPoint", rotpoint);
 		//
-		texture.save(new TagCWI(compound));
+		texture.save(compound);
 		//
-		NBTTagList flist = new NBTTagList();
+		TagLW flist = TagLW.create();
 		for(PartFunction func : functions.values()){
-			NBTTagCompound com = new NBTTagCompound();
-			com.setString("id", func.getId()); com = func.save(new TagCWI(com)).local();
-			if(com != null) flist.appendTag(com);
-		} compound.setTag("Functions", flist);
+			TagCW com = TagCW.create();
+			com.set("id", func.getId());
+			com = func.save(com);
+			if(com != null) flist.add(com);
+		}
+		compound.set("Functions", flist);
 		return compound;
 	}
 
 	@Override
-	public PartData read(NBTTagCompound compound){
+	public PartData read(TagCW compound){
 		//if(!compound.hasKey("Part")) return null;
 		//type = Resources.getPart(compound.getString("Part"));
 		//if(type == null) return null;//TODO add "placeholder" for "missing" items
 		currentpos = frNBT("CurrentPos", compound);
-		currentrot = Rot.fromNBT("CurrentRot", new TagCWI(compound));
-		rotpoint = compound.hasKey("SwivelPoint") ? compound.getString("SwivelPoint") : null;
+		currentrot = Rot.fromNBT("CurrentRot", compound);
+		rotpoint = compound.has("SwivelPoint") ? compound.getString("SwivelPoint") : null;
 		//
-		texture.load(new TagCWI(compound), type);
+		texture.load(compound, type);
 		//
-		NBTTagList flist = (NBTTagList)compound.getTag("Functions");
-		if(flist != null){
-			for(NBTBase base : flist){
-				NBTTagCompound com = (NBTTagCompound)base;
-				PartFunction func = this.getFunction(com.getString("id"));
-				if(func != null) func.load(new TagCWI(com));
+		if(compound.has("Functions")){
+			TagLW list = compound.getList("Functions");
+			for(int i = 0; i < list.size(); i++){
+				TagCW com = list.getCompound(i);
+				PartFunction func = getFunction(com.getString("id"));
+				if(func != null) func.load(com);
 			}
 		}
 		return this;
 	}
 
 	@Override
-	public PartData parse(JsonObject obj){
+	public PartData parse(JsonMap map){
 		//ResourceLocation regname = DataUtil.getRegistryName("Part", obj);
 		//if(regname == null || Resources.getPart(regname) == null) return null;
 		//this.type = Resources.getPart(regname);
-		currentpos = Pos.fromJson(obj.get("CurrentPos"), true);
-		currentrot = Rot.fromJson(obj, "CurrentPos");
+		currentpos = Pos.frJson(map.getMap("CurrentPos"), true);
+		//TODO currentrot = Rot.fromJson(map, "CurrentPos");
 		//
 		return this;
 	}
 
 	@Override
-	public JsonObject toJson(){
-		JsonObject obj = new JsonObject();
-		obj.addProperty("Part", type.getRegistryName().toString());
-		obj.add("CurrentPos", currentpos.toJson(true));
-		if(!currentrot.isNull()) obj.add("CurrentRot", currentrot.toJson());
+	public JsonMap toJson(){
+		JsonMap obj = new JsonMap();
+		obj.add("Part", type.getIDS());
+		//TODO obj.add("CurrentPos", currentpos);
+		//TODO if(!currentrot.isNull()) obj.add("CurrentRot", currentrot.toJson());
 		//
 		return obj;
 	}
 
 	public ItemStack newItemStack(){
-		ItemStack stack = this.type.newItemStack();
-		stack.setTagCompound(this.write(new NBTTagCompound()));
+		ItemStack stack = this.type.getNewStack().local();
+		stack.setTagCompound(this.write(new TagCWI()).local());
 		return stack;
 	}
 

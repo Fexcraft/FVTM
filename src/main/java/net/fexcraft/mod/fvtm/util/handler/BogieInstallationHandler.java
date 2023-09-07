22a473bfa267406a93f893b129775c4a7e9df4b9
Ferdinand Calo <fercalo96@yahoo.de>
2023-09-07 21:55:08 +0200

    Update BogieInstallationHandler.java

--- src/main/java/net/fexcraft/mod/fvtm/util/handler/BogieInstallationHandler.java
+++ src/main/java/net/fexcraft/mod/fvtm/util/handler/BogieInstallationHandler.java
@@ -1,38 +1,36 @@
 package net.fexcraft.mod.fvtm.util.handler;
 
-import javax.annotation.Nullable;
-
-import com.google.gson.JsonObject;
-import net.fexcraft.lib.common.json.JsonUtil;
-import net.fexcraft.lib.mc.utils.Print;
+import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.mod.fvtm.data.part.PartData;
 import net.fexcraft.mod.fvtm.data.part.PartInstallHandler;
 import net.fexcraft.mod.fvtm.data.vehicle.VehicleData;
 import net.fexcraft.mod.fvtm.function.BogieFunction;
 import net.fexcraft.mod.uni.Pos;
-import net.minecraft.command.ICommandSender;
+import net.fexcraft.mod.uni.world.MessageSender;
 
 public class BogieInstallationHandler extends PartInstallHandler {
 
+	public static final BogieInstallationHandler INSTANCE = new BogieInstallationHandler();
+
 	@Override
-	public boolean allowInstall(@Nullable ICommandSender sender, PartData part, String cat, VehicleData data){
+	public boolean validInstall(MessageSender sender, PartData part, String cat, VehicleData data){
 		if(!data.getType().getVehicleType().isRailVehicle()){
-			Print.chatnn(sender, "handler.install.fvtm.bogie.not_rail_vehicle");
+			sender.send("handler.install.fvtm.bogie.not_rail_vehicle");
 			return false;
 		}
 		if(data.getParts().containsKey(cat)){
-			Print.chatnn(sender, "handler.install.fvtm.bogie.category_occupied");
+			sender.send("handler.install.fvtm.bogie.category_occupied");
 			return false;
 		}
 		if(!data.getWheelSlots().containsKey(cat)){
-			Print.chatnn(sender, "handler.install.fvtm.bogie.bogieslot_missing");
+			sender.send("handler.install.fvtm.bogie.bogieslot_missing");
 			return false;
 		}
-		Print.chatnn(sender, "handler.install.fvtm.bogie.check_passed");
+		sender.send("handler.install.fvtm.bogie.check_passed");
 		return true;
 	}
 	@Override
-	public boolean processInstall(@Nullable ICommandSender sender, PartData part, String cat, VehicleData data){
+	public boolean processInstall(MessageSender sender, PartData part, String cat, VehicleData data){
 		data.getParts().put(cat, part);
 		part.setInstalledPos(new Pos(data.getWheelSlots().get(cat).position));
 		BogieFunction func = part.getFunction("fvtm:bogie");
@@ -41,27 +39,27 @@ public class BogieInstallationHandler extends PartInstallHandler {
 		Pos partpos = part.getInstalledPos();
 		data.getWheelPositions().put(cat, new Pos(partpos.x, -partpos.y - idata.height, -partpos.z).toV3D());
 		//Print.debug("New BogiePos: " + data.getWheelPositions().get(cat));
-		Print.chatnn(sender, "handler.install.fvtm.bogie.success");
+		sender.send("handler.install.fvtm.bogie.success");
 		return true;
 	}
 
 	@Override
-	public boolean allowUninstall(@Nullable ICommandSender sender, PartData part, String is_category, VehicleData from){
+	public boolean validUninstall(MessageSender sender, PartData part, String is_category, VehicleData from){
 		BogieData idata = part.getType().getInstallationHandlerData();
 		if(idata != null && !idata.removable){
-			Print.chatnn(sender, "handler.deinstall.fvtm.bogie.part_not_removable");
+			sender.send("handler.deinstall.fvtm.bogie.part_not_removable");
 			return false;
 		}
-		Print.chatnn(sender, "handler.deinstall.fvtm.bogie.check_passed");
+		sender.send("handler.deinstall.fvtm.bogie.check_passed");
 		return true;
 	}
 
 	@Override
-	public boolean processUninstall(ICommandSender sender, PartData part, String cat, VehicleData data){
+	public boolean processUninstall(MessageSender sender, PartData part, String cat, VehicleData data){
 		part.setInstalledPos(new Pos(0, 0, 0));
 		data.getParts().remove(cat);
 		data.getWheelPositions().remove(cat);
-		Print.chatnn(sender, "handler.deinstall.fvtm.bogie.success");
+		sender.send("handler.deinstall.fvtm.bogie.success");
 		return true;
 	}
 	
@@ -72,9 +70,9 @@ public class BogieInstallationHandler extends PartInstallHandler {
 		/** from 0 (connection/install) point to rail (e.g. wheel base) */
 		private float height;
 		
-		public BogieData(JsonObject obj){ if(obj == null) return;
-			removable = JsonUtil.getIfExists(obj, "Removable", true);
-			height = JsonUtil.getIfExists(obj, "Height", 8).floatValue();
+		public BogieData(JsonMap map){
+			removable = map.getBoolean("Removable", true);
+			height = map.getFloat("Height", 8);
 		}
 		
 	}
@@ -87,6 +85,11 @@ public class BogieInstallationHandler extends PartInstallHandler {
 	@Override
 	public String[] getValidCategories(PartData part, VehicleData vehicle){
 		return new String[]{ "bogie_front", "bogie_rear" };
+	}
+
+	@Override
+	public Object parseData(JsonMap map){
+		return new BogieData(map);
 	}
 
 }

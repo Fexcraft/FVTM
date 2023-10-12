60fb7196dd3b04113efdaafe3d9e1b39b1524e56
Ferdinand Calo <fercalo96@yahoo.de>
2023-10-13 00:23:05 +0200

    Create ConstructorContainer.java

---
+++ src/main/java/net/fexcraft/mod/fvtm/gui/construct/ConstructorContainer.java
@@ -0,0 +1,36 @@
+package net.fexcraft.mod.fvtm.gui.construct;
+
+import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.mod.uni.tag.TagCW;
+import net.fexcraft.mod.uni.ui.ContainerInterface;
+import net.minecraft.entity.player.EntityPlayer;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class ConstructorContainer extends ContainerInterface {
+
+	public ConstructorContainer(JsonMap map, EntityPlayer player, int x, int y, int z){
+		super(map);
+	}
+
+	@Override
+	public Object get(String key, Object... objs){
+		//
+		return null;
+	}
+
+	@Override
+	public void packet(TagCW com, boolean client){
+		String task = com.getString("task");
+		switch(task){
+			//
+		}
+	}
+
+	@Override
+	public void onClosed(){
+		//
+	}
+
+}

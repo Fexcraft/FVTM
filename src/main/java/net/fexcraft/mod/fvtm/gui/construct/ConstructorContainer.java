7696773b70ea86b45681886a19496fc936426dd8
Ferdinand Calo <fercalo96@yahoo.de>
2023-10-13 22:09:00 +0200

    Update ConstructorContainer.java

--- src/main/java/net/fexcraft/mod/fvtm/gui/construct/ConstructorContainer.java
+++ src/main/java/net/fexcraft/mod/fvtm/gui/construct/ConstructorContainer.java
@@ -1,18 +1,23 @@
 package net.fexcraft.mod.fvtm.gui.construct;
 
 import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.mod.fvtm.block.ConstructorEntity;
 import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.ui.ContainerInterface;
 import net.fexcraft.mod.uni.world.WrapperHolder;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.math.BlockPos;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public class ConstructorContainer extends ContainerInterface {
 
+	private ConstructorEntity tile;
+
 	public ConstructorContainer(JsonMap map, EntityPlayer player, int x, int y, int z){
 		super(map, WrapperHolder.getPlayer(player));
+		tile = (ConstructorEntity)player.world.getTileEntity(new BlockPos(x, y, z));
 	}
 
 	@Override

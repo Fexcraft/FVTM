0be658133cdd72615066a0865a39d377626e1f84
Ferdinand Calo <fercalo96@yahoo.de>
2023-10-13 23:11:57 +0200

    Update ConstructorBlock.java

--- src/main/java/net/fexcraft/mod/fvtm/block/ConstructorBlock.java
+++ src/main/java/net/fexcraft/mod/fvtm/block/ConstructorBlock.java
@@ -1,6 +1,7 @@
 package net.fexcraft.mod.fvtm.block;
 
 import static net.fexcraft.mod.fvtm.gui.GuiHandler.CONSTRUCTOR_MAIN;
+import static net.fexcraft.mod.fvtm.util.Properties.FACING;
 
 import net.fexcraft.lib.mc.utils.Print;
 import net.fexcraft.mod.fvtm.FVTM;
@@ -12,7 +13,6 @@ import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.MapColor;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.properties.IProperty;
-import net.minecraft.block.properties.PropertyDirection;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.resources.I18n;
@@ -24,6 +24,8 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.BlockPos.MutableBlockPos;
+import net.minecraft.util.text.TextComponentTranslation;
 import net.minecraft.world.World;
 
 /**
@@ -31,7 +33,6 @@ import net.minecraft.world.World;
  */
 public class ConstructorBlock extends Block implements ITileEntityProvider {
 
-    public static final PropertyDirection FACING = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
 	public static ConstructorBlock INSTANCE;
 	public static ItemBlock ITEM;
 
@@ -39,7 +40,7 @@ public class ConstructorBlock extends Block implements ITileEntityProvider {
         super(Material.ANVIL, MapColor.OBSIDIAN);
 		setRegistryName("fvtm:constructor");
 		setTranslationKey(getRegistryName().toString());
-        setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
+        setDefaultState(blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
     }
 
     @Override
@@ -54,7 +55,7 @@ public class ConstructorBlock extends Block implements ITileEntityProvider {
 
     @Override
     public IBlockState getStateForPlacement(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer){
-        return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
+        return getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
     }
 
     @Override
@@ -66,7 +67,7 @@ public class ConstructorBlock extends Block implements ITileEntityProvider {
     public IBlockState getStateFromMeta(int meta){
         EnumFacing facing = EnumFacing.byIndex(meta);
         facing = facing.getAxis() == EnumFacing.Axis.Y ? EnumFacing.NORTH : facing;
-        return this.getDefaultState().withProperty(FACING, facing);
+        return getDefaultState().withProperty(FACING, facing);
     }
 
     @Override
@@ -85,6 +86,35 @@ public class ConstructorBlock extends Block implements ITileEntityProvider {
 		if(player.isSneaking()) return true;
         ConstructorEntity te = (ConstructorEntity)world.getTileEntity(pos);
 		if(te == null) return false;
+		if(te.getLiftPos() == null){
+			player.sendMessage(new TextComponentTranslation("interact.fvtm.constructor.nolift"));
+			MutableBlockPos mpos = new MutableBlockPos();
+			boolean found = false;
+			for(int y = -1; y < 2; y++){
+				if(found) break;
+				for(int x = -7; x < 8; x++){
+					if(found) break;
+					for(int z = -7; z < 8; z++){
+						TileEntity tile = world.getTileEntity(mpos.setPos(te.getPos().getX() + x, te.getPos().getY() + y, te.getPos().getZ() + z));
+						if(tile == null || tile instanceof ConstCenterEntity == false) continue;
+						ConstCenterEntity lte = (ConstCenterEntity)tile;
+						if(lte.getConstPos() == null || lte.getConstTile() == null){
+							lte.setConst(te);
+							te.setLiftPos(lte.getPos());
+							found = true;
+							break;
+						}
+					}
+				}
+			}
+			if(found){
+				player.sendMessage(new TextComponentTranslation("interact.fvtm.constructor.liftfound", mpos.getX(), mpos.getY(), mpos.getZ()));
+			}
+			else{
+				player.sendMessage(new TextComponentTranslation("interact.fvtm.constructor.noliftfound"));
+			}
+			return true;
+		}
         ItemStack held = player.getHeldItem(hand);
         if(held.isEmpty()){
         	player.openGui(FVTM.getInstance(), CONSTRUCTOR_MAIN, world, pos.getX(), pos.getY(), pos.getZ());

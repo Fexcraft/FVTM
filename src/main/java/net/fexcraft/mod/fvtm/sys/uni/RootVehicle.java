0a474b8b7528e4244bc09efd3af7671b600f80ae
Ferdinand Calo <fercalo96@yahoo.de>
2023-10-25 00:58:38 +0200

    Update RootVehicle.java

--- src/main/java/net/fexcraft/mod/fvtm/sys/uni/RootVehicle.java
+++ src/main/java/net/fexcraft/mod/fvtm/sys/uni/RootVehicle.java
@@ -1,7 +1,12 @@
 package net.fexcraft.mod.fvtm.sys.uni;
 
+import java.util.ArrayList;
+
+import net.fexcraft.mod.fvtm.sys.pro.NWheelEntity;
+import net.fexcraft.mod.uni.world.EntityWI;
 import net.minecraft.entity.Entity;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.world.World;
 
 /**
@@ -10,10 +15,12 @@ import net.minecraft.world.World;
 public class RootVehicle extends Entity {
 
 	public VehicleInstance vehicle;
+	public ArrayList<NWheelEntity> wheels = new ArrayList<>();
+	public AxisAlignedBB renderbox;
 
 	public RootVehicle(World world){
 		super(world);
-		//
+		vehicle = new VehicleInstance(new EntityWI(this), null);
 	}
 
 	@Override

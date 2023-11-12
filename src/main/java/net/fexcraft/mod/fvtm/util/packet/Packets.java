b4ff5f7dc182ed5fb86e709841095f32be830160
Ferdinand Calo <fexcalo@gmail.com>
2023-11-12 21:24:06 +0100

    Update Packets.java

--- src/main/java/net/fexcraft/mod/fvtm/util/packet/Packets.java
+++ src/main/java/net/fexcraft/mod/fvtm/util/packet/Packets.java
@@ -13,6 +13,9 @@ import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.relauncher.Side;
 
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
 public class Packets {
 	
 	private static final SimpleNetworkWrapper instance = NetworkRegistry.INSTANCE.newSimpleChannel("fvtm");
@@ -22,14 +25,13 @@ public class Packets {
 		//
 		instance.registerMessage(PKTH_SeatUpdate.Client.class, PKT_SeatUpdate.class, 0, Side.CLIENT);
 		instance.registerMessage(PKTH_SeatUpdate.Server.class, PKT_SeatUpdate.class, 1, Side.SERVER);
-		//instance.registerMessage(PKTH_SeatDismount.Client.class, PKT_SeatDismount.class, 2, Side.CLIENT);
-		instance.registerMessage(PKTH_VehKeyPress.class, PKT_VehKeyPress.class, 3, Side.SERVER);
-		instance.registerMessage(PKTH_VehControl.Client.class, PKT_VehControl.class, 4, Side.CLIENT);
-		instance.registerMessage(PKTH_VehControl.Server.class, PKT_VehControl.class, 5, Side.SERVER);
-		instance.registerMessage(PKTH_SPUpdate.Client.class, PKT_SPUpdate.class, 6, Side.CLIENT);
-		instance.registerMessage(PKTH_SPUpdate.Server.class, PKT_SPUpdate.class, 7, Side.SERVER);
-		instance.registerMessage(PKTH_VehKeyPressState.Client.class, PKT_VehKeyPressState.class, 8, Side.CLIENT);
-		instance.registerMessage(PKTH_VehKeyPressState.Server.class, PKT_VehKeyPressState.class, 9, Side.SERVER);
+		instance.registerMessage(PKTH_VehKeyPress.class, PKT_VehKeyPress.class, 2, Side.SERVER);
+		instance.registerMessage(PKTH_VehControl.Client.class, PKT_VehControl.class, 3, Side.CLIENT);
+		instance.registerMessage(PKTH_VehControl.Server.class, PKT_VehControl.class, 4, Side.SERVER);
+		instance.registerMessage(PKTH_SPUpdate.Client.class, PKT_SPUpdate.class, 4, Side.CLIENT);
+		instance.registerMessage(PKTH_SPUpdate.Server.class, PKT_SPUpdate.class, 5, Side.SERVER);
+		instance.registerMessage(PKTH_VehKeyPressState.Client.class, PKT_VehKeyPressState.class, 6, Side.CLIENT);
+		instance.registerMessage(PKTH_VehKeyPressState.Server.class, PKT_VehKeyPressState.class, 7, Side.SERVER);
 		//
 		instance.registerMessage(PKTH_UT.Client.class, PKT_UT.class, 10, Side.CLIENT);
 		instance.registerMessage(PKTH_UT.Server.class, PKT_UT.class, 10, Side.SERVER);
@@ -38,6 +40,9 @@ public class Packets {
 		FvtmLogger.LOGGER.log("Starting Packet Listener registration.");
 		PKTH_UT.LIS_SERVER.put("ui", (com, player) -> {
 			((UniCon)player.openContainer).container().packet(new TagCWI(com), false);
+		});
+		PKTH_UT.LIS_SERVER.put("vehicle", (com, player) -> {
+
 		});
 		if(EnvInfo.CLIENT){
 			PKTH_UT.LIS_CLIENT.put("ui", (com, player) -> {

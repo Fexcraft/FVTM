60c2e468a8a4b2581386e4141f4823c4d950525d
Ferdinand Calo <fexcalo@gmail.com>
2023-11-12 21:24:11 +0100

    Update IWI.java

--- src/main/java/net/fexcraft/mod/uni/impl/IWI.java
+++ src/main/java/net/fexcraft/mod/uni/impl/IWI.java
@@ -1,7 +1,14 @@
 package net.fexcraft.mod.uni.impl;
 
+import net.fexcraft.mod.fvtm.FvtmRegistry;
+import net.fexcraft.mod.fvtm.data.Consumable;
+import net.fexcraft.mod.fvtm.data.Material;
+import net.fexcraft.mod.fvtm.item.ConsumableItem;
+import net.fexcraft.mod.fvtm.item.MaterialItem;
 import net.fexcraft.mod.uni.item.ItemWrapper;
 import net.minecraft.item.Item;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraftforge.oredict.OreDictionary;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -16,7 +23,30 @@ public class IWI extends ItemWrapper {
 
 	@Override
 	public void linkContainer(){
+		if(item instanceof MaterialItem){
+			Material mat = ((MaterialItem)item).getContent();
+			if(mat.getItemContainer() == null) return;
+			item.setContainerItem(Item.getByNameOrId(mat.getItemContainer()));
+		}
+		if(item instanceof ConsumableItem){
+			Consumable con = ((ConsumableItem)item).getContent();
+			if(con.getItemContainer() == null) return;
+			item.setContainerItem(Item.getByNameOrId(con.getItemContainer()));
+		}
+	}
 
+	@Override
+	public void regToDict(){
+		if(item instanceof MaterialItem){
+			Material mat = ((MaterialItem)item).getContent();
+			if(mat.getOreDictId() == null) return;
+			OreDictionary.registerOre(mat.getOreDictId(), (Item)mat.getItemWrapper().direct());
+		}
+		if(item instanceof ConsumableItem){
+			Consumable con = ((ConsumableItem)item).getContent();
+			if(con.getOreDictId() == null) return;
+			OreDictionary.registerOre(con.getOreDictId(), (Item)con.getItemWrapper().direct());
+		}
 	}
 
 	@Override

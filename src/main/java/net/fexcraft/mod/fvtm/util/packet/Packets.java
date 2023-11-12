bed0bd3ac069c8abf86eeeb852663fa78487b44a
Ferdinand Calo <fexcalo@gmail.com>
2023-11-12 23:53:00 +0100

    Update Packets.java

--- src/main/java/net/fexcraft/mod/fvtm/util/packet/Packets.java
+++ src/main/java/net/fexcraft/mod/fvtm/util/packet/Packets.java
@@ -28,10 +28,10 @@ public class Packets {
 		instance.registerMessage(PKTH_VehKeyPress.class, PKT_VehKeyPress.class, 2, Side.SERVER);
 		instance.registerMessage(PKTH_VehControl.Client.class, PKT_VehControl.class, 3, Side.CLIENT);
 		instance.registerMessage(PKTH_VehControl.Server.class, PKT_VehControl.class, 4, Side.SERVER);
-		instance.registerMessage(PKTH_SPUpdate.Client.class, PKT_SPUpdate.class, 4, Side.CLIENT);
-		instance.registerMessage(PKTH_SPUpdate.Server.class, PKT_SPUpdate.class, 5, Side.SERVER);
-		instance.registerMessage(PKTH_VehKeyPressState.Client.class, PKT_VehKeyPressState.class, 6, Side.CLIENT);
-		instance.registerMessage(PKTH_VehKeyPressState.Server.class, PKT_VehKeyPressState.class, 7, Side.SERVER);
+		instance.registerMessage(PKTH_SPUpdate.Client.class, PKT_SPUpdate.class, 5, Side.CLIENT);
+		instance.registerMessage(PKTH_SPUpdate.Server.class, PKT_SPUpdate.class, 6, Side.SERVER);
+		instance.registerMessage(PKTH_VehKeyPressState.Client.class, PKT_VehKeyPressState.class, 7, Side.CLIENT);
+		instance.registerMessage(PKTH_VehKeyPressState.Server.class, PKT_VehKeyPressState.class, 8, Side.SERVER);
 		//
 		instance.registerMessage(PKTH_UT.Client.class, PKT_UT.class, 10, Side.CLIENT);
 		instance.registerMessage(PKTH_UT.Server.class, PKT_UT.class, 10, Side.SERVER);

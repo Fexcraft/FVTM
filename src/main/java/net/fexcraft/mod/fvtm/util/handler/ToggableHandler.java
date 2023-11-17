41fca33108cbb8133b4f802404c763d15786c6dc
Ferdinand Calo <fexcalo@gmail.com>
2023-11-17 21:15:58 +0100

    Update ToggableHandler.java

--- src/main/java/net/fexcraft/mod/fvtm/util/handler/ToggableHandler.java
+++ src/main/java/net/fexcraft/mod/fvtm/util/handler/ToggableHandler.java
@@ -18,14 +18,13 @@ import net.fexcraft.mod.fvtm.data.Seat;
 import net.fexcraft.mod.fvtm.data.attribute.AttrBox;
 import net.fexcraft.mod.fvtm.data.attribute.Attribute;
 import net.fexcraft.mod.fvtm.data.part.PartData;
+import net.fexcraft.mod.fvtm.data.part.PartSlot;
 import net.fexcraft.mod.fvtm.data.part.PartSlots;
 import net.fexcraft.mod.fvtm.data.vehicle.SwivelPoint;
 import net.fexcraft.mod.fvtm.data.vehicle.VehicleEntity;
 import net.fexcraft.mod.fvtm.gui.ServerReceiver;
 import net.fexcraft.mod.fvtm.item.PartItem;
-import net.fexcraft.mod.fvtm.sys.uni.GenericVehicle;
-import net.fexcraft.mod.fvtm.sys.uni.KeyPress;
-import net.fexcraft.mod.fvtm.sys.uni.SeatCache;
+import net.fexcraft.mod.fvtm.sys.uni.*;
 import net.fexcraft.mod.fvtm.util.Command;
 import net.fexcraft.mod.fvtm.util.Resources;
 import net.fexcraft.mod.fvtm.handler.DefaultPartInstallHandler.DPIHData;
@@ -51,62 +50,42 @@ public class ToggableHandler {
 	private static final float SBBS = .375f;
 	public static final AxisAlignedBB NULBB = new AxisAlignedBB(0, 0, 0, 0, 0, 0);
 
-	public static boolean handleClick(KeyPress press, VehicleEntity entity, SeatCache seat, EntityPlayer player, ItemStack stack){
+	public static boolean handleClick(KeyPress press, RootVehicle entity, SeatInstance seat, EntityPlayer player, ItemStack stack){
 		if(press == KeyPress.MOUSE_RIGHT && foundSeat(entity, seat, player, stack)) return true;
-		if(!stack.isEmpty()){
-			//Print.debug("item");
-			if(stack.getItem() instanceof PartItem){
-				PartData part = stack.getCapability(Capabilities.VAPDATA, null).getPartData();
-				if(part.getType().getInstallHandlerData() instanceof DPIHData == false) return false;
-        		DPIHData idata = part.getType().getInstallHandlerData();
-        		if(idata.swappable){
-        			ArrayList<Collidable> colls = new ArrayList<>();
-        			for(Entry<String, PartSlots> data : entity.getVehicleData().getPartSlotProviders().entrySet()){
-        				for(int i = 0; i < data.getValue().size(); i++){
-        					String type = data.getValue().get(i).type;
-        					for(String str : part.getType().getCategories()){
-        						if(str.equals(type)){
-        							SwivelPoint point = entity.getVehicleData().getRotationPointOfPart(data.getKey());
-        							colls.add(new Collidable(point, data.getKey(), data.getValue(), i));
-        						}
-        					}
-        				}
-        				Collidable coll = getCollided(seat == null, entity, player, colls);
-        				if(coll != null){
-        					if(coll.id().equals(last) && Time.getDate() < tilltime){
-        						return true;
-        					}
-        					NBTTagCompound packet = new NBTTagCompound();
-        					packet.setString("target_listener", Resources.UTIL_LISTENER);
-        					packet.setString("task", "hot_install");
-        					packet.setString("source", coll.source);
-        					packet.setInteger("index", coll.index);
-        					packet.setInteger("entity", entity.getEntity().getEntityId());
-        					PacketHandler.getInstance().sendToServer(new PacketNBTTagCompound(packet));
-        					last = coll.id(); tilltime = Time.getDate() + 20;
-        					return true;
-        				}
-        			}
-        			
-        		}
-	        }
-			return false;
+		if(!stack.isEmpty() && stack.getItem() instanceof PartItem){
+			PartData data = stack.getCapability(Capabilities.VAPDATA, null).getPartData();
+			if(data.getType().getInstallHandlerData() instanceof DPIHData == false) return false;
+			DPIHData idata = data.getType().getInstallHandlerData();
+			if(!idata.swappable) return false;
+			ArrayList<Collidable> colls = new ArrayList<>();
+			SwivelPoint point = null;
+			for(Entry<String, PartSlots> entry : entity.vehicle.data.getPartSlotProviders().entrySet()){
+				point = entity.vehicle.data.getRotationPointOfPart(entry.getKey());
+				for(Entry<String, PartSlot> sentry : entry.getValue().entrySet()){
+					String type = sentry.getValue().type;
+					for(String str : data.getType().getCategories()){
+						if(str.equals(type)){
+							colls.add(new Collidable(point, entry.getKey(), entry.getValue(), sentry.getKey()));
+						}
+					}
+					Collidable coll = getCollided(seat == null, entity, player, colls);
+					if(coll != null){
+						if(coll.id().equals(last) && Time.getDate() < tilltime) return true;
+						NBTTagCompound packet = new NBTTagCompound();
+						packet.setString("target_listener", Resources.UTIL_LISTENER);
+						packet.setString("task", "install");
+						packet.setString("source", coll.source);
+						packet.setString("category", coll.category);
+						packet.setInteger("entity", entity.getEntityId());
+						PacketHandler.getInstance().sendToServer(new PacketNBTTagCompound(packet));
+						last = coll.id();
+						tilltime = Time.getDate() + 20;
+						return true;
+					}
+				}
+			}
 		}
-		Collection<Attribute<?>> attributes = entity.getVehicleData().getAttributes().values().stream().filter(pr -> pr.hasBoxes() && (pr.valuetype.isTristate() || pr.valuetype.isNumber()) && (seat == null ? pr.external : (seat.seatdata.driver || pr.access.contains(seat.seatdata.name)))).collect(Collectors.toList());
-		if(attributes.size() == 0){
-			/*Print.debug(player, "none found");*/ return false;
-		}
-		ArrayList<Collidable> colls = new ArrayList<>();
-		attributes.forEach(attr -> colls.add(new Collidable(attr)));
-		Collidable coll = getCollided(seat == null, entity, player, colls);
-		if(coll == null){
-			/*Print.debug(player, "none hit");*/ return false;
-		}
-		Attribute<?> attr = coll.attr;
-		if(attr.id.equals(last) && Time.getDate() < tilltime){
-			/*Print.debug(player, "skipping till cooldown");*/ return true;
-		}
-		return sendToggle(attr, entity, press, null, player);
+		return false;
 	}
 
 	public static boolean sendToggle(Attribute<?> attr, VehicleEntity entity, KeyPress press, Float val, EntityPlayer player){
@@ -192,11 +171,11 @@ public class ToggableHandler {
 		return true;
 	}
 
-	private static boolean foundSeat(VehicleEntity entity, SeatCache seatfrom, EntityPlayer player, ItemStack stack){
+	private static boolean foundSeat(RootVehicle entity, SeatInstance seatfrom, EntityPlayer player, ItemStack stack){
 		if("seat".equals(last) && Time.getDate() < tilltime) return false;
 		if(!stack.isEmpty() && !(stack.getItem() instanceof ItemLead)) return false;
 		if(seatfrom == null && player.getRidingEntity() instanceof GenericVehicle){
-			seatfrom = ((GenericVehicle)player.getRidingEntity()).getSeatOf(player);
+			seatfrom = ((RootVehicle)player.getRidingEntity()).vehicle.getSeatOf(player);
 		}
 		Entity renent = Minecraft.getMinecraft().getRenderViewEntity();
 		Vec3d vec = renent.getPositionEyes(Minecraft.getMinecraft().getRenderPartialTicks());
@@ -205,14 +184,14 @@ public class ToggableHandler {
 		V3D vecto = new V3D(temp0.x, temp0.y, temp0.z);
 		V3D temp1;
 		Vec3f vec0 = new Vec3f(vec.x, vec.y, vec.z), vec1 = new Vec3f(vecto.x, vecto.y, vecto.z);
-		for(int i = 0; i < entity.getVehicleData().getSeats().size(); i++){
-			if(seatfrom != null && seatfrom.seatindex == i) continue;
-			Seat seat = entity.getVehicleData().getSeat(i);
-			SeatCache ent = ((GenericVehicle)entity).seats[i];
+		for(int i = 0; i < entity.vehicle.data.getSeats().size(); i++){
+			if(seatfrom != null && seatfrom.index == i) continue;
+			Seat seat = entity.vehicle.data.getSeat(i);
+			SeatInstance ent = entity.vehicle.seats.get(i);
 			if(ent == null || ent.passenger() == null){
-				SwivelPoint point = entity.getVehicleData().getRotationPoint(seat.swivel_point);
+				SwivelPoint point = entity.vehicle.data.getRotationPoint(seat.swivel_point);
 				temp1 = point.getRelativeVector(seat.pos);
-				temp1 = temp1.add(entity.getEntity().posX, entity.getEntity().posY, entity.getEntity().posZ);
+				temp1 = temp1.add(entity.posX, entity.posY, entity.posZ);
 				AxisAlignedBB aabb = NULBB.offset(temp1.x, temp1.y, temp1.z).grow(SBBS * seat.scale());
 				for(float f = 0; f < 4; f += Static.sixteenth / 2){
 					Vec3f dis = vec0.distance(vec1, f);
@@ -221,7 +200,7 @@ public class ToggableHandler {
 						NBTTagCompound packet = new NBTTagCompound();
 						packet.setString("target_listener", "fvtm:gui");
 						packet.setString("task", "toggle_seat");
-						packet.setInteger("entity", entity.getEntity().getEntityId());
+						packet.setInteger("entity", entity.getEntityId());
 						packet.setInteger("seat", i);
 						packet.setBoolean("main", true);//stack == EnumHand.MAIN_HAND);
 						if(player.world.isRemote){
@@ -242,7 +221,7 @@ public class ToggableHandler {
 	}
 
 	@SideOnly(Side.CLIENT) // Eventually checks about which is closest?
-	private static Collidable getCollided(boolean external, VehicleEntity vehicle, EntityPlayer player, Collection<Collidable> collidables){
+	private static Collidable getCollided(boolean external, RootVehicle vehicle, EntityPlayer player, Collection<Collidable> collidables){
 		Entity entity = Minecraft.getMinecraft().getRenderViewEntity();
 		if(entity == null || entity.world == null) return null;
 		Vec3d vec = entity.getPositionEyes(Minecraft.getMinecraft().getRenderPartialTicks());
@@ -257,7 +236,7 @@ public class ToggableHandler {
 		for(float f = 0; f < (external ? 3 : 2); f += Static.sixteenth / 2){
 			Vec3f dis = vec0.distance(vec1, f);
 			vec = new Vec3d(dis.x, dis.y, dis.z);
-			if(Command.TOGGABLE) vehicle.getEntity().world.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, dis.x, dis.y, dis.z, 0, 0, 0);
+			if(Command.TOGGABLE) vehicle.world.spawnParticle(EnumParticleTypes.SMOKE_NORMAL, dis.x, dis.y, dis.z, 0, 0, 0);
 			for(Collidable coll : collidables)
 				if(aabbs.containsKey(coll.id()) && aabbs.get(coll.id()).contains(vec)) return coll;
 		}
@@ -266,15 +245,14 @@ public class ToggableHandler {
 
 	public static boolean handleClick(KeyPress press, ItemStack stack){
 		if(!stack.isEmpty() && !(stack.getItem() instanceof PartItem || stack.getItem() instanceof ItemLead)) return false;
+		RootVehicle rentity = null;
 		for(Entity entity : Minecraft.getMinecraft().world.loadedEntityList){
-			if(entity instanceof VehicleEntity == false) continue;
-			//Print.debug("test");
-			if(((VehicleEntity)entity).getVehicleData().getAttribute("collision_range").asFloat() + 1 < entity.getDistance(Minecraft.getMinecraft().player)){
-				//Print.debug("veh dis: " + ((VehicleEntity)entity).getVehicleData().getAttribute("collision_range").float_value() + " " + entity.getName());
-				//Print.debug("ply dis: " + entity.getDistance(Minecraft.getMinecraft().player));
+			if(entity instanceof RootVehicle == false) continue;
+			rentity = (RootVehicle)entity;
+			if(rentity.vehicle.data.getAttribute("collision_range").asFloat() + 1 < entity.getDistance(Minecraft.getMinecraft().player)){
 				continue;
 			}
-			if(handleClick(press, (VehicleEntity)entity, null, Minecraft.getMinecraft().player, stack)) return true;
+			if(handleClick(press, rentity, null, Minecraft.getMinecraft().player, stack)) return true;
 		}
 		return false;
 	}
@@ -284,45 +262,43 @@ public class ToggableHandler {
 		protected Attribute<?> attr;
 		private SwivelPoint point;
 		private PartSlots slots;
-		//private DPIHData data;
 		private String source;
-		private int index;
+		private String category;
 		
 		public Collidable(Attribute<?> attr){
 			this.attr = attr;
 		}
 
-		public Collidable(SwivelPoint point, String source, PartSlots slots, int index){
+		public Collidable(SwivelPoint point, String source, PartSlots slots, String cat){
 			this.point = point;
 			this.source = source;
 			this.slots = slots;
-			this.index = index;
+			category = cat;
 		}
 
 		public String id(){
-			return attr == null ? source + ":" + index : attr.id;
+			return attr == null ? source + ":" + category : attr.id;
 		}
 
-		public void collectAABBs(boolean external, VehicleEntity vehicle, EntityPlayer player, TreeMap<String, AxisAlignedBB> aabbs, V3D temp){
+		public void collectAABBs(boolean external, RootVehicle entity, EntityPlayer player, TreeMap<String, AxisAlignedBB> aabbs, V3D temp){
 			if(attr != null){
 				String attrid = (external ? "external-" : "") + attr.asString();
 				AttrBox abb = attr.getBox(attrid);
 				if(abb == null) return;
-				SwivelPoint point = vehicle.getVehicleData().getRotationPoint(abb.swivel_point);
+				SwivelPoint point = entity.vehicle.data.getRotationPoint(abb.swivel_point);
 				temp = point.getRelativeVector(abb.pos.x, -abb.pos.y, -abb.pos.z);
-				temp = temp.add(vehicle.getEntity().posX, vehicle.getEntity().posY, vehicle.getEntity().posZ);
+				temp = temp.add(entity.posX, entity.posY, entity.posZ);
 				float te = abb.size;
 				aabbs.put(attr.id, new AxisAlignedBB(temp.x - te, temp.y - te, temp.z - te, temp.x + te, temp.y + te, temp.z + te));
 			}
 			else{
-				V3D pos = new V3D(slots.get(index).pos);
-				if(!source.equals("vehicle")) pos = pos.add(vehicle.getVehicleData().getPart(source).getInstalledPos());
+				V3D pos = new V3D(slots.get(category).pos);
+				if(!source.equals("vehicle")) pos = pos.add(entity.vehicle.data.getPart(source).getInstalledPos());
 				temp = point.getRelativeVector(pos.x, point.isVehicle() ? -pos.y : pos.y, -pos.z);
-				temp = temp.add(vehicle.getEntity().posX, vehicle.getEntity().posY, vehicle.getEntity().posZ);
-				float te = slots.get(index).radius / 2;
+				temp = temp.add(entity.posX, entity.posY, entity.posZ);
+				float te = slots.get(category).radius / 2;
 				aabbs.put(id(), new AxisAlignedBB(temp.x - te, temp.y - te, temp.z - te, temp.x + te, temp.y + te, temp.z + te));
 			}
-			//if(Command.TOGGABLE) vehicle.getEntity().world.spawnParticle(EnumParticleTypes.FLAME, temp.x, temp.y, temp.z, 0, 0, 0);
 		}
 		
 	}

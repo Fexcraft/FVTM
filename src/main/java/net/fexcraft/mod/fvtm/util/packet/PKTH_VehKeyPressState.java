2a84e2b948f44144038452629e7a809fa85bff3b
Ferdinand Calo <fexcalo@gmail.com>
2023-11-19 22:32:51 +0100

    Update PKTH_VehKeyPressState.java

--- src/main/java/net/fexcraft/mod/fvtm/util/packet/PKTH_VehKeyPressState.java
+++ src/main/java/net/fexcraft/mod/fvtm/util/packet/PKTH_VehKeyPressState.java
@@ -1,7 +1,10 @@
 package net.fexcraft.mod.fvtm.util.packet;
 
+import net.fexcraft.mod.fvtm.data.Capabilities;
 import net.fexcraft.mod.fvtm.sys.uni.GenericVehicle;
+import net.fexcraft.mod.fvtm.sys.uni.RootVehicle;
 import net.fexcraft.mod.fvtm.sys.uni.SeatCache;
+import net.fexcraft.mod.fvtm.sys.uni.SeatInstance;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
@@ -22,10 +25,10 @@ public class PKTH_VehKeyPressState {
                 @Override
                 public void run(){
                     EntityPlayerMP player = ctx.getServerHandler().player;
-                    if(player.getRidingEntity() != null && player.getRidingEntity() instanceof GenericVehicle){
-                    	SeatCache seat = ((GenericVehicle)player.getRidingEntity()).getSeatOf(player);
-                        seat.vehicle.onKeyPress(packet.keypress, seat.seatdata, player, packet.state);
-                        Packets.sendToAllAround(packet, seat.vehicle);
+                    if(player.getRidingEntity() != null && player.getRidingEntity() instanceof RootVehicle){
+                    	SeatInstance seat = ((RootVehicle)player.getRidingEntity()).getSeatOf(player);
+                        seat.root.onKeyPress(packet.keypress, seat.seat, player.getCapability(Capabilities.PASSENGER, null).asWrapper(), packet.state);
+                        Packets.sendToAllAround(packet, seat.root.entity.direct());
                     }
                 }
             }); return null;

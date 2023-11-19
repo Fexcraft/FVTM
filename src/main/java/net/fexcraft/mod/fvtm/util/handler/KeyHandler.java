9c6e309cf42ca8dc33ce555856686ac27163d6ea
Ferdinand Calo <fexcalo@gmail.com>
2023-11-19 22:32:46 +0100

    Update KeyHandler.java

--- src/main/java/net/fexcraft/mod/fvtm/util/handler/KeyHandler.java
+++ src/main/java/net/fexcraft/mod/fvtm/util/handler/KeyHandler.java
@@ -1,6 +1,11 @@
 package net.fexcraft.mod.fvtm.util.handler;
 
+import net.fexcraft.lib.mc.utils.Print;
+import net.fexcraft.mod.fvtm.data.Capabilities;
 import net.fexcraft.mod.fvtm.sys.uni.RootVehicle;
+import net.fexcraft.mod.fvtm.sys.uni.SeatInstance;
+import net.fexcraft.mod.uni.world.EntityW;
+import net.minecraft.entity.player.EntityPlayer;
 import org.lwjgl.input.Keyboard;
 
 import net.fexcraft.mod.fvtm.gui.VehicleSteeringOverlay;
@@ -20,6 +25,7 @@ import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickItem
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
+import org.lwjgl.input.Mouse;
 
 public class KeyHandler {
 
@@ -29,6 +35,7 @@ public class KeyHandler {
     public static KeyBinding trailerToggle, wagonToggle, reset, brake, pbrake;
     public static KeyBinding arrow_up, arrow_down, arrow_left, arrow_right;
     public static final String category = "keycompound.fvtm.controls";
+    private static boolean toggables;
 
     public KeyHandler(){
         minecraft = Minecraft.getMinecraft();
@@ -76,10 +83,14 @@ public class KeyHandler {
     //TODO subscribe key-input event
     @SubscribeEvent
     public void clientTick(TickEvent.ClientTickEvent event){
+        if(minecraft.player == null || minecraft.world == null) return;
         switch(event.phase){
-            case START: { break; }
+            case START: {
+                if(minecraft.player.getRidingEntity() instanceof RootVehicle){
+                    handleKeyboardInput();
+                }
+            }
             case END: {
-                if(minecraft.player == null || minecraft.world == null) return;
                 /*if(minecraft.player.getRidingEntity() instanceof RootVehicle && minecraft.currentScreen == null){
                     RootVehicle veh = (RootVehicle)minecraft.player.getRidingEntity();
                 	if(veh.getSeatOf(minecraft.player) == null) return;//temp
@@ -88,6 +99,89 @@ public class KeyHandler {
                 break;
             }
         }
+    }
+
+    public void handleKeyboardInput(){
+        EntityW player = minecraft.player.getCapability(Capabilities.PASSENGER, null).asWrapper();
+        SeatInstance seat = ((RootVehicle)minecraft.player.getRidingEntity()).getSeatOf(minecraft.player);
+        if(seat == null) return;
+        boolean uni12 = false;
+        if(isKeyDown(minecraft.gameSettings.keyBindForward.getKeyCode())){
+            seat.onKeyPress(seat.root.type.isAirVehicle() ? KeyPress.TURN_DOWN : KeyPress.ACCELERATE, player);
+        }
+        if(isKeyDown(minecraft.gameSettings.keyBindBack.getKeyCode())){
+            seat.onKeyPress(seat.root.type.isAirVehicle() ? KeyPress.TURN_UP : KeyPress.DECELERATE, player);
+        }
+        if(isKeyDown(minecraft.gameSettings.keyBindLeft.getKeyCode())){
+            seat.onKeyPress(KeyPress.TURN_LEFT, player);
+        }
+        if(isKeyDown(minecraft.gameSettings.keyBindRight.getKeyCode())){
+            seat.onKeyPress(KeyPress.TURN_RIGHT, player);
+        }
+        if(isKeyDown(KeyHandler.arrow_up.getKeyCode())){
+            /*if(toggables) scroll(0, -1);
+            else*/ seat.onKeyPress(seat.root.type.isAirVehicle() ? KeyPress.ACCELERATE : KeyPress.GEAR_UP, player);
+        }
+        if(isKeyDown(KeyHandler.arrow_down.getKeyCode())){
+            /*if(toggables) scroll(0, 1);
+            else*/ seat.onKeyPress(seat.root.type.isAirVehicle() ? KeyPress.DECELERATE : KeyPress.GEAR_DOWN, player);
+        }
+        if(isKeyDown(KeyHandler.arrow_left.getKeyCode())){
+            /*if(toggables) scroll(-1, 0);
+            else*/ seat.onKeyPress(KeyPress.ROLL_LEFT, player);
+        }
+        if(isKeyDown(KeyHandler.arrow_right.getKeyCode())){
+            /*if(toggables) scroll(1, 0);
+            else*/ seat.onKeyPress(KeyPress.ROLL_RIGHT, player);
+        }
+        if(uni12){
+            if(isKeyDown(KeyHandler.pbrake.getKeyCode())){
+                seat.onKeyPress(KeyPress.PBRAKE, player);
+            }
+            boolean state = isKeyDown(KeyHandler.brake.getKeyCode());
+            if(state != seat.root.getKeyPressState(KeyPress.BRAKE)){
+                seat.root.onKeyPress(KeyPress.BRAKE, seat.seat, player, state);
+            }
+        }
+        else{
+            if(isKeyDown(KeyHandler.brake.getKeyCode())){
+                seat.onKeyPress(KeyPress.BRAKE, player);
+            }
+        }
+        if(isKeyDown(KeyHandler.engineToggle.getKeyCode())){
+            seat.onKeyPress(KeyPress.ENGINE, player);
+        }
+        if(isKeyDown(minecraft.gameSettings.keyBindSneak.getKeyCode())){
+            seat.onKeyPress(KeyPress.DISMOUNT, player);
+        }
+        if(isKeyDown(KeyHandler.openInventory.getKeyCode())){
+            //root.resetview = false;
+            seat.onKeyPress(KeyPress.INVENTORY, player);
+        }
+        if(isKeyDown(KeyHandler.doorToggle.getKeyCode())){
+            seat.onKeyPress(KeyPress.TOGGABLES, player);
+        }
+        if(isKeyDown(KeyHandler.scriptsGUI.getKeyCode())){
+            //root.resetview = false;
+            seat.onKeyPress(KeyPress.SCRIPTS, player);
+        }
+        if(isKeyDown(KeyHandler.lightsToggle.getKeyCode())){
+            seat.onKeyPress(KeyPress.LIGHTS, player);
+        }
+        if(isKeyDown(KeyHandler.trailerToggle.getKeyCode())){
+            seat.onKeyPress(KeyPress.COUPLER_REAR, player);
+        }
+        if(isKeyDown(KeyHandler.wagonToggle.getKeyCode())){
+            seat.onKeyPress(KeyPress.COUPLER_FRONT, player);
+        }
+        if(isKeyDown(KeyHandler.reset.getKeyCode())){
+            /*if(toggables) processToggleClick(2);
+            else*/ seat.onKeyPress(KeyPress.RESET, player);
+        }
+    }
+
+    public boolean isKeyDown(int keycode){
+        return keycode < 0 ? Mouse.isButtonDown(keycode + 100) : keycode > 255 ? /* invalid code - PASS */ false : Keyboard.isKeyDown(keycode);
     }
 
     @SubscribeEvent

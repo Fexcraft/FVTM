ca75109c3d48c8ae7383ccff02fd71e9b0e5e708
Ferdinand Calo <fexcalo@gmail.com>
2023-11-24 19:40:09 +0100

    Update WrapperHolderImpl.java

--- src/main/java/net/fexcraft/mod/uni/world/WrapperHolderImpl.java
+++ src/main/java/net/fexcraft/mod/uni/world/WrapperHolderImpl.java
@@ -1,7 +1,10 @@
 package net.fexcraft.mod.uni.world;
 
+import net.fexcraft.lib.common.math.V3I;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 /**
@@ -9,13 +12,13 @@ import net.minecraft.world.World;
  */
 public class WrapperHolderImpl extends WrapperHolder {
 
-	@Override
+	/*@Override
 	public PlayerW getPlayer0(Object o){
 		if(!PLAYERS.containsKey(o)){
 			PLAYERS.put(o, new PlayerWI((EntityPlayer)o));
 		}
 		return PLAYERS.get(o);
-	}
+	}*/
 
 	@Override
 	public EntityW getEntity0(Object o){
@@ -31,6 +34,26 @@ public class WrapperHolderImpl extends WrapperHolder {
 			WORLDS.put(o, new WorldWI((World)o));
 		}
 		return WORLDS.get(o);
+	}
+
+	@Override
+	public V3I getPos0(Object o){
+		BlockPos pos = (BlockPos)o;
+		return new V3I(pos.getX(), pos.getY(), pos.getZ());
+	}
+
+	@Override
+	public BlockSide getSide0(Object o){
+		EnumFacing facing = (EnumFacing)o;
+		switch(facing){
+			case UP: return BlockSide.UP;
+			case DOWN: return BlockSide.DOWN;
+			case NORTH: return BlockSide.NORTH;
+			case WEST: return BlockSide.WEST;
+			case EAST: return BlockSide.EAST;
+			case SOUTH: return BlockSide.SOUTH;
+		}
+		return BlockSide.NORTH;
 	}
 
 }

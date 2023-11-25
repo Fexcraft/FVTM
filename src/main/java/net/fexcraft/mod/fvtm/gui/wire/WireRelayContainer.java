68470e96234ecb12ef93aaf19a7ffdec31d3298a
Ferdinand Calo <fexcalo@gmail.com>
2023-11-25 19:18:08 +0100

    Update WireRelayContainer.java

--- src/main/java/net/fexcraft/mod/fvtm/gui/wire/WireRelayContainer.java
+++ src/main/java/net/fexcraft/mod/fvtm/gui/wire/WireRelayContainer.java
@@ -121,8 +121,8 @@ public class WireRelayContainer extends GenericContainer {
 						}
 						BlockTileEntity tile0 = (BlockTileEntity)system.getWorld().getTileEntity(BlockPos.fromLong(stack.getTagCompound().getLong("fvtm:wirepoint")));
 						RelayData data0 = tile0.getBlockData().getType().getRelayData();
-						V3D r0 = data0.getVec(stack.getTagCompound().getString("fvtm:wirepoint_slot"), tile0.getPos(), tile0.meta, tile0.data.getType().getBlockType());
-						V3D r1 = data.getVec(relid, tile.getPos(), tile.meta, tile.data.getType().getBlockType());
+						V3D r0 = data0.getVec(stack.getTagCompound().getString("fvtm:wirepoint_slot"), tile0.getV3I(), tile0.meta, tile0.data.getType().getBlockType());
+						V3D r1 = data.getVec(relid, tile.getV3I(), tile.meta, tile.data.getType().getBlockType());
 						Wire wire = new Wire(relay0, relay, type, r0, r1);
 						if(relay0.isDuplicate(wire) || relay.isDuplicate(wire)){
 							Print.chat(player, "&cWire has same start/end as another wire.");

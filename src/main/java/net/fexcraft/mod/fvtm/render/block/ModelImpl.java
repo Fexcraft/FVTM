50211ef5fc6089ef7277d90f019fd84c7af95787
Ferdinand Calo <fexcalo@gmail.com>
2023-11-25 23:01:22 +0100

    Create ModelImpl.java

---
+++ src/main/java/net/fexcraft/mod/fvtm/render/block/ModelImpl.java
@@ -0,0 +1,103 @@
+package net.fexcraft.mod.fvtm.render.block;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import net.fexcraft.app.json.JsonArray;
+import net.fexcraft.app.json.JsonHandler;
+import net.fexcraft.app.json.JsonValue;
+import net.fexcraft.lib.mc.registry.NamedResourceLocation;
+import net.fexcraft.mod.fvtm.FvtmRegistry;
+import net.fexcraft.mod.fvtm.data.block.Block;
+import net.fexcraft.mod.fvtm.model.BlockModel;
+import net.fexcraft.mod.uni.IDL;
+import net.minecraft.client.renderer.block.model.IBakedModel;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.vertex.VertexFormat;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.IModel;
+import net.minecraftforge.common.model.IModelPart;
+import net.minecraftforge.common.model.IModelState;
+import net.minecraftforge.common.model.TRSRTransformation;
+
+import java.util.*;
+import java.util.function.Function;
+
+import static net.fexcraft.mod.fvtm.render.block.FvtmBlockModelLoader.MODELS;
+
+class ModelImpl implements IModel {
+
+    protected final ResourceLocation modelloc;
+    protected BlockModel model;
+    protected Block block;
+    protected boolean ambocl;
+    //
+    protected HashMap<NamedResourceLocation, TextureAtlasSprite> tex_sprites = new HashMap<>();
+    protected ArrayList<NamedResourceLocation> textures;
+    protected ArrayList<ResourceLocation> copy;
+    protected TextureAtlasSprite particle;
+    //
+    protected static final IModelState default_state = new IModelState() {
+        @Override
+        public Optional<TRSRTransformation> apply(Optional<? extends IModelPart> part){
+            return Optional.empty();
+        }
+    };
+
+    public ModelImpl(ResourceLocation rs){
+        modelloc = rs;
+        MODELS.put(rs, this);
+        block = FvtmRegistry.BLOCKS.get(FvtmBlockModelLoader.getInstance().getBlockIdFromResLoc(rs));
+        model = (BlockModel)block.getModel();
+        getTexturesFromModel();
+    }
+
+    public ModelImpl(ModelImpl impl, ImmutableMap<String, String> data) {
+        modelloc = impl.modelloc;
+        block = impl.block;
+        model = impl.model;
+        ambocl = block.getModelData().get("AmbientOcclusion");
+        getTexturesFromModel();
+    }
+
+    private void getTexturesFromModel() {
+        ArrayList<NamedResourceLocation> list = new ArrayList<>();
+        for(IDL idl : block.getDefaultTextures()) list.add(idl.local());
+        if(list.size() == 0){
+            String str = modelloc.toString().replace("models/block", "blocks");
+            if(str.contains(".")) str = str.substring(0, str.indexOf("."));
+            list.add(new NamedResourceLocation("main;" + str));
+        }
+        textures = list;
+        copy = new ArrayList<>(textures);
+    }
+
+    @Override
+    public Collection<ResourceLocation> getDependencies(){
+        return Collections.emptyList();
+    }
+
+    @Override
+    public IModelState getDefaultState(){
+        return default_state;
+    }
+
+    @Override
+    public IBakedModel bake(IModelState state, VertexFormat format, Function<ResourceLocation, TextureAtlasSprite> func) {
+        for(NamedResourceLocation loc : textures){
+            tex_sprites.put(loc, func.apply(loc));
+            if(loc.getName().equals("particle")) particle = tex_sprites.get(loc);
+        }
+        return new BakedModelImpl(modelloc, this, format, model);
+    }
+
+    @Override
+    public Collection<ResourceLocation> getTextures(){
+        return copy;
+    }
+
+    @Override
+    public IModel process(ImmutableMap<String, String> data){
+        return this;
+    }
+
+}

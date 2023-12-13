fd704550d2312d726084ea4364e1a58869596528
Ferdinand Calo <fexcalo@gmail.com>
2023-12-13 21:00:08 +0100

    Update BakedModelImpl.java

--- src/main/java/net/fexcraft/mod/fvtm/render/block/BakedModelImpl.java
+++ src/main/java/net/fexcraft/mod/fvtm/render/block/BakedModelImpl.java
@@ -103,7 +103,7 @@ public class BakedModelImpl implements IBakedModel {
                     String str = prop.getName() + "=" + state.getValue(prop) + "," + group.name;
                     if(model.tg.containsKey(str)){
                         sprite = getTex(root, model.tg.get(str));
-                        break;
+                        if(sprite != null) break;
                     }
                 }
                 if(sprite == null && model.tg.containsKey(group.name)) sprite = getTex(root, model.tg.get(group.name));

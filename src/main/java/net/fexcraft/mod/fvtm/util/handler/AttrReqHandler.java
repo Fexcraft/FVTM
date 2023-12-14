c315543d97837a4d92738440260d465ecd341496
Ferdinand Calo <fexcalo@gmail.com>
2023-12-14 22:28:54 +0100

    Update AttrReqHandler.java

--- src/main/java/net/fexcraft/mod/fvtm/util/handler/AttrReqHandler.java
+++ src/main/java/net/fexcraft/mod/fvtm/util/handler/AttrReqHandler.java
@@ -10,8 +10,10 @@ import net.fexcraft.mod.fvtm.sys.rail.RailEntity;
 import net.fexcraft.mod.fvtm.sys.rail.RailSystem;
 import net.fexcraft.mod.fvtm.sys.rail.vis.RailVehicle;
 import net.fexcraft.mod.fvtm.sys.uni.GenericVehicle;
+import net.fexcraft.mod.fvtm.sys.uni.RootVehicle;
 import net.fexcraft.mod.fvtm.sys.uni.SystemManager;
 import net.fexcraft.mod.fvtm.sys.uni.SystemManager.Systems;
+import net.fexcraft.mod.fvtm.sys.uni.VehicleInstance;
 import net.fexcraft.mod.fvtm.util.Perms;
 import net.fexcraft.mod.fvtm.util.Resources;
 import net.minecraft.entity.player.EntityPlayer;
@@ -24,27 +26,27 @@ public class AttrReqHandler {
 
 	public static void processToggleRequest(World world, EntityPlayerMP player, NBTTagCompound packet){
 		boolean bool = packet.getBoolean("bool");
-		VehicleEntity veh = (VehicleEntity)world.getEntityByID(packet.getInteger("entity"));
+		RootVehicle vehent = (RootVehicle)world.getEntityByID(packet.getInteger("entity"));
 		String attribute = packet.getString("attr");
-		final Attribute<?> attr = veh.getVehicleData().getAttribute(attribute);
+		final Attribute<?> attr = vehent.vehicle.data.getAttribute(attribute);
 		if(!attr.editable && !Perms.EDIT_INTERNAL_ATTRIBUTES.has(player) && (attr.hasPerm() ? !PermissionAPI.hasPermission(player, attr.perm) : true)){
 			Print.chat(player, "No permission. [ED]");
 			return;
 		}
-		if(player.getRidingEntity() != veh.getEntity() && !attr.external &&!Perms.EDIT_INTERNAL_ATTRIBUTES.has(player) && (attr.hasPerm() ? !PermissionAPI.hasPermission(player, attr.perm) : true)){
+		if(player.getRidingEntity() != vehent && !attr.external &&!Perms.EDIT_INTERNAL_ATTRIBUTES.has(player) && (attr.hasPerm() ? !PermissionAPI.hasPermission(player, attr.perm) : true)){
 			Print.chat(player, "No permission. [EX]");
 			return;
 		}
 		Object old = attr.value();
 		toggleAttr(attr, bool, packet, false, null);
 		Object syncval = attr.value();
-		PacketHandler.getInstance().sendToAllAround(new PacketNBTTagCompound(packet), Resources.getTargetPoint(veh.getEntity()));
-		veh.getVehicleData().getScripts().forEach(script -> {
-			script.onAttributeToggle(veh.getEntity(), attr, old, player);
+		PacketHandler.getInstance().sendToAllAround(new PacketNBTTagCompound(packet), Resources.getTargetPoint(vehent));
+		vehent.vehicle.data.getScripts().forEach(script -> {
+			script.onAttributeToggle(vehent, attr, old, player);
 		});
 		if(!attr.sync) return;
-		if(veh.getVehicleType().isRailVehicle()){
-			RailVehicle rail = (RailVehicle)veh;
+		if(vehent.vehicle.type.isRailVehicle()){
+			/*RailVehicle rail = (RailVehicle)vehent;
 			Compound com = rail.rek.ent().getCompound();
 			if(com.isSingular() || !com.isHead(rail.rek.ent()) && !com.isEnd(rail.rek.ent())) return;
 			boolean mirror = attr.valuetype.isBoolean() && attr.group != null && attr.group.contains("mirror_lr");
@@ -67,20 +69,20 @@ public class AttrReqHandler {
 					if(ent == rail.rek.ent()) continue;
 					toggleAttrRailEnt(ent, attribute, bool, compound, true, syncval);
 				}
-			}
+			}*///TODO
 		}
 		else{
-			if(veh.getFrontCoupledEntity() != null) return;
-			VehicleEntity trailer = veh.getRearCoupledEntity();
+			if(vehent.vehicle.front != null) return;
+			VehicleInstance trailer = vehent.vehicle.rear;
 			while(trailer != null){
-				Attribute<?> attr0 = trailer.getVehicleData().getAttribute(attribute);
+				Attribute<?> attr0 = trailer.data.getAttribute(attribute);
 				if(attr0 != null){
 					NBTTagCompound compound = packet.copy();
 					toggleAttr(attr0, bool, compound, true, syncval);
-					compound.setInteger("entity", trailer.getEntity().getEntityId());
-					PacketHandler.getInstance().sendToAllAround(new PacketNBTTagCompound(compound), Resources.getTargetPoint(trailer.getEntity()));
+					compound.setInteger("entity", trailer.entity.getId());
+					PacketHandler.getInstance().sendToAllAround(new PacketNBTTagCompound(compound), Resources.getTargetPoint(trailer.entity.local()));
 				}
-				trailer = trailer.getRearCoupledEntity();
+				trailer = trailer.rear;
 			}
 		}
 	}
@@ -121,13 +123,13 @@ public class AttrReqHandler {
 
 	public static void processUpdateRequest(World world, EntityPlayerMP player, NBTTagCompound packet){
 		boolean reset = packet.hasKey("reset") && packet.getBoolean("reset");
-		VehicleEntity veh = (VehicleEntity)player.world.getEntityByID(packet.getInteger("entity"));
-		Attribute<?> attr = veh.getVehicleData().getAttribute(packet.getString("attr"));
+		RootVehicle vehent = (RootVehicle)player.world.getEntityByID(packet.getInteger("entity"));
+		Attribute<?> attr = vehent.vehicle.data.getAttribute(packet.getString("attr"));
 		if(!attr.editable && !Perms.EDIT_INTERNAL_ATTRIBUTES.has(player) && (attr.hasPerm() ? !PermissionAPI.hasPermission(player, attr.perm) : true)){
 			Print.chat(player, "No permission. [ED]");
 			return;
 		}
-		if(player.getRidingEntity() != veh.getEntity() && !attr.external &&!Perms.EDIT_INTERNAL_ATTRIBUTES.has(player) && (attr.hasPerm() ? !PermissionAPI.hasPermission(player, attr.perm) : true)){
+		if(player.getRidingEntity() != vehent && !attr.external &&!Perms.EDIT_INTERNAL_ATTRIBUTES.has(player) && (attr.hasPerm() ? !PermissionAPI.hasPermission(player, attr.perm) : true)){
 			Print.chat(player, "No permission. [EX]");
 			return;
 		}
@@ -137,20 +139,20 @@ public class AttrReqHandler {
 		else{
 			attr.set(attr.parse(packet.getString("value")));
 		}
-		((GenericVehicle)veh).sendAttributeUpdate(attr);
+		vehent.sendAttributeUpdate(attr);
 	}
 
 	public static void processToggleResponse(World world, EntityPlayer player, NBTTagCompound packet){
 		boolean bool = packet.getBoolean("bool");
 		String attribute = packet.getString("attr");
 		Attribute<?> attr = null;
-		VehicleEntity veh = (VehicleEntity)player.world.getEntityByID(packet.getInteger("entity"));
-		if(veh == null && packet.hasKey("railid")){
+		RootVehicle vehent = (RootVehicle)player.world.getEntityByID(packet.getInteger("entity"));
+		if(vehent == null && packet.hasKey("railid")){
 			RailEntity ent = SystemManager.get(Systems.RAIL, player.world, RailSystem.class).getEntity(packet.getLong("railid"), false);
 			attr = ent.vehdata.getAttribute(attribute);
 		}
-		else if(veh != null){
-			attr = veh.getVehicleData().getAttribute(attribute);
+		else if(vehent != null){
+			attr = vehent.vehicle.data.getAttribute(attribute);
 		}
 		else{
 			Print.debug("Received packet for entity not found on client side!");
@@ -169,8 +171,8 @@ public class AttrReqHandler {
 	}
 
 	public static void processUpdateResponse(World world, EntityPlayer player, NBTTagCompound packet){
-		VehicleEntity veh = (VehicleEntity)player.world.getEntityByID(packet.getInteger("entity"));
-		Attribute<?> attr = veh.getVehicleData().getAttribute(packet.getString("attr"));
+		RootVehicle vehent = (RootVehicle)player.world.getEntityByID(packet.getInteger("entity"));
+		Attribute<?> attr = vehent.vehicle.data.getAttribute(packet.getString("attr"));
 		if(attr.valuetype.isTristate()){
 			if(packet.hasKey("reset") && packet.getBoolean("reset")){
 				attr.set(null);

e76edeb93cd9e87635acacab17d11a4d0ee5e504
Ferdinand Calo <fexcalo@gmail.com>
2024-01-02 23:54:32 +0100

    Update Cloth.java

--- src/main/java/net/fexcraft/mod/fvtm/data/Cloth.java
+++ src/main/java/net/fexcraft/mod/fvtm/data/Cloth.java
@@ -1,31 +1,20 @@
 package net.fexcraft.mod.fvtm.data;
 
-import com.google.gson.JsonObject;
 import net.fexcraft.app.json.JsonMap;
-import net.fexcraft.lib.common.json.JsonUtil;
-import net.fexcraft.lib.mc.registry.NamedResourceLocation;
-import net.fexcraft.lib.mc.utils.Static;
 import net.fexcraft.mod.fvtm.FvtmRegistry;
 import net.fexcraft.mod.fvtm.FvtmResources;
 import net.fexcraft.mod.fvtm.data.root.ItemTextureable;
-import net.fexcraft.mod.fvtm.data.root.TypeCore;
 import net.fexcraft.mod.fvtm.data.root.WithItem;
 import net.fexcraft.mod.fvtm.item.ClothItem;
 import net.fexcraft.mod.fvtm.model.ClothModel;
 import net.fexcraft.mod.fvtm.model.Model;
 import net.fexcraft.mod.fvtm.model.ModelData;
-import net.fexcraft.mod.fvtm.model.PartModel;
 import net.fexcraft.mod.fvtm.util.ContentConfigUtil;
-import net.fexcraft.mod.fvtm.util.DataUtil;
-import net.fexcraft.mod.fvtm.util.Resources;
 import net.fexcraft.mod.uni.EnvInfo;
 import net.fexcraft.mod.uni.IDL;
 import net.fexcraft.mod.uni.IDLManager;
 import net.fexcraft.mod.uni.item.ClothMaterial;
 import net.minecraft.inventory.EntityEquipmentSlot;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.ResourceLocation;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -50,7 +39,7 @@ public class Cloth extends Content<Cloth> implements ItemTextureable, WithItem {
 		if((id = ContentConfigUtil.getID(pack, map)) == null) return null;
 		//
 		name = map.getString("Name", "Unnamed Clothing");
-		description = description = ContentConfigUtil.getStringList(map, "Description");
+		description = ContentConfigUtil.getStringList(map, "Description");
 		max_health = (short)map.getInteger("MaxItemDamage", 0);
 		eq_slot = EntityEquipmentSlot.fromString(map.getString("EquipmentSlot", "head").toLowerCase());
 		if(map.has("ClothMaterial")){

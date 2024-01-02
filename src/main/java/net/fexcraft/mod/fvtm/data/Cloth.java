b1e0d9737fa8a14bacfe77328d5545e4c686353b
Ferdinand Calo <fexcalo@gmail.com>
2024-01-02 23:05:44 +0100

    Update Cloth.java

--- src/main/java/net/fexcraft/mod/fvtm/data/Cloth.java
+++ src/main/java/net/fexcraft/mod/fvtm/data/Cloth.java
@@ -1,6 +1,7 @@
 package net.fexcraft.mod.fvtm.data;
 
 import com.google.gson.JsonObject;
+import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.lib.common.json.JsonUtil;
 import net.fexcraft.lib.mc.registry.NamedResourceLocation;
 import net.fexcraft.lib.mc.utils.Static;
@@ -8,11 +9,16 @@ import net.fexcraft.mod.fvtm.FvtmRegistry;
 import net.fexcraft.mod.fvtm.FvtmResources;
 import net.fexcraft.mod.fvtm.data.root.ItemTextureable;
 import net.fexcraft.mod.fvtm.data.root.TypeCore;
+import net.fexcraft.mod.fvtm.data.root.WithItem;
 import net.fexcraft.mod.fvtm.item.ClothItem;
 import net.fexcraft.mod.fvtm.model.ClothModel;
+import net.fexcraft.mod.fvtm.model.Model;
 import net.fexcraft.mod.fvtm.model.ModelData;
+import net.fexcraft.mod.fvtm.model.PartModel;
+import net.fexcraft.mod.fvtm.util.ContentConfigUtil;
 import net.fexcraft.mod.fvtm.util.DataUtil;
 import net.fexcraft.mod.fvtm.util.Resources;
+import net.fexcraft.mod.uni.EnvInfo;
 import net.fexcraft.mod.uni.IDL;
 import net.fexcraft.mod.uni.IDLManager;
 import net.fexcraft.mod.uni.item.ClothMaterial;
@@ -24,60 +30,49 @@ import net.minecraft.util.ResourceLocation;
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
-public class Cloth extends TypeCore<Cloth> implements ItemTextureable {
+public class Cloth extends Content<Cloth> implements ItemTextureable, WithItem {
 	
-	protected short maxHealth;
+	protected short max_health;
 	protected ClothItem item;
 	protected String ctab, modelid;
 	protected EntityEquipmentSlot eq_slot;
 	protected ClothMaterial material;
 	protected ClothModel model;
 	protected ModelData modeldata;
-	protected ResourceLocation texture;
+	protected IDL texture;
 	protected IDL itemloc;
 	
 	public Cloth(){}
 
 	@Override
-	public ResourceLocation getRegistryName(){
-		return this.registryname;
-	}
-
-	@Override
-	public Cloth parse(JsonObject obj){
-		this.pack = DataUtil.getAddon(obj);
-		if(pack == null) return null;
-		this.registryname = DataUtil.getRegistryName(pack, obj);
-		if(registryname == null) return null;
+	public Cloth parse(JsonMap map){
+		if((pack = ContentConfigUtil.getAddon(map)) == null) return null;
+		if((id = ContentConfigUtil.getID(pack, map)) == null) return null;
 		//
-		this.name = JsonUtil.getIfExists(obj, "Name", "Unnamed Clothing");
-		this.description = DataUtil.getStringArray(obj, "Description", true, true);
-		this.maxHealth = JsonUtil.getIfExists(obj, "MaxItemDamage", 0).shortValue();
-		this.eq_slot = EntityEquipmentSlot.fromString(JsonUtil.getIfExists(obj, "EquipmentSlot", "head").toLowerCase());
-		this.material = parseMaterial(obj);
-		//
-		this.texture = new NamedResourceLocation(JsonUtil.getIfExists(obj, "Texture", Resources.NULL_TEXTURE.toString()));
-		if(Static.isClient()){
-			modelid = obj.has("Model") ? obj.get("Model").getAsString() : null;
-			modeldata = new ModelData();//TODO
+		name = map.getString("Name", "Unnamed Clothing");
+		description = description = ContentConfigUtil.getStringList(map, "Description");
+		max_health = (short)map.getInteger("MaxItemDamage", 0);
+		eq_slot = EntityEquipmentSlot.fromString(map.getString("EquipmentSlot", "head").toLowerCase());
+		if(map.has("ClothMaterial")){
+			String mat = map.get("ClothMaterial").string_value().toLowerCase();
+			material = ClothMaterial.get(mat.contains(":") ? mat : getAddon().getID().id() + ":" + mat);
 		}
-        this.ctab = JsonUtil.getIfExists(obj, "CreativeTab", "default");
-        this.itemloc = IDLManager.getIDLCached(DataUtil.getItemTexture(registryname, getDataType(), obj).toString());
-		this.item = new ClothItem(this);
+		else material = ClothMaterial.get(FvtmRegistry.NONE_CLOTH_MAT);
+		//
+		texture = IDLManager.getIDLNamed(map.get("Texture", FvtmResources.NULL_TEXTURE.colon()));
+		if(EnvInfo.CLIENT){
+			modelid = map.getString("Model", null);
+			modeldata = new ModelData(map);
+		}
+        ctab = map.getString("CreativeTab", "default");
+        itemloc = ContentConfigUtil.getItemTexture(id, getContentType(), map);
+		item = new ClothItem(this);
 		return this;
 	}
 
-	private ClothMaterial parseMaterial(JsonObject obj){
-		if(obj.has("ClothMaterial")){
-			String mat = obj.get("ClothMaterial").getAsString().toLowerCase();
-			return ClothMaterial.get(mat.contains(":") ? mat : getAddon().getID().id() + ":" + mat);
-		}
-		return ClothMaterial.get(FvtmRegistry.NONE_CLOTH_MAT);
-	}
-
 	@Override
-	public ContentType getDataType(){
-		return ContentType.MATERIAL;
+	public ContentType getContentType(){
+		return ContentType.CLOTH;
 	}
 
 	@Override
@@ -88,21 +83,17 @@ public class Cloth extends TypeCore<Cloth> implements ItemTextureable {
 	public ClothItem getClothItem(){
 		return item;
 	}
-	
-	@Override
-	public Item getItem(){
-		return item;
-	}
-
-	public ItemStack newItemStack(){
-		return new ItemStack(item, 1);
-	}
 
 	public int getMaxDamage(){
-		return this.maxHealth;
+		return max_health;
 	}
 
-	//@Override
+	@Override
+	public String getItemContainer(){
+		return null;
+	}
+
+	@Override
 	public String getCreativeTab(){
 		return ctab;
 	}
@@ -115,16 +106,21 @@ public class Cloth extends TypeCore<Cloth> implements ItemTextureable {
 		return material;
 	}
 
-	public ClothModel getModel(){
+	@Override
+	public Model getModel(){
+		return model;
+	}
+
+	public ClothModel getClothModel(){
 		return model;
 	}
 	
 	@Override
 	public void loadModel(){
-		this.model = (ClothModel)FvtmResources.getModel(modelid, modeldata, ClothModel.class);
+		model = (ClothModel)FvtmResources.getModel(modelid, modeldata, ClothModel.class);
 	}
 	
-	public ResourceLocation getTexture(){
+	public IDL getTexture(){
 		return texture;
 	}
 
@@ -132,5 +128,4 @@ public class Cloth extends TypeCore<Cloth> implements ItemTextureable {
 	public IDL getItemTexture(){
 		return itemloc;
 	}
-
 }

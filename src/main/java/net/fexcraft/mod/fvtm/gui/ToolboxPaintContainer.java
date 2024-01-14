2dd8d719c1fd2fbadd6ff9b8cea8161c47f38abc
Ferdinand Calo <fexcalo@gmail.com>
2024-01-14 22:01:48 +0100

    Create ToolboxPaintContainer.java

---
+++ src/main/java/net/fexcraft/mod/fvtm/gui/ToolboxPaintContainer.java
@@ -0,0 +1,52 @@
+package net.fexcraft.mod.fvtm.gui;
+
+import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.mod.fvtm.data.DecorationData;
+import net.fexcraft.mod.fvtm.data.root.Colorable;
+import net.fexcraft.mod.fvtm.entity.Decoration;
+import net.fexcraft.mod.fvtm.sys.uni.RootVehicle;
+import net.fexcraft.mod.fvtm.ui.DecoEditor;
+import net.fexcraft.mod.uni.Pos;
+import net.fexcraft.mod.uni.tag.TagCW;
+import net.fexcraft.mod.uni.ui.ContainerInterface;
+import net.fexcraft.mod.uni.world.WrapperHolder;
+import net.minecraft.entity.player.EntityPlayer;
+
+import static net.fexcraft.mod.fvtm.FvtmRegistry.DECORATIONS;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class ToolboxPaintContainer extends ContainerInterface {
+
+	protected Colorable colorable;
+	protected RootVehicle vehicle;
+
+	public ToolboxPaintContainer(JsonMap map, EntityPlayer player, int entid){
+		super(map, WrapperHolder.getEntity(player));
+		vehicle = (RootVehicle)player.world.getEntityByID(entid);
+		colorable = vehicle.vehicle.data;
+	}
+
+	@Override
+	public Object get(String key, Object... objs){
+		switch(key){
+			//
+		}
+		return null;
+	}
+
+	@Override
+	public void packet(TagCW com, boolean client){
+		String task = com.getString("task");
+		switch(task){
+			//
+		}
+	}
+
+	@Override
+	public void onClosed(){
+		//
+	}
+
+}

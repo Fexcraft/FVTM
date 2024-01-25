80b98ad69eea559ec41c6296aa99789b5ea77e02
Ferdinand Calo <fexcalo@gmail.com>
2024-01-25 21:21:31 +0100

    Update RenderCacheHandler.java

--- src/main/java/net/fexcraft/mod/fvtm/util/caps/RenderCacheHandler.java
+++ src/main/java/net/fexcraft/mod/fvtm/util/caps/RenderCacheHandler.java
@@ -3,9 +3,13 @@ package net.fexcraft.mod.fvtm.util.caps;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 import net.fexcraft.mod.fvtm.data.Capabilities;
+import net.fexcraft.mod.fvtm.model.DefaultModel;
+import net.fexcraft.mod.fvtm.model.ModelRenderData;
+import net.fexcraft.mod.fvtm.model.Program;
 import net.fexcraft.mod.fvtm.model.RenderCache;
 import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
@@ -45,17 +49,19 @@ public class RenderCacheHandler implements ICapabilitySerializable<NBTBase>{
 		@Override
 		public NBTBase writeNBT(Capability<RenderCache> capability, RenderCache instance, EnumFacing side){
 			NBTTagCompound compound = new NBTTagCompound();
-			instance.fltmap().forEach((key, value) -> {
-				compound.setFloat(key, value);
-			}); return compound;
+			instance.map().forEach((key, value) -> {
+				//TODO compound.setFloat(key, value);
+			});
+			return compound;
 		}
 
 		@Override
 		public void readNBT(Capability<RenderCache> capability, RenderCache instance, EnumFacing side, NBTBase nbt){
 			if(nbt == null || nbt instanceof NBTTagCompound == false) return;
-			NBTTagCompound compound = (NBTTagCompound)nbt; if(compound.isEmpty()) return;
+			NBTTagCompound compound = (NBTTagCompound)nbt;
+			if(compound.isEmpty()) return;
 			for(String str : compound.getKeySet()){
-				instance.set(str, compound.getFloat(str));
+				//TODO instance.set(str, compound.getFloat(str));
 			}
 		}
 		
@@ -71,59 +77,34 @@ public class RenderCacheHandler implements ICapabilitySerializable<NBTBase>{
 	}
 	
 	public static class Instance implements RenderCache {
-		
-		protected LinkedHashMap<String, Float> floats = new LinkedHashMap<>();
-		protected LinkedHashMap<String, Object> objects = new LinkedHashMap<>();
+
+		protected LinkedHashMap<Program, Object> objects = new LinkedHashMap<>();
 
 		public Instance(){}
 
 		@Override
-		public Map<String, Float> fltmap(){
-			return floats;
-		}
-
-		@Override
-		public Map<String, Object> map(){
+		public Map<Program, Object> map(){
 			return objects;
 		}
 
 		@Override
-		public Float getFlt(String id){
-			return floats.get(id);
+		public <V> V get(Program prog){
+			return (V)objects.get(prog);
 		}
 
 		@Override
-		public Float getFlt(String id, Float def){
-			Float val = floats.get(id);
-			return val == null ? def : val;
-		}
-
-		@Override
-		public Float setFlt(String id, Float value){
-			if(id == null) return 0f;
-			if(value == null) floats.remove(id);
-			return floats.put(id, value);
-		}
-
-		@Override
-		public <V> V get(String id){
-			return (V)objects.get(id);
-		}
-
-		@Override
-		public <V> V get(String id, Supplier<V> def){
-			V obj = (V)objects.get(id);
+		public <V> V get(Program prog, Function<ModelRenderData, V> def){
+			V obj = (V)objects.get(prog);
 			if(obj == null){
-				objects.put(id, obj = def.get());
+				objects.put(prog, obj = def.apply(DefaultModel.RENDERDATA));
 			}
 			return obj;
 		}
 
 		@Override
-		public <V> V set(String id, V value){
-			if(id == null) return value;
-			if(value == null) objects.remove(id);
-			return (V)objects.put(id, value);
+		public <V> V set(Program prog, V value){
+			if(value == null) objects.remove(prog);
+			return (V)objects.put(prog, value);
 		}
 		
 	}

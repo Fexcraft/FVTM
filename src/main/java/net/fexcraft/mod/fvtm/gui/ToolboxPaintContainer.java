7b41418a687542ac6fa31e010ceb4331323a15e9
Ferdinand Calo <fexcalo@gmail.com>
2024-01-30 18:25:06 +0100

    Update ToolboxPaintContainer.java

--- src/main/java/net/fexcraft/mod/fvtm/gui/ToolboxPaintContainer.java
+++ src/main/java/net/fexcraft/mod/fvtm/gui/ToolboxPaintContainer.java
@@ -1,6 +1,7 @@
 package net.fexcraft.mod.fvtm.gui;
 
 import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.lib.common.math.V3I;
 import net.fexcraft.mod.fvtm.data.DecorationData;
 import net.fexcraft.mod.fvtm.data.root.Colorable;
 import net.fexcraft.mod.fvtm.entity.Decoration;
@@ -26,7 +27,7 @@ public class ToolboxPaintContainer extends ContainerInterface {
 	protected RootVehicle vehicle;
 
 	public ToolboxPaintContainer(JsonMap map, EntityPlayer player, int entid){
-		super(map, WrapperHolder.getEntity(player));
+		super(map, WrapperHolder.getEntity(player), new V3I(entid, 0, 0));
 		vehicle = (RootVehicle)player.world.getEntityByID(entid);
 		colorable = vehicle.vehicle.data;
 	}

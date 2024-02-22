e6a67360347a256a3421b59af9eb79df461f531f
Ferdinand Calo <fexcalo@gmail.com>
2024-02-22 21:11:13 +0100

    Delete BakedModelLoader.java

--- src/main/java/net/fexcraft/mod/fvtm/util/BakedModelLoader.java
+++
@@ -1,9 +0,0 @@
-package net.fexcraft.mod.fvtm.util;
-
-public class BakedModelLoader {
-	
-	public static void register(){
-		net.minecraftforge.client.model.ModelLoaderRegistry.registerLoader(net.fexcraft.mod.fvtm.render.block.FvtmBlockModelLoader.getInstance());
-	}
-
-}

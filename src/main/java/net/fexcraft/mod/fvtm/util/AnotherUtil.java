bd07fac5ad78d218a9e82f64dc4ed99dd44a7b14
Ferdinand Calo <fexcalo@gmail.com>
2024-02-23 19:24:22 +0100

    Update AnotherUtil.java

--- src/main/java/net/fexcraft/mod/fvtm/util/AnotherUtil.java
+++ src/main/java/net/fexcraft/mod/fvtm/util/AnotherUtil.java
@@ -1,7 +1,13 @@
 package net.fexcraft.mod.fvtm.util;
 
+import net.fexcraft.lib.common.math.V3D;
+import net.fexcraft.mod.fvtm.data.part.PartData;
+import net.fexcraft.mod.fvtm.data.vehicle.SwivelPoint;
+import net.fexcraft.mod.fvtm.data.vehicle.VehicleData;
+import net.fexcraft.mod.fvtm.render.EffectRenderer;
 import net.fexcraft.mod.uni.Pos;
 import net.minecraft.util.math.Vec3d;
+import org.lwjgl.opengl.GL11;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -13,8 +19,35 @@ public class AnotherUtil {
 		return new Vec3d(pos.x16, pos.y16, pos.z16);
 	}
 
-	private static String prefix(String prefix){
-		return prefix == null ? "" : prefix + "_";
+	public static void translateAndRotatePartOnSwivelPoint(VehicleData vehicle, PartData data, float ticks){
+		SwivelPoint point = vehicle.getRotationPoint(data.getSwivelPointInstalledOn());
+		V3D pos = data.getInstalledPos();
+		V3D temp0 = point.getRelativeVector(pos);
+		V3D temp1 = point.getPrevRelativeVector(pos);
+		GL11.glRotated(-180f, 0.0F, 1.0F, 0.0F);
+		GL11.glRotated(-180f, 0.0F, 0.0F, 1.0F);
+		GL11.glTranslated(temp1.x + (temp0.x - temp1.x) * ticks, temp1.y + (temp0.y - temp1.y) * ticks, temp1.z + (temp0.z - temp1.z) * ticks);
+		GL11.glRotated(180f, 0.0F, 1.0F, 0.0F);
+		GL11.glRotated(180f, 0.0F, 0.0F, 1.0F);
+		V3D rot = EffectRenderer.getRotations(point, ticks);
+		GL11.glRotated(rot.z, 1.0F, 0.0F, 0.0F);
+		GL11.glRotated(rot.y, 0.0F, 0.0F, 1.0F);
+		GL11.glRotated(rot.x, 0.0F, 1.0F, 0.0F);
+		data.getInstalledRot().rotate112();
+	}
+
+	public static void translateAndRotatePartOnSwivelPointFast(VehicleData vehicle, PartData data){
+		SwivelPoint point = vehicle.getRotationPoint(data.getSwivelPointInstalledOn());
+		V3D pos = point.getRelativeVector(data.getInstalledPos());
+		GL11.glRotated(-180f, 0.0F, 1.0F, 0.0F);
+		GL11.glRotated(-180f, 0.0F, 0.0F, 1.0F);
+		GL11.glTranslated(pos.x, pos.y, pos.z);
+		GL11.glRotated(180f, 0.0F, 1.0F, 0.0F);
+		GL11.glRotated(180f, 0.0F, 0.0F, 1.0F);
+		GL11.glRotatef(point.getPivot().deg_roll(), 1.0F, 0.0F, 0.0F);
+		GL11.glRotatef(point.getPivot().deg_pitch(), 0.0F, 0.0F, 1.0F);
+		GL11.glRotatef(point.getPivot().deg_yaw(), 0.0F, 1.0F, 0.0F);
+		data.getInstalledRot().rotate112();
 	}
 
 }

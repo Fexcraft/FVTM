92ff79cac8268206d660c20a84aebe45225fb640
Ferdinand Calo <fexcalo@gmail.com>
2024-02-25 09:51:44 +0100

    Update PacketsImpl.java

--- src/main/java/net/fexcraft/mod/fvtm/packet/PacketsImpl.java
+++ src/main/java/net/fexcraft/mod/fvtm/packet/PacketsImpl.java
@@ -1,5 +1,6 @@
 package net.fexcraft.mod.fvtm.packet;
 
+import io.netty.buffer.ByteBuf;
 import net.fexcraft.lib.common.math.V3D;
 import net.fexcraft.lib.common.math.V3I;
 import net.fexcraft.lib.mc.network.PacketHandler;
@@ -9,6 +10,7 @@ import net.fexcraft.mod.fvtm.FvtmLogger;
 import net.fexcraft.mod.fvtm.block.generated.BlockTileEntity;
 import net.fexcraft.mod.fvtm.data.block.BlockData;
 import net.fexcraft.mod.fvtm.data.block.BlockFunction;
+import net.fexcraft.mod.fvtm.sys.uni.Passenger;
 import net.fexcraft.mod.fvtm.sys.uni.SeatInstance;
 import net.fexcraft.mod.fvtm.sys.uni.VehicleInstance;
 import net.fexcraft.mod.uni.EnvInfo;
@@ -17,9 +19,11 @@ import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.ui.UniCon;
 import net.fexcraft.mod.uni.world.WorldW;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.common.network.ByteBufUtils;
 import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.NetworkRegistry.TargetPoint;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
@@ -42,6 +46,10 @@ public class PacketsImpl extends Packets {
 		FvtmLogger.LOGGER.log("Starting Packet Handler initialization.");
 		//
 		PACKETS.put(Packet_VehKeyPress.class, Packets12.PI_VehKeyPress.class);
+		PACKETS.put(Packet_VehKeyPressState.class, Packets12.PI_VehKeyPressState.class);
+		PACKETS.put(Packet_SeatUpdate.class, Packets12.PI_SeatUpdate.class);
+		PACKETS.put(Packet_SPUpdate.class, Packets12.PI_SPUpdate.class);
+		PACKETS.put(Packet_TagListener.class, Packets12.PI_TagListener.class);
 		//
 		instance.registerMessage(Packets12.HI_SeatUpdate_C.class, Packets12.PI_SeatUpdate.class, 0, Side.CLIENT);
 		instance.registerMessage(Packets12.HI_SeatUpdate_S.class, Packets12.PI_SeatUpdate.class, 1, Side.SERVER);
@@ -52,24 +60,33 @@ public class PacketsImpl extends Packets {
 		instance.registerMessage(Packets12.HI_SPUpdate_S.class, Packets12.PI_SPUpdate.class, 6, Side.SERVER);
 		instance.registerMessage(Packets12.HI_VehKeyPressState_C.class, Packets12.PI_VehKeyPressState.class, 7, Side.CLIENT);
 		instance.registerMessage(Packets12.HI_VehKeyPressState_S.class, Packets12.PI_VehKeyPressState.class, 8, Side.SERVER);
-		//
-		instance.registerMessage(PKTH_UT.Client.class, PKT_UT.class, 10, Side.CLIENT);
-		instance.registerMessage(PKTH_UT.Server.class, PKT_UT.class, 10, Side.SERVER);
+		instance.registerMessage(Packets12.HI_TagListener_C.class, Packets12.PI_TagListener.class, 9, Side.CLIENT);
+		instance.registerMessage(Packets12.HI_TagListener_S.class, Packets12.PI_TagListener.class, 10, Side.SERVER);
 		//
 		FvtmLogger.LOGGER.log("Done initialising Packet Handler.");
 		FvtmLogger.LOGGER.log("Starting Packet Listener registration.");
-		PKTH_UT.LIS_SERVER.put("ui", (com, player) -> {
-			((UniCon)player.openContainer).container().packet(new TagCWI(com), false);
+		LIS_SERVER.put("ui", (tag, player) -> {
+			((UniCon)((EntityPlayer)player.local()).openContainer).container().packet(tag, false);
 		});
-		PKTH_UT.LIS_SERVER.put("vehicle", (com, player) -> {
-
+		LIS_SERVER.put("vehicle", (com, player) -> {
+			//
 		});
 		if(EnvInfo.CLIENT){
-			PKTH_UT.LIS_CLIENT.put("ui", (com, player) -> {
-				((UniCon)player.openContainer).container().packet(new TagCWI(com), true);
+			LIS_CLIENT.put("ui", (tag, player) -> {
+				((UniCon)((EntityPlayer)player.local()).openContainer).container().packet(tag, true);
 			});
 		}
 		FvtmLogger.LOGGER.log("Completed Packet Listener registration.");
+	}
+
+	@Override
+	public void writeTag(ByteBuf buffer, TagCW tag){
+		ByteBufUtils.writeTag(buffer, tag.local());
+	}
+
+	@Override
+	public TagCW readTag(ByteBuf buffer){
+		return TagCW.wrap(ByteBufUtils.readTag(buffer));
 	}
 
 	@Override
@@ -137,6 +154,19 @@ public class PacketsImpl extends Packets {
 	public void sendToAll0(Class<? extends PacketBase> packet, Object... data){
 		try{
 			instance.sendToAll((IMessage)PACKETS.get(packet).newInstance().fill(data));
+		}
+		catch(IllegalAccessException e){
+			throw new RuntimeException(e);
+		}
+		catch(InstantiationException e){
+			throw new RuntimeException(e);
+		}
+	}
+
+	@Override
+	public void sendTo0(Class<? extends PacketBase> packet, Passenger to, Object... data){
+		try{
+			instance.sendTo((IMessage)PACKETS.get(packet).newInstance().fill(data), to.local());
 		}
 		catch(IllegalAccessException e){
 			throw new RuntimeException(e);

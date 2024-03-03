423b2a490faa684dd9ec6531a79f160f852a2bd3
Ferdinand Calo <fexcalo@gmail.com>
2024-03-03 12:37:36 +0100

    Update InvHandlerItemImpl.java

--- src/main/java/net/fexcraft/mod/fvtm/data/impl/InvHandlerItemImpl.java
+++ src/main/java/net/fexcraft/mod/fvtm/data/impl/InvHandlerItemImpl.java
@@ -1,26 +1,9 @@
 package net.fexcraft.mod.fvtm.data.impl;
 
-import net.fexcraft.lib.common.math.V3I;
 import net.fexcraft.mod.fvtm.data.inv.InvHandlerItem;
 import net.fexcraft.mod.fvtm.data.inv.ItemStackHandler;
-import net.fexcraft.mod.fvtm.util.handler.ContentFilter;
-import net.fexcraft.mod.uni.impl.TagLWI;
 import net.fexcraft.mod.uni.item.StackWrapper;
-import net.fexcraft.mod.uni.tag.TagCW;
-import net.fexcraft.mod.uni.world.EntityW;
-import net.fexcraft.mod.uni.world.WorldW;
-import net.minecraft.entity.Entity;
-import net.minecraft.entity.item.EntityItem;
-import net.minecraft.inventory.ItemStackHelper;
-import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTBase;
-import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.nbt.NBTTagList;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import net.minecraftforge.items.ItemHandlerHelper;
-
-import java.util.ArrayList;
 
 public class InvHandlerItemImpl extends InvHandlerItem {
 

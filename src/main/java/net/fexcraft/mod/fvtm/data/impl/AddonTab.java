447483c94a2e36a4d8f4ba091bf8caa7e74855d7
Ferdinand Calo <fexcalo@gmail.com>
2024-03-03 18:34:27 +0100

    Update AddonTab.java

--- src/main/java/net/fexcraft/mod/fvtm/data/impl/AddonTab.java
+++ src/main/java/net/fexcraft/mod/fvtm/data/impl/AddonTab.java
@@ -17,41 +17,51 @@ import net.minecraftforge.fml.relauncher.SideOnly;
  */
 public class AddonTab extends CreativeTab implements CTab {
 
-    private NonNullList<ItemStack> list;
-    private int icon, sec;
+	private NonNullList<ItemStack> list;
+	private String defitem;
+	private int icon, sec;
 	private Addon addon;
 
 	@Deprecated
-	public AddonTab(Addon addon, String string){
+	public AddonTab(Addon addon, String string, String icon){
 		super(addon.getID().id() + (string.equals(DEFAULT) ? "" : "." + string));
+		this.defitem = icon;
 		this.addon = addon;
 	}
 
-    @Override
-    public ItemStack createIcon(){
-        return null;
-    }
+	@Override
+	public ItemStack createIcon(){
+		return null;
+	}
 
-    @SideOnly(Side.CLIENT)
-    public String getTranslatedTabLabel(){
-        return addon.getName();
-    }
+	@SideOnly(Side.CLIENT)
+	public String getTranslatedTabLabel(){
+		return addon.getName();
+	}
 
-    public Addon getAddon(){
-        return addon;
-    }
+	public Addon getAddon(){
+		return addon;
+	}
 
-    @Override
-    public ItemStack getIcon(){
-        if(list == null){ list = NonNullList.create(); this.displayAllRelevantItems(list); }
-        if(sec != Time.getSecond()){
-        	sec = Time.getSecond(); this.icon++; if(icon >= list.size()){ icon = 0; }
-        } return icon >= list.size() ? ItemStack.EMPTY : list.get(icon);
-    }
-    
-    public static final AddonTab getTab(IDL addonid){
-    	return (AddonTab)TABS.get(addonid);
-    }
+	@Override
+	public ItemStack getIcon(){
+		if(list == null){
+			list = NonNullList.create();
+			this.displayAllRelevantItems(list);
+		}
+		if(sec != Time.getSecond()){
+			sec = Time.getSecond();
+			this.icon++;
+			if(icon >= list.size()){
+				icon = 0;
+			}
+		}
+		return icon >= list.size() ? ItemStack.EMPTY : list.get(icon);
+	}
+
+	public static final AddonTab getTab(IDL addonid){
+		return (AddonTab)TABS.get(addonid);
+	}
 
 	public static Collection<CTab> getTabs(){
 		return TABS.values();

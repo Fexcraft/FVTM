c3252ab326ddd23bb01c68b3ea598613ad0cc243
Ferdinand Calo <fexcalo@gmail.com>
2024-03-14 18:26:28 +0100

    Update RenderCacheHandler.java

--- src/main/java/net/fexcraft/mod/fvtm/util/caps/RenderCacheHandler.java
+++ src/main/java/net/fexcraft/mod/fvtm/util/caps/RenderCacheHandler.java
@@ -1,16 +1,8 @@
 package net.fexcraft.mod.fvtm.util.caps;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.function.Function;
-import java.util.function.Supplier;
-
 import net.fexcraft.mod.fvtm.data.Capabilities;
-import net.fexcraft.mod.fvtm.model.DefaultModel;
-import net.fexcraft.mod.fvtm.model.ModelRenderData;
-import net.fexcraft.mod.fvtm.model.Program;
 import net.fexcraft.mod.fvtm.model.RenderCache;
+import net.fexcraft.mod.fvtm.model.RenderCacheI;
 import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumFacing;
@@ -22,7 +14,7 @@ public class RenderCacheHandler implements ICapabilitySerializable<NBTBase>{
 	
 	private RenderCache instance;
 	
-	public RenderCacheHandler(){ instance = new Instance(); }
+	public RenderCacheHandler(){ instance = new RenderCacheI(); }
 
 	@Override
 	public boolean hasCapability(Capability<?> capability, EnumFacing facing){
@@ -71,43 +63,7 @@ public class RenderCacheHandler implements ICapabilitySerializable<NBTBase>{
 
 		@Override
 		public RenderCache call() throws Exception {
-			return new Instance();
-		}
-		
-	}
-	
-	public static class Instance implements RenderCache {
-
-		protected LinkedHashMap<Program, Object> objects = new LinkedHashMap<>();
-
-		public Instance(){}
-
-		@Override
-		public Map<Program, Object> map(){
-			return objects;
-		}
-
-		@Override
-		public <V> V get(Program prog){
-			return (V)objects.get(prog);
-		}
-
-		@Override
-		public <V> V get(Program prog, Function<ModelRenderData, V> def){
-			V obj = (V)objects.get(prog);
-			if(obj == null){
-				objects.put(prog, obj = def.apply(DefaultModel.RENDERDATA));
-			}
-			return obj;
-		}
-
-		@Override
-		public <V> V set(Program prog, V value){
-			if(value == null){
-				objects.remove(prog);
-				return null;
-			}
-			return (V)objects.put(prog, value);
+			return new RenderCacheI();
 		}
 		
 	}

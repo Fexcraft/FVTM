c922171ada5cf65a0b77f6e8ee3ccad7d011a664
Ferdinand Calo <fexcalo@gmail.com>
2024-03-16 14:06:06 +0100

    Update ListenerServer.java

--- src/main/java/net/fexcraft/mod/fvtm/util/ListenerServer.java
+++ src/main/java/net/fexcraft/mod/fvtm/util/ListenerServer.java
@@ -42,28 +42,6 @@ public class ListenerServer implements IPacketListener<PacketNBTTagCompound> {
 				}
 				return;
 			}
-			case "install":{
-				PartData data = player.getHeldItem(EnumHand.MAIN_HAND).getCapability(Capabilities.VAPDATA, null).getPartData();
-				RootVehicle entity = (RootVehicle)player.world.getEntityByID(packet.nbt.getInteger("entity"));
-				PartSlots source = entity.vehicle.data.getPartSlotsProvider(packet.nbt.getString("source"));
-				String category = packet.nbt.getString("category");
-				if(entity.vehicle.data.getPart(category) != null){
-					PartData oldpart = entity.vehicle.data.getPart(category);
-					boolean valid = oldpart.getType().getInstallHandlerData() instanceof DPIHData && ((DPIHData)oldpart.getType().getInstallHandlerData()).swappable;
-					if(valid && entity.vehicle.data.deinstallPart(player.getCapability(Capabilities.PASSENGER, null).asSender(), category, true)){
-						player.addItemStackToInventory(oldpart.getNewStack().local());
-					}
-					else return;
-				}
-				data = entity.vehicle.data.installPart(player.getCapability(Capabilities.PASSENGER, null).asSender(), data, packet.nbt.getString("source") + ":" + category, true);
-				if(data == null){
-					player.getHeldItem(EnumHand.MAIN_HAND).shrink(1);
-					NBTTagCompound compound = entity.vehicle.data.write(TagCW.create()).local();
-					compound.setString("task", "update_vehicledata");
-					PacketHandler.getInstance().sendToAllAround(new PacketEntityUpdate(entity, compound), PacketsImpl.getTargetPoint(entity));
-				}
-				return;
-			}
 			case "ts_ck_sync":{
 				TrafficSigns signs = player.world.getChunk(packet.nbt.getInteger("x"), packet.nbt.getInteger("z")).getCapability(Capabilities.TRAFFIC_SIGNS, null);
 				packet.nbt.setTag("signs", signs.write(null));

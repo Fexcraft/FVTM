11a3e2170b7ab353d4fb37345aba21b6ebbed652
Ferdinand Calo <fexcalo@gmail.com>
2024-03-16 21:23:27 +0100

    Update ToggableHandler.java

--- src/main/java/net/fexcraft/mod/fvtm/util/handler/ToggableHandler.java
+++ src/main/java/net/fexcraft/mod/fvtm/util/handler/ToggableHandler.java
@@ -1,32 +1,18 @@
 package net.fexcraft.mod.fvtm.util.handler;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Map.Entry;
-import java.util.TreeMap;
-
 import net.fexcraft.lib.common.Static;
 import net.fexcraft.lib.common.math.Time;
-import net.fexcraft.lib.common.math.V3D;
-import net.fexcraft.lib.common.math.Vec3f;
 import net.fexcraft.lib.mc.network.PacketHandler;
 import net.fexcraft.lib.mc.network.packet.PacketNBTTagCompound;
 import net.fexcraft.lib.mc.utils.Print;
 import net.fexcraft.mod.fvtm.data.Capabilities;
-import net.fexcraft.mod.fvtm.data.Seat;
-import net.fexcraft.mod.fvtm.data.attribute.AttrBox;
 import net.fexcraft.mod.fvtm.data.attribute.Attribute;
-import net.fexcraft.mod.fvtm.data.part.Part;
-import net.fexcraft.mod.fvtm.data.part.PartData;
-import net.fexcraft.mod.fvtm.data.part.PartSlot;
-import net.fexcraft.mod.fvtm.data.part.PartSlots;
-import net.fexcraft.mod.fvtm.data.vehicle.SwivelPoint;
 import net.fexcraft.mod.fvtm.gui.ServerReceiver;
 import net.fexcraft.mod.fvtm.handler.InteractionHandler;
 import net.fexcraft.mod.fvtm.item.PartItem;
-import net.fexcraft.mod.fvtm.sys.uni.*;
-import net.fexcraft.mod.fvtm.util.Command;
-import net.fexcraft.mod.fvtm.handler.DefaultPartInstallHandler.DPIHData;
+import net.fexcraft.mod.fvtm.sys.uni.KeyPress;
+import net.fexcraft.mod.fvtm.sys.uni.Passenger;
+import net.fexcraft.mod.fvtm.sys.uni.RootVehicle;
 import net.fexcraft.mod.uni.impl.SWI;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.Entity;
@@ -34,13 +20,6 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemLead;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.EnumParticleTypes;
-import net.minecraft.util.math.AxisAlignedBB;
-import net.minecraft.util.math.Vec3d;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-
-import static net.fexcraft.mod.fvtm.util.PacketsImpl.UTIL_LISTENER;
 
 /**
  * @author Ferdinand Calo' (FEX___96)

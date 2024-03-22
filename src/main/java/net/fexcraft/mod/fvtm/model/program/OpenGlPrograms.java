6499c5bf08c7777fabd2671f2ddddcd41397b986
Ferdinand Calo <fexcalo@gmail.com>
2024-03-22 11:05:01 +0100

    Update OpenGlPrograms.java

--- src/main/java/net/fexcraft/mod/fvtm/model/program/OpenGlPrograms.java
+++ src/main/java/net/fexcraft/mod/fvtm/model/program/OpenGlPrograms.java
@@ -19,7 +19,7 @@ import static net.fexcraft.mod.fvtm.model.ProgramUtils.FloatBool;
 public class OpenGlPrograms {
 
 	public static void init(){
-		ModelGroup.PROGRAMS.add(new Program(){
+		ModelGroup.PROGRAMS.add(new Program() {
 			public String id(){
 				return "fvtm:no_cullface";
 			}
@@ -30,6 +30,41 @@ public class OpenGlPrograms {
 				GL11.glEnable(GL11.GL_CULL_FACE);
 			}
 		});
+		ModelGroup.PROGRAMS.add(new AlphaFunc());
+	}
+
+	public static class AlphaFunc implements Program {
+
+		private int func;
+		private float ref;
+
+		public AlphaFunc(){}
+
+		public AlphaFunc(int func, float ref){
+			this.func = func;
+			this.ref = ref;
+		}
+
+		@Override
+		public String id(){
+			return "opengl:alpha_func";
+		}
+
+		@Override
+		public void pre(ModelGroup list, ModelRenderData data){
+			GL11.glAlphaFunc(func, ref);
+		}
+
+		@Override
+		public void post(ModelGroup list, ModelRenderData data){
+			//
+		}
+
+		@Override
+		public Program parse(String[] args){
+			return new AlphaFunc(Integer.parseInt(args[0]), Float.parseFloat(args[1]));
+		}
+
 	}
 
 }

f5d98db4f0813d54a06327c692df57aa7f604473
Ferdinand Calo <fexcalo@gmail.com>
2024-04-10 21:36:35 +0200

    Update ListenerServer.java

--- src/main/java/net/fexcraft/mod/fvtm/util/ListenerServer.java
+++ src/main/java/net/fexcraft/mod/fvtm/util/ListenerServer.java
@@ -2,22 +2,14 @@ package net.fexcraft.mod.fvtm.util;
 
 import net.fexcraft.lib.mc.api.packet.IPacketListener;
 import net.fexcraft.lib.mc.network.PacketHandler;
-import net.fexcraft.lib.mc.network.packet.PacketEntityUpdate;
 import net.fexcraft.lib.mc.network.packet.PacketNBTTagCompound;
 import net.fexcraft.mod.fvtm.data.Capabilities;
-import net.fexcraft.mod.fvtm.data.part.PartData;
-import net.fexcraft.mod.fvtm.data.part.PartSlots;
 import net.fexcraft.mod.fvtm.sys.tsign.TrafficSigns;
-import net.fexcraft.mod.fvtm.sys.uni.RootVehicle;
-import net.fexcraft.mod.fvtm.util.handler.AttrReqHandler;
-import net.fexcraft.mod.uni.tag.TagCW;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraft.util.EnumHand;
 
-import static net.fexcraft.mod.fvtm.handler.DefaultPartInstallHandler.*;
 import static net.fexcraft.mod.fvtm.util.PacketsImpl.UTIL_LISTENER;
 
 public class ListenerServer implements IPacketListener<PacketNBTTagCompound> {

7895c4ca7445b41f11ad80a06cbd5ff921482442
Ferdinand Calo <fexcalo@gmail.com>
2024-04-26 00:26:00 +0200

    Update SeparateRenderCache.java

--- src/main/java/net/fexcraft/mod/fvtm/render/SeparateRenderCache.java
+++ src/main/java/net/fexcraft/mod/fvtm/render/SeparateRenderCache.java
@@ -9,13 +9,15 @@ import net.fexcraft.mod.fvtm.data.block.BlockData;
 import net.fexcraft.mod.fvtm.data.vehicle.VehicleData;
 import net.fexcraft.mod.fvtm.data.vehicle.VehicleEntity;
 import net.fexcraft.mod.fvtm.model.SortedModelGroup.SeparateSortedModelGroup;
+import net.fexcraft.mod.fvtm.sys.uni.RootVehicle;
+import net.fexcraft.mod.fvtm.sys.uni.VehicleInstance;
 import net.minecraft.tileentity.TileEntity;
 
 public class SeparateRenderCache {
 
 	public static final ArrayList<SeparateSortedModelGroup> SORTED_VEH_QUEUE = new ArrayList<>();
 	public static final ArrayList<VehicleData> SORTED_VEH_DATA = new ArrayList<>();
-	public static final ArrayList<VehicleEntity> SORTED_VEH_ENTITY = new ArrayList<>();
+	public static final ArrayList<RootVehicle> SORTED_VEH_ENTITY = new ArrayList<>();
 	public static final HashMap<Integer, V3D> SORTED_VEH_ROT = new HashMap<>();
 	public static final HashMap<Integer, double[]> SORTED_VEH_POS = new HashMap<>();
 	//

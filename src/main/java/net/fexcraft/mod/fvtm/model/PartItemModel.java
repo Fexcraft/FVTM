97ae8f800c8c46235c4438bca7150d6c93bd7ce7
Ferdinand Calo <fexcalo@gmail.com>
2024-04-28 23:33:12 +0200

    Update PartItemModel.java

--- src/main/java/net/fexcraft/mod/fvtm/model/PartItemModel.java
+++ src/main/java/net/fexcraft/mod/fvtm/model/PartItemModel.java
@@ -1,11 +1,13 @@
 package net.fexcraft.mod.fvtm.model;
 
+import static net.fexcraft.mod.fvtm.handler.TireInstallationHandler.*;
 import static net.fexcraft.mod.fvtm.util.TexUtil.bindTexture;
 
 import net.fexcraft.lib.common.Static;
 import net.fexcraft.lib.mc.render.FCLItemModel;
 import net.fexcraft.mod.fvtm.data.Capabilities;
 import net.fexcraft.mod.fvtm.data.part.PartData;
+import net.fexcraft.mod.fvtm.handler.TireInstallationHandler;
 import net.fexcraft.mod.fvtm.item.PartItem;
 import net.fexcraft.mod.fvtm.handler.WheelInstallationHandler.WheelData;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms.TransformType;
@@ -57,10 +59,19 @@ public class PartItemModel implements FCLItemModel {
 			}
 			case GUI: {
 				GL11.glRotatef(90f, 0F, 1F, 0F);
-				WheelData ihdata = data.getType().getInstallHandlerData();
-				if(ihdata.getRadius() > 0.5){
-					for(float v = ihdata.getRadius(); v > 0.5; v -= 0.0625)
-					GL11.glScalef(1 - Static.sixteenth, 1 - Static.sixteenth, 1 - Static.sixteenth);
+				if(data.getType().getInstallHandlerData() instanceof WheelData){
+					WheelData ihdata = data.getType().getInstallHandlerData();
+					if(ihdata.getRadius() > 0.5){
+						for(float v = ihdata.getRadius(); v > 0.5; v -= 0.0625)
+						GL11.glScalef(1 - Static.sixteenth, 1 - Static.sixteenth, 1 - Static.sixteenth);
+					}
+				}
+				if(data.getType().getInstallHandlerData() instanceof TireData){
+					TireData ihdata = data.getType().getInstallHandlerData();
+					if(ihdata.getOuterRadius() > 0.5){
+						for(float v = ihdata.getOuterRadius(); v > 0.5; v -= 0.0625)
+						GL11.glScalef(1 - Static.sixteenth, 1 - Static.sixteenth, 1 - Static.sixteenth);
+					}
 				}
 				break;
 			}

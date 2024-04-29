b90176e2d31ed95659fc4cd29723197a5f059d82
Ferdinand Calo <fexcalo@gmail.com>
2024-04-29 22:29:12 +0200

    Create VehicleLiftRenderer.java

---
+++ src/main/java/net/fexcraft/mod/fvtm/render/VehicleLiftRenderer.java
@@ -0,0 +1,16 @@
+package net.fexcraft.mod.fvtm.render;
+
+import net.fexcraft.mod.fvtm.block.VehicleLiftEntity;
+import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class VehicleLiftRenderer extends TileEntitySpecialRenderer<VehicleLiftEntity> {
+
+	@Override
+	public void render(VehicleLiftEntity tile, double x, double y, double z, float ticks, int stage, float a){
+		//
+	}
+
+}

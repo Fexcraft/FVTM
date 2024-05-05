4d1bd00846d4d351944bf846891f11be8deab1fa
Ferdinand Calo <fexcalo@gmail.com>
2024-05-05 21:59:33 +0200

    Update VehicleLiftRenderer.java

--- src/main/java/net/fexcraft/mod/fvtm/render/VehicleLiftRenderer.java
+++ src/main/java/net/fexcraft/mod/fvtm/render/VehicleLiftRenderer.java
@@ -1,7 +1,11 @@
 package net.fexcraft.mod.fvtm.render;
 
+import net.fexcraft.mod.fvtm.FvtmLogger;
 import net.fexcraft.mod.fvtm.block.VehicleLiftEntity;
 import net.fexcraft.mod.fvtm.data.block.BlockType;
+import net.fexcraft.mod.fvtm.data.vehicle.SwivelPoint;
+import net.fexcraft.mod.fvtm.data.vehicle.VehicleData;
+import net.fexcraft.mod.fvtm.model.DefaultModel;
 import net.fexcraft.mod.fvtm.model.block.Lift2024Model;
 import net.fexcraft.mod.fvtm.util.TexUtil;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
@@ -14,6 +18,7 @@ import org.lwjgl.opengl.GL11;
 public class VehicleLiftRenderer extends TileEntitySpecialRenderer<VehicleLiftEntity> {
 
 	public static final ResourceLocation TEXTURE = new ResourceLocation("fvtm:textures/block/lift2024.png");
+	private VehicleData data;
 
 	@Override
 	public void render(VehicleLiftEntity tile, double x, double y, double z, float ticks, int stage, float a){
@@ -23,9 +28,16 @@ public class VehicleLiftRenderer extends TileEntitySpecialRenderer<VehicleLiftEn
 		TexUtil.bindTexture(TEXTURE);
 		Lift2024Model.control.render();
 		Lift2024Model.struct.render();
-		if(tile.getVehicleData() != null && tile.getVehicleData().getType().getModel() != null){
-			TexUtil.bindTexture(tile.getVehicleData().getCurrentTexture() );
-			tile.getVehicleData().getType().getModel().render(Lift2024Model.RENDERDATA);
+		data = tile.getVehicleData();
+		if(data != null){
+			GL11.glTranslated(0, tile.liftstate, 0);
+			if(data.getType().getModel() != null){
+				TexUtil.bindTexture(data.getCurrentTexture());
+				data.getType().getModel().render(DefaultModel.RENDERDATA.set(data, null, null, false, ticks));
+			}
+			if(data.getParts().size() > 0){
+				VehicleRenderer.renderPoint(data.getRotationPoint(SwivelPoint.DEFAULT), null, data, null, ticks);
+			}
 		}
 		GL11.glPopMatrix();
 	}

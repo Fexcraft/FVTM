3e080dcf5f4983bf21d120e3717baf9dd3a50f9b
Ferdinand Calo <fexcalo@gmail.com>
2024-06-27 01:12:16 +0200

    Update ConstructorContainer.java

--- src/main/java/net/fexcraft/mod/fvtm/gui/construct/ConstructorContainer.java
+++ src/main/java/net/fexcraft/mod/fvtm/gui/construct/ConstructorContainer.java
@@ -3,6 +3,7 @@ package net.fexcraft.mod.fvtm.gui.construct;
 import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.lib.common.math.V3I;
 import net.fexcraft.mod.fvtm.block.ConstructorEntity;
+import net.fexcraft.mod.uni.UniEntity;
 import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.ui.ContainerInterface;
 import net.fexcraft.mod.uni.world.WrapperHolder;
@@ -17,7 +18,7 @@ public class ConstructorContainer extends ContainerInterface {
 	private ConstructorEntity tile;
 
 	public ConstructorContainer(JsonMap map, EntityPlayer player, int x, int y, int z){
-		super(map, WrapperHolder.getEntity(player), new V3I(x, y, z));
+		super(map, UniEntity.get(player).entity, new V3I(x, y, z));
 		tile = (ConstructorEntity)player.world.getTileEntity(new BlockPos(x, y, z));
 	}
 

0fadd043cc0e7c7d6ac7781aeb1c812e324cae24
Ferdinand Calo <fexcalo@gmail.com>
2024-06-28 23:05:41 +0200

    Update EntityWIE.java

--- src/main/java/net/fexcraft/mod/uni/world/EntityWIE.java
+++ src/main/java/net/fexcraft/mod/uni/world/EntityWIE.java
@@ -2,28 +2,16 @@ package net.fexcraft.mod.uni.world;
 
 import net.fexcraft.lib.common.math.V3D;
 import net.fexcraft.lib.common.math.V3I;
-import net.fexcraft.lib.common.utils.Formatter;
 import net.fexcraft.mod.fvtm.FVTM;
-import net.fexcraft.mod.fvtm.FvtmResources;
 import net.fexcraft.mod.fvtm.sys.uni.Passenger;
 import net.fexcraft.mod.fvtm.sys.uni.RootVehicle;
 import net.fexcraft.mod.fvtm.sys.uni.SeatInstance;
-import net.fexcraft.mod.fvtm.ui.UIKey;
-import net.fexcraft.mod.uni.item.StackWrapper;
+import net.fexcraft.mod.fvtm.ui.UIKeys;
+import net.fexcraft.mod.uni.ui.UIKey;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.EntityLiving;
-import net.minecraft.entity.monster.EntityMob;
-import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.Vec3d;
-import net.minecraft.util.text.TextComponentString;
-import net.minecraftforge.fml.common.registry.EntityEntry;
-import net.minecraftforge.fml.common.registry.EntityRegistry;
-
-import java.util.UUID;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -32,13 +20,6 @@ public class EntityWIE extends EntityWI implements Passenger {
 
 	public EntityWIE(Entity ent){
 		super(ent);
-	}
-
-	@Override
-	@Deprecated
-	public void openUI(UIKey key, V3I pos){
-		if(entity instanceof EntityPlayer == false) return;
-		((EntityPlayer)entity).openGui(FVTM.getInstance(), key.id, world.local(), pos.x, pos.y, pos.z);
 	}
 
 	@Override

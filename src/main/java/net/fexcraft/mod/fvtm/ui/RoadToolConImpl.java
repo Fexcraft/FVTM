078e5972c6b8f95e7e767d77427f883d1d1c5478
Ferdinand Calo <fexcalo@gmail.com>
2024-06-28 15:20:51 +0200

    Update RoadToolConImpl.java

--- src/main/java/net/fexcraft/mod/fvtm/ui/RoadToolConImpl.java
+++ src/main/java/net/fexcraft/mod/fvtm/ui/RoadToolConImpl.java
@@ -3,6 +3,7 @@ package net.fexcraft.mod.fvtm.ui;
 import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.lib.common.math.V3I;
 import net.fexcraft.mod.fvtm.ui.road.RoadToolCon;
+import net.fexcraft.mod.uni.UniEntity;
 import net.fexcraft.mod.uni.impl.SWI;
 import net.fexcraft.mod.uni.item.StackWrapper;
 import net.fexcraft.mod.uni.tag.TagCW;
@@ -18,7 +19,7 @@ public class RoadToolConImpl extends RoadToolCon {
 	private RoadInventory inv = new RoadInventory();
 	private SWI wrapper = new SWI(ItemStack.EMPTY);
 
-	public RoadToolConImpl(JsonMap map, EntityW player, V3I pos){
+	public RoadToolConImpl(JsonMap map, UniEntity player, V3I pos){
 		super(map, player, pos);
 		initInv();
 	}

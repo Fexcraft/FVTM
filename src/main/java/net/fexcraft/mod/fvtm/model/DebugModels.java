9169ab45b97a94112b28be10313682af0ecf123d
Ferdinand Calo <fexcalo@gmail.com>
2024-07-15 18:38:10 +0200

    Update DebugModels.java

--- src/main/java/net/fexcraft/mod/fvtm/model/DebugModels.java
+++ src/main/java/net/fexcraft/mod/fvtm/model/DebugModels.java
@@ -5,37 +5,44 @@ import net.fexcraft.lib.tmt.ModelRendererTurbo;
 import net.fexcraft.mod.fvtm.data.container.ContainerType;
 
 public class DebugModels {
-	
+
 	public static ModelRendererTurbo[] CONTAINER = {
-		new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(- 8.1f, -48.1f, -24.1f,  16.2f, 48.2f, 48.2f).setRotationPoint(0, 0, 0),
-		new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-16.1f, -48.1f, -24.1f,  32.2f, 48.2f, 48.2f).setRotationPoint(0, 0, 0),
-		new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-24.1f, -48.1f, -24.1f,  48.2f, 48.2f, 48.2f).setRotationPoint(0, 0, 0),
-		new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-48.1f, -48.1f, -24.1f,  96.2f, 48.2f, 48.2f).setRotationPoint(0, 0, 0),
+		new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-8.1f, -48.1f, -24.1f, 16.2f, 48.2f, 48.2f).setRotationPoint(0, 0, 0),
+		new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-16.1f, -48.1f, -24.1f, 32.2f, 48.2f, 48.2f).setRotationPoint(0, 0, 0),
+		new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-24.1f, -48.1f, -24.1f, 48.2f, 48.2f, 48.2f).setRotationPoint(0, 0, 0),
+		new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-48.1f, -48.1f, -24.1f, 96.2f, 48.2f, 48.2f).setRotationPoint(0, 0, 0),
 		new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-96.1f, -48.1f, -24.1f, 192.2f, 48.2f, 48.2f).setRotationPoint(0, 0, 0)
 	};
+
 	static{
 		for(int i = 0; i < ContainerType.values().length; i++){
 			RGB rgb = new RGB(ContainerType.values()[i].getRGB().packed * 2);
 			CONTAINER[i].setTextured(false).setColor(rgb);
 		}
 	}
-    public static final ModelRendererTurbo CENTERSPHERE = new ModelRendererTurbo(null, 0, 0, 16, 16).addSphere(0, 0, 0, 1, 16, 16, 8, 8).setLines(new RGB(0xcdcdcd));
-    public static final ModelRendererTurbo HOTINSTALLCUBE = new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-0.5f, -0.5f, -0.5f, 1, 1, 1).setLines(new RGB(0x00ffff));
-    public static final ModelRendererTurbo REDINSTALLCUBE = new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-0.5f, -0.5f, -0.5f, 1, 1, 1).setLines(new RGB(0xff0000));
-    public static final ModelRendererTurbo GRNINSTALLCUBE = new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-0.5f, -0.5f, -0.5f, 1, 1, 1).setLines(new RGB(0x00ff00));
-    public static final ModelRendererTurbo YLWINSTALLCUBE = new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-0.5f, -0.5f, -0.5f, 1, 1, 1).setLines(new RGB(0xffff00));
-    public static final ModelRendererTurbo TRAFFICSIGNCUBE = new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-0.5f, -0.5f, -0.5f, 1, 1, 1).setLines(new RGB(0x00ff00));
-    public static final ModelRendererTurbo ATTRBOXCUBE = new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-0.5f, -0.5f, -0.5f, 1, 1, 1).setLines(new RGB(0x1d8228/*ffbb00*/));
-    public static final ModelRendererTurbo SEAT_CUBE_SITTING = new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-0.5f, 0, -0.5f, 1, 1, 1).setLines(new RGB(0xeb8500));
-    public static final ModelRendererTurbo SEAT_CUBE_STANDING = new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-0.5f, 0, -0.5f, 1, 1, 1).setLines(new RGB(0xcbcf00));
-    public static final ModelRendererTurbo SEAT_CUBE_OCCUPIED = new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-0.5f, 0, -0.5f, 1, 1, 1).setLines(new RGB(0x619900));
+
+	public static final ModelRendererTurbo SPHERE_GRY = new ModelRendererTurbo(null, 0, 0, 16, 16).addSphere(0, 0, 0, 1, 16, 16, 8, 8).setLines(new RGB(0xcdcdcd));
+	public static final ModelRendererTurbo SPHERE_RED = new ModelRendererTurbo(null, 0, 0, 16, 16).addSphere(0, 0, 0, 1, 16, 16, 8, 8).setLines(new RGB(0xff0000));
+	public static final ModelRendererTurbo SPHERE_GRN = new ModelRendererTurbo(null, 0, 0, 16, 16).addSphere(0, 0, 0, 1, 16, 16, 8, 8).setLines(new RGB(0x00ff00));
+	public static final ModelRendererTurbo SPHERE_BLU = new ModelRendererTurbo(null, 0, 0, 16, 16).addSphere(0, 0, 0, 1, 16, 16, 8, 8).setLines(new RGB(0x0000ff));
+	public static final ModelRendererTurbo SPHERE_YLW = new ModelRendererTurbo(null, 0, 0, 16, 16).addSphere(0, 0, 0, 1, 16, 16, 8, 8).setLines(new RGB(0xffff00));
+	public static final ModelRendererTurbo SPHERE_CYN = new ModelRendererTurbo(null, 0, 0, 16, 16).addSphere(0, 0, 0, 1, 16, 16, 8, 8).setLines(new RGB(0x00ffff));
+	public static final ModelRendererTurbo CUBE_CYN = new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-0.5f, -0.5f, -0.5f, 1, 1, 1).setLines(new RGB(0x00ffff));
+	public static final ModelRendererTurbo CUBE_RED = new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-0.5f, -0.5f, -0.5f, 1, 1, 1).setLines(new RGB(0xff0000));
+	public static final ModelRendererTurbo CUBE_GRN = new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-0.5f, -0.5f, -0.5f, 1, 1, 1).setLines(new RGB(0x00ff00));
+	public static final ModelRendererTurbo CUBE_YLW = new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-0.5f, -0.5f, -0.5f, 1, 1, 1).setLines(new RGB(0xffff00));
+	public static final ModelRendererTurbo CUBE_ORG = new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-0.5f, -0.5f, -0.5f, 1, 1, 1).setLines(new RGB(0x1d8228/*ffbb00*/));
+	public static final ModelRendererTurbo SEAT_CUBE_SITTING = new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-0.5f, 0, -0.5f, 1, 1, 1).setLines(new RGB(0xeb8500));
+	public static final ModelRendererTurbo SEAT_CUBE_STANDING = new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-0.5f, 0, -0.5f, 1, 1, 1).setLines(new RGB(0xcbcf00));
+	public static final ModelRendererTurbo SEAT_CUBE_OCCUPIED = new ModelRendererTurbo(null, 0, 0, 16, 16).addBox(-0.5f, 0, -0.5f, 1, 1, 1).setLines(new RGB(0x619900));
 
 	public static final ModelRendererTurbo centermarker0 = new ModelRendererTurbo(null, 0, 0, 0, 0).addBox(-0.25f, -8, -0.25f, .5f, 16, .5f).setTextured(false).setColor(RGB.GREEN.copy());
 	public static final ModelRendererTurbo centermarker1 = new ModelRendererTurbo(null, 0, 0, 0, 0).addBox(-8, -0.25f, -0.25f, 16, .5f, .5f).setTextured(false).setColor(RGB.RED.copy());
 	public static final ModelRendererTurbo centermarker2 = new ModelRendererTurbo(null, 0, 0, 0, 0).addBox(-0.25f, -0.25f, -8, .5f, .5f, 16).setTextured(false).setColor(RGB.BLUE.copy());
-    public static ModelGroup group0, center, arm_left, arm_right;
-    public static ModelRendererTurbo chest, leg, reg, br, bl;
-    static {
+	public static ModelGroup group0, center, arm_left, arm_right;
+	public static ModelRendererTurbo chest, leg, reg, br, bl;
+
+	static{
 		group0 = new ModelGroup("group0");
 		group0.add(new ModelRendererTurbo(group0, -1, -1, 16, 16).newCylinderBuilder()
 			.setPosition(0, 0, 0).setRadius(6, 5).setLength(3).setSegments(8, 0).setScale(1, 1).setDirection(4)
@@ -85,6 +92,6 @@ public class DebugModels {
 		center.add(centermarker0);
 		center.add(centermarker1);
 		center.add(centermarker2);
-    }
+	}
 
 }

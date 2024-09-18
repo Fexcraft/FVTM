f1eaa402cc2bd436ec8cd7ce2e5865e5746f9612
Ferdinand Calo <fexcalo@gmail.com>
2024-09-18 22:47:57 +0200

    Update F2SW_4ROT_TE.java

--- forge12/src/main/java/net/fexcraft/mod/fvtm/block/generated/F2SW_4ROT_TE.java
+++ forge12/src/main/java/net/fexcraft/mod/fvtm/block/generated/F2SW_4ROT_TE.java
@@ -2,6 +2,7 @@ package net.fexcraft.mod.fvtm.block.generated;
 
 import net.fexcraft.mod.fvtm.data.block.Block;
 import net.fexcraft.mod.fvtm.item.JunctionToolItem;
+import net.fexcraft.mod.uni.UniEntity;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -31,7 +32,7 @@ public class F2SW_4ROT_TE extends G_4ROT_TE {
 			ItemStack held = player.getHeldItemMainhand();
 			if(held.isEmpty()){
 				SwitchTileEntity tile = (SwitchTileEntity)world.getTileEntity(pos);
-				if(tile.getJunction() != null) tile.junction.onSwitchInteract(player, tile, false);
+				if(tile.getJunction() != null) tile.junction.onSwitchInteract(UniEntity.getEntity(player), tile, false);
 				return true;
 			}
 			else if(held.getItem() instanceof JunctionToolItem && held.hasTagCompound() && held.getTagCompound().hasKey("fvtm:junction")){

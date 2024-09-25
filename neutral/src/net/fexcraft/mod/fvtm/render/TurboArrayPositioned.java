c7a45f1f2bfe3c7d1b5377a539e43d2802483104
Ferdinand Calo <fexcalo@gmail.com>
2024-09-25 15:08:01 +0200

    Update TurboArrayPositioned.java

--- neutral/src/net/fexcraft/mod/fvtm/render/TurboArrayPositioned.java
+++ neutral/src/net/fexcraft/mod/fvtm/render/TurboArrayPositioned.java
@@ -4,7 +4,6 @@ import net.fexcraft.lib.common.math.RGB;
 import net.fexcraft.lib.common.math.V3D;
 import net.fexcraft.lib.tmt.ModelRendererTurbo;
 import net.fexcraft.mod.fvtm.sys.uni.Path;
-import net.fexcraft.mod.fvtm.sys.wire.Wire;
 import org.lwjgl.opengl.GL11;
 
 public class TurboArrayPositioned {
@@ -25,7 +24,7 @@ public class TurboArrayPositioned {
 		}
 	}
 
-	public TurboArrayPositioned(Wire wire, RGB colour){
+	/*public TurboArrayPositioned(Wire wire, RGB colour){
 		int i = (int)wire.getLength(null);
 		if(wire.length % 1f > 0) i++;
 		if(i == 0) i = 1;
@@ -36,7 +35,7 @@ public class TurboArrayPositioned {
 			if(colour != null) turbos[k].setColor(colour);
 			positions[k] = wire.getVectorPosition(k == 0 ? 0.125f : k == i - 1 ? wire.length - 0.125f : k, false);
 		}
-	}
+	}*///TODO
 
 	public void clearDisplayLists(){
 		for(ModelRendererTurbo turbo : turbos)
@@ -45,7 +44,7 @@ public class TurboArrayPositioned {
 
 	public void render(){
 		for(int m = 0; m < turbos.length; m++){
-			int i = RailRenderer.getBrightness(positions[m]), j = i % 65536, k = i / 65536;
+			//TODO int i = RailRenderer.getBrightness(positions[m]), j = i % 65536, k = i / 65536;
 			//TODO setLightmapTextureCoords(lightmapTexUnit, (float)j / 1.0F, (float)k / 1.0F);
 			//TODO color(1.0F, 1.0F, 1.0F, 1.0F);
 			turbos[m].render(1f);

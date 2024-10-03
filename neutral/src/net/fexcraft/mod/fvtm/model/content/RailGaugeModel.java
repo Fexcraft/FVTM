0e28aa79cd41e25a313d787556fbd1025bbd72e5
Ferdinand Calo <fexcalo@gmail.com>
2024-10-03 15:13:24 +0200

    Update RailGaugeModel.java

--- neutral/src/net/fexcraft/mod/fvtm/model/content/RailGaugeModel.java
+++ neutral/src/net/fexcraft/mod/fvtm/model/content/RailGaugeModel.java
@@ -1,19 +1,21 @@
-package net.fexcraft.mod.fvtm.model;
+package net.fexcraft.mod.fvtm.model.content;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import net.fexcraft.lib.common.math.V3D;
-import net.fexcraft.mod.fvtm.render.TurboArrayPositioned;
-import org.lwjgl.opengl.GL11;
 
+import net.fexcraft.mod.fvtm.model.DefaultModel;
+import net.fexcraft.mod.fvtm.model.ModelData;
+import net.fexcraft.mod.fvtm.model.ModelGroup;
+import net.fexcraft.mod.fvtm.model.ModelRenderData;
 import net.fexcraft.mod.fvtm.render.RailRenderer;
 import net.fexcraft.mod.fvtm.sys.rail.EntryDirection;
 import net.fexcraft.mod.fvtm.sys.rail.Junction;
-import net.fexcraft.mod.fvtm.sys.rail.Track;
-import net.fexcraft.mod.fvtm.util.TexUtil;
-import net.fexcraft.mod.fvtm.util.QV3D;
 
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
 public class RailGaugeModel extends DefaultModel {
 
 	public static final RailGaugeModel EMPTY = new RailGaugeModel();
@@ -21,20 +23,14 @@ public class RailGaugeModel extends DefaultModel {
 	public boolean rail_tempcull = false;
 	public float ties_distance = 0.5f;
 	public float signal_offset = 0.25f;
-	public float buffer_length = 2f;
-	//
-	public Track buffer_track;
-	//
-	//signals
-	
-	////-///---/---///-////
+	//public float buffer_length = 2f;
 	
 	@Override
 	public RailGaugeModel parse(ModelData data){
 		rail_tempcull = data.getBoolean("RailCulling", false);
 		ties_distance = data.getFloat("TiesDistance", ties_distance);
 		signal_offset = data.getFloat("SignalOffset", signal_offset);
-		buffer_length = data.getFloat("BufferLength", buffer_length);
+		//buffer_length = data.getFloat("BufferLength", buffer_length);
 		List<String> rails = data.getArray("Rail").toStringList();
 		if(rails.isEmpty()) return this;
 		for(String rail : rails){
@@ -96,22 +92,6 @@ public class RailGaugeModel extends DefaultModel {
 
 	public void renderSignal(Junction junction, EntryDirection dir, boolean state){
 		RailRenderer.junction_signal.render();
-	}
-
-	public void renderBuffer(Junction junc){
-		if(!groups.contains("buffer")) return;
-		if(buffer_track == null){
-			buffer_track = new Track(null, new QV3D[]{ new QV3D() }, new QV3D(buffer_length, 0, 0), junc.tracks.get(0).gauge);
-		}
-		if(buffer_track.railmodel == null){ RailRenderer.generateTrackModel(buffer_track, this); }
-		TexUtil.bindTexture(buffer_track.gauge.getModelTexture());
-		GL11.glRotatef(180, 0, 0, 1); GL11.glRotatef(180, 0, 1, 0);
-		groups.get("buffer").render();
-		GL11.glRotatef(180, 0, 0, 1); GL11.glRotatef(-90, 0, 1, 0);
-		TexUtil.bindTexture(buffer_track.gauge.getTiesTexture());
-		((TurboArrayPositioned)buffer_track.restmodel).renderPlain();
-		TexUtil.bindTexture(buffer_track.gauge.getRailTexture());
-		((TurboArrayPositioned)buffer_track.railmodel).renderPlain();
 	}
 	
 }

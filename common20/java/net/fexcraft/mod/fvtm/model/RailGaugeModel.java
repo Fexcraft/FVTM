7a9f99512644f3fd798eb5ccda6f874ae335e2be
Ferdinand Calo <fexcalo@gmail.com>
2024-10-03 15:16:41 +0200

    Delete RailGaugeModel.java

--- common20/java/net/fexcraft/mod/fvtm/model/RailGaugeModel.java
+++
@@ -1,9 +0,0 @@
-package net.fexcraft.mod.fvtm.model;
-
-import net.fexcraft.mod.fvtm.model.DefaultModel;
-
-public class RailGaugeModel extends DefaultModel {
-
-	public static RailGaugeModel EMPTY = new RailGaugeModel();
-
-}

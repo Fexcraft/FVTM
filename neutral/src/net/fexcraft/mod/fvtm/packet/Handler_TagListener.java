f3863ee169d2254ef74d1941d84a9e6f952ef6e7
Ferdinand Calo <fexcalo@gmail.com>
2024-10-25 12:19:42 +0200

    Update Handler_TagListener.java

--- neutral/src/net/fexcraft/mod/fvtm/packet/Handler_TagListener.java
+++ neutral/src/net/fexcraft/mod/fvtm/packet/Handler_TagListener.java
@@ -1,5 +1,6 @@
 package net.fexcraft.mod.fvtm.packet;
 
+import net.fexcraft.mod.fvtm.FvtmLogger;
 import net.fexcraft.mod.fvtm.sys.uni.Passenger;
 
 /**
@@ -9,12 +10,20 @@ public class Handler_TagListener implements PacketHandler<Packet_TagListener> {
 
 	@Override
 	public Runnable handleServer(Packet_TagListener packet, Passenger player){
-		return () -> Packets.LIS_SERVER.get(packet.to).handle(packet.tag, player);
+		return () -> {
+			PacketListener lis = Packets.LIS_SERVER.get(packet.to);
+			if(lis != null) lis.handle(packet.tag, player);
+			else FvtmLogger.log("Tag Packet Listener not found: " + packet.to);
+		};
 	}
 
 	@Override
 	public Runnable handleClient(Packet_TagListener packet, Passenger player){
-		return () -> Packets.LIS_CLIENT.get(packet.to).handle(packet.tag, player);
+		return () -> {
+			PacketListener lis = Packets.LIS_CLIENT.get(packet.to);
+			if(lis != null) lis.handle(packet.tag, player);
+			else FvtmLogger.log("Tag Packet Listener not found: " + packet.to);
+		};
 	}
 
 }

306c64f663aeca9338a0a30c269e63cadc50a02f
Ferdinand Calo <fexcalo@gmail.com>
2024-11-17 20:23:11 +0100

    Create NRailVehicle.java

---
+++ forge12/src/main/java/net/fexcraft/mod/fvtm/sys/pro/NRailVehicle.java
@@ -0,0 +1,123 @@
+package net.fexcraft.mod.fvtm.sys.pro;
+
+import net.fexcraft.lib.common.math.V3D;
+import net.fexcraft.mod.fvtm.data.vehicle.VehicleData;
+import net.fexcraft.mod.fvtm.sys.rail.Junction;
+import net.fexcraft.mod.fvtm.sys.rail.RailEntity;
+import net.fexcraft.mod.fvtm.sys.rail.RailEntity.TRO;
+import net.fexcraft.mod.fvtm.sys.rail.RailSystem;
+import net.fexcraft.mod.fvtm.sys.rail.Track;
+import net.fexcraft.mod.fvtm.sys.uni.RootVehicle;
+import net.fexcraft.mod.fvtm.sys.uni.SystemManager;
+import net.fexcraft.mod.uni.tag.TagCW;
+import net.fexcraft.mod.uni.world.WrapperHolder;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.World;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class NRailVehicle extends RootVehicle {
+
+	private VehicleData data;
+	public Track current;
+	public Track last;
+	public double passed;
+	public RailSystem sys;
+	private double frbogiedis;
+	private double rrbogiedis;
+
+	public NRailVehicle(World world){
+		super(world);
+		preventEntitySpawning = true;
+		ignoreFrustumCheck = true;
+		setSize(0.5f, 0.5f);
+		if(world.isRemote){
+			setRenderDistanceWeight(1D);
+		}
+		sys = SystemManager.get(SystemManager.Systems.RAIL, WrapperHolder.getWorld(world));
+	}
+
+	public NRailVehicle(World world, RailEntity ent){
+		this(world);
+		ent.vehicle.entity = vehicle.entity;
+		vehicle = ent.vehicle;
+		//
+		ent.alignEntity(true);
+		init(null);
+	}
+
+	@Override
+	public void writeSpawnData(TagCW com){
+		com.set("fr_bogie", vehicle.railent.frbogiedis);
+		com.set("rr_bogie", vehicle.railent.rrbogiedis);
+		com.set("Track", vehicle.railent.current.write(null));
+		com.set("RID", vehicle.railent.uid);
+	}
+
+	@Override
+	public void readSpawnData(TagCW com){
+		//railent = sys.getEntity(com.getLong("RID"), true);
+		current = new Track(null).read(com.getCompound("Track"));
+		frbogiedis = com.getDouble("fr_bogie");
+		rrbogiedis = com.getDouble("rr_bogie");
+	}
+
+	@Override
+	protected void writeEntityToNBT(NBTTagCompound compound){
+		super.writeEntityToNBT(compound);
+		compound.setLong("RailEntity", vehicle.railent.uid);
+		super.writeEntityToNBT(compound);
+	}
+
+	@Override
+	protected void readEntityFromNBT(NBTTagCompound compound){
+		super.readEntityFromNBT(compound);
+		setDead();
+	}
+
+	public V3D moveOnly(double passed){
+		TRO tro = getTrack(current, passed);
+		return tro.track == null ? null : tro.track.getVectorPosition(tro.passed, false);
+	}
+
+	private TRO getTrack(Track track, double passed){
+		if(track == null) return new TRO(track, passed);
+		while(passed > track.length){
+			Junction junk = sys.getJunction(track.end.pos);
+			if(junk == null){ new TRO(track, track.length); }
+			Track newtrack = junk.getNext(null, track.getOppositeId(), false);
+			if(newtrack != null){
+				passed -= track.length; track = newtrack;
+			} else return new TRO(track, track.length);
+		}
+		while(passed < 0){
+			Junction junk = sys.getJunction(track.start.pos);
+			if(junk == null){ return new TRO(track, 0); }
+			Track newtrack = junk.getNext(null, track.getId(), false);
+			if(newtrack != null){
+				passed += newtrack.length; track = newtrack.createOppositeCopy();
+			} else return new TRO(track, 0);
+
+		} return new TRO(track, passed);
+	}
+
+	@Override
+	public void onUpdate(){
+		super.onUpdate();
+	}
+
+	public void updBogieRot(){
+		if(current == null) return;
+		V3D bf0 = moveOnly(passed + 0.1f), bf1 = moveOnly(passed - 0.1f);
+		V3D br0 = moveOnly(passed - frbogiedis - rrbogiedis + 0.1f);
+		V3D br1 = moveOnly(passed - frbogiedis - rrbogiedis - 0.1f);
+		if(bf0 != null && br0 != null && bf1 != null && br1 != null){
+			float front = (float)(Math.toDegrees(Math.atan2(bf0.z - bf1.z, bf0.x - bf1.x)) - vehicle.point.getPivot().deg_yaw());
+			float rear  = (float)(Math.toDegrees(Math.atan2(br0.z - br1.z, br0.x - br1.x)) - vehicle.point.getPivot().deg_yaw());
+			vehicle.data.getAttribute("bogie_front_angle").set(front);
+			vehicle.data.getAttribute("bogie_rear_angle").set(rear);
+		}
+	}
+
+}

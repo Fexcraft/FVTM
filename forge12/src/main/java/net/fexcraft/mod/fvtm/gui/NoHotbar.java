67b49081cb8bfbafe4be2b3624af301f3b9579cf
Ferdinand Calo <fexcalo@gmail.com>
2024-11-18 16:09:52 +0100

    Update NoHotbar.java

--- forge12/src/main/java/net/fexcraft/mod/fvtm/gui/NoHotbar.java
+++ forge12/src/main/java/net/fexcraft/mod/fvtm/gui/NoHotbar.java
@@ -2,7 +2,7 @@ package net.fexcraft.mod.fvtm.gui;
 
 import static net.fexcraft.mod.fvtm.Config.OVERLAY_ON_BOTTOM;
 
-import net.fexcraft.mod.fvtm.data.vehicle.VehicleEntity;
+import net.fexcraft.mod.fvtm.sys.uni.RootVehicle;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
@@ -14,7 +14,7 @@ public class NoHotbar {
 	
     @SubscribeEvent
     public static void render(RenderGameOverlayEvent.Pre event){
-    	if(Minecraft.getMinecraft().player.getRidingEntity() instanceof VehicleEntity && event.getType() == ElementType.HOTBAR){
+    	if(Minecraft.getMinecraft().player.getRidingEntity() instanceof RootVehicle && event.getType() == ElementType.HOTBAR){
     		if(Minecraft.getMinecraft().gameSettings.hideGUI || OVERLAY_ON_BOTTOM) event.setCanceled(true);
     	}
     }

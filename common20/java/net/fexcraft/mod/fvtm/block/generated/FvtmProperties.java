0d3704637da4097b665060b76f25e1f2fd427c4b
Ferdinand Calo <fexcalo@gmail.com>
2024-11-25 22:50:18 +0100

    Create FvtmProperties.java

---
+++ common20/java/net/fexcraft/mod/fvtm/block/generated/FvtmProperties.java
@@ -0,0 +1,15 @@
+package net.fexcraft.mod.fvtm.block.generated;
+
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class FvtmProperties {
+
+	public static final DirectionProperty FACING = DirectionProperty.create("facing");
+	public static final IntegerProperty PROP_ROT8 = IntegerProperty.create("rotation", 0, 7);
+	public static final IntegerProperty PROP_ROT16 = IntegerProperty.create("rotation", 0, 7);
+
+}

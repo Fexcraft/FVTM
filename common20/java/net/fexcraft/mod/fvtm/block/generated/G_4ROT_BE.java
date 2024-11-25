88dd38639ac3ae43b6c837750ad0ada74e371fe4
Ferdinand Calo <fexcalo@gmail.com>
2024-11-25 22:50:32 +0100

    Create G_4ROT_BE.java

---
+++ common20/java/net/fexcraft/mod/fvtm/block/generated/G_4ROT_BE.java
@@ -0,0 +1,33 @@
+package net.fexcraft.mod.fvtm.block.generated;
+
+import net.fexcraft.lib.common.Static;
+import net.fexcraft.mod.fvtm.data.block.Block;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+
+import javax.annotation.Nullable;
+
+import static net.fexcraft.mod.fvtm.block.generated.FvtmProperties.FACING;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class G_4ROT_BE extends BlockBase {
+
+	public G_4ROT_BE(Block type){
+		super(type);
+	}
+
+	@Override
+	protected void createBlockStateDefinition(StateDefinition.Builder<net.minecraft.world.level.block.Block, BlockState> sd){
+		sd.add(FACING);
+	}
+
+	@Nullable
+	public BlockState getStateForPlacement(BlockPlaceContext context) {
+		return defaultBlockState().setValue(FACING, type.isRandomRot() ? Direction.values()[Static.random.nextInt(4) + 2] : context.getPlayer().getDirection().getOpposite());
+	}
+
+}

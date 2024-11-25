142c5f358eeaae74e4842de6d682620eea5c2ae4
Ferdinand Calo <fexcalo@gmail.com>
2024-11-25 22:51:18 +0100

    Update BlockTypeImpl.java

--- common20/java/net/fexcraft/mod/fvtm/util/BlockTypeImpl.java
+++ common20/java/net/fexcraft/mod/fvtm/util/BlockTypeImpl.java
@@ -1,7 +1,6 @@
 package net.fexcraft.mod.fvtm.util;
 
-import net.fexcraft.mod.fvtm.block.generated.BlockBase;
-import net.fexcraft.mod.fvtm.block.generated.PlainBase;
+import net.fexcraft.mod.fvtm.block.generated.*;
 import net.fexcraft.mod.fvtm.data.block.BlockType;
 
 /**
@@ -12,10 +11,10 @@ public class BlockTypeImpl {
     public static Class get(BlockType type, boolean func, boolean plain){
         plain = plain && !func;
         switch(type){
-            /*case GENERIC_4ROT: return plain ? G_4ROT.class : G_4ROT_TE.class;
-            case GENERIC_4X4ROT: return plain ? G_4x4ROT.class : G_4x4ROT_TE.class;
-            case GENERIC_8ROT: return plain ? G_8ROT.class : G_8ROT_TE.class;
-            case GENERIC_16ROT: return plain ? G_16ROT.class : G_16ROT_TE.class;*/
+            case GENERIC_4ROT: return plain ? G_4ROT.class : G_4ROT_BE.class;
+            /*case GENERIC_4X4ROT: return plain ? G_4x4ROT.class : G_4x4ROT_TE.class;
+            case GENERIC_8ROT: return plain ? G_8ROT.class : G_8ROT_TE.class;*/
+            case GENERIC_16ROT: return plain ? G_16ROT.class : G_16ROT_BE.class;
             case GENERIC_SIMPLE: return plain ? PlainBase.class : BlockBase.class;
             /*case GENERIC_2VAR: return plain ? G_VAR.T2.class : G_VAR_TE.T2.class;
             case GENERIC_3VAR: return plain ? G_VAR.T3.class : G_VAR_TE.T3.class;

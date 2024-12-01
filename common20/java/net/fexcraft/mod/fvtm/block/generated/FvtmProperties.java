251af75a00f9da164675b2412c8a326086827b08
Ferdinand Calo <fexcalo@gmail.com>
2024-12-01 20:37:32 +0100

    Update FvtmProperties.java

--- common20/java/net/fexcraft/mod/fvtm/block/generated/FvtmProperties.java
+++ common20/java/net/fexcraft/mod/fvtm/block/generated/FvtmProperties.java
@@ -1,5 +1,6 @@
 package net.fexcraft.mod.fvtm.block.generated;
 
+import net.minecraft.core.Direction;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 
@@ -8,7 +9,7 @@ import net.minecraft.world.level.block.state.properties.IntegerProperty;
  */
 public class FvtmProperties {
 
-	public static final DirectionProperty FACING = DirectionProperty.create("facing");
+	public static final DirectionProperty FACING = DirectionProperty.create("facing", Direction.Plane.HORIZONTAL);
 	public static final IntegerProperty PROP_ROT8 = IntegerProperty.create("rotation", 0, 7);
 	public static final IntegerProperty PROP_ROT16 = IntegerProperty.create("rotation", 0, 7);
 

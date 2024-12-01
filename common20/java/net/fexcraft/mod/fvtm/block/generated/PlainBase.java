8f6e5ffd7899906c5ba951cfafaf88f304f0ca89
Ferdinand Calo <fexcalo@gmail.com>
2024-12-01 20:38:14 +0100

    Update PlainBase.java

--- common20/java/net/fexcraft/mod/fvtm/block/generated/PlainBase.java
+++ common20/java/net/fexcraft/mod/fvtm/block/generated/PlainBase.java
@@ -1,8 +1,20 @@
 package net.fexcraft.mod.fvtm.block.generated;
 
+import net.fexcraft.lib.common.math.V3D;
 import net.fexcraft.mod.fvtm.data.block.Block;
+import net.fexcraft.mod.fvtm.data.block.BlockFunction;
+import net.fexcraft.mod.uni.UniEntity;
+import net.fexcraft.mod.uni.world.StateWrapper;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+
+import static net.fexcraft.mod.uni.world.WrapperHolder.*;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -28,6 +40,17 @@ public class PlainBase extends net.minecraft.world.level.block.Block {
 	@Override
 	public RenderShape getRenderShape(BlockState state){
 		return type.isInvisible() ? RenderShape.INVISIBLE : RenderShape.MODEL;
+	}
+
+	@Override
+	public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit){
+		if(level.isClientSide || hand == InteractionHand.OFF_HAND) return InteractionResult.PASS;
+		if(!player.isCrouching() && type.getFunctions().size() > 0){
+			for(BlockFunction func : type.getFunctions()){
+				if(func.onClick(getWorld(level), getPos(pos), new V3D(0, 0, 0), StateWrapper.of(state), getSide(hit.getDirection()), UniEntity.getCasted(player), hand == InteractionHand.MAIN_HAND)) return InteractionResult.SUCCESS;
+			}
+		}
+		return InteractionResult.PASS;
 	}
 
 }

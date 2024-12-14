4567309606bd12001e736476c5785e84bc632e39
Ferdinand Calo <fexcalo@gmail.com>
2024-12-14 19:09:27 +0100

    Update VehicleItemModel.java

--- forge12/src/main/java/net/fexcraft/mod/fvtm/model/VehicleItemModel.java
+++ forge12/src/main/java/net/fexcraft/mod/fvtm/model/VehicleItemModel.java
@@ -25,18 +25,17 @@ public class VehicleItemModel implements FCLItemModel {
 
 	@Override
 	public void renderItem(ItemCameraTransforms.TransformType type, ItemStack item, EntityLivingBase entity){
-		if(item.getItem() instanceof VehicleItem == false){ return; }
+		if(item.getItem() instanceof VehicleItem == false) return;
 		VehicleData data = item.getCapability(Capabilities.VAPDATA, null).getVehicleData();
-		if(data == null){ return; }
+		if(data == null) return;
 		VehicleModel model = (VehicleModel)data.getType().getModel();
-		if(model == null){
-			EffectRenderer.preMeshCalls();
+		if(model == null || model == DefaultModel.EMPTY){
 			DebugModels.SPHERE_RED.render(0.5f);
 			RGB.glColorReset();
-			EffectRenderer.postMeshCalls();
 			return;
 		}
 		//
+		GL11.glPushMatrix();
 		Vec3f translate = model.item_translate.get(type.name());
 		GL11.glTranslatef(translate.x, translate.y, translate.z);
 		if(data.getType().isTrailer() && !data.getType().getVehicleType().isRailVehicle()){
@@ -48,7 +47,6 @@ public class VehicleItemModel implements FCLItemModel {
 			}
 		}
 		Vec3f rotate = model.item_rotate.get(type.name());
-		GL11.glPushMatrix();
 		GL11.glRotatef(rotate.y, 0F, 1F, 0F);
 		if(type == ItemCameraTransforms.TransformType.THIRD_PERSON_RIGHT_HAND || type == ItemCameraTransforms.TransformType.THIRD_PERSON_LEFT_HAND){
 			GL11.glRotatef(data.getType().getVehicleType().isAirVehicle() ? -90f : 90f, 0F, 1F, 0F);

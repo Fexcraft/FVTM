61c450002d740221c5049fa44f849dea312b6404
Ferdinand Calo <fexcalo@gmail.com>
2024-12-24 21:36:43 +0100

    Update WireModel.java

--- neutral/src/net/fexcraft/mod/fvtm/model/content/WireModel.java
+++ neutral/src/net/fexcraft/mod/fvtm/model/content/WireModel.java
@@ -22,6 +22,7 @@ public class WireModel extends DefaultModel {
 	public static final WireModel EMPTY = new WireModel();
 	public ArrayList<V3D[]> wire_model = new ArrayList<>();
 	public ArrayList<float[]> wire_vv = new ArrayList<>();
+	public ArrayList<Float> wire_ang = new ArrayList<>();
 
 	@Override
 	public WireModel parse(ModelData data){
@@ -41,8 +42,9 @@ public class WireModel extends DefaultModel {
 				float io = args.length > 7 ? Float.parseFloat(args[7]) : 0;
 				float iw = args.length > 8 ? Float.parseFloat(args[8]) : 1;
 				float ih = args.length > 9 ? Float.parseFloat(args[9]) : 1;
+				float an = args.length > 10 ? Float.parseFloat(args[10]) : 0;
 				if(rect){
-					this.addWireRect(scale, sx, sy, w, h, m, new float[]{ io, iw, ih });
+					this.addWireRect(scale, sx, sy, w, h, m, new float[]{ io, iw, ih }, an);
 				}
 				else{
 					//V3D tl = new V3D(args, 7), tr = new V3D(args, 10), bl = new V3D(args, 13), br = new V3D(args, 16);
@@ -51,25 +53,27 @@ public class WireModel extends DefaultModel {
 			}
 		}
 		if(data.has("Wires")){
-			JsonMap rails = data.getMap("Wires");
+			JsonMap wires = data.getMap("Wires");
 			JsonMap map = null;
-			for(Map.Entry<String, JsonValue<?>> entry : rails.entries()){
+			for(Map.Entry<String, JsonValue<?>> entry : wires.entries()){
 				map = entry.getValue().asMap();
 				float scl = map.getFloat("scale", 0.0625f);
 				float[] pos = map.has("pos") ? map.getArray("pos").toFloatArray() : new float[]{ -.5f, -.5f };
 				float[] siz = map.has("size") ? map.getArray("size").toFloatArray() : new float[]{ 1, 1 };
 				float[] vv = map.has("v") ? map.getArray("v").toFloatArray() : new float[]{ 0, 0.125f, 0.125f };
+				float ang = map.getFloat("angle", 0);
 				boolean mir = map.getBoolean("mirror", false);
-				addWireRect(scl, pos[0], pos[1], siz[0], siz[1], mir, vv);
+				addWireRect(scl, pos[0], pos[1], siz[0], siz[1], mir, vv, ang);
 			}
 		}
 		return this;
 	}
 
-	public void addWireRect(float scale, float start_x, float start_y, float width, float height, boolean mirror, float[] vv){
+	public void addWireRect(float scale, float start_x, float start_y, float width, float height, boolean mirror, float[] vv, float ang){
 		wire_model.add(new V3D[]{ new V3D(start_x + width, start_y, 0).scale(scale), new V3D(start_x, start_y, 0).scale(scale) });
 		float buff = 0;
 		wire_vv.add(new float[]{ vv[0], vv[0] + (buff += vv[1])});
+		wire_ang.add(ang);
 		if(height > 0){
 			wire_model.add(new V3D[]{ new V3D(start_x, start_y, 0).scale(scale), new V3D(start_x, start_y - height, 0).scale(scale) });
 			wire_vv.add(new float[]{ vv[0] + buff, vv[0] + (buff += vv[2]) });
@@ -77,13 +81,17 @@ public class WireModel extends DefaultModel {
 			wire_vv.add(new float[]{ vv[0] + buff, vv[0] + (buff += vv[2]) });
 			wire_model.add(new V3D[]{ new V3D(start_x, start_y - height, 0).scale(scale), new V3D(start_x + width, start_y - height, 0).scale(scale) });
 			wire_vv.add(new float[]{  vv[0] + buff, vv[0] + (buff + vv[1])  });
+			wire_ang.add(ang);
+			wire_ang.add(ang);
+			wire_ang.add(ang);
 		}
 		else{
 			float h = 0.01f / scale;
 			wire_model.add(new V3D[]{ new V3D(start_x, start_y - h, 0).scale(scale), new V3D(start_x + width, start_y - h, 0).scale(scale) });
 			wire_vv.add(new float[]{  vv[0] + buff, vv[0] + (buff + vv[1])  });
+			wire_ang.add(ang);
 		}
-		if(mirror) addWireRect(scale, -start_x - width, start_y, width, height, false, vv);
+		if(mirror) addWireRect(scale, -start_x - width, start_y, width, height, false, vv, ang);
 	}
 
 }

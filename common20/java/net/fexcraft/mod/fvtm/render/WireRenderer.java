9b389e51fe6ae75cc948a70c6910065bcad84bbd
Ferdinand Calo <fexcalo@gmail.com>
2024-12-25 23:17:37 +0100

    Create WireRenderer.java

---
+++ common20/java/net/fexcraft/mod/fvtm/render/WireRenderer.java
@@ -0,0 +1,11 @@
+package net.fexcraft.mod.fvtm.render;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class WireRenderer {
+
+	public static final double ANGLE = 0;
+	public static final double ANGLE_DOWN = 0;
+
+}

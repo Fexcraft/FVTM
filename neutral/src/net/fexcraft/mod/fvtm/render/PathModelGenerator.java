1f3d9e6ffc1afbc7cf890c02095241ac2ed1cd7b
Ferdinand Calo <fexcalo@gmail.com>
2024-12-27 11:21:16 +0100

    Update PathModelGenerator.java

--- neutral/src/net/fexcraft/mod/fvtm/render/PathModelGenerator.java
+++ neutral/src/net/fexcraft/mod/fvtm/render/PathModelGenerator.java
@@ -138,15 +138,16 @@ public class PathModelGenerator {
 			for(int k = 0; k < wire.vecpath.length - 1; k++){
 				nbuf += (float)wire.vecpath[k].dis(wire.vecpath[k + 1]);
 				if(nbuf > 1f){
-					nbuf = obuf - nbuf;
-					obuf = 0;
+					nbuf -= 1f;
+					obuf -= 1f;
 				}
 				vert0 = new TexturedVertex(path.get(k * 2), obuf * u, vv[1]);
 				vert1 = new TexturedVertex(path.get(k * 2 + 1), obuf * u, vv[0]);
 				vert2 = new TexturedVertex(path.get((k + 1) * 2), nbuf * u, vv[1]);
 				vert3 = new TexturedVertex(path.get((k + 1) * 2 + 1), nbuf * u, vv[0]);
 				poly0 = new TexturedPolygon(new TexturedVertex[]{ vert1, vert0, vert2, vert3 });
-				int pess = (int)passed; if(pess >= tarp.hedrons.length) pess = tarp.hedrons.length - 1;
+				int pess = (int)passed;
+				if(pess >= tarp.hedrons.length) pess = tarp.hedrons.length - 1;
 				tarp.hedrons[pess].importMRT(poly0, 1f);
 				passed += wire.vecpath[k].dis(wire.vecpath[k + 1]);
 				obuf = nbuf;

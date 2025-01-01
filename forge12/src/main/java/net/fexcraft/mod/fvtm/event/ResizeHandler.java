c8c4453f11a7acf380feb7f3ef554a4ec1ec9e92
Ferdinand Calo <fexcalo@gmail.com>
2025-01-01 09:59:15 +0100

    Update ResizeHandler.java

--- forge12/src/main/java/net/fexcraft/mod/fvtm/event/ResizeHandler.java
+++ forge12/src/main/java/net/fexcraft/mod/fvtm/event/ResizeHandler.java
@@ -1,7 +1,7 @@
 package net.fexcraft.mod.fvtm.event;
 
-import net.fexcraft.mod.fvtm.sys.uni.GenericVehicle;
-import net.fexcraft.mod.fvtm.sys.uni.SeatCache;
+import net.fexcraft.mod.fvtm.sys.uni.RootVehicle;
+import net.fexcraft.mod.fvtm.sys.uni.SeatInstance;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.math.AxisAlignedBB;
@@ -29,11 +29,11 @@ public class ResizeHandler {
     }
     
     private boolean resize(Entity entity) throws Exception {
-    	if(entity.getRidingEntity() instanceof GenericVehicle == false) return false;
-    	SeatCache cache = null;//TODO ((GenericVehicle)entity.getRidingEntity()).getSeatOf(entity);
-    	if(cache == null || cache.seatdata == null || cache.seatdata.scale == null) return false;
-    	float width = entity.width * cache.seatdata.scale;
-		float height = entity.height * cache.seatdata.scale;
+    	if(entity.getRidingEntity() instanceof RootVehicle == false) return false;
+    	SeatInstance cache = ((RootVehicle)entity.getRidingEntity()).getSeatOf(entity);
+    	if(cache == null || cache.seat == null || cache.seat.scale == null) return false;
+    	float width = entity.width * cache.seat.scale;
+		float height = entity.height * cache.seat.scale;
 		entity.ignoreFrustumCheck = true;
 		boolean isplayer = entity instanceof EntityPlayer;
 		if(isplayer){
@@ -44,17 +44,17 @@ public class ResizeHandler {
 		entity.setEntityBoundingBox(new AxisAlignedBB(entity.posX - hw, entity.posY + (isplayer ? SITH : 0), entity.posZ - hw, entity.posX + hw, entity.posY + height + (isplayer ? SITH : 0), entity.posZ + hw));
         if(isplayer){
         	EntityPlayer player = (EntityPlayer)entity;
-			player.eyeHeight = player.getDefaultEyeHeight() * cache.seatdata.scale;
+			player.eyeHeight = player.getDefaultEyeHeight() * cache.seat.scale;
 			player.eyeHeight += SITH;
         }
         return true;
 	}
 
 	public static float getScale(Entity entity){
-    	if(entity.getRidingEntity() instanceof GenericVehicle == false) return 1f;
-    	SeatCache cache = null;//TODO ((GenericVehicle)entity.getRidingEntity()).getSeatOf(entity);
-    	if(cache == null || cache.seatdata == null || cache.seatdata.scale == null) return 1f;
-    	return cache.seatdata.scale;
+    	if(entity.getRidingEntity() instanceof RootVehicle == false) return 1f;
+    	SeatInstance cache = ((RootVehicle)entity.getRidingEntity()).getSeatOf(entity);
+    	if(cache == null || cache.seat == null || cache.seat.scale == null) return 1f;
+    	return cache.seat.scale;
 	}
 	
 }

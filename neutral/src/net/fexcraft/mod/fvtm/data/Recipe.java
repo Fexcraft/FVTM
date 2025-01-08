2dda911a2077f94fbfd761157f42c325b317179e
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-08 21:18:45 +0100

    Update Recipe.java

--- neutral/src/net/fexcraft/mod/fvtm/data/Recipe.java
+++ neutral/src/net/fexcraft/mod/fvtm/data/Recipe.java
@@ -33,23 +33,29 @@ public class Recipe extends Content<Recipe> {
         String type = map.getString("Type", "fcl").toLowerCase();
         fcl = type.equals("fcl:crafting");
         output = parseStack(null, map.getMap("Result"));
+        boolean v12 = EnvInfo.is112();
+        boolean v20 = EnvInfo.is120();
         for(Map.Entry<String, JsonValue<?>> entry : map.get("Components").asMap().entries()){
             JsonMap val = entry.getValue().asMap();
             String ctype = val.getString("type", "item");
             switch(ctype){
                 case "item":{
+                    if(val.has("1.12") && !v12) continue;
                     StackWrapper item = parseStack(entry.getKey(), val);
                     if(!item.empty()) fclcomps.add(new FclRecipe.Component(item));
                     break;
                 }
                 case "ore":{
-                    if(!EnvInfo.is112()) continue;
+                    if(!v12) continue;
                     fclcomps.add(new FclRecipe.Component(entry.getKey(), val.getInteger("amount", 1)));
                     break;
                 }
                 case "tag":{
-                    if(EnvInfo.is112()) continue;
-                    fclcomps.add(new FclRecipe.Component(entry.getKey(), val.getInteger("amount", 1)));
+                    if(v12) continue;
+                    String key = entry.getKey();
+                    if(v20 && key.startsWith("c:")) key = key.replace("c:", "forge:");
+                    else if(!v20 && key.startsWith("forge:")) key = key.replace("forge:", "c:");
+                    fclcomps.add(new FclRecipe.Component(key, val.getInteger("amount", 1)));
                     break;
                 }
             }

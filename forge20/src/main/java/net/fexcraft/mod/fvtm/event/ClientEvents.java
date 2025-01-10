68fc33c4c8fce089900be2dbaab151937d17030b
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-10 23:03:27 +0100

    Update ClientEvents.java

--- forge20/src/main/java/net/fexcraft/mod/fvtm/event/ClientEvents.java
+++ forge20/src/main/java/net/fexcraft/mod/fvtm/event/ClientEvents.java
@@ -60,6 +60,7 @@ public class ClientEvents {
 	@SubscribeEvent
 	public static void renderInit(EntityRenderersEvent.RegisterRenderers event){
 		event.registerBlockEntityRenderer(FvtmGetters.LIFT_ENTITY.get(), con -> new VehicleLiftRenderer());
+		event.registerBlockEntityRenderer(FvtmGetters.CONST_ENTITY.get(), con -> new ConstRenderer());
 		event.registerBlockEntityRenderer(FvtmGetters.BLOCK_ENTITY.get(), con -> new BaseBlockRenderer());
 	}
 

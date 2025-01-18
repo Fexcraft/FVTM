96f1a5e2172de73cf4ed95aca29add8a860a1bb5
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-18 20:10:18 +0100

    Create FuelFillerRenderer.java

---
+++ forge12/src/main/java/net/fexcraft/mod/fvtm/render/FuelFillerRenderer.java
@@ -0,0 +1,33 @@
+package net.fexcraft.mod.fvtm.render;
+
+import net.fexcraft.mod.fvtm.FvtmResources;
+import net.fexcraft.mod.fvtm.block.FuelFillerEntity;
+import net.fexcraft.mod.fvtm.data.block.BlockType;
+import net.fexcraft.mod.fvtm.model.DefaultModel;
+import net.fexcraft.mod.fvtm.model.ModelData;
+import net.fexcraft.mod.fvtm.model.content.BlockModel;
+import net.fexcraft.mod.fvtm.util.TexUtil;
+import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class FuelFillerRenderer extends TileEntitySpecialRenderer<FuelFillerEntity> {
+
+	public static final ResourceLocation TEXTURE = new ResourceLocation("fvtm:textures/block/fuelfiller.png");
+	public static BlockModel MODEL;
+
+	@Override
+	public void render(FuelFillerEntity tile, double x, double y, double z, float ticks, int stage, float a){
+		GL11.glPushMatrix();
+		GL11.glTranslated(x + 0.5, y, z + 0.5);
+		GL11.glRotated(BlockType.GENERIC_4ROT.getRotationFor(tile.getBlockMetadata()) - 180, 0, 1, 0);
+		TexUtil.bindTexture(TEXTURE);
+		if(MODEL == null) MODEL = (BlockModel)FvtmResources.getModel("fvtm:models/block/fuelfiller.fmf", new ModelData(), BlockModel.class);
+		if(MODEL != null) MODEL.render(DefaultModel.RENDERDATA.set(null, null, null));
+		GL11.glPopMatrix();
+	}
+
+}

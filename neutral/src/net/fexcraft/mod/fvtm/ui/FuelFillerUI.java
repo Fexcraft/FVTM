4b5b18a5db3b291971099048b86dbfd174f54628
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-19 19:44:50 +0100

    Create FuelFillerUI.java

---
+++ neutral/src/net/fexcraft/mod/fvtm/ui/FuelFillerUI.java
@@ -0,0 +1,16 @@
+package net.fexcraft.mod.fvtm.ui;
+
+import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.mod.uni.ui.ContainerInterface;
+import net.fexcraft.mod.uni.ui.UserInterface;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class FuelFillerUI extends UserInterface {
+
+	public FuelFillerUI(JsonMap map, ContainerInterface container) throws Exception{
+		super(map, container);
+	}
+
+}

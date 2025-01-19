97632b7d29ff4203c8361aedd01a58be52433948
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-19 20:18:26 +0100

    Update FuelFillerCon.java

--- neutral/src/net/fexcraft/mod/fvtm/ui/FuelFillerCon.java
+++ neutral/src/net/fexcraft/mod/fvtm/ui/FuelFillerCon.java
@@ -2,19 +2,24 @@ package net.fexcraft.mod.fvtm.ui;
 
 import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.lib.common.math.V3I;
+import net.fexcraft.mod.fvtm.data.FuelFiller;
 import net.fexcraft.mod.uni.UniEntity;
 import net.fexcraft.mod.uni.item.UniInventory;
 import net.fexcraft.mod.uni.ui.ContainerInterface;
-import net.fexcraft.mod.uni.ui.UserInterface;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public class FuelFillerCon extends ContainerInterface {
 
+	protected FuelFiller.FuelFillerContainer tile;
+	protected FuelFiller filler;
+
 	public FuelFillerCon(JsonMap map, UniEntity ply, V3I pos){
 		super(map, ply, pos);
 		inventory = UniInventory.create(2).drop(true);
+		tile = (FuelFiller.FuelFillerContainer)ply.entity.getWorld().getBlockEntity(pos);
+		filler = tile.getFuelFiller();
 	}
 
 }

f1a5e82070c505824baa25598880ff87a3791f9a
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-19 20:18:31 +0100

    Update FuelFillerUI.java

--- neutral/src/net/fexcraft/mod/fvtm/ui/FuelFillerUI.java
+++ neutral/src/net/fexcraft/mod/fvtm/ui/FuelFillerUI.java
@@ -9,8 +9,26 @@ import net.fexcraft.mod.uni.ui.UserInterface;
  */
 public class FuelFillerUI extends UserInterface {
 
+	private FuelFillerCon con;
+
 	public FuelFillerUI(JsonMap map, ContainerInterface container) throws Exception{
 		super(map, container);
+		con = (FuelFillerCon)container;
+	}
+
+	@Override
+	public void init(){
+		texts.get("selected").value(con.filler.selected.getName());
+		texts.get("info").value("ui.fvtm.fuel_filler.warning");
+		texts.get("info").translate();
+	}
+
+	@Override
+	public void predraw(float t, int x, int y){
+		texts.get("stored").value("ui.fvtm.fuel_filler.stored");
+		texts.get("stored").translate(con.filler.stored);
+		texts.get("converted").value("ui.fvtm.fuel_filler.converted");
+		texts.get("converted").translate(con.filler.converted);
 	}
 
 }

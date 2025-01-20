773c79da5ad390f1f267d97b41dac74407aade67
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-20 14:32:13 +0100

    Update FuelFiller.java

--- neutral/src/net/fexcraft/mod/fvtm/data/FuelFiller.java
+++ neutral/src/net/fexcraft/mod/fvtm/data/FuelFiller.java
@@ -1,8 +1,8 @@
 package net.fexcraft.mod.fvtm.data;
 
 import net.fexcraft.mod.fvtm.FvtmRegistry;
-import net.fexcraft.mod.fvtm.FvtmResources;
 import net.fexcraft.mod.uni.IDL;
+import net.fexcraft.mod.uni.IDLManager;
 import net.fexcraft.mod.uni.tag.TagCW;
 
 /**
@@ -10,25 +10,35 @@ import net.fexcraft.mod.uni.tag.TagCW;
  */
 public class FuelFiller {
 
-	public int converted;
-	public int stored;
+	public long converted;
+	public long stored;
 	public IDL fluid;
-	public String category;
 	public Fuel selected;
 
 	public FuelFiller(){
-		category = FvtmRegistry.FUELS.get(0).primary;
 		selected = FvtmRegistry.FUELS.get(0);
+		fluid = IDLManager.getIDLCached("minecraft:lava");
 	}
 
 	public TagCW save(){
 		TagCW com = TagCW.create();
-
+		com.set("fuel", selected.getIDS());
+		com.set("stored", stored);
+		com.set("converted", converted);
+		com.set("fluid", fluid.colon());
 		return com;
 	}
 
 	public void load(TagCW com){
+		selected = FvtmRegistry.getFuel(com.getString("fuel"));
+		if(selected == null) selected = FvtmRegistry.FUELS.get(0);
+		stored = com.getLong("stored");
+		converted = com.getLong("converted");
+		fluid = IDLManager.getIDLCached("fluid");
+	}
 
+	public void update(){
+		//
 	}
 
 	public static interface FuelFillerContainer {

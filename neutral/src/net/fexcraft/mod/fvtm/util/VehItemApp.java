c8d390a46ff9a32e84334678506e10984c6a155b
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-21 23:03:15 +0100

    Update VehItemApp.java

--- neutral/src/net/fexcraft/mod/fvtm/util/VehItemApp.java
+++ neutral/src/net/fexcraft/mod/fvtm/util/VehItemApp.java
@@ -5,22 +5,23 @@ import net.fexcraft.mod.fvtm.data.vehicle.VehicleData;
 import net.fexcraft.mod.uni.Appendable;
 import net.fexcraft.mod.uni.item.ItemType;
 import net.fexcraft.mod.uni.item.StackWrapper;
+import net.fexcraft.mod.uni.item.UniStack;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
-public class VehItemApp implements Appendable<StackWrapper> {
+public class VehItemApp implements Appendable<UniStack> {
 
 	public final VehicleData data;
 
-	public VehItemApp(StackWrapper stack){
+	public VehItemApp(UniStack stack){
 		if(stack == null) data = null;
-		else data = stack.getContent(ContentType.VEHICLE);
+		else data = stack.stack.getContent(ContentType.VEHICLE);
 	}
 
 	@Override
-	public Appendable<StackWrapper> create(StackWrapper stack){
-		if(!stack.isItemOf(ItemType.VEHICLE)) return null;
+	public Appendable<UniStack> create(UniStack stack){
+		if(!stack.stack.isItemOf(ItemType.VEHICLE)) return null;
 		return new VehItemApp(stack);
 	}
 

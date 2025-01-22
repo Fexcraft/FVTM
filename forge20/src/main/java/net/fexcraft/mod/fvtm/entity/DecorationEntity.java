5c896ee6992e5cb4e829473ffd05290d80b7c10a
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-22 14:13:15 +0100

    Update DecorationEntity.java

--- forge20/src/main/java/net/fexcraft/mod/fvtm/entity/DecorationEntity.java
+++ forge20/src/main/java/net/fexcraft/mod/fvtm/entity/DecorationEntity.java
@@ -10,6 +10,7 @@ import net.fexcraft.mod.fvtm.packet.Packets;
 import net.fexcraft.mod.fvtm.ui.UIKeys;
 import net.fexcraft.mod.uni.UniEntity;
 import net.fexcraft.mod.uni.item.StackWrapper;
+import net.fexcraft.mod.uni.item.UniStack;
 import net.fexcraft.mod.uni.tag.TagCW;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.nbt.ListTag;
@@ -119,7 +120,7 @@ public class DecorationEntity extends Entity {
 		}
 		if(stack.isEmpty() || stack.getItem() instanceof DecorationItem){
 			if(stack.getItem() instanceof DecorationItem){
-				DecorationData data = ((DecorationItem)stack.getItem()).getData(StackWrapper.wrap(stack));
+				DecorationData data = ((DecorationItem)stack.getItem()).getData(UniStack.getStack(stack));
 				if(data != null){
 					decos.add(data);
 					updateClient();

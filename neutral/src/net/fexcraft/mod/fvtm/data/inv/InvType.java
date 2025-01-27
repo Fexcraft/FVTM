c7b1d51db39f1ec006bad8433e566a67e4408dc2
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-27 23:29:51 +0100

    Update InvType.java

--- neutral/src/net/fexcraft/mod/fvtm/data/inv/InvType.java
+++ neutral/src/net/fexcraft/mod/fvtm/data/inv/InvType.java
@@ -2,76 +2,75 @@ package net.fexcraft.mod.fvtm.data.inv;
 
 import net.fexcraft.lib.common.math.RGB;
 import net.fexcraft.mod.fvtm.ui.UIKeys;
+import net.fexcraft.mod.uni.ui.UIKey;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
-public class InvType {
+public enum InvType {
 
-	public static InvType ITEM = new InvType("item", new RGB("733f00"), "stacks");
-	public static InvType FLUID = new InvType("fluid", new RGB("02d9e0"), "mB");
-	public static InvType ENERGY = new InvType("energy", new RGB("0b8c01"), "FE");
-	public static InvType CONTAINER = new InvType("container", new RGB("c40202"), "CNTNRS");
-	public static InvType VARIABLE = new InvType("variable", null, null, true);
+	STACK("stack", new RGB("733f00"), "stacks"),
+	ITEM("item", new RGB("c96f02"), "stacks"),
+	FLUID("fluid", new RGB("02d9e0"), "mB"),
+	ENERGY("energy", new RGB("0b8c01"), "FE"),
+	CONTAINER("container", new RGB("c40202"), "con"),
+	VARIABLE("variable", new RGB("db0272"), "var");
 
 	public final RGB color;
 	public final String name;
-	public final boolean variable;
 	public final String unit_suffix;
 
-	public InvType(String name, RGB color, String suffix){
-		this(name, color, suffix, false);
-	}
-
-	public InvType(String name, RGB color, String suffix, boolean variable){
-		this.color = color == null ? new RGB("db0272") : color;
-		unit_suffix = suffix == null ? "vr" : suffix;
-		this.variable = variable;
-		this.name = name;
-	}
-
-	public boolean isFluid(){
-		return this == FLUID;
+	InvType(String nome, RGB rgb, String suffix){
+		color = rgb;
+		unit_suffix = suffix;
+		name = nome;
 	}
 
 	public boolean isItem(){
 		return this == ITEM;
 	}
 
+	public boolean isStack(){
+		return this == STACK;
+	}
+
+	public boolean isFluid(){
+		return this == FLUID;
+	}
+
+	public boolean isEnergy(){
+		return this == ENERGY;
+	}
+
+	public boolean isVariable(){
+		return this == VARIABLE;
+	}
+
 	public boolean isContainer(){
 		return this == CONTAINER;
 	}
 
-	public boolean isVariable(){
-		return variable;
-	}
-
-	public static InvType parse(String str, boolean vars){
+	public static InvType parse(String str){
 		str = str.toLowerCase();
+		for(InvType type : values()){
+			if(type.name.equals(str)) return type;
+		}
 		switch(str){
-			case "item": return ITEM;
-			case "fluid": return FLUID;
-			case "energy": return ENERGY;
+			case "items": return ITEM;
+			case "stacks": return STACK;
 			case "var":
-			case "value":
-			case "variable":{
-				if(vars) return VARIABLE;
-				else break;
-			}
-			case "container": return CONTAINER;
+			case "value": return VARIABLE;
 		}
 		return ITEM;
 	}
 
-	public String name(){
-		return name;
-	}
-
-	public int guiId(){
-		if(isItem()) return UIKeys.MULTIBLOCK_INVENTORY_ITEM.id;
-		if(isFluid()) return UIKeys.MULTIBLOCK_INVENTORY_FLUID.id;
-		if(isVariable()) return UIKeys.MULTIBLOCK_INVENTORY_VAR.id;
-		return 0;
+	public UIKey getMBUI(){
+		switch(this){
+			case ITEM: return UIKeys.MULTIBLOCK_INVENTORY_ITEM;
+			case FLUID: return UIKeys.MULTIBLOCK_INVENTORY_FLUID;
+			case VARIABLE: return UIKeys.MULTIBLOCK_INVENTORY_VAR;
+		}
+		return null;
 	}
 
 }

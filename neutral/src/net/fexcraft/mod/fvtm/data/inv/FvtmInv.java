417bb05f1c91737a2b11ec859dbce8b2fdbe836e
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-31 22:41:01 +0100

    Create FvtmInv.java

---
+++ neutral/src/net/fexcraft/mod/fvtm/data/inv/FvtmInv.java
@@ -0,0 +1,36 @@
+package net.fexcraft.mod.fvtm.data.inv;
+
+import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.lib.common.math.V3D;
+import net.fexcraft.mod.uni.tag.TagCW;
+
+import java.util.ArrayList;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public abstract class FvtmInv {
+
+	public ArrayList<String> access;
+	public final InvType type;
+	public String point;
+	public V3D pos;
+
+	protected FvtmInv(InvType type){
+		this.type = type;
+	}
+
+	public void init0(JsonMap map){
+		if(map.has("access")) access = map.getArray("access").toStringList();
+		else access = new ArrayList<>();
+	}
+
+	public abstract <Inv extends FvtmInv> Inv init(JsonMap map);
+
+	public abstract TagCW save(TagCW com, String ctag);
+
+	public abstract void load(TagCW com, String ctag);
+
+	public abstract <Inv extends FvtmInv> Inv copy();
+
+}

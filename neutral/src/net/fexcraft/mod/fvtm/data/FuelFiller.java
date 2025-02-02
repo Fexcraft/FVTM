1cc2e55547f141d1d1eb343b3644a8d8e2a17c45
Ferdinand Calo <fercalo96@yahoo.de>
2025-02-02 22:50:43 +0100

    Update FuelFiller.java

--- neutral/src/net/fexcraft/mod/fvtm/data/FuelFiller.java
+++ neutral/src/net/fexcraft/mod/fvtm/data/FuelFiller.java
@@ -3,6 +3,8 @@ package net.fexcraft.mod.fvtm.data;
 import net.fexcraft.mod.fvtm.FvtmRegistry;
 import net.fexcraft.mod.uni.IDL;
 import net.fexcraft.mod.uni.IDLManager;
+import net.fexcraft.mod.uni.inv.StackWrapper;
+import net.fexcraft.mod.uni.inv.UniFluidTank;
 import net.fexcraft.mod.uni.inv.UniInventory;
 import net.fexcraft.mod.uni.inv.UniStack;
 import net.fexcraft.mod.uni.tag.TagCW;
@@ -13,6 +15,7 @@ import net.fexcraft.mod.uni.tag.TagCW;
 public class FuelFiller {
 
 	public UniInventory items;
+	public UniFluidTank tank;
 	public long converted;
 	public long stored;
 	public IDL fluid;
@@ -23,6 +26,7 @@ public class FuelFiller {
 		fluid = IDLManager.getIDLCached("minecraft:lava");
 		items = UniInventory.create(2).stacksize(1);
 		items.addValidator(1, (idx, stack) -> stack.getItem().direct() instanceof Fuel.FuelItem);
+		tank = UniFluidTank.create(16000);
 	}
 
 	public TagCW save(){
@@ -31,6 +35,7 @@ public class FuelFiller {
 		com.set("stored", stored);
 		com.set("converted", converted);
 		com.set("fluid", fluid.colon());
+		com.set("tank", tank.save());
 		for(int i = 0; i < 2; i++){
 			if(items.get(i).empty()) continue;
 			TagCW tag = TagCW.create();
@@ -46,6 +51,8 @@ public class FuelFiller {
 		stored = com.getLong("stored");
 		converted = com.getLong("converted");
 		fluid = IDLManager.getIDLCached("fluid");
+		tank.load(com.getCompound("tank"));
+		tank.addValidator(stack -> selected.isSourceFluid(tank.getFluidFromStack(stack)));
 		for(int i = 0; i < 2; i++){
 			if(!com.has("item" + i)) continue;
 			items.set(i, UniStack.createStack(com.getCompound("item" + i)));
@@ -53,7 +60,9 @@ public class FuelFiller {
 	}
 
 	public void update(){
-		//
+		StackWrapper stack = items.get(0);
+		if(stack.empty() || !tank.isValid(stack)) return;
+
 	}
 
 	public static interface FuelFillerContainer {

3e21d7637baccfc11966040c877e4357f8a241e7
Ferdinand Calo <fexcalo@gmail.com>
2025-02-16 11:55:00 +0100

    Create FuelFillerBlock.java

---
+++ forge20/src/main/java/net/fexcraft/mod/fvtm/block/FuelFillerBlock.java
@@ -0,0 +1,69 @@
+package net.fexcraft.mod.fvtm.block;
+
+import net.fexcraft.mod.fvtm.FvtmGetters;
+import net.fexcraft.mod.fvtm.ui.UIKeys;
+import net.fexcraft.mod.uni.UniEntity;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.phys.BlockHitResult;
+
+import javax.annotation.Nullable;
+
+import static net.fexcraft.mod.fvtm.block.generated.FvtmProperties.FACING;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class FuelFillerBlock extends Block implements EntityBlock {
+
+    public FuelFillerBlock(){
+        super(Properties.of().noOcclusion().explosionResistance(64).strength(2));
+    }
+
+    @Override
+    public BlockEntity newBlockEntity(BlockPos pos, BlockState state){
+        return FvtmGetters.FUELFILLER_ENT.get().create(pos, state);
+    }
+
+    @Override
+    public RenderShape getRenderShape(BlockState pState){
+        return RenderShape.INVISIBLE;
+    }
+
+    @Override
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult res){
+        if(!level.isClientSide && hand != InteractionHand.OFF_HAND){
+            UniEntity.getEntity(player).openUI(UIKeys.FUEL_FILLER, pos.getX(), pos.getY(), pos.getZ());
+        }
+        return InteractionResult.SUCCESS;
+    }
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> sd){
+        sd.add(FACING);
+    }
+
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return defaultBlockState().setValue(FACING, context.getPlayer().getDirection().getOpposite());
+    }
+
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState state, BlockEntityType<T> type){
+        return (BlockEntityTicker<T>)new FuelFillerEntity.Ticker();
+    }
+
+}
+

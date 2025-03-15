53e1af902f3efa7470a53a1810ef61a01397e234
Ferdinand Calo <fexcalo@gmail.com>
2025-03-15 20:30:12 +0100

    Update Handler_VehMove.java

--- neutral/src/net/fexcraft/mod/fvtm/packet/Handler_VehMove.java
+++ neutral/src/net/fexcraft/mod/fvtm/packet/Handler_VehMove.java
@@ -2,6 +2,7 @@ package net.fexcraft.mod.fvtm.packet;
 
 import net.fexcraft.mod.fvtm.sys.uni.FvtmWorld;
 import net.fexcraft.mod.fvtm.sys.uni.Passenger;
+import net.fexcraft.mod.fvtm.sys.uni.VehicleInstance;
 import net.fexcraft.mod.uni.packet.PacketHandler;
 import net.fexcraft.mod.uni.world.EntityW;
 
@@ -12,12 +13,18 @@ public class Handler_VehMove implements PacketHandler<Packet_VehMove> {
 
 	@Override
 	public Runnable handleServer(Packet_VehMove packet, EntityW player){
-		return () -> ((FvtmWorld)player.getWorld()).onVehicleMove(packet);
+		return () -> {
+			VehicleInstance veh = ((FvtmWorld)player.getWorld()).getVehicle(packet.entid);
+			if(veh != null) veh.onVehMovePkt(packet);
+		};
 	}
 
 	@Override
 	public Runnable handleClient(Packet_VehMove packet, EntityW player){
-		return () -> ((FvtmWorld)player.getWorld()).onVehicleMove(packet);
+		return () -> {
+			VehicleInstance veh = ((FvtmWorld)player.getWorld()).getVehicle(packet.entid);
+			if(veh != null) veh.onVehMovePkt(packet);
+		};
 	}
 
 

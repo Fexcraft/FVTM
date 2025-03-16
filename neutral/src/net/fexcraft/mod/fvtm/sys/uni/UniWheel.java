f0bcc715c744e4dabdf3f685f1e85f1b0e56a987
Ferdinand Calo <fexcalo@gmail.com>
2025-03-16 18:20:15 +0100

    Update UniWheel.java

--- neutral/src/net/fexcraft/mod/fvtm/sys/uni/UniWheel.java
+++ neutral/src/net/fexcraft/mod/fvtm/sys/uni/UniWheel.java
@@ -1,14 +1,24 @@
 package net.fexcraft.mod.fvtm.sys.uni;
 
+import net.fexcraft.lib.common.math.V3D;
+
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public interface UniWheel {
 
-	public void setPosAsPrev();
+	public void updatePrevPos();
 
 	public void remove();
 
 	public boolean isAdded();
+
+	public V3D pos();
+
+	public void move(float yaw);
+
+	public WheelTireData wtd();
+
+	public void addMotion(double x, double y, double z);
 
 }

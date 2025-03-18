7ef88992b725243fabbd9b14f2a444f7ff41f55f
Ferdinand Calo <fexcalo@gmail.com>
2025-03-18 23:50:18 +0100

    Create ToolboxItem.java

---
+++ fabric21/src/main/java/net/fexcraft/mod/fvtm/item/ToolboxItem.java
@@ -0,0 +1,58 @@
+package net.fexcraft.mod.fvtm.item;
+
+import net.fexcraft.mod.uni.inv.StackWrapper;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
+
+import java.util.List;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class ToolboxItem extends Item {
+
+	public final int var;
+
+	public ToolboxItem(int var){
+		super(new Properties().stacksTo(1));
+		this.var = var;
+	}
+
+	@Override
+	public void appendHoverText(ItemStack stack, TooltipContext ctx, List<Component> tooltip, TooltipFlag flag){
+		switch(var){
+			case 0:{
+				tooltip.add(Component.literal("Part Removal and Maintenance Toolbox"));
+				break;
+			}
+			case 1:{
+				tooltip.add(Component.literal("Livery/Texture Management Toolbox"));
+				break;
+			}
+			case 2:{
+				tooltip.add(Component.literal("Color Channel Painting Toolbox"));
+				break;
+			}
+			case 3:{
+				tooltip.add(Component.literal("Wire removal Toolbox"));
+				break;
+			}
+			case 4:{
+				tooltip.add(Component.literal("Wire Slack Adjustment Toolbox"));
+				break;
+			}
+		}
+	}
+
+	public static int getToolboxType(ItemStack stack){
+		return ((ToolboxItem)stack.getItem()).var;
+	}
+
+	public static int getToolboxType(StackWrapper stack){
+		return ((ToolboxItem)stack.getItem().local()).var;
+	}
+
+}

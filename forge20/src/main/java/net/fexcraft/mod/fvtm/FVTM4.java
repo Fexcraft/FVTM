0fae5c514b87133aca1046025ceeb47b984305e9
Ferdinand Calo <fexcalo@gmail.com>
2025-03-19 15:35:18 +0100

    Update FVTM4.java

--- forge20/src/main/java/net/fexcraft/mod/fvtm/FVTM4.java
+++ forge20/src/main/java/net/fexcraft/mod/fvtm/FVTM4.java
@@ -153,7 +153,6 @@ public class FVTM4 {
 		if(EnvInfo.CLIENT){
 			CTab.IMPL[0] = TabInitializerF.class;
 		}
-		FVTM20.CONFIG = new Config(new File(FMLPaths.CONFIGDIR.get().toFile(), "fvtm.json"));
 		FVTM20.init0();
 		FvtmRegistry.ADDONS.forEach(addon -> ITEM_REGISTRY.put(addon.getID().id(), DeferredRegister.create(Registries.ITEM, addon.getID().id())));
 		FvtmRegistry.ADDONS.forEach(addon -> BLOCK_REGISTRY.put(addon.getID().id(), DeferredRegister.create(Registries.BLOCK, addon.getID().id())));

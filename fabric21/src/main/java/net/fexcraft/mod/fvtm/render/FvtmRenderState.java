d214b8329586fc49c3f71904a8652ed234827f4a
Ferdinand Calo <fexcalo@gmail.com>
2025-03-20 23:15:29 +0100

    Create FvtmRenderState.java

---
+++ fabric21/src/main/java/net/fexcraft/mod/fvtm/render/FvtmRenderState.java
@@ -0,0 +1,16 @@
+package net.fexcraft.mod.fvtm.render;
+
+import net.fexcraft.mod.fvtm.entity.RootVehicle;
+import net.fexcraft.mod.fvtm.sys.uni.VehicleInstance;
+import net.minecraft.client.renderer.entity.state.EntityRenderState;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class FvtmRenderState extends EntityRenderState {
+
+	public RootVehicle entity;
+	public VehicleInstance vehicle;
+	public float f;
+
+}

8917413f7c0c09536e58e160ca8446a7cf28f7da
Ferdinand Calo <fexcalo@gmail.com>
2025-03-21 23:12:50 +0100

    Update PartSlots.java

--- neutral/src/net/fexcraft/mod/fvtm/data/part/PartSlots.java
+++ neutral/src/net/fexcraft/mod/fvtm/data/part/PartSlots.java
@@ -1,12 +1,12 @@
 package net.fexcraft.mod.fvtm.data.part;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map.Entry;
-
 import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.app.json.JsonValue;
 import net.fexcraft.mod.uni.EnvInfo;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map.Entry;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -21,7 +21,7 @@ public class PartSlots extends LinkedHashMap<String, PartSlot> {
 		for(Entry<String, JsonValue<?>> entry : map.entries()){
 			put(entry.getKey(), new PartSlot(entry.getKey(), entry.getValue()));
 		}
-		if(EnvInfo.CLIENT){
+		if(EnvInfo.CLIENT || EnvInfo.is121()){
 			count = new HashMap<>();
 			for(PartSlot slot : this.values()){
 				if(count.containsKey(slot.type)) count.put(slot.type, count.get(slot.type) + 1);

5e505b1860fcc6bddec434507a8c657d09447525
Ferdinand Calo <fexcalo@gmail.com>
2025-03-21 20:38:46 +0100

    Update MaterialItem.java

--- fabric21/src/main/java/net/fexcraft/mod/fvtm/item/MaterialItem.java
+++ fabric21/src/main/java/net/fexcraft/mod/fvtm/item/MaterialItem.java
@@ -2,14 +2,19 @@ package net.fexcraft.mod.fvtm.item;
 
 import net.fexcraft.mod.fvtm.data.ContentItem;
 import net.fexcraft.mod.fvtm.data.ContentType;
+import net.fexcraft.mod.fvtm.data.Fuel;
 import net.fexcraft.mod.fvtm.data.Material;
+import net.fexcraft.mod.fvtm.util.GenericUtils;
+import net.fexcraft.mod.uni.inv.StackWrapper;
 import net.fexcraft.mod.uni.inv.UniStack;
+import net.minecraft.client.resources.language.I18n;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 
 import java.util.List;
+import java.util.UUID;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -29,12 +34,8 @@ public class MaterialItem extends Item implements ContentItem<Material> {
 
 	@Override
 	public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltip, TooltipFlag flag){
-		/*tooltip.add(GenericUtils.format("&9Name: &7" + material.getName()));
-		for(String s : material.getDescription())
-			tooltip.add(GenericUtils.format(I18n.get(s)));
-		if(material.getOreDictId() != null){
-			tooltip.add(GenericUtils.format("&9OreDict: &7" + material.getOreDictId()));
-		}
+		tooltip.add(GenericUtils.format("&9Name: &7" + material.getName()));
+		for(String s : material.getDescription()) tooltip.add(GenericUtils.format(I18n.get(s)));
 		if(material.isVehicleKey()){
 			tooltip.add(GenericUtils.format("&9LockCode: &7" + getLockCode(stack)));
 		}
@@ -43,16 +44,16 @@ public class MaterialItem extends Item implements ContentItem<Material> {
 			tooltip.add(GenericUtils.format("&9Container: &7" + (material.isUniversalFuelContainer() ? "universal" : ((material.getFuelType() == null) ? material.getFuelGroup() : material.getFuelType().getName()))));
 			tooltip.add(GenericUtils.format("&9Fuel Stored: &7" + Fuel.getStoredName(unistack.stack)));
 			tooltip.add(GenericUtils.format("&9Fuel Amount: &7" + Fuel.getStoredAmount(unistack.stack) + "mB"));
-		}*/
+		}
 	}
 
 	public String getLockCode(ItemStack stack){
-		/*if(!material.isVehicleKey()) return null;
-		if(stack.getTag() == null) stack.setTag(new CompoundTag());
-		if(!stack.getTag().contains("LockCode"))
-			stack.getTag().putString("LockCode", UUID.randomUUID().toString().replace("-", "").substring(0, 7));
-		return stack.getTag().getString("LockCode");*/
-		return "";
+		if(!material.isVehicleKey()) return null;
+		StackWrapper sw = UniStack.getStack(stack);
+		if(!sw.directTag().has("LockCode")){
+			sw.updateTag(com -> com.set("LockCode", UUID.randomUUID().toString().replace("-", "").substring(0, 7)));
+		}
+		return sw.directTag().getString("LockCode");
 	}
 
 	@Override

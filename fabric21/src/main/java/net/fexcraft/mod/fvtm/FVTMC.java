34d2dc7b1fdeb4ced3e5bb4c1c3364d8dadd69b3
Ferdinand Calo <fexcalo@gmail.com>
2025-03-21 20:38:14 +0100

    Update FVTMC.java

--- fabric21/src/main/java/net/fexcraft/mod/fvtm/FVTMC.java
+++ fabric21/src/main/java/net/fexcraft/mod/fvtm/FVTMC.java
@@ -1,16 +1,26 @@
 package net.fexcraft.mod.fvtm;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fexcraft.lib.frl.GLO;
 import net.fexcraft.lib.frl.Renderer;
+import net.fexcraft.mod.fvtm.impl.Packets21;
 import net.fexcraft.mod.fvtm.model.GLObject;
 import net.fexcraft.mod.fvtm.model.program.DefaultPrograms;
 import net.fexcraft.mod.fvtm.render.*;
 import net.fexcraft.mod.fvtm.util.Resources21;
+import net.fexcraft.mod.fvtm.util.SpawnPacket;
 import net.fexcraft.mod.uni.EnvInfo;
+import net.fexcraft.mod.uni.UniEntity;
+import net.fexcraft.mod.uni.packet.PacketBase;
+import net.fexcraft.mod.uni.packet.PacketHandler;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
+import net.minecraft.world.entity.Entity;
+
+import static net.fexcraft.mod.fvtm.impl.Packets21.*;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -39,6 +49,26 @@ public class FVTMC implements ClientModInitializer {
 		EntityRendererRegistry.register(Resources21.WHEEL_ENTITY, context -> new EmptyRenderer(context));
 		EntityRendererRegistry.register(Resources21.VEHICLE_ENTITY, context -> new RVRenderer(context));
 		EntityRendererRegistry.register(Resources21.RAIL_ENTITY, context -> new RVRenderer(context));
+		Packets21.PACKET_HANDLERS.add(() -> {
+			ClientPlayNetworking.registerGlobalReceiver(SPAWN_PACKET_TYPE, (packet, context) -> {
+				context.client().execute(() -> {
+					Entity ent = context.player().level().getEntity(packet.entity());
+					if(ent instanceof SpawnPacket.PacketEntity pe){
+						pe.readSpawnData(packet.com());
+					}
+				});
+			});
+			registerClientPacket(TAG_PACKET_TYPE, HTL);
+			registerClientPacket(VEHMOVE_PACKET_TYPE, HVM);
+			registerClientPacket(VEHKEYPRESS_PACKET_TYPE, HVK);
+			registerClientPacket(VEHKEYSTATE_PACKET_TYPE, HVKS);
+			registerClientPacket(SEATUPDATE_PACKET_TYPE, HSU);
+			registerClientPacket(SPUPDATE_PACKET_TYPE, HSPU);
+		});
+	}
+
+	public static <T extends CustomPacketPayload> void registerClientPacket(CustomPacketPayload.Type<T> type, PacketHandler ph){
+		ClientPlayNetworking.registerGlobalReceiver(type, (packet, context) -> context.client().execute(() -> ph.handleServer((PacketBase)packet, UniEntity.getEntity(context.player()))));
 	}
 
 }

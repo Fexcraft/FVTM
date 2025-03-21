a43f6498468d75185571aaa171a84f0ccd1f9e79
Ferdinand Calo <fexcalo@gmail.com>
2025-03-21 23:12:56 +0100

    Update RailEntity.java

--- neutral/src/net/fexcraft/mod/fvtm/sys/rail/RailEntity.java
+++ neutral/src/net/fexcraft/mod/fvtm/sys/rail/RailEntity.java
@@ -20,9 +20,6 @@ import net.fexcraft.mod.fvtm.util.SaveUtils;
 import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.tag.TagLW;
 import net.fexcraft.mod.uni.world.EntityW;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.level.Level;
 
 /**
  * First prototype of RailEntity system.

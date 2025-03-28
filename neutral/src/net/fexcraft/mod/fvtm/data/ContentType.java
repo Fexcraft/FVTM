cc43429e3c0a80ed6c4db0c751f4446193a90a5f
Ferdinand Calo <fexcalo@gmail.com>
2025-03-28 16:15:52 +0100

    Update ContentType.java

--- neutral/src/net/fexcraft/mod/fvtm/data/ContentType.java
+++ neutral/src/net/fexcraft/mod/fvtm/data/ContentType.java
@@ -30,7 +30,7 @@ public enum ContentType {
 	WIRE(".wire", "wires", "fvtm:wire", WireType.class),
 	WIREDECO(".wiredeco", "wires", "fvtm:wiredeco", WireDeco.class),
 	DECORATION(".deco", "decos", "fvtm:decoration", Decoration.class),
-	SIGN(".sign", "signs", "fvtm:sign", Decoration.class),
+	SIGN(".sign", "signs", "fvtm:sign", Sign.class),
 	RECIPE(".json", "recipes", null, Recipe.class),
 	;
 

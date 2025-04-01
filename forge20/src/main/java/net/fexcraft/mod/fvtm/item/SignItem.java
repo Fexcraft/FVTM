189efe616286482b6bc283e1e7aa1bba3e416321
Ferdinand Calo <fexcalo@gmail.com>
2025-04-01 14:09:41 +0200

    Update SignItem.java

--- forge20/src/main/java/net/fexcraft/mod/fvtm/item/SignItem.java
+++ forge20/src/main/java/net/fexcraft/mod/fvtm/item/SignItem.java
@@ -2,22 +2,14 @@ package net.fexcraft.mod.fvtm.item;
 
 import net.fexcraft.mod.fvtm.data.*;
 import net.fexcraft.mod.fvtm.data.root.ItemTextureable;
-import net.fexcraft.mod.fvtm.sys.sign.SignInstance;
-import net.fexcraft.mod.fvtm.sys.sign.SignSystem;
-import net.fexcraft.mod.fvtm.sys.uni.SystemManager;
 import net.fexcraft.mod.fvtm.util.GenericUtils;
-import net.fexcraft.mod.fvtm.util.QV3D;
-import net.fexcraft.mod.uni.UniEntity;
 import net.fexcraft.mod.uni.inv.StackWrapper;
 import net.fexcraft.mod.uni.inv.UniStack;
 import net.fexcraft.mod.uni.tag.TagCW;
-import net.fexcraft.mod.uni.world.EntityW;
 import net.minecraft.network.chat.Component;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 
 import javax.annotation.Nullable;
@@ -55,27 +47,6 @@ public class SignItem extends Item implements ContentItem.ContentDataItem<Sign, 
 				}
 			}
 		}
-		tooltip.add(GenericUtils.format("&9Right-click on a block to place or upgrade a sign."));
-	}
-
-	@Override
-	public InteractionResult useOn(UseOnContext context){
-		if(context.getLevel().isClientSide) return InteractionResult.PASS;
-		ItemStack stack = context.getItemInHand();
-		EntityW ply = UniEntity.getEntity(context.getPlayer());
-		SignData data = getData(UniStack.getStack(stack));
-		QV3D vec = new QV3D(context.getClickedPos().getX(), context.getClickedPos().getY(), context.getClickedPos().getZ());
-		SignSystem system = SystemManager.get(SystemManager.Systems.SIGN, ply.getWorld());
-		if(system == null){
-			ply.send("sign system not found");
-			return InteractionResult.FAIL;
-		}
-		SignInstance inst = system.addSign(vec);
-		inst.yaw = -context.getHorizontalDirection().toYRot() + 90;
-		inst.components.add(data);
-		inst.updateClient();
-		if(!context.getPlayer().isCreative()) stack.shrink(1);
-		return InteractionResult.SUCCESS;
 	}
 
 	@Override

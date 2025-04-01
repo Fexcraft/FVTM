113939c9fbbbe61bf57400a2bdd9f8d9b9576c3c
Ferdinand Calo <fexcalo@gmail.com>
2025-04-01 14:09:45 +0200

    Update ToolboxItem.java

--- forge20/src/main/java/net/fexcraft/mod/fvtm/item/ToolboxItem.java
+++ forge20/src/main/java/net/fexcraft/mod/fvtm/item/ToolboxItem.java
@@ -1,10 +1,20 @@
 package net.fexcraft.mod.fvtm.item;
 
+import net.fexcraft.lib.common.Static;
+import net.fexcraft.mod.fvtm.data.ToolboxType;
+import net.fexcraft.mod.fvtm.sys.sign.SignInstance;
+import net.fexcraft.mod.fvtm.sys.sign.SignSystem;
+import net.fexcraft.mod.fvtm.sys.uni.SystemManager;
+import net.fexcraft.mod.fvtm.util.QV3D;
+import net.fexcraft.mod.uni.UniEntity;
 import net.fexcraft.mod.uni.inv.StackWrapper;
+import net.fexcraft.mod.uni.world.EntityW;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 
 import javax.annotation.Nullable;
@@ -45,6 +55,10 @@ public class ToolboxItem extends Item {
 				tooltip.add(Component.literal("Wire Slack Adjustment Toolbox"));
 				break;
 			}
+			case 5:{
+				tooltip.add(Component.literal("Sign Adjustment and Removal Toolbox"));
+				break;
+			}
 		}
 	}
 
@@ -54,6 +68,30 @@ public class ToolboxItem extends Item {
 
 	public static int getToolboxType(StackWrapper stack){
 		return ((ToolboxItem)stack.getItem().local()).var;
+	}
+
+	@Override
+	public InteractionResult useOn(UseOnContext context){
+		if(context.getLevel().isClientSide) return InteractionResult.PASS;
+		ItemStack stack = context.getItemInHand();
+		var type = ((ToolboxItem)stack.getItem()).var;
+		if(type != ToolboxType.SIGN_ADJREM.idx || context.getPlayer().isShiftKeyDown()) return InteractionResult.PASS;
+		EntityW ply = UniEntity.getEntity(context.getPlayer());
+		QV3D vec = new QV3D(context.getClickLocation().x, context.getClickLocation().y, context.getClickLocation().z);
+		SignSystem system = SystemManager.get(SystemManager.Systems.SIGN, ply.getWorld());
+		if(system == null){
+			ply.send("sign system not found");
+			return InteractionResult.FAIL;
+		}
+		SignInstance inst = system.getSign(vec.pos);
+		if(inst == null){
+			inst = system.addSign(vec.pos);
+			inst.vec = vec;
+			inst.yaw = -context.getHorizontalDirection().toYRot() + 90;
+			inst.yaw *= Static.rad1;
+			inst.updateClient();
+		}
+		return InteractionResult.SUCCESS;
 	}
 
 }

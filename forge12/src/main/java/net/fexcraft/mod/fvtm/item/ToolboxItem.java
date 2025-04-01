eee7164e54eeb3226a49c1652872d48a20e81b63
Ferdinand Calo <fexcalo@gmail.com>
2025-04-01 10:28:56 +0200

    Update ToolboxItem.java

--- forge12/src/main/java/net/fexcraft/mod/fvtm/item/ToolboxItem.java
+++ forge12/src/main/java/net/fexcraft/mod/fvtm/item/ToolboxItem.java
@@ -1,12 +1,23 @@
 package net.fexcraft.mod.fvtm.item;
 
 import net.fexcraft.mod.fvtm.data.ToolboxType;
+import net.fexcraft.mod.fvtm.sys.sign.SignInstance;
+import net.fexcraft.mod.fvtm.sys.sign.SignSystem;
+import net.fexcraft.mod.fvtm.sys.uni.SystemManager;
+import net.fexcraft.mod.fvtm.util.QV3D;
+import net.fexcraft.mod.uni.UniEntity;
 import net.fexcraft.mod.uni.inv.StackWrapper;
+import net.fexcraft.mod.uni.world.EntityW;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
 import net.minecraft.util.NonNullList;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -76,6 +87,25 @@ public class ToolboxItem extends Item {
 
 	public static int getToolboxType(ItemStack stack){
 		return stack.getItemDamage();
+	}
+
+	@Override
+	public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ){
+		if(world.isRemote) return EnumActionResult.PASS;
+		ItemStack stack = player.getHeldItem(hand);
+		if(stack.getItemDamage() != ToolboxType.SIGN_ADJREM.idx) return EnumActionResult.PASS;
+		EntityW ply = UniEntity.getEntity(player);
+		QV3D vec = new QV3D(pos.getX() + hitX, pos.getY() + hitY, pos.getZ() + hitZ);
+		SignSystem system = SystemManager.get(SystemManager.Systems.SIGN, ply.getWorld());
+		if(system == null){
+			ply.send("sign system not found");
+			return EnumActionResult.FAIL;
+		}
+		SignInstance inst = system.addSign(vec);
+		inst.yaw = -facing.getHorizontalAngle() - 90;
+		inst.updateClient();
+		if(!player.capabilities.isCreativeMode) stack.shrink(1);
+		return EnumActionResult.SUCCESS;
 	}
 
 }

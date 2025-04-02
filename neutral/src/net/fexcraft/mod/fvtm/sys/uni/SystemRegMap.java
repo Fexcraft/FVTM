e280b2f776c0845e70cf95aefa560ccad605b0f2
Ferdinand Calo <fexcalo@gmail.com>
2025-04-02 22:59:14 +0200

    Update SystemRegMap.java

--- neutral/src/net/fexcraft/mod/fvtm/sys/uni/SystemRegMap.java
+++ neutral/src/net/fexcraft/mod/fvtm/sys/uni/SystemRegMap.java
@@ -19,15 +19,15 @@ public class SystemRegMap<R extends DetachedSystem<R, V>, V extends SysObj> exte
 	}
 
 	public SystemRegion<R, V> get(int x, int z){
-		for(RegionKey key : keySet()){
-			if(x == key.x && z == key.z) return get(key);
+		for(SystemRegion<R, V> reg : values()){
+			if(x == reg.key.x && z == reg.key.z) return reg;
 		}
 		return null;
 	}
 
 	public SystemRegion<R, V> get(int[] xz){
-		for(RegionKey key : keySet()){
-			if(xz[0] == key.x && xz[1] == key.z) return get(key);
+		for(SystemRegion<R, V> reg : values()){
+			if(xz[0] == reg.key.x && xz[1] == reg.key.z) return reg;
 		}
 		return null;
 	}
@@ -35,7 +35,8 @@ public class SystemRegMap<R extends DetachedSystem<R, V>, V extends SysObj> exte
 	public SystemRegion<R, V> get(V3I pos, boolean load){
 		SystemRegion<R, V> region = get(RegionKey.getRegionXZ(pos));
 		if(region != null || !load) return region;
-		put(new RegionKey(RegionKey.getRegionXZ(pos)), region = function.apply(new RegionKey(RegionKey.getRegionXZ(pos))));
+		region = function.apply(new RegionKey(RegionKey.getRegionXZ(pos)));
+		put(region.key, region);
 		region.load().sendSync(pos);
 		return region;
 	}
@@ -47,7 +48,8 @@ public class SystemRegMap<R extends DetachedSystem<R, V>, V extends SysObj> exte
 	public SystemRegion<R, V> get(int[] xz, boolean load){
 		SystemRegion<R, V> region = get(xz);
 		if(region != null || !load) return region;
-		put(new RegionKey(xz), region = function.apply(new RegionKey(xz[0], xz[1])));
+		region = function.apply(new RegionKey(xz[0], xz[1]));
+		put(region.key, region);
 		return region.load();
 	}
 
@@ -56,9 +58,10 @@ public class SystemRegMap<R extends DetachedSystem<R, V>, V extends SysObj> exte
 	}
 
 	public SystemRegion<R, V> get(RegionKey xz, boolean load){
-		SystemRegion<R, V> region = get(xz);
+		SystemRegion<R, V> region = get(xz.x, xz.z);
 		if(region != null || !load) return region;
-		put(new RegionKey(xz.x, xz.z), region = function.apply(new RegionKey(xz.x, xz.z)));
+		region = function.apply(xz);
+		put(region.key, region);
 		return region.load();
 	}
 

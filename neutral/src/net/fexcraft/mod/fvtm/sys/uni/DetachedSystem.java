e6c572efd56d4efbb21c136e6b0f6dfb4a34ee2e
Ferdinand Calo <fexcalo@gmail.com>
2025-04-02 17:20:28 +0200

    Update DetachedSystem.java

--- neutral/src/net/fexcraft/mod/fvtm/sys/uni/DetachedSystem.java
+++ neutral/src/net/fexcraft/mod/fvtm/sys/uni/DetachedSystem.java
@@ -1,5 +1,8 @@
 package net.fexcraft.mod.fvtm.sys.uni;
 
+import net.fexcraft.lib.common.math.V3I;
+import net.fexcraft.mod.fvtm.sys.uni.SystemManager.Systems;
+import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.world.ChunkW;
 import net.fexcraft.mod.uni.world.EntityW;
 import net.fexcraft.mod.uni.world.WorldW;
@@ -11,8 +14,9 @@ import java.util.Timer;
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
-public abstract class DetachedSystem {
-	
+public abstract class DetachedSystem<S extends DetachedSystem<S, V>, V extends SysObj> {
+
+	protected SystemRegMap<S, V> regions;
 	protected WorldW world;
 	protected Timer timer;
 	protected File root;
@@ -20,15 +24,14 @@ public abstract class DetachedSystem {
 	public DetachedSystem(WorldW world){
 		this.world = world;
 		root = WrapperHolder.getWorldFolder(world, "fvtm");
+		regions = new SystemRegMap<>((S)this);
 	}
 	
 	public WorldW getWorld(){
 		return world;
 	}
-	
-	public int getDimension(){
-		return world.dim();
-	}
+
+	public abstract Systems getType();
 	
 	public void setupTimer(long time){
 		if(!hasTimer()) return;
@@ -47,7 +50,9 @@ public abstract class DetachedSystem {
 	public File getSaveRoot(){
 		return root;
 	}
-	
+
+	public abstract String getRegFolderName();
+
 	public abstract void unload();
 	
 	public abstract void onChunkLoad(ChunkW chunk);
@@ -63,7 +68,56 @@ public abstract class DetachedSystem {
 	}
 
 	public void syncPlayer(EntityW entity){
-
+		for(SystemRegion region : regions.values()){
+			region.sendSync(entity);
+		}
 	}
+
+	public SystemRegMap<S, V> getRegions(){
+		return regions;
+	}
+
+	public void updateRegion(TagCW compound, Passenger player){
+		RegionKey key = new RegionKey(compound.getIntArray("xz"));
+		if(world.isClient()){
+			SystemRegion<S, V> region = regions.get(key);
+			if(region == null)regions.put(key, region = new SystemRegion(this, key, false));
+			region.read(compound);
+		}
+		else{
+			SystemRegion<S, V> region = regions.get(key, true);
+			region.sendSync(player);
+		}
+	}
+
+	//
+
+	public abstract V create(SystemRegion<S, V> region, V3I pos);
+
+	public V get(V3I pos){
+		SystemRegion<S, V> region = regions.get(pos, false);
+		return region == null ? null : region.get(pos);
+	}
+
+	public V get(V3I pos, boolean load){
+		SystemRegion<S, V> region = regions.get(pos, load);
+		return region.get(pos);
+	}
+
+	public V add(V3I pos){
+		SystemRegion<S, V> region = regions.get(pos, true);
+		return region.add(pos);
+	}
+
+	public void del(V3I pos){
+		SystemRegion<S, V> region = regions.get(pos, true);
+		if(region != null) region.del(pos);
+	}
+
+	//
+
+	public abstract void writeRegion(SystemRegion<S, V> region, TagCW com);
+
+	public abstract void readRegion(SystemRegion<S, V> region, TagCW com);
 
 }

19e1855a5c3065703dc0821bc5b685b3df8d704d
Ferdinand Calo <fexcalo@gmail.com>
2025-04-07 22:26:35 +0200

    Update MRWrapper.java

--- forge12/src/main/java/net/fexcraft/mod/fvtm/model/MRWrapper.java
+++ forge12/src/main/java/net/fexcraft/mod/fvtm/model/MRWrapper.java
@@ -1,21 +1,22 @@
 package net.fexcraft.mod.fvtm.model;
 
-import static net.fexcraft.mod.fvtm.model.DefaultModel.RENDERDATA;
-
-import java.util.ArrayList;
-
-import org.lwjgl.opengl.GL11;
-
 import net.fexcraft.lib.common.math.RGB;
 import net.fexcraft.mod.fvtm.data.Capabilities;
 import net.fexcraft.mod.fvtm.item.ClothItem;
 import net.fexcraft.mod.fvtm.util.Command;
 import net.fexcraft.mod.fvtm.util.TexUtil;
+import net.fexcraft.mod.uni.UniEntity;
+import net.fexcraft.mod.uni.world.EntityW;
 import net.minecraft.client.model.ModelBase;
 import net.minecraft.client.model.ModelRenderer;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.entity.RenderLivingBase;
 import net.minecraft.entity.EntityLivingBase;
+import org.lwjgl.opengl.GL11;
+
+import java.util.ArrayList;
+
+import static net.fexcraft.mod.fvtm.model.DefaultModel.RENDERDATA;
 
 /** @author Ferdinand Calo' (FEX___96) */
 public class MRWrapper extends ModelRenderer {
@@ -52,6 +53,7 @@ public class MRWrapper extends ModelRenderer {
 		if(cloth_models.isEmpty()) return;
     	GL11.glRotatef(90, 0, 1, 0);
     	int deftex = GL11.glGetInteger(GL11.GL_TEXTURE_BINDING_2D);
+		EntityW ent = UniEntity.getEntity(entity);
     	RenderCache cache = entity.getCapability(Capabilities.RENDERCACHE, null);
 		for(int i = 0; i < cloth_items.size(); i++){
 			TexUtil.bindTexture(cloth_items.get(i).getContent().getTexture());
@@ -60,7 +62,7 @@ public class MRWrapper extends ModelRenderer {
 				if(id.contains("right")) GL11.glTranslatef(0, 0, 0.25f);
 				GL11.glRotated(parent.rotateAngleX * 5, 0, 0, 1);
 			}
-			cloth_items.get(i).getContent().getModel().render(RENDERDATA.set(cloth_items.get(i), cloth_models.get(i), entity, cache));
+			cloth_items.get(i).getContent().getModel().render(RENDERDATA.set(cloth_items.get(i), cloth_models.get(i), ent).rc(cache));
 			if(cloth_groups.get(i).startsWith("skirt")) GL11.glPopMatrix();
 		}
 		cloth_items.clear();

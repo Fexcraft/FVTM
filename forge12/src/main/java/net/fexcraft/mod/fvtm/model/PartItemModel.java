e3b221bc70fa82e2c1ac3225d3813bf1d2c0fe74
Ferdinand Calo <fexcalo@gmail.com>
2025-04-07 22:26:37 +0200

    Update PartItemModel.java

--- forge12/src/main/java/net/fexcraft/mod/fvtm/model/PartItemModel.java
+++ forge12/src/main/java/net/fexcraft/mod/fvtm/model/PartItemModel.java
@@ -1,20 +1,19 @@
 package net.fexcraft.mod.fvtm.model;
 
-import static net.fexcraft.mod.fvtm.handler.TireInstallationHandler.*;
-import static net.fexcraft.mod.fvtm.util.TexUtil.bindTexture;
-
 import net.fexcraft.lib.common.Static;
 import net.fexcraft.lib.mc.render.FCLItemModel;
 import net.fexcraft.mod.fvtm.data.Capabilities;
 import net.fexcraft.mod.fvtm.data.part.PartData;
-import net.fexcraft.mod.fvtm.handler.TireInstallationHandler;
-import net.fexcraft.mod.fvtm.item.PartItem;
 import net.fexcraft.mod.fvtm.handler.WheelInstallationHandler.WheelData;
+import net.fexcraft.mod.fvtm.item.PartItem;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.block.model.ItemCameraTransforms.TransformType;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 import org.lwjgl.opengl.GL11;
+
+import static net.fexcraft.mod.fvtm.handler.TireInstallationHandler.TireData;
+import static net.fexcraft.mod.fvtm.util.TexUtil.bindTexture;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -83,7 +82,7 @@ public class PartItemModel implements FCLItemModel {
 			default: break;
 		}
 		bindTexture(data.getCurrentTexture());
-		model.render(DefaultModel.RENDERDATA.set(null, null, null, data, null, false, Minecraft.getMinecraft().getRenderPartialTicks()));
+		model.render(DefaultModel.RENDERDATA.set(null, null, data, null, Minecraft.getMinecraft().getRenderPartialTicks()));
 		GL11.glPopMatrix();
 	}
 	

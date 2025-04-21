99e2c09c795d40aae2232e5d12684e3238074909
Ferdinand Calo <fexcalo@gmail.com>
2025-04-21 11:57:12 +0200

    Update RenderRV.java

--- forge12/src/main/java/net/fexcraft/mod/fvtm/render/RenderRV.java
+++ forge12/src/main/java/net/fexcraft/mod/fvtm/render/RenderRV.java
@@ -18,11 +18,14 @@ import org.lwjgl.opengl.GL11;
 import static net.fexcraft.mod.fvtm.Config.RENDER_VEHICLES_SEPARATELY;
 import static net.fexcraft.mod.fvtm.model.DefaultModel.RENDERDATA;
 import static net.fexcraft.mod.fvtm.render.EffectRenderer.drawString;
+import static net.fexcraft.mod.fvtm.render.SeparateRenderCache.SEP_VEH_CACHE;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public class RenderRV extends Render<RootVehicle> implements IRenderFactory<RootVehicle> {
+
+	private SeparateRenderCache.SepVehCache sepcache;
 
     public RenderRV(RenderManager renderManager){
         super(renderManager); shadowSize = 0.5F;
@@ -31,13 +34,15 @@ public class RenderRV extends Render<RootVehicle> implements IRenderFactory<Root
     @Override
     public void doRender(RootVehicle rv, double x, double y, double z, float entity_yaw, float ticks){
         if(RENDER_VEHICLES_SEPARATELY || rv.vehicle.data == null || rv.vehicle.point == null) return;
+		if(rv.vehicle.cache == null) rv.vehicle.cache = rv.getCapability(Capabilities.RENDERCACHE, null);
+		sepcache = rv.vehicle.cache.get(SEP_VEH_CACHE, data -> new SeparateRenderCache.SepVehCache());
         GL11.glPushMatrix();
-		SeparateRenderCache.SORTED_VEH_POS.put(rv.getEntityId(), new double[]{ x, y, z });
 		GL11.glTranslated(x, y, z);
 		V3D rot = EffectRenderer.getRotations(rv, ticks);
 		GL11.glRotated(-rot.x, 0.0F, 1.0F, 0.0F);
 		GL11.glRotated(rot.y, 1.0F, 0.0F, 0.0F);
 		GL11.glRotated(rot.z, 0.0F, 0.0F, 1.0F);
+		sepcache.set(x, y, z, rot);
 		if(Minecraft.getMinecraft().getRenderManager().isDebugBoundingBox()){
 			RootVehicle veh = Static.getServer().isSinglePlayer() ? (RootVehicle)Static.getServer().getWorld(rv.dimension).getEntityByID(rv.getEntityId()) : rv;
 			if(veh == null) veh = rv;
@@ -51,14 +56,12 @@ public class RenderRV extends Render<RootVehicle> implements IRenderFactory<Root
 			drawString(veh.vehicle.pivot().deg_roll() + "", 0, 0, 0, true, true, 0.8f, 0x000000, null);
 			GL11.glPopMatrix();
 		}
-		SeparateRenderCache.SORTED_VEH_ROT.put(rv.getEntityId(), rot);
 		GL11.glPushMatrix();
-		RenderCache cache = rv.getCapability(Capabilities.RENDERCACHE, null);
 		Model vehmod = rv.vehicle.data.getType().getModel();
 		if(vehmod != null){
 			GL11.glPushMatrix();
 			TexUtil.bindTexture(rv.vehicle.data.getCurrentTexture());
-			vehmod.render(RENDERDATA.set(rv.vehicle, ticks).rc(cache));
+			vehmod.render(RENDERDATA.set(rv.vehicle, ticks).rc(rv.vehicle.cache));
 			GL11.glPopMatrix();
 		}
 		else{
@@ -66,7 +69,7 @@ public class RenderRV extends Render<RootVehicle> implements IRenderFactory<Root
 			DebugModels.SPHERE_RED.render(0.5f);
 		}
 		if(rv.vehicle.data.getParts().size() > 0){
-			VehicleRenderer.renderPoint(rv.vehicle.point, rv, rv.vehicle.data, cache, ticks);
+			VehicleRenderer.renderPoint(rv.vehicle.point, rv, rv.vehicle.data, rv.vehicle.cache, ticks);
 		}
 		EffectRenderer.renderVehicleInfo(rv.vehicle, rv.vehicle.entity.getPos(), rv.vehicle.data);
 		GL11.glPopMatrix();

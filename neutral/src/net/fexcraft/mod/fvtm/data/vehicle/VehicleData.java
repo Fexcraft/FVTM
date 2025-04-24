de69277afe697879abada719f0b8ca7f98a926d2
Ferdinand Calo <fexcalo@gmail.com>
2025-04-25 01:26:57 +0200

    Update VehicleData.java

--- neutral/src/net/fexcraft/mod/fvtm/data/vehicle/VehicleData.java
+++ neutral/src/net/fexcraft/mod/fvtm/data/vehicle/VehicleData.java
@@ -91,7 +91,7 @@ public class VehicleData extends ContentData<Vehicle, VehicleData> implements Co
 					}
 				}
 				catch(Exception e){
-					FvtmLogger.log(e, "vehicledata (" + type.getID() + ") part pre-install of " + entry.getValue().colon() + " as " + entry.getKey());
+					if(!EnvInfo.DEV) FvtmLogger.log(e, "vehicledata (" + type.getID() + ") part pre-install of " + entry.getValue().colon() + " as " + entry.getKey());
 				}
 			}
 		}

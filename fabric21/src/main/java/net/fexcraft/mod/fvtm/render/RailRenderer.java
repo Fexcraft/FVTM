45a82366544d9a1eaa06d31b33d710d3730b3f31
Ferdinand Calo <fexcalo@gmail.com>
2025-05-02 23:05:03 +0200

    Update RailRenderer.java

--- fabric21/src/main/java/net/fexcraft/mod/fvtm/render/RailRenderer.java
+++ fabric21/src/main/java/net/fexcraft/mod/fvtm/render/RailRenderer.java
@@ -2,16 +2,24 @@ package net.fexcraft.mod.fvtm.render;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
+import net.fexcraft.mod.fvtm.data.JunctionGridItem;
 import net.fexcraft.mod.fvtm.data.SignData;
 import net.fexcraft.mod.fvtm.data.ToolboxType;
+import net.fexcraft.mod.fvtm.item.JunctionTool;
 import net.fexcraft.mod.fvtm.item.SignItem;
 import net.fexcraft.mod.fvtm.item.ToolboxItem;
 import net.fexcraft.mod.fvtm.model.RenderCache;
+import net.fexcraft.mod.fvtm.model.content.RailGaugeModel;
+import net.fexcraft.mod.fvtm.sys.rail.Junction;
+import net.fexcraft.mod.fvtm.sys.rail.RailSystem;
+import net.fexcraft.mod.fvtm.sys.rail.Track;
 import net.fexcraft.mod.fvtm.sys.sign.SignInstance;
 import net.fexcraft.mod.fvtm.sys.sign.SignSystem;
 import net.fexcraft.mod.fvtm.sys.uni.SystemManager;
 import net.fexcraft.mod.fvtm.sys.uni.SystemRegion;
 import net.fexcraft.mod.fvtm.util.DebugUtils;
+import net.fexcraft.mod.uni.IDL;
+import net.fexcraft.mod.uni.IDLManager;
 import net.fexcraft.mod.uni.world.WrapperHolder;
 import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
@@ -19,70 +27,75 @@ import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.core.BlockPos;
 import org.joml.Matrix4f;
 
+import java.util.HashSet;
+
 import static net.fexcraft.lib.frl.Renderer.RENDERER;
+import static net.fexcraft.mod.fvtm.Config.DISABLE_RAILS;
 import static net.fexcraft.mod.fvtm.Config.DISABLE_SIGNS;
+import static net.fexcraft.mod.fvtm.FvtmResources.WHITE_TEXTURE;
 import static net.fexcraft.mod.fvtm.model.DefaultModel.RENDERDATA;
 import static net.fexcraft.mod.fvtm.render.Renderer21.AY;
-import static net.fexcraft.mod.fvtm.util.DebugUtils.COL_ORG;
-import static net.fexcraft.mod.fvtm.util.DebugUtils.COL_RED;
+import static net.fexcraft.mod.fvtm.util.DebugUtils.*;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public class RailRenderer {
 
-	private static SignSystem sys;
+	private static RailSystem sys;
 	private static boolean holding;
 	private static BlockPos.MutableBlockPos pos = new BlockPos.MutableBlockPos();
+	private static HashSet<Junction> juncset = new HashSet<>();
 
-	public static void renderSigns(WorldRenderContext event){
-		if(DISABLE_SIGNS) return;
-		sys = SystemManager.get(SystemManager.Systems.SIGN, WrapperHolder.getWorld(event.camera().getEntity().level()));
-		if(sys == null || event.matrixStack() == null) return;
+	public static void renderRails(WorldRenderContext event){
+		if(DISABLE_RAILS) return;
+		sys = SystemManager.get(SystemManager.Systems.RAIL, WrapperHolder.getWorld(event.camera().getEntity().level()));
+		if(sys == null) return;
 		Camera camera = event.camera();
 		double cx = camera.getPosition().x;
 		double cy = camera.getPosition().y;
 		double cz = camera.getPosition().z;
 		PoseStack pose = event.matrixStack();
 		Renderer21.set(pose, Minecraft.getInstance().renderBuffers().bufferSource(), 0);
-		holding = Minecraft.getInstance().player.getMainHandItem().getItem() instanceof ToolboxItem && ((ToolboxItem)Minecraft.getInstance().player.getMainHandItem().getItem()).var == ToolboxType.SIGN_ADJREM.idx;
+		holding = Minecraft.getInstance().player.getMainHandItem().getItem() instanceof JunctionTool;
 		pose.pushPose();
 		pose.translate(-cx, -cy, -cz);
 		Renderer21.resetColor();
-		for(SystemRegion<?, SignInstance> reg : sys.getRegions().values()){
-			for(SignInstance sign : reg.getObjects().values()){
-				//TODO distance check
+		FvtmRenderTypes.setCutout(WHITE_TEXTURE);
+		for(SystemRegion<RailSystem, Junction> reg : sys.getRegions().values()){
+			juncset.clear();
+			juncset.addAll(reg.getObjects().values());
+			for(Junction junc : juncset){
 				pose.pushPose();
-				pose.translate(sign.vec.vec.x, sign.vec.vec.y, sign.vec.vec.z);
-				if(sign.components.size() == 0){
+				pose.translate(junc.getV3D().x, junc.getV3D().y, junc.getV3D().z);
+				Renderer21.light = LevelRenderer.getLightColor(camera.getEntity().level(), pos.set(junc.getV3D().x, junc.getV3D().y + 0.1, junc.getV3D().z));
+				JUNC_CORE.render();
+				pose.popPose();
+				if(junc.tracks.size() == 0 || holding || Minecraft.getInstance().player.getMainHandItem().getItem() instanceof JunctionGridItem){
 					DebugUtils.renderBB(0.5f, COL_ORG);
 				}
-				else{
-					if(holding || Minecraft.getInstance().player.getMainHandItem().getItem() instanceof SignItem){
-						DebugUtils.renderBB(0.5f, COL_ORG);
-					}
-					RenderCache cache = sign.getRenderCache();
-					pose.mulPose(new Matrix4f().rotate(sign.yaw, AY));
-					for(SignData scom : sign.components){
-						if(scom.getType().getModel() == null){
-							DebugUtils.renderBB(0.25f, COL_RED);
-						}
-						else{
-							Renderer21.light = LevelRenderer.getLightColor(camera.getEntity().level(), pos.set(sign.vec.pos.x, sign.vec.pos.y, sign.vec.pos.z));;
-							pose.pushPose();
-							pose.translate(scom.offset.x, scom.offset.y, scom.offset.z);
-							if(scom.roty != 0f) RENDERER.rotate(scom.roty, 0, 1, 0);
-							if(scom.rotz != 0f) RENDERER.rotate(scom.rotz, 0, 0, 1);
-							if(scom.rotx != 0f) RENDERER.rotate(scom.rotx, 1, 0, 0);
-							if(scom.sclx != 1f || scom.scly != 1f || scom.sclz != 1f) pose.scale(scom.sclx, scom.scly, scom.sclz);
-							FvtmRenderTypes.setCutout(scom.getTexture().getTexture());
-							scom.getType().getModel().render(RENDERDATA.set(scom, sign).rc(cache));
-							pose.popPose();
-						}
-					}
-				}
-				pose.popPose();
+				renderRails(pose, junc);
 			}
+		}
+		pose.popPose();
+	}
+
+	private static void renderRails(PoseStack pose, Junction junc){
+		pose.pushPose();
+		Renderer21.resetColor();
+		for(int i = 0; i < junc.size(); i++){
+			if(i > 2) pose.translate(0, -0.02, 0);
+			if(junc.tracks.get(i).isOppositeCopy()) continue;
+			pose.pushPose();
+			Track track = junc.tracks.get(i);
+			pose.translate(track.vecpath[0].x, track.vecpath[0].y, track.vecpath[0].z);
+			RailGaugeModel model = track.gauge.getModel();
+			if(track.railmodel == null) PathModelGenerator.generateTrackModel(track, model);
+			FvtmRenderTypes.setCutout(track.gauge.getRailTexture());
+			track.railmodel.render();
+			FvtmRenderTypes.setCutout(track.gauge.getTiesTexture());
+			track.restmodel.render();
+			pose.popPose();
 		}
 		pose.popPose();
 	}

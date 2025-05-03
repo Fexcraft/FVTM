6b0384934feae0eb44e4d72d1b88fb54e0c386db
Ferdinand Calo <fexcalo@gmail.com>
2025-05-03 23:48:30 +0200

    Update RailPlacerContainer.java

--- forge12/src/main/java/net/fexcraft/mod/fvtm/gui/rail/RailPlacerContainer.java
+++ forge12/src/main/java/net/fexcraft/mod/fvtm/gui/rail/RailPlacerContainer.java
@@ -1,38 +1,23 @@
 package net.fexcraft.mod.fvtm.gui.rail;
 
 import net.fexcraft.lib.mc.gui.GenericContainer;
-import net.fexcraft.lib.mc.gui.GenericGui;
-import net.fexcraft.lib.mc.utils.Print;
-import net.fexcraft.mod.fvtm.FVTM;
-import net.fexcraft.mod.fvtm.gui.GuiCommandSender;
-import net.fexcraft.mod.fvtm.gui.GuiHandler;
-import net.fexcraft.mod.fvtm.item.RailGaugeItem;
-import net.fexcraft.mod.fvtm.sys.rail.Junction;
-import net.fexcraft.mod.fvtm.sys.rail.RailSystem;
-import net.fexcraft.mod.fvtm.sys.uni.SystemManager;
-import net.fexcraft.mod.fvtm.sys.uni.SystemManager.Systems;
-import net.fexcraft.mod.fvtm.util.Perms;
-import net.fexcraft.mod.fvtm.util.QV3D;
-import net.fexcraft.mod.uni.tag.TagCW;
-import net.fexcraft.mod.uni.world.WrapperHolder;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.fml.relauncher.Side;
 
 public class RailPlacerContainer extends GenericContainer {
 	
-	protected GenericGui<RailPlacerContainer> gui;
+	/*protected GenericGui<RailPlacerContainer> gui;
 	protected GuiCommandSender sender;
 	private int itemslot, zoom;
-	private RailSystem system;
+	private RailSystem system;*/
 	
 	public RailPlacerContainer(EntityPlayer player, int x, int y, int z){
 		super(player);
-		itemslot = x; zoom = y;
+		/*itemslot = x; zoom = y;
 		system = SystemManager.get(Systems.RAIL, WrapperHolder.getWorld(player.world), RailSystem.class);
 		sender = new GuiCommandSender(player);
-		if(!player.world.isRemote && !Perms.RAIL_PLACER_GUI.has(player)) player.closeScreen();
+		if(!player.world.isRemote && !Perms.RAIL_PLACER_GUI.has(player)) player.closeScreen();*/
 	}
 
 	@Override
@@ -42,7 +27,7 @@ public class RailPlacerContainer extends GenericContainer {
 
 	@Override
 	protected void packet(Side side, NBTTagCompound packet, EntityPlayer player){
-		if(system == null){
+		/*if(system == null){
 			Print.chat(player, "Error, RailSysCap is null.");
 			return;
 		}
@@ -84,12 +69,12 @@ public class RailPlacerContainer extends GenericContainer {
 				}
 				break;
 			}
-		}
+		}*/
 	}
 
-    private void reopen(){
+    /*private void reopen(){
     	player.openGui(FVTM.getInstance(), GuiHandler.RAILPLACER, player.world, itemslot, zoom, 0);
-	}
+	}*/
 
 	@Override
     public boolean canInteractWith(EntityPlayer player){

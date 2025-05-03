d27995c12f524747b064b5824365328d87b49174
Ferdinand Calo <fexcalo@gmail.com>
2025-05-03 23:48:20 +0200

    Update UniFluidInvContainer.java

--- forge12/src/main/java/net/fexcraft/mod/fvtm/gui/inv/UniFluidInvContainer.java
+++ forge12/src/main/java/net/fexcraft/mod/fvtm/gui/inv/UniFluidInvContainer.java
@@ -10,7 +10,7 @@ import net.fexcraft.mod.fvtm.block.generated.BlockTileEntity;
 import net.fexcraft.mod.fvtm.block.generated.MultiblockTileEntity;
 import net.fexcraft.mod.fvtm.data.inv.InvHandler;
 import net.fexcraft.mod.fvtm.data.part.PartData;
-import net.fexcraft.mod.fvtm.gui.GuiHandler;
+import net.fexcraft.mod.fvtm.util.GuiHandler;
 import net.fexcraft.mod.fvtm.sys.uni.RootVehicle;
 import net.fexcraft.mod.fvtm.sys.uni.SeatInstance;
 import net.fexcraft.mod.fvtm.ui.UIKeys;

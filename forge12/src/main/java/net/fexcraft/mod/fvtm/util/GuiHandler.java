3da4e66ad0bbcb4f71053d00cac99a0567892274
Ferdinand Calo <fexcalo@gmail.com>
2025-05-03 23:47:44 +0200

    Update GuiHandler.java

--- forge12/src/main/java/net/fexcraft/mod/fvtm/util/GuiHandler.java
+++ forge12/src/main/java/net/fexcraft/mod/fvtm/util/GuiHandler.java
@@ -1,48 +1,23 @@
-package net.fexcraft.mod.fvtm.gui;
+package net.fexcraft.mod.fvtm.util;
 
 import net.fexcraft.mod.uni.ui.*;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.IGuiHandler;
 
-import java.util.HashMap;
-
 public class GuiHandler implements IGuiHandler {
-	
-	public static NBTTagCompound CLIENT_GUIDATA_CACHE;
-	public static final HashMap<String, NBTTagCompound> SERVER_GUIDATA_CACHE = new HashMap<>();
+
 	public static final String LISTENERID = "fvtm:gui";
 
 	/* 7xx - other */
-	public static final int STREETSIGN_ADJUSTER = 700;
-	//public static final int ROADTOOL = 702-704;
-	//public static final int SPAWNSYS = 705;
-	public static final int RAILPLACER = 706;
-	public static final int TSEDITOR = 709;
-	public static final int WIRE_RELAY_MAIN = 710;
 	public static final int WIRE_RELAY_EDIT = 711;
 	public static final int WIRE_EDIT = 712;
-	//public static final int DECORATION_EDITOR = 713;
 	public static final int VEHICLE_AND_PART_INFO = 714;
 	/* 90x - constructor main */
-	//
 	/* 91x - part cache */
-	//
 	/* 92x - installed part */
-	//
 	/* 93x - vehicle */
-	//public static final int VEHICLE_MAIN = 930;
-	//public static final int VEHICLE_ATTRIBUTE_EDITOR = 932;
-	//public static final int VEHICLE_FUEL = 933;
-	//public static final int VEHICLE_TOGGABLES = 934;
-	//public static final int VEHICLE_INVENTORIES = 935;
-	//public static final int VEHICLE_INVENTORY_ITEM = 9361;
-	//public static final int VEHICLE_INVENTORY_FLUID = 9362;
-	//public static final int VEHICLE_INVENTORY_VAR = 9363;
-	//public static final int VEHICLE_CONTAINERS = 937;
 	public static final int VEHICLE_CONTAINER = 938;
-	//public static final int VEHICLE_CONNECTORS = 939;
 	/* 94x - container */
 	public static final int CONTAINER_INVENTORY_ITEM = 941;
 	public static final int CONTAINER_INVENTORY_FLUID = 942;

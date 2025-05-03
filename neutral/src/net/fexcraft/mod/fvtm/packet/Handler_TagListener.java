efb510e6928aa94396c350b4772e041c8ed73670
Ferdinand Calo <fexcalo@gmail.com>
2025-05-03 14:46:24 +0200

    Update Handler_TagListener.java

--- neutral/src/net/fexcraft/mod/fvtm/packet/Handler_TagListener.java
+++ neutral/src/net/fexcraft/mod/fvtm/packet/Handler_TagListener.java
@@ -1,9 +1,8 @@
 package net.fexcraft.mod.fvtm.packet;
 
 import net.fexcraft.mod.fvtm.FvtmLogger;
-import net.fexcraft.mod.fvtm.sys.uni.Passenger;
 import net.fexcraft.mod.uni.packet.PacketHandler;
-import net.fexcraft.mod.uni.packet.PacketListener;
+import net.fexcraft.mod.uni.packet.PacketTagListener;
 import net.fexcraft.mod.uni.world.EntityW;
 
 /**
@@ -14,7 +13,7 @@ public class Handler_TagListener implements PacketHandler<Packet_TagListener> {
 	@Override
 	public Runnable handleServer(Packet_TagListener packet, EntityW player){
 		return () -> {
-			PacketListener lis = Packets.LIS_SERVER.get(packet.to);
+			PacketTagListener lis = Packets.LIS_SERVER.get(packet.to);
 			if(lis != null) lis.handle(packet.tag, player);
 			else FvtmLogger.log("S | Tag Packet Listener not found: " + packet.to);
 		};
@@ -24,7 +23,7 @@ public class Handler_TagListener implements PacketHandler<Packet_TagListener> {
 	public Runnable handleClient(Packet_TagListener packet, EntityW player){
 		return () -> {
 			try{
-				PacketListener lis = Packets.LIS_CLIENT.get(packet.to);
+				PacketTagListener lis = Packets.LIS_CLIENT.get(packet.to);
 				if(lis != null) lis.handle(packet.tag, player);
 				else FvtmLogger.log("C | Tag Packet Listener not found: " + packet.to);
 			}

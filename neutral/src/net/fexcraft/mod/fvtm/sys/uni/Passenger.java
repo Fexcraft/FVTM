311109c22555e4c014adfcefefcb040594dd9893
Ferdinand Calo <fexcalo@gmail.com>
2025-05-03 23:49:37 +0200

    Update Passenger.java

--- neutral/src/net/fexcraft/mod/fvtm/sys/uni/Passenger.java
+++ neutral/src/net/fexcraft/mod/fvtm/sys/uni/Passenger.java
@@ -1,7 +1,11 @@
 package net.fexcraft.mod.fvtm.sys.uni;
 
 import net.fexcraft.lib.common.math.V3D;
+import net.fexcraft.mod.fvtm.packet.Packets;
+import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.world.EntityW;
+
+import static net.fexcraft.mod.fvtm.packet.Packets.PKT_TAG;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -25,5 +29,19 @@ public interface Passenger extends EntityW {
 	public V3D getLookVec();
 
 	public boolean isShiftDown();
+
+	public default void sendPassUpdate(int ent, int veh, int seat){
+		TagCW com = TagCW.create();
+		com.set("ent", ent);
+		com.set("veh", veh);
+		com.set("seat", seat);
+		Packets.sendToAllTrackingEnt(PKT_TAG, this, "upd_pass", com);
+	}
+
+	public default void reqPassUpdate(){
+		TagCW com = TagCW.create();
+		com.set("ent", getId());
+		Packets.sendTo(PKT_TAG, this, "upd_pass", com);
+	}
 
 }

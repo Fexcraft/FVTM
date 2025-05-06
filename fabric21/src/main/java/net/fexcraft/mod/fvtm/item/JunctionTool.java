59d77d410222122e2a2d7c4b740b5feff1c88f83
Ferdinand Calo <fexcalo@gmail.com>
2025-05-06 21:37:45 +0200

    Update JunctionTool.java

--- fabric21/src/main/java/net/fexcraft/mod/fvtm/item/JunctionTool.java
+++ fabric21/src/main/java/net/fexcraft/mod/fvtm/item/JunctionTool.java
@@ -4,8 +4,11 @@ import net.fexcraft.mod.fvtm.sys.rail.Junction;
 import net.fexcraft.mod.fvtm.sys.rail.RailSystem;
 import net.fexcraft.mod.fvtm.sys.uni.Passenger;
 import net.fexcraft.mod.fvtm.sys.uni.SystemManager;
+import net.fexcraft.mod.fvtm.ui.UIKeys;
 import net.fexcraft.mod.fvtm.util.QV3D;
 import net.fexcraft.mod.uni.UniEntity;
+import net.fexcraft.mod.uni.inv.StackWrapper;
+import net.fexcraft.mod.uni.inv.UniStack;
 import net.fexcraft.mod.uni.world.WorldW;
 import net.fexcraft.mod.uni.world.WrapperHolder;
 import net.minecraft.network.chat.Component;
@@ -17,12 +20,10 @@ import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.component.TooltipDisplay;
 import net.minecraft.world.item.context.UseOnContext;
 
-import java.util.List;
 import java.util.function.Consumer;
 
 import static net.fexcraft.mod.fvtm.Config.DISABLE_RAILS;
-import static net.fexcraft.mod.uni.ui.ContainerInterface.translate;
-import static net.minecraft.network.chat.Component.literal;
+import static net.minecraft.network.chat.Component.translatable;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -35,14 +36,13 @@ public class JunctionTool extends Item {
 
 	@Override
 	public void appendHoverText(ItemStack stack, TooltipContext context, TooltipDisplay disp, Consumer<Component> cons, TooltipFlag flag){
-		cons.accept(literal(translate("&9Junction Editing Toolbox")));
-		/*tooltip.add(literal(translate("&9- - - - - - &7-")));
-		if(stack.getTag() != null && stack.getTag().contains("fvtm:junction")){
-			tooltip.add(literal(translate("&9Junction Selected: &7" + new QV3D(TagCW.wrap(stack.getTag()), "fvtm:junction"))));
+		StackWrapper wrap = UniStack.getStack(stack);
+		if(wrap.hasTag() && wrap.directTag().has("fvtm:junction")){
+			cons.accept(translatable("item.fvtm.junction_tool.selected", new QV3D(wrap.directTag(), "fvtm:junction")));
 		}
 		else{
-			tooltip.add(literal("No Junction Position Cached."));
-		}*/
+			cons.accept(translatable("item.fvtm.junction_tool.none"));
+		}
 	}
 
 	@Override
@@ -50,56 +50,54 @@ public class JunctionTool extends Item {
 		if(context.getLevel().isClientSide || DISABLE_RAILS) return InteractionResult.PASS;
 		WorldW world = WrapperHolder.getWorld(context.getLevel());
 		RailSystem railsys = SystemManager.get(SystemManager.Systems.RAIL, world);
+		Player player = context.getPlayer();
+		Passenger pass = (Passenger)UniEntity.getEntity(player);
 		if(railsys == null){
-			/*context.getPlayer().sendSystemMessage(GenericUtils.format("RailSystem not found on this Level."));*/
+			pass.bar("item.fvtm.junction_tool.nosys");
 			return InteractionResult.FAIL;
 		}
 		QV3D vector = new QV3D(context.getClickLocation().x, context.getClickLocation().y, context.getClickLocation().z), cached;
-		Player player = context.getPlayer();
-		Passenger pass = (Passenger)UniEntity.getEntity(player);
-		ItemStack stack = player.getItemInHand(context.getHand());
+		StackWrapper stack = UniStack.getStack(player.getItemInHand(context.getHand()));
 		if(player.isCrouching()){
 			Junction junc = railsys.getJunction(vector.pos);
 			if(junc == null){
-				pass.bar("&cNo junction at position.");
+				pass.bar("item.fvtm.junction_tool.nojunc");
 			}
 			else if(junc.size() > 0){
-				pass.bar("&cDisconnect all tracks before removing a Junction.");
+				pass.bar("item.fvtm.junction_tool.fulljunc");
 			}
 			else{
 				railsys.delJunction(vector.pos);
-				pass.bar("&c&oRemoving Junction...");
+				pass.bar("item.fvtm.junction_tool.remjunc");
 			}
 			return InteractionResult.SUCCESS;
 		}
-		/*if(stack.getTag() == null) stack.setTag(new CompoundTag());
 		Junction junk = railsys.getJunction(vector.pos, true);
 		if(junk == null){
-			pass.bar("&cNo Junction at this Position.");
+			pass.bar("item.fvtm.junction_tool.nojunc");
 			return InteractionResult.SUCCESS;
 		}
 		else{
-			if(stack.getTag().contains("fvtm:junction")){
-				cached = new QV3D(TagCW.wrap(stack.getTag()), "fvtm:junction");
+			if(stack.directTag().has("fvtm:junction")){
+				cached = new QV3D(stack.directTag(), "fvtm:junction");
 				if(cached.equals(vector)){
 					pass.openUI(UIKeys.RAIL_JUNCTION, cached.pos.x, cached.pos.y, cached.pos.z);
 					return InteractionResult.SUCCESS;
 				}
 				if(junk.tracks.size() <= 2){
-					stack.getTag().remove("fvtm:junction");
-					pass.bar("&7&oResetting previous Cached Position.");
+					stack.updateTag(tag -> tag.rem("fvtm:junction"));
+					pass.bar("item.fvtm.junction_tool.reset");
 				}
 			}
 			if(junk.tracks.size() < 2){
 				pass.openUI(UIKeys.RAIL_JUNCTION, vector.pos.x, vector.pos.y, vector.pos.z);
 			}
 			else{
-				vector.write(TagCW.wrap(stack.getTag()), "fvtm:junction");
-				pass.bar("&a&lJunction Position Cached.");
+				stack.updateTag(tag -> vector.write(tag, "fvtm:junction"));
+				pass.bar("item.fvtm.junction_tool.cached");
 			}
 			return InteractionResult.SUCCESS;
-		}*/
-		return InteractionResult.SUCCESS;
+		}
 	}
 
 }

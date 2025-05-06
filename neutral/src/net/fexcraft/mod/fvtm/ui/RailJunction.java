23a024ca33821e71c84d53265fec3933a4d4ea1f
Ferdinand Calo <fexcalo@gmail.com>
2025-05-06 23:38:57 +0200

    Update RailJunction.java

--- neutral/src/net/fexcraft/mod/fvtm/ui/RailJunction.java
+++ neutral/src/net/fexcraft/mod/fvtm/ui/RailJunction.java
@@ -3,13 +3,15 @@ package net.fexcraft.mod.fvtm.ui;
 import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.lib.common.math.RGB;
 import net.fexcraft.lib.common.math.V3D;
-import net.fexcraft.lib.common.math.V3I;
 import net.fexcraft.mod.fvtm.sys.rail.Track;
 import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.ui.ContainerInterface;
 import net.fexcraft.mod.uni.ui.UIButton;
 import net.fexcraft.mod.uni.ui.UserInterface;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -21,7 +23,9 @@ public class RailJunction extends UserInterface {
 	public static final RGB RED = new RGB(0xCC1A29);
 	public static final RGB GRE = new RGB(0x00FF21);
 	public static final RGB ORA = new RGB(0xFF6A00);
-	public static final float[][] TRACK_COLOR = { BLU.toFloatArray(), RED.toFloatArray(), GRE.toFloatArray(), ORA.toFloatArray() };
+	private static BufferedImage conv = new BufferedImage(64, 64, BufferedImage.TYPE_INT_RGB);
+	private static int[][] grid = new int[64][64];
+	public static Color[] TRACK_COLOR = { Color.BLUE, Color.RED, Color.GREEN, Color.ORANGE };
 	private RailJunctionContainer menu;
 
 	public RailJunction(JsonMap map, ContainerInterface con) throws Exception{
@@ -32,6 +36,7 @@ public class RailJunction extends UserInterface {
 	@Override
 	public void init(){
 		texts.get("title").value(menu.junc.posString());
+		updateGrid();
 	}
 
 	@Override
@@ -99,38 +104,19 @@ public class RailJunction extends UserInterface {
 		if(menu.junc == null) return;
 		drawer.bindTabTex(this, "main");
 		drawer.draw(gLeft + 73 + menu.junc.type.ordinal() * 18, gTop + 21, 0, 238, 18, 18);
-		//
-		for(int i = 0; i < menu.junc.size(); i++){
-			drawTrack(menu.junc.tracks.get(i), TRACK_COLOR[i], menu.junc.getPos().pos);
-		}
 	}
 
-	private void drawTrack(Track track, float[] color, V3I pos){
-		V3D vec0, vec1;
-		if(track.vecpath.length == 2){
-			vec0 = track.vecpath[0];
-			vec1 = vec0.distance(track.vecpath[1], 15);
-			drawer.drawLine(
-				(vec0.x - pos.x + 16) * 2 + gLeft + 7, (vec0.z - pos.z + 16) * 2 + gTop + 21,
-				(vec1.x - pos.x + 16) * 2 + gLeft + 7, (vec1.z - pos.z + 16) * 2 + gTop + 21,
-				color);
-			return;
+	@Override
+	public void postdraw(float ticks, int mx, int my){
+		if(menu.junc == null) return;
+		for(int xo = 0; xo < 64; xo++){
+			for(int yo = 0; yo < 64; yo++){
+				if(grid[xo][yo] == 0) continue;
+				drawer.apply(new RGB(grid[xo][yo]));
+				drawer.draw(xo + gLeft + 7, yo + gTop + 21, 7, 21, 1, 1);
+			}
 		}
-		double x0, x1, z0, z1;
-		for(int j = 0; j < track.vecpath.length - 1; j++){
-			vec0 = track.vecpath[j];
-			vec1 = track.vecpath[j + 1];
-			x0 = vec0.x - pos.x;
-			z0 = vec0.z - pos.z;
-			if(x0 < -16 || z0 < -16 || x0 > 16 || z0 > 16) continue;
-			x1 = vec1.x - pos.x;
-			z1 = vec1.z - pos.z;
-			if(x1 < -16 || z1 < -16 || x1 > 16 || z1 > 16) continue;
-			drawer.drawLine(
-				(x0 + 16) * 2 + gLeft + 7, (z0 + 16) * 2 + gTop + 21,
-				(x1 + 16) * 2 + gLeft + 7, (z1 + 16) * 2 + gTop + 21,
-				color);
-		}
+		drawer.applyWhite();
 	}
 
 	@Override
@@ -176,6 +162,46 @@ public class RailJunction extends UserInterface {
 		}
 		if(buttons.get("signal").hovered()){
 			list.add(menu.TRANSFORMAT.apply("ui.fvtm.rail_junction.signal", new Object[]{ menu.junc.signal == null ? "none" : menu.junc.signal }));
+		}
+	}
+
+	public void updateGrid(){
+		for(int x = 0; x < 64; x++) for(int y = 0; y < 64; y++) conv.setRGB(x, y, Color.BLACK.getRGB());
+		Graphics g = conv.createGraphics();
+		ArrayList<Track> tracks = new ArrayList<>(menu.junc.tracks);
+		for(int i = 0; i < tracks.size(); i++){
+			Track track = tracks.get(i);
+			g.setColor(TRACK_COLOR[i]);
+			V3D vec0, vec1;
+			if(track.vecpath.length == 2){
+				vec0 = track.vecpath[0];
+				vec1 = vec0.distance(track.vecpath[1], 15);
+				g.drawLine((int)((vec0.x - menu.pos.x + 16) * 2), (int)((vec0.z - menu.pos.z + 16) * 2),
+					(int)((vec1.x - menu.pos.x + 16) * 2), (int)((vec1.z - menu.pos.z + 16) * 2));
+				continue;
+			}
+			double x0, x1, z0, z1;
+			for(int j = 0; j < track.vecpath.length - 1; j++){
+				vec0 = track.vecpath[j];
+				vec1 = track.vecpath[j + 1];
+				x0 = vec0.x - menu.pos.x;
+				z0 = vec0.z - menu.pos.z;
+				if(x0 < -16 || z0 < -16 || x0 > 16 || z0 > 16) continue;
+				x1 = vec1.x - menu.pos.x;
+				z1 = vec1.z - menu.pos.z;
+				if(x1 < -16 || z1 < -16 || x1 > 16 || z1 > 16) continue;
+				g.drawLine(
+					(int)((x0 + 16) * 2), (int)((z0 + 16) * 2),
+					(int)((x1 + 16) * 2), (int)((z1 + 16) * 2));
+			}
+		}
+		g.dispose();
+		int c;
+		for(int xo = 0; xo < 64; xo++){
+			for(int yo = 0; yo < 64; yo++){
+				c = conv.getRGB(xo, yo);
+				grid[xo][yo] = c == Color.BLACK.getRGB() ? 0 : c;
+			}
 		}
 	}
 

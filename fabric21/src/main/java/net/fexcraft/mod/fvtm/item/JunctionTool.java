9b00c0c91f06e1352eedf63dc40a5b22e59b61a7
Ferdinand Calo <fexcalo@gmail.com>
2025-05-07 18:01:16 +0200

    Update JunctionTool.java

--- fabric21/src/main/java/net/fexcraft/mod/fvtm/item/JunctionTool.java
+++ fabric21/src/main/java/net/fexcraft/mod/fvtm/item/JunctionTool.java
@@ -1,5 +1,6 @@
 package net.fexcraft.mod.fvtm.item;
 
+import net.fexcraft.mod.fvtm.data.JunctionGridItem;
 import net.fexcraft.mod.fvtm.sys.rail.Junction;
 import net.fexcraft.mod.fvtm.sys.rail.RailSystem;
 import net.fexcraft.mod.fvtm.sys.uni.Passenger;
@@ -28,7 +29,7 @@ import static net.minecraft.network.chat.Component.translatable;
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
-public class JunctionTool extends Item {
+public class JunctionTool extends Item implements JunctionGridItem {
 
 	public JunctionTool(Properties prop){
 		super(prop.stacksTo(1));

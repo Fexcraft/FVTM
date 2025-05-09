bcef0c01da70e2fe89fbd6bc55ceec1e45be3852
Ferdinand Calo <fexcalo@gmail.com>
2025-05-09 20:23:10 +0200

    Update JunctionToolItem.java

--- forge12/src/main/java/net/fexcraft/mod/fvtm/item/JunctionToolItem.java
+++ forge12/src/main/java/net/fexcraft/mod/fvtm/item/JunctionToolItem.java
@@ -7,23 +7,23 @@ import java.util.List;
 import javax.annotation.Nullable;
 
 import net.fexcraft.lib.common.utils.Formatter;
-import net.fexcraft.lib.mc.utils.Print;
 import net.fexcraft.mod.fvtm.data.JunctionGridItem;
 import net.fexcraft.mod.fvtm.sys.rail.Junction;
 import net.fexcraft.mod.fvtm.sys.rail.RailSystem;
 import net.fexcraft.mod.fvtm.sys.uni.Passenger;
 import net.fexcraft.mod.fvtm.sys.uni.SystemManager;
-import net.fexcraft.mod.fvtm.sys.uni.SystemManager.Systems;
 import net.fexcraft.mod.fvtm.ui.UIKeys;
 import net.fexcraft.mod.fvtm.util.QV3D;
 import net.fexcraft.mod.uni.UniEntity;
+import net.fexcraft.mod.uni.inv.StackWrapper;
+import net.fexcraft.mod.uni.inv.UniStack;
 import net.fexcraft.mod.uni.tag.TagCW;
+import net.fexcraft.mod.uni.world.WorldW;
 import net.fexcraft.mod.uni.world.WrapperHolder;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
@@ -49,8 +49,6 @@ public class JunctionToolItem extends Item implements JunctionGridItem {
 	@SideOnly(Side.CLIENT)
 	@Override
 	public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flagIn){
-		tooltip.add(Formatter.format("&9Junction Editing Toolbox"));
-		tooltip.add(Formatter.format("&9- - - - - - &7-"));
 		if(stack.getTagCompound() != null && stack.getTagCompound().hasKey("fvtm:junction")){
 			tooltip.add(Formatter.format("&9Junction Selected: &7" + new QV3D(TagCW.wrap(stack.getTagCompound()), "fvtm:junction").toString()));
 		}
@@ -60,56 +58,56 @@ public class JunctionToolItem extends Item implements JunctionGridItem {
 	}
 
 	@Override
-	public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ){
-		if(world.isRemote || DISABLE_RAILS){
+	public EnumActionResult onItemUse(EntityPlayer player, World level, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ){
+		if(level.isRemote || DISABLE_RAILS){
 			return EnumActionResult.PASS;
 		}
-		RailSystem syscap = SystemManager.get(Systems.RAIL, WrapperHolder.getWorld(world));
-		if(syscap == null){
-			Print.chat(player, "&cWorld Capability not found.");
+		WorldW world = WrapperHolder.getWorld(level);
+		RailSystem railsys = SystemManager.get(SystemManager.Systems.RAIL, world);
+		Passenger pass = (Passenger)UniEntity.getEntity(player);
+		if(railsys == null){
+			pass.bar("item.fvtm.junction_tool.nosys");
 			return EnumActionResult.FAIL;
 		}
 		QV3D vector = new QV3D(pos.getX() + hitX, pos.getY() + hitY, pos.getZ() + hitZ), cached;
-		Passenger pass = (Passenger)UniEntity.getEntity(player);
-		ItemStack stack = player.getHeldItem(hand);
+		StackWrapper stack = UniStack.getStack(player.getHeldItem(hand));
 		if(player.isSneaking()){
-			Junction junc = syscap.getJunction(vector.pos);
+			Junction junc = railsys.getJunction(vector.pos);
 			if(junc == null){
-				Print.bar(player, "&cNo junction at position.");
+				pass.bar("item.fvtm.junction_tool.nojunc");
 			}
 			else if(junc.size() > 0){
-				Print.bar(player, "&cDisconnect all tracks before removing a Junction.");
+				pass.bar("item.fvtm.junction_tool.fulljunc");
 			}
 			else{
-				syscap.delJunction(vector.pos);
-				Print.bar(player, "&c&oRemoving Junction...");
+				railsys.delJunction(vector.pos);
+				pass.bar("item.fvtm.junction_tool.remjunc");
 			}
 			return EnumActionResult.SUCCESS;
 		}
-		if(stack.getTagCompound() == null) stack.setTagCompound(new NBTTagCompound());
-		Junction junk = syscap.getJunction(vector.pos, true);
+		Junction junk = railsys.getJunction(vector.pos, true);
 		if(junk == null){
-			Print.bar(player, "&cNo Junction at this Position.");
+			pass.bar("item.fvtm.junction_tool.nojunc");
 			return EnumActionResult.SUCCESS;
 		}
 		else{
-			if(stack.getTagCompound().hasKey("fvtm:junction")){
-				cached = new QV3D(TagCW.wrap(stack.getTagCompound()), "fvtm:junction");
+			if(stack.directTag().has("fvtm:junction")){
+				cached = new QV3D(stack.directTag(), "fvtm:junction");
 				if(cached.equals(vector)){
 					pass.openUI(UIKeys.RAIL_JUNCTION, cached.pos.x, cached.pos.y, cached.pos.z);
 					return EnumActionResult.SUCCESS;
 				}
 				if(junk.tracks.size() <= 2){
-					stack.getTagCompound().removeTag("fvtm:junction");
-					Print.bar(player, "&7&oResetting previous Cached Position.");
+					stack.updateTag(tag -> tag.rem("fvtm:junction"));
+					pass.bar("item.fvtm.junction_tool.reset");
 				}
 			}
 			if(junk.tracks.size() < 2){
 				pass.openUI(UIKeys.RAIL_JUNCTION, vector.pos.x, vector.pos.y, vector.pos.z);
 			}
 			else{
-				vector.write(TagCW.wrap(stack.getTagCompound()), "fvtm:junction");
-				Print.bar(player, "&a&lJunction Position Cached.");
+				stack.updateTag(tag -> vector.write(tag, "fvtm:junction"));
+				pass.bar("item.fvtm.junction_tool.cached");
 			}
 			return EnumActionResult.SUCCESS;
 		}

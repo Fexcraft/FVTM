a117fb435206390892ed262d79369f5dd7ad0dee
Ferdinand Calo <fexcalo@gmail.com>
2025-05-11 23:34:42 +0200

    Update RailGaugeItem.java

--- forge12/src/main/java/net/fexcraft/mod/fvtm/item/RailGaugeItem.java
+++ forge12/src/main/java/net/fexcraft/mod/fvtm/item/RailGaugeItem.java
@@ -59,36 +59,13 @@ public class RailGaugeItem extends Item implements ContentItem<RailGauge>, Junct
     @SideOnly(Side.CLIENT)
     @Override
     public void addInformation(ItemStack stack, @Nullable World worldIn, List<String> tooltip, ITooltipFlag flag){
-        tooltip.add(Formatter.format("&9Name: &7" + gauge.getName()));
-        for(String s : gauge.getDescription()){
-            tooltip.add(Formatter.format(I18n.format(s)));
-        }
-        tooltip.add(Formatter.format("&9Width: &7" + gauge.getWidth()));
+        for(String s : gauge.getDescription()) tooltip.add(Formatter.format(I18n.format(s)));
+        tooltip.add(Formatter.format(I18n.format("item.fvtm.railgauge.width", gauge.getWidth())));
         if(gauge.getCompatible().size() > 0){
-            tooltip.add(Formatter.format("&9Compatible with:"));
+			tooltip.add(Formatter.format(I18n.format("item.fvtm.railgauge.compatible")));
             for(String str : gauge.getCompatible()){
-                tooltip.add(Formatter.format("&7 - " + str));
+                tooltip.add(Formatter.format("- " + str));
             }
-        }
-        tooltip.add(Formatter.format("&9- - - - - - &7-"));
-        if(flag.isAdvanced()){
-            tooltip.add(Formatter.format("&6Usage:"));
-            tooltip.add(Formatter.format("&b- Rightclick twice in the same position to create a Junction."));
-            tooltip.add(Formatter.format("&b- Rightclick in sequence between 2 Junctions to create a track."));
-            tooltip.add(Formatter.format("&b- Rightclick + Sneak to reset point cache (sequence)."));
-        }
-        else{
-            tooltip.add(Formatter.format("&6Enable advanced tooltips for item usage info."));
-        }
-        tooltip.add(Formatter.format("&9- - - - - - &7-"));
-        if(stack.getTagCompound() != null && stack.getTagCompound().hasKey("fvtm:railpoints")){
-        	NBTTagList list = (NBTTagList)stack.getTagCompound().getTag("fvtm:railpoints");
-    		for(int k = 0; k < list.tagCount(); k++){
-            	tooltip.add(Formatter.format("&9PT" + k + " POS:" + new QV3D(TagCW.wrap(list.getCompoundTagAt(k)), null).toString()));
-    		}
-        }
-        else{
-        	tooltip.add("No Connection data.");
         }
     }
 	
@@ -104,74 +81,6 @@ public class RailGaugeItem extends Item implements ContentItem<RailGauge>, Junct
 		RailPlacingUtil.place(syscap, UniEntity.getEntity(player), gauge, vector);
 		return EnumActionResult.SUCCESS;
     }
-	
-	public EnumActionResult placeTrack(EntityPlayer player, World world, ItemStack stack, RailSystem syscap, QV3D vector, ICommandSender sender, boolean noblocks){
-		Junction junk = syscap.getJunction(vector.pos, true);
-		NBTTagList list = stack.getTagCompound().hasKey("fvtm:railpoints") ? (NBTTagList)stack.getTagCompound().getTag("fvtm:railpoints") : new NBTTagList();
-		if(junk == null || list.isEmpty()){
-			if(list.isEmpty() || !createdJunction(sender, syscap, player, list, vector)){
-				list.appendTag(vector.write(null, null).local());
-				stack.getTagCompound().setTag("fvtm:railpoints", list);
-				Print.chatbar(sender, list.tagCount() + (getSuffix(list.tagCount())) + " Point Added!");
-				return EnumActionResult.SUCCESS;
-			}
-			else{ stack.getTagCompound().removeTag("fvtm:railpoints"); return EnumActionResult.SUCCESS; }
-		}
-		else{
-			if(!junk.tracks.isEmpty() && junk.tracks.size() < 2 && !junk.tracks.get(0).isCompatibleGauge(gauge)){
-				Print.chat(sender, "&9Item Gauge not compatible with the &7Junction's Gauge&9.");
-				return EnumActionResult.FAIL;
-			}
-			if(junk.signal != null){
-				Print.chat(sender, "&9Please remove the signal first.");
-				stack.getTagCompound().removeTag("fvtm:railpoints"); return EnumActionResult.FAIL;
-			}
-			if(junk.tracks.size() >= 4){
-				Print.chat(sender, "&9Junction reached track limit (4)\n&c&oPoint cache reset.");
-				stack.getTagCompound().removeTag("fvtm:railpoints"); return EnumActionResult.FAIL;
-			}
-			Track track = new Track(junk, getVectors(TagLW.wrap(list)), vector, gauge);
-			if(track.length > MAX_RAIL_TRACK_LENGTH){
-				Print.chat(sender, "&cTrack length exceeds the configured max length.");
-				return EnumActionResult.FAIL;
-			}
-			Junction second = syscap.getJunction(track.start.pos);
-			//track.blockless = noblocks;
-			if(second != null){
-				//if(!TrackPlacer.set(UniEntity.getEntity(player), null, track).place()/*.blocks(!noblocks)*/.consume().result()) return EnumActionResult.SUCCESS;
-				second.addnew(track);
-				junk.addnew(track.createOppositeCopy());
-				second.checkTrackSectionConsistency();
-				Print.chat(sender, "&aTrack Created!");
-				stack.getTagCompound().removeTag("fvtm:railpoints");
-				/*if(!player.capabilities.isCreativeMode){
-					stack.shrink(1);
-				}*/
-			}
-			else Print.chat(sender, "&cNo Junction at starting point found!");
-			return EnumActionResult.SUCCESS;
-		}
-	}
-
-	private boolean createdJunction(ICommandSender sender, RailSystem syscap, EntityPlayer player, NBTTagList list, QV3D vector){
-		if(list.tagCount() != 1) return false; QV3D vec = getFirstVector(TagLW.wrap(list)); if(!vec.equals(vector)) return false;
-		syscap.addJunction(vector); Print.chat(sender, "Junction Created!"); return true;
-	}
-
-	private String getSuffix(int tagCount){
-		if(tagCount < 4) return tagCount == 1 ? "st" : tagCount == 2 ? "nd" : "rd"; else return "th";
-	}
-
-	public QV3D[] getVectors(TagLW list){
-		QV3D[] arr = new QV3D[list.size()];
-		for(int i = 0; i < arr.length; i++){
-			arr[i] = new QV3D(list.getCompound(i), null);
-		} return arr;
-	}
-
-	private QV3D getFirstVector(TagLW list){
-		return new QV3D(list.getCompound(0), null);
-	}
 
 	@Override
 	public RailGauge getContent(){

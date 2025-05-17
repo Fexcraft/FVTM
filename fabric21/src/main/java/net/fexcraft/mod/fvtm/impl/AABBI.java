7a52a8f30b3a63fd5ef23776134946e1ab63d50f
Ferdinand Calo <fexcalo@gmail.com>
2025-05-17 16:50:10 +0200

    Update AABBI.java

--- fabric21/src/main/java/net/fexcraft/mod/fvtm/impl/AABBI.java
+++ fabric21/src/main/java/net/fexcraft/mod/fvtm/impl/AABBI.java
@@ -56,4 +56,34 @@ public class AABBI extends net.fexcraft.mod.fvtm.data.block.AABB {
 		return aabb.contains((Vec3)vec);
 	}
 
+	@Override
+	public double minX(){
+		return aabb.minX;
+	}
+
+	@Override
+	public double minY(){
+		return aabb.minY;
+	}
+
+	@Override
+	public double minZ(){
+		return aabb.minZ;
+	}
+
+	@Override
+	public double maxX(){
+		return aabb.maxX;
+	}
+
+	@Override
+	public double maxY(){
+		return aabb.maxY;
+	}
+
+	@Override
+	public double maxZ(){
+		return aabb.maxZ;
+	}
+
 }

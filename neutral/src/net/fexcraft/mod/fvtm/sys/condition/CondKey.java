69057d5b697d10b0d274733f644a958da89679ca
Ferdinand Calo <fexcalo@gmail.com>
2025-05-27 23:32:06 +0200

    Create CondKey.java

---
+++ neutral/src/net/fexcraft/mod/fvtm/sys/condition/CondKey.java
@@ -0,0 +1,38 @@
+package net.fexcraft.mod.fvtm.sys.condition;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class CondKey {
+
+	public CondType type;
+	public CondMode mode;
+	public String target;
+
+	protected CondKey(CondType ct, CondMode cm, String tar){
+		type = ct;
+		mode = cm;
+		target = tar;
+	}
+
+	@Override
+	public boolean equals(Object obj){
+		if(obj instanceof CondKey){
+			CondKey ck = (CondKey)obj;
+			return type == ck.type && mode == ck.mode && target.equals(ck.target);
+		}
+		if(obj instanceof String) return toString().equals(obj.toString());
+		return false;
+	}
+
+	@Override
+	public String toString(){
+		return type.key + "-" + mode + "-" + target;
+	}
+
+	@Override
+	public int hashCode(){
+		return toString().hashCode();
+	}
+
+}

8c62aa86315951b477156a6d9d1c743f22792368
Ferdinand Calo <fexcalo@gmail.com>
2025-05-28 14:01:32 +0200

    Update EventAction.java

--- neutral/src/net/fexcraft/mod/fvtm/sys/event/EventAction.java
+++ neutral/src/net/fexcraft/mod/fvtm/sys/event/EventAction.java
@@ -17,62 +17,12 @@ import java.util.function.BiConsumer;
  */
 public class EventAction {
 
-	public static EventData DATA = new EventData();
 	public static LinkedHashMap<String, EventAction> ACTIONS = new LinkedHashMap<>();
-	public static EventAction NONE = new EventAction("none");
-	public static EventAction LOGGER = new EventAction("logger").set((data, lis, args) ->{
-		String str = lis.args[0];
-		if(lis.args.length > 1) for(int i = 1; i < lis.args.length; i++) str += " " + lis.args[i];
-		FvtmLogger.log(str);
-	});
-	public static EventAction SEND_MSG = new EventAction("msg").set((data, lis, args) ->{
-		if(data.pass == null) return;
-		String str = lis.args[0];
-		if(lis.args.length > 1) for(int i = 1; i < lis.args.length; i++) str += " " + lis.args[i];
-		if(lis.type.equals(EventType.ATTRIBUTE_UPDATE)){
-			str = str.replace("{attr}", ((Attribute)args[0]).id);
-			str = str.replace("{value}", ((Attribute)args[0]).asString());
-		}
-		data.pass.send(str);
-	});
-	public static EventAction SET_ATTR = new EventAction("set_attr").set((data, lis, args) -> {
-		Attribute attr = data.vehicle.getAttribute(lis.args[0]);
-		if(attr != null){
-			attr.set(lis.args[1]);
-			if(data.vehent != null) data.vehent.sendUpdate(VehicleInstance.PKT_UPD_VEHICLEDATA);//TODO packet to update only attribute
-		}
-	});
-	public static EventAction RESET_ATTR = new EventAction("reset_attr").set((data, lis, args) -> {
-		Attribute attr = data.vehicle.getAttribute(lis.args[0]);
-		if(attr != null){
-			attr.reset();
-			if(data.vehent != null) data.vehent.sendUpdate(VehicleInstance.PKT_UPD_VEHICLEDATA);//TODO packet to update only attribute
-		}
-	});
-	public static EventAction PLAY_SOUND = new EventAction("play_sound").set((data, lis, args) -> {
-		String ori = lis.args.length > 1 ? lis.args[1] : null;
-		String sound = lis.args[0];
-		Sound s = null;
-		if(ori != null){
-			PartData part = data.vehicle.getPart(ori);
-			if(part != null) s = part.getType().getSounds().get(sound);
-		}
-		if(s == null) s = data.holder.sounds.getSounds().get(sound);
-		if(s != null) data.vehent.entity.playSound(s.event, s.volume, s.pitch);
-	});
-	public static EventAction START_SOUND = new EventAction("start_sound").set((data, lis, args) -> {
-		String sound = lis.args[0];
-		Sound s = data.holder.sounds.getSounds().get(sound);
-		if(s != null) data.vehent.startSound(sound);
-	});
-	public static EventAction STOP_SOUND = new EventAction("stop_sound").set((data, lis, args) -> {
-		data.vehent.stopSound(lis.args[0]);
-	});
-
+	public static EventData DATA = new EventData();
 	public final String key;
 	private TriConsumer<EventData, EventListener, Object[]> consumer;
 
-	private EventAction(String ky){
+	protected EventAction(String ky){
 		key = ky;
 		ACTIONS.put(key, this);
 	}
@@ -86,7 +36,7 @@ public class EventAction {
 		for(EventAction et : ACTIONS.values()){
 			if(et.key.equals(type)) return et;
 		}
-		return NONE;
+		return EventActions.NONE;
 	}
 
 	public void run(EventData data, EventListener lis, Object[] args){

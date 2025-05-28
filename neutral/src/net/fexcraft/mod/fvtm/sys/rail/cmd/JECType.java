19ffb32e5c925348ae92f6c4b73aa8ce69412ca9
Ferdinand Calo <fexcalo@gmail.com>
2025-05-28 15:16:36 +0200

    Update JECType.java

--- neutral/src/net/fexcraft/mod/fvtm/sys/rail/cmd/JECType.java
+++ neutral/src/net/fexcraft/mod/fvtm/sys/rail/cmd/JECType.java
@@ -4,8 +4,7 @@ package net.fexcraft.mod.fvtm.sys.rail.cmd;
  * @author Ferdinand Calo' (FEX___96)
  */
 public enum JECType {
-	
-	SIGNAL_WAIT(true, 20),//check if signal changed once per second
+
 	STOP(true, 0),//apply instantly (decrease throttle to 0), remove command afterwards
 	WAIT(true, 20),//check every second if time passed
 	ADJUST_SPEED(true, 1),//apply instantly, remove command afterwards
@@ -27,7 +26,6 @@ public enum JECType {
 		switch(this){
 			case SET_SIGNAL: return CMD_SetSignal.class;
 			case SET_STATE: return CMD_SetSwitch.class;
-			case SIGNAL_WAIT: return CMD_SignalWait.class;
 			default: return null;
 		}
 	}

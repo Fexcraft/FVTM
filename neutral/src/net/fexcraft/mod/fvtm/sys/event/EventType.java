cf36aff1c21903c5ae9c4e2cf4462f9648f7ec03
Ferdinand Calo <fexcalo@gmail.com>
2025-05-30 00:23:01 +0200

    Update EventType.java

--- neutral/src/net/fexcraft/mod/fvtm/sys/event/EventType.java
+++ neutral/src/net/fexcraft/mod/fvtm/sys/event/EventType.java
@@ -16,9 +16,11 @@ public class EventType {
 	public static EventType ATTRIBUTE_UPDATE = new EventType("attr_update");
 	public static EventType PART_INSTALL = new EventType("part_install");
 	public static EventType PART_UNINSTALL = new EventType("part_deinstall");
+	public static EventType JUNC_PASS = new EventType("rail_poll_pass");
 	public static EventType JUNC_SWITCH = new EventType("rail_poll_switch");
 	public static EventType JUNC_SIGNAL = new EventType("rail_poll_signal");
 	static{
+		JUNCTION_EVENTS.add(JUNC_PASS);
 		JUNCTION_EVENTS.add(JUNC_SWITCH);
 		JUNCTION_EVENTS.add(JUNC_SIGNAL);
 	}

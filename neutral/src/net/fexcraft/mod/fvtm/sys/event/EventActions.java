0bd56faddbcce5a9842e2096be3744851e087914
Ferdinand Calo <fexcalo@gmail.com>
2025-05-29 23:57:05 +0200

    Update EventActions.java

--- neutral/src/net/fexcraft/mod/fvtm/sys/event/EventActions.java
+++ neutral/src/net/fexcraft/mod/fvtm/sys/event/EventActions.java
@@ -12,36 +12,46 @@ import net.fexcraft.mod.fvtm.sys.uni.VehicleInstance;
 public class EventActions {
 
 	public static EventAction NONE = new EventAction("none");
-	public static EventAction LOGGER = new EventAction("logger").set((data, lis, args) ->{
-		String str = lis.args[0];
-		if(lis.args.length > 1) for(int i = 1; i < lis.args.length; i++) str += " " + lis.args[i];
+	public static EventAction LOGGER = new EventAction("logger").set((data, lis, objs) ->{
+		String str = lis.argString();
+		str = str.replace("{event}", lis.type.key);
+		if(lis.type.equals(EventType.ATTRIBUTE_UPDATE)){
+			str = str.replace("{attr}", ((Attribute)objs[0]).id);
+			str = str.replace("{value}", ((Attribute)objs[0]).asString());
+		}
+		if(lis.type.key.startsWith("rail_")){
+			str = str.replace("{junction}", data.holder.junction().posString());
+		}
 		FvtmLogger.log(str);
 	});
-	public static EventAction SEND_MSG = new EventAction("msg").set((data, lis, args) ->{
+	public static EventAction SEND_MSG = new EventAction("msg").set((data, lis, objs) ->{
 		if(data.pass == null) return;
-		String str = lis.args[0];
-		if(lis.args.length > 1) for(int i = 1; i < lis.args.length; i++) str += " " + lis.args[i];
+		String str = lis.argString();
+		str = str.replace("{event}", lis.type.key);
 		if(lis.type.equals(EventType.ATTRIBUTE_UPDATE)){
-			str = str.replace("{attr}", ((Attribute)args[0]).id);
-			str = str.replace("{value}", ((Attribute)args[0]).asString());
+			str = str.replace("{attr}", ((Attribute)objs[0]).id);
+			str = str.replace("{value}", ((Attribute)objs[0]).asString());
+		}
+		if(lis.type.key.startsWith("rail_")){
+			str = str.replace("{junction}", data.holder.junction().posString());
 		}
 		data.pass.send(str);
 	});
-	public static EventAction SET_ATTR = new EventAction("set_attr").set((data, lis, args) -> {
+	public static EventAction SET_ATTR = new EventAction("set_attr").set((data, lis, objs) -> {
 		Attribute attr = data.vehicle.getAttribute(lis.args[0]);
 		if(attr != null){
 			attr.set(lis.args[1]);
 			if(data.vehent != null) data.vehent.sendUpdate(VehicleInstance.PKT_UPD_VEHICLEDATA);//TODO packet to update only attribute
 		}
 	});
-	public static EventAction RESET_ATTR = new EventAction("reset_attr").set((data, lis, args) -> {
+	public static EventAction RESET_ATTR = new EventAction("reset_attr").set((data, lis, objs) -> {
 		Attribute attr = data.vehicle.getAttribute(lis.args[0]);
 		if(attr != null){
 			attr.reset();
 			if(data.vehent != null) data.vehent.sendUpdate(VehicleInstance.PKT_UPD_VEHICLEDATA);//TODO packet to update only attribute
 		}
 	});
-	public static EventAction PLAY_SOUND = new EventAction("play_sound").set((data, lis, args) -> {
+	public static EventAction PLAY_SOUND = new EventAction("play_sound").set((data, lis, objs) -> {
 		String ori = lis.args.length > 1 ? lis.args[1] : null;
 		String sound = lis.args[0];
 		Sound s = null;
@@ -52,16 +62,16 @@ public class EventActions {
 		if(s == null) s = data.sounds().getSounds().get(sound);
 		if(s != null) data.vehent.entity.playSound(s.event, s.volume, s.pitch);
 	});
-	public static EventAction START_SOUND = new EventAction("start_sound").set((data, lis, args) -> {
+	public static EventAction START_SOUND = new EventAction("start_sound").set((data, lis, objs) -> {
 		String sound = lis.args[0];
 		Sound s = data.sounds().getSounds().get(sound);
 		if(s != null) data.vehent.startSound(sound);
 	});
-	public static EventAction STOP_SOUND = new EventAction("stop_sound").set((data, lis, args) -> {
+	public static EventAction STOP_SOUND = new EventAction("stop_sound").set((data, lis, objs) -> {
 		data.vehent.stopSound(lis.args[0]);
 	});
 	//
-	public static EventAction RAIL_REVERSE = new EventAction("rail_reverse").set((data, lis, args) -> {
+	public static EventAction RAIL_REVERSE = new EventAction("rail_reverse").set((data, lis, objs) -> {
 		data.vehent.railent.setForward(null, !data.vehent.railent.isHeadingForward());
 	});
 

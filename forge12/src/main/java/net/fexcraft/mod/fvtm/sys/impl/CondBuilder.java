50a3edcba52489511962fc68f80218e0e18f3c10
Ferdinand Calo <fexcalo@gmail.com>
2025-06-10 21:35:25 +0200

    Update CondBuilder.java

--- forge12/src/main/java/net/fexcraft/mod/fvtm/sys/impl/CondBuilder.java
+++ forge12/src/main/java/net/fexcraft/mod/fvtm/sys/impl/CondBuilder.java
@@ -15,53 +15,6 @@ import static net.fexcraft.mod.fvtm.sys.condition.ConditionRegistry.COND_FALSE;
 public class CondBuilder {
 
 	public static Function<CondKey, Conditional> run(){
-		Function<CondKey, Conditional> con = CondBuilderRoot.run();
-		if(con != null) return con;
-		return (key) -> {
-			switch(key.type){
-				case WORLDTIME:{
-					switch(key.mode){
-						case EQUAL:{
-							return (cd, data) -> {
-								World world = data.entity == null ? data.tile == null ? null : ((TileEntity)data.tile).getWorld() : ((Entity)data.entity).world;
-								return world == null ? false : world.getWorldTime() == cd.value.integer_value();
-							};
-						}
-						case NEQUAL:{
-							return (cd, data) -> {
-								World world = data.entity == null ? data.tile == null ? null : ((TileEntity)data.tile).getWorld() : ((Entity)data.entity).world;
-								return world == null ? false : world.getWorldTime() != cd.value.integer_value();
-							};
-						}
-						case LEQUAL:{
-							return (cd, data) -> {
-								World world = data.entity == null ? data.tile == null ? null : ((TileEntity)data.tile).getWorld() : ((Entity)data.entity).world;
-								return world == null ? false : world.getWorldTime() <= cd.value.integer_value();
-							};
-						}
-						case GEQUAL:{
-							return (cd, data) -> {
-								World world = data.entity == null ? data.tile == null ? null : ((TileEntity)data.tile).getWorld() : ((Entity)data.entity).world;
-								return world == null ? false : world.getWorldTime() >= cd.value.integer_value();
-							};
-						}
-						case LESS:{
-							return (cd, data) -> {
-								World world = data.entity == null ? data.tile == null ? null : ((TileEntity)data.tile).getWorld() : ((Entity)data.entity).world;
-								return world == null ? false : world.getWorldTime() < cd.value.integer_value();
-							};
-						}
-						case GREATER:{
-							return (cd, data) -> {
-								World world = data.entity == null ? data.tile == null ? null : ((TileEntity)data.tile).getWorld() : ((Entity)data.entity).world;
-								return world == null ? false : world.getWorldTime() > cd.value.integer_value();
-							};
-						}
-					}
-					break;
-				}
-			}
-			return COND_FALSE;
-		};
-	};
+		return CondBuilderRoot.run();
+	}
 }

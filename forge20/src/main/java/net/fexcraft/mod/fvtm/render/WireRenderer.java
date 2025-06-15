8998e611e94121fe3ba8720ef554c63aef131513
Ferdinand Calo <fexcalo@gmail.com>
2025-06-15 22:03:05 +0200

    Update WireRenderer.java

--- forge20/src/main/java/net/fexcraft/mod/fvtm/render/WireRenderer.java
+++ forge20/src/main/java/net/fexcraft/mod/fvtm/render/WireRenderer.java
@@ -2,8 +2,8 @@ package net.fexcraft.mod.fvtm.render;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.fexcraft.mod.fvtm.item.ToolboxItem;
+import net.fexcraft.mod.fvtm.item.WireDecoItem;
 import net.fexcraft.mod.fvtm.item.WireItem;
-import net.fexcraft.mod.fvtm.model.content.WireModel;
 import net.fexcraft.mod.fvtm.sys.uni.SystemManager;
 import net.fexcraft.mod.fvtm.sys.uni.SystemRegion;
 import net.fexcraft.mod.fvtm.sys.wire.*;
@@ -33,14 +33,12 @@ import static net.fexcraft.mod.fvtm.util.DebugUtils.COL_ORG;
 @Mod.EventBusSubscriber(modid = "fvtm", bus = Mod.EventBusSubscriber.Bus.FORGE, value = { Dist.CLIENT })
 public class WireRenderer {
 
-	public static Wire CURRENT;
-	public static double ANGLE = 0;
-	public static double ANGLE_DOWN = 0;
-	//
 	private static WireSystem wiredata;
 	private static ItemStack held;
 	private static boolean holding_wire;
 	private static boolean holding_slack;
+	private static boolean holding_relaydeco;
+	private static boolean holding_deco;
 
 	@SubscribeEvent
 	public static void renderWires(RenderLevelStageEvent event){
@@ -49,8 +47,15 @@ public class WireRenderer {
 		wiredata = SystemManager.get(SystemManager.Systems.WIRE, WrapperHolder.getWorld(event.getCamera().getEntity().level()), WireSystem.class);
 		if(wiredata == null || wiredata.getRegions() == null) return;
 		held = Minecraft.getInstance().player.getMainHandItem();
-		holding_wire = held.getItem() instanceof WireItem || (held.getItem() instanceof ToolboxItem && WIRE_REMOVAL.eq(getToolboxType(held)));
-		holding_slack = held.getItem() instanceof ToolboxItem && WIRE_SLACK.eq(getToolboxType(held));
+		holding_wire = DebugUtils.ACTIVE || held.getItem() instanceof WireItem || (held.getItem() instanceof ToolboxItem && WIRE_REMOVAL.eq(getToolboxType(held)));
+		holding_slack = DebugUtils.ACTIVE || held.getItem() instanceof ToolboxItem && WIRE_SLACK.eq(getToolboxType(held));
+		if(held.getItem() instanceof WireDecoItem){
+			WireDecoItem item = (WireDecoItem)held.getItem();
+			holding_relaydeco = item.getContent().getType().equals("relay");
+			holding_deco = !holding_relaydeco;
+		}
+		else holding_relaydeco = holding_deco = false;
+		//
 		Camera camera = event.getCamera();
 		double cx = camera.getPosition().x;
 		double cy = camera.getPosition().y;
@@ -76,36 +81,23 @@ public class WireRenderer {
 								holder.hasRef() ? holder.ref().getSize(relay.getKey()) * 2 : 0.25f,COL_ORG);
 						}
 					}
-					renderWires(pose, relay);
+					if(relay.wires.size() > 0){
+						if(holding_slack || holding_deco){
+							for(Wire wire : relay.wires){
+								if(wire.copy) continue;
+								DebugUtils.renderBB(wire.getVectorPosition(wire.length * 0.5, false),
+									holder.hasRef() ? holder.ref().getSize(relay.getKey()) * 2 : 0.25f, COL_ORG);
+							}
+						}
+						if(holding_relaydeco){
+							for(Wire wire : relay.wires){
+								DebugUtils.renderBB(wire.getVectorPosition(holder.hasRef() ? holder.ref().getSize(relay.getKey()) * 2 : 0.25f, false),
+									holder.hasRef() ? holder.ref().getSize(relay.getKey()) * 2 : 0.25f, COL_CYN);
+							}
+						}
+					}
+					UniWireRenderer.renderRelay(relay, 0, 0, 0);
 				}
-			}
-		}
-		pose.popPose();
-	}
-
-	private static void renderWires(PoseStack pose, WireRelay relay){
-		pose.pushPose();
-		for(int i = 0; i < relay.size(); i++){
-			if(relay.wires.get(i).copy) continue;
-			Wire wire = relay.wires.get(i);
-			if(wire.vecpath == null || wire.getWireType() == null) continue;
-			WireModel model = wire.getWireType().getModel();
-			if(wire.model.wiremodel == null) PathModelGenerator.generateWireModel(wire, model);
-			FvtmRenderTypes.setCutout(wire.getWireType().getTexture());
-			pose.translate(wire.vecpath[0].x, wire.vecpath[0].y, wire.vecpath[0].z);
-			wire.model.wiremodel.render();
-			if(relay.getTile() != null){
-				CURRENT = wire;
-				ANGLE = wire.model.end_angle;
-				if(wire.model.deco_s != null){
-					ANGLE_DOWN = wire.model.start_angle_down;
-					//TODO wire deco
-				}
-				if(wire.model.deco_e != null){
-					ANGLE_DOWN = wire.model.end_angle_down;
-					//TODO wire deco
-				}
-				//TODO wire deco
 			}
 		}
 		pose.popPose();

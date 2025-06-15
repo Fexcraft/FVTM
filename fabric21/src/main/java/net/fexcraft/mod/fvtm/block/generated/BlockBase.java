47dc7ac59fb0313238d65e329e8007439d5afc75
Ferdinand Calo <fexcalo@gmail.com>
2025-06-15 22:24:29 +0200

    Create BlockBase.java

---
+++ fabric21/src/main/java/net/fexcraft/mod/fvtm/block/generated/BlockBase.java
@@ -0,0 +1,56 @@
+package net.fexcraft.mod.fvtm.block.generated;
+
+import net.fexcraft.mod.fvtm.data.block.Block;
+import net.fexcraft.mod.fvtm.item.BlockItem21;
+import net.fexcraft.mod.fvtm.sys.uni.SystemManager;
+import net.fexcraft.mod.fvtm.sys.wire.WireSystem;
+import net.fexcraft.mod.fvtm.util.Resources21;
+import net.fexcraft.mod.uni.inv.UniStack;
+import net.fexcraft.mod.uni.world.WrapperHolder;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.EntityBlock;
+import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class BlockBase extends PlainBase implements EntityBlock {
+
+	public BlockBase(Properties prop, Block type){
+		super(prop, type);
+	}
+
+	@Override
+	public @Nullable BlockEntity newBlockEntity(BlockPos pos, BlockState state){
+		return Resources21.BASE_ENTITY.create(pos, state);
+	}
+
+	@Override
+	public void setPlacedBy(Level level, BlockPos pos, BlockState state, LivingEntity placer, ItemStack stack){
+		BlockEntity entity = level.getBlockEntity(pos);
+		if(entity == null) return;
+		BaseBlockEntity base = (BaseBlockEntity)entity;
+		base.data = ((BlockItem21)stack.getItem()).getData(UniStack.getStack(stack));
+		base.regRelay();
+	}
+
+	@Deprecated
+	public void onRemove(BlockState state0, Level level, BlockPos pos, BlockState state1, boolean bool){
+		if(type.hasRelay() && SystemManager.active(SystemManager.Systems.WIRE)){
+			SystemManager.get(SystemManager.Systems.WIRE, WrapperHolder.getWorld(level), WireSystem.class).deregister(level.getBlockEntity(pos));
+		}
+		//TODO super.onRemove(state0, level, pos, state1, bool);
+	}
+
+	@Override
+	public RenderShape getRenderShape(BlockState state){
+		return type.isInvisible() ? RenderShape.INVISIBLE : RenderShape.MODEL;
+	}
+
+}

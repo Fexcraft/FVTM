ddc2b7f10dd47c9092eaf1c91cc73829eff2f9fd
Ferdinand Calo <fexcalo@gmail.com>
2025-06-15 21:39:59 +0200

    Update WireRenderer.java

--- forge12/src/main/java/net/fexcraft/mod/fvtm/render/WireRenderer.java
+++ forge12/src/main/java/net/fexcraft/mod/fvtm/render/WireRenderer.java
@@ -3,19 +3,10 @@ package net.fexcraft.mod.fvtm.render;
 import net.fexcraft.lib.common.math.RGB;
 import net.fexcraft.lib.common.math.TexturedPolygon;
 import net.fexcraft.lib.common.math.V3D;
-import net.fexcraft.lib.frl.Polyhedron;
 import net.fexcraft.lib.tmt.ModelRendererTurbo;
-import net.fexcraft.mod.fvtm.block.generated.BlockTileEntity;
-import net.fexcraft.mod.fvtm.data.WireDeco;
-import net.fexcraft.mod.fvtm.data.block.BlockData;
 import net.fexcraft.mod.fvtm.item.ToolboxItem;
 import net.fexcraft.mod.fvtm.item.WireDecoItem;
 import net.fexcraft.mod.fvtm.item.WireItem;
-import net.fexcraft.mod.fvtm.model.ModelGroup;
-import net.fexcraft.mod.fvtm.model.Program;
-import net.fexcraft.mod.fvtm.model.content.WireMD;
-import net.fexcraft.mod.fvtm.model.content.WireModel;
-import net.fexcraft.mod.fvtm.model.program.WirePrograms;
 import net.fexcraft.mod.fvtm.sys.uni.SystemManager;
 import net.fexcraft.mod.fvtm.sys.uni.SystemManager.Systems;
 import net.fexcraft.mod.fvtm.sys.uni.SystemRegion;
@@ -25,7 +16,6 @@ import net.fexcraft.mod.fvtm.sys.wire.WireRelay;
 import net.fexcraft.mod.fvtm.sys.wire.WireSystem;
 import net.fexcraft.mod.fvtm.util.Command;
 import net.fexcraft.mod.fvtm.util.DebugUtils;
-import net.fexcraft.mod.fvtm.util.TexUtil;
 import net.fexcraft.mod.uni.world.WrapperHolder;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.BufferBuilder;
@@ -36,22 +26,13 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import org.lwjgl.opengl.GL11;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map.Entry;
-
 import static net.fexcraft.mod.fvtm.Config.DISABLE_WIRES;
 import static net.fexcraft.mod.fvtm.data.ToolboxType.WIRE_REMOVAL;
 import static net.fexcraft.mod.fvtm.data.ToolboxType.WIRE_SLACK;
-import static net.fexcraft.mod.fvtm.model.DefaultModel.RENDERDATA;
-import static net.fexcraft.mod.fvtm.render.EffectRenderer.drawString;
 import static net.fexcraft.mod.fvtm.util.DebugUtils.COL_CYN;
 
 public class WireRenderer {
-    
-	public static Wire CURRENT;
-	public static double ANGLE;
-	public static double ANGLE_DOWN;
+
 	protected static final ModelRendererTurbo model, model0, model1;
 	protected static final ModelRendererTurbo[] all;
 	static{
@@ -125,135 +106,49 @@ public class WireRenderer {
 							}
 						}
 					}
-            		renderWires(relay, cx, cy, cz);
+					if(Command.OTHER) renderDebugRelay(relay, cx, cy, cz);
+					else UniWireRenderer.renderRelay(relay, cx, cy, cz);
             	}
         	}
         }
 		GL11.glPopMatrix();
     }
 
-	private static void renderWires(WireRelay relay, double cx, double cy, double cz){
-        if(Command.OTHER){
-    		Tessellator tessellator = Tessellator.getInstance();
-            BufferBuilder bufferbuilder = tessellator.getBuffer();
-			V3D vec0, vec1;
-			float flfl, glgl;
-    		for(int o = 0; o < relay.wires.size(); o++){
-    			Wire conn = relay.wires.get(o);
-        		if(conn.vecpath == null) return;
-    	        flfl = conn.copy ? 1 : 0;
-    	        glgl = conn.copy ? 0 : 1;
-                GL11.glPushMatrix();
-                GlStateManager.enableBlend();
-                GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
-                GlStateManager.glLineWidth(2.0F);
-                GlStateManager.disableTexture2D();
-                GlStateManager.depthMask(false);
-    			for(int j = 0; j < conn.vecpath.length - 1; j++){
-    				vec0 = conn.vecpath[j]; vec1 = conn.vecpath[j + 1];
-                    bufferbuilder.begin(3, DefaultVertexFormats.POSITION_COLOR);
-                    bufferbuilder.pos(vec0.x - cx, vec0.y + (conn.copy ? 0.1f : 0) - cy, vec0.z - cz).color(0f, glgl, flfl, 1F).endVertex();
-                    bufferbuilder.pos(vec1.x - cx, vec1.y + (conn.copy ? 0.1f : 0) - cy, vec1.z - cz).color(0f, glgl, flfl, 1F).endVertex();
-                    tessellator.draw();
-    			}
-                GlStateManager.depthMask(true);
-                GlStateManager.enableTexture2D();
-                GlStateManager.disableBlend();
-                GL11.glPopMatrix();
-                //
-                /*float[] vec = conn.getPosition(conn.length * (Time.getSecond() / 60f));
-                if(vec.length == 1){ continue; }
-                GL11.glPushMatrix();
-                GL11.glTranslatef(vec[0], vec[1] + (conn.copy ? 0.1f : 0), vec[2]);
-                (conn.copy ? model1 : model0).render();
-                GL11.glPopMatrix();*///TODO
-    		}
-        }
-        else{
-			BlockData data;
-        	GL11.glPushMatrix();
-        	GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-        	for(int i = 0; i < relay.size(); i++){
-        		if(relay.wires.get(i).copy) continue;
-        		Wire wire = relay.wires.get(i);
-        		if(wire.vecpath == null || wire.getWireType() == null) continue;
-				if(wire.model == null) new WireMD(wire);
-        		TexUtil.bindTexture(wire.getWireType().getTexture());
-				GL11.glPushMatrix();
-				GL11.glTranslated(wire.vecpath[0].x - cx, wire.vecpath[0].y - cy, wire.vecpath[0].z - cz);
-        		wire.model.wiremodel.render();
-				GL11.glPopMatrix();
-				data = relay.getTile() == null ? null : ((BlockTileEntity)relay.getTile()).getBlockData();
-				CURRENT = wire;
-				if(wire.model.deco_s != null){
-					ANGLE = wire.model.start_angle;
-					ANGLE_DOWN = wire.model.start_angle_down;
-					GL11.glPushMatrix();
-					GL11.glTranslated(wire.vecpath[0].x - cx, wire.vecpath[0].y - cy, wire.vecpath[0].z - cz);
-					//GL11.glRotated(wire.model.start_angle, 0, 1, 0);
-					TexUtil.bindTexture(wire.model.deco_s.getTexture());
-					wire.model.deco_s.getModel().render(RENDERDATA.set(data, relay.getTile(), null));
-					GL11.glPopMatrix();
-				}
-				if(wire.model.deco_e != null){
-					ANGLE = wire.model.end_angle;
-					ANGLE_DOWN = wire.model.end_angle_down;
-					int l = wire.vecpath.length - 1;
-					GL11.glPushMatrix();
-					GL11.glTranslated(wire.vecpath[l].x - cx, wire.vecpath[l].y - cy, wire.vecpath[l].z - cz);
-					//GL11.glRotated(wire.model.end_angle, 0, 1, 0);
-					TexUtil.bindTexture(wire.model.deco_e.getTexture());
-					wire.model.deco_e.getModel().render(RENDERDATA.set(data, relay.getTile(), null));
-					GL11.glPopMatrix();
-				}
-				if(wire.model.deco_d.size() > 0){
-					WireModel wm;
-					for(Entry<String, WireDeco> dm : wire.decos.entrySet()){
-						wm = dm.getValue().getModel();
-						for(ModelGroup list : wm.groups){
-							if(wire.model.deco_d.get(dm.getKey()).containsKey(list.name)){
-								//ArrayList<Polyhedron> tlist = wire.model.deco_g.containsKey(dm.getKey()) ? wire.model.deco_g.get(dm.getKey()).get(list.name) : null;
-								//int didx = 0;
-								for(V3D vec : wire.model.deco_d.get(dm.getKey()).get(list.name)){
-									GL11.glPushMatrix();
-									GL11.glTranslated(vec.x - cx, vec.y - cy, vec.z - cz);
-									wm.transforms.apply();
-									TexUtil.bindTexture(dm.getValue().getTexture());
-									list.render(RENDERDATA.set(data, relay.getTile(), null));
-									/*if(tlist != null){
-										TexUtil.bindTexture(wire.getWireType().getTexture());
-										tlist.get(didx++).render();
-									}*/
-									wm.transforms.deapply();
-									GL11.glPopMatrix();
-								}
-							}
-							else{
-								GL11.glPushMatrix();
-								GL11.glTranslated(wire.vecpath[0].x, wire.vecpath[0].y, wire.vecpath[0].z);
-								wm.transforms.apply();
-								TexUtil.bindTexture(dm.getValue().getTexture());
-								list.render(RENDERDATA.set(data, relay.getTile(), null));
-								wm.transforms.deapply();
-								GL11.glPopMatrix();
-							}
-						}
-					}
-				}
-        	}
-        	if(Command.OTHER){
-        		Wire wire;
-        		for(int i = 0; i < relay.size(); i++){
-        			wire = relay.wires.get(i);
-					V3D pos = wire.getVectorPosition(wire.length * 0.5, false);
-	    			double off = wire.copy ? 0.125 : -0.125;
-	    			double deg = Minecraft.getMinecraft().player.getHorizontalFacing().getHorizontalIndex() * 90d;
-	    			drawString(wire.getUnit().section().getUID() + "", pos.x + off, pos.y + 0.5, pos.z, true, true, 0.8f, wire.copy ? 0xb8bc38 : 0x32a852, deg);
-        		}
-        	}
-    		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-    		GL11.glPopMatrix();
-        }
+	private static void renderDebugRelay(WireRelay relay, double cx, double cy, double cz){
+		Tessellator tessellator = Tessellator.getInstance();
+		BufferBuilder bufferbuilder = tessellator.getBuffer();
+		V3D vec0, vec1;
+		float flfl, glgl;
+		for(int o = 0; o < relay.wires.size(); o++){
+			Wire conn = relay.wires.get(o);
+			if(conn.vecpath == null) return;
+			flfl = conn.copy ? 1 : 0;
+			glgl = conn.copy ? 0 : 1;
+			GL11.glPushMatrix();
+			GlStateManager.enableBlend();
+			GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+			GlStateManager.glLineWidth(2.0F);
+			GlStateManager.disableTexture2D();
+			GlStateManager.depthMask(false);
+			for(int j = 0; j < conn.vecpath.length - 1; j++){
+				vec0 = conn.vecpath[j]; vec1 = conn.vecpath[j + 1];
+				bufferbuilder.begin(3, DefaultVertexFormats.POSITION_COLOR);
+				bufferbuilder.pos(vec0.x - cx, vec0.y + (conn.copy ? 0.1f : 0) - cy, vec0.z - cz).color(0f, glgl, flfl, 1F).endVertex();
+				bufferbuilder.pos(vec1.x - cx, vec1.y + (conn.copy ? 0.1f : 0) - cy, vec1.z - cz).color(0f, glgl, flfl, 1F).endVertex();
+				tessellator.draw();
+			}
+			GlStateManager.depthMask(true);
+			GlStateManager.enableTexture2D();
+			GlStateManager.disableBlend();
+			GL11.glPopMatrix();
+			//
+			/*float[] vec = conn.getPosition(conn.length * (Time.getSecond() / 60f));
+			if(vec.length == 1){ continue; }
+			GL11.glPushMatrix();
+			GL11.glTranslatef(vec[0], vec[1] + (conn.copy ? 0.1f : 0), vec[2]);
+			(conn.copy ? model1 : model0).render();
+			GL11.glPopMatrix();*/
+		}
 	}
 
 }

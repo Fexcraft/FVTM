3ea7fd5e15b2fbe0fc03c5112fe83ad7f25aaddd
Ferdinand Calo <fexcalo@gmail.com>
2025-06-15 22:24:42 +0200

    Create PlainBase.java

---
+++ fabric21/src/main/java/net/fexcraft/mod/fvtm/block/generated/PlainBase.java
@@ -0,0 +1,56 @@
+package net.fexcraft.mod.fvtm.block.generated;
+
+import net.fexcraft.lib.common.math.V3D;
+import net.fexcraft.mod.fvtm.data.block.Block;
+import net.fexcraft.mod.fvtm.data.block.BlockFunction;
+import net.fexcraft.mod.uni.UniEntity;
+import net.fexcraft.mod.uni.world.StateWrapper;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
+
+import static net.fexcraft.mod.uni.world.WrapperHolder.*;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class PlainBase extends net.minecraft.world.level.block.Block {
+
+	public Block type;
+
+	public PlainBase(Properties prop, Block type){
+		super(getProps(prop, type));
+		this.type = type;
+	}
+
+	private static Properties getProps(Properties prop, Block type){
+		prop.destroyTime(type.getHardness());
+		prop.lightLevel(state -> (int)(type.getLightLevel() * 16));
+		prop.explosionResistance(type.getResistance());
+		if(!type.isFullBlock() || type.isCutout() || !type.isOpaque()) prop.noOcclusion();
+		return prop;
+	}
+
+	@Override
+	public RenderShape getRenderShape(BlockState state){
+		return type.isInvisible() ? RenderShape.INVISIBLE : RenderShape.MODEL;
+	}
+
+	@Override
+	public InteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult res){
+		if(level.isClientSide || hand == InteractionHand.OFF_HAND) return InteractionResult.PASS;
+		if(!player.isCrouching() && type.getFunctions().size() > 0){
+			for(BlockFunction func : type.getFunctions()){
+				if(func.onClick(getWorld(level), getPos(pos), new V3D(0, 0, 0), StateWrapper.of(state), getSide(res.getDirection()), UniEntity.getCasted(player), hand == InteractionHand.MAIN_HAND)) return InteractionResult.SUCCESS;
+			}
+		}
+		return InteractionResult.PASS;
+	}
+
+}

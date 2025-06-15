b7832e1bc89938e9d4f455a308e328ee41ff6c62
Ferdinand Calo <fexcalo@gmail.com>
2025-06-15 22:26:24 +0200

    Create BlockTypeImpl.java

---
+++ fabric21/src/main/java/net/fexcraft/mod/fvtm/util/BlockTypeImpl.java
@@ -0,0 +1,21 @@
+package net.fexcraft.mod.fvtm.util;
+
+import net.fexcraft.mod.fvtm.block.generated.*;
+import net.fexcraft.mod.fvtm.data.block.BlockType;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class BlockTypeImpl {
+
+    public static Class get(BlockType type, boolean func, boolean plain){
+        plain = plain && !func;
+        switch(type){
+            case GENERIC_4ROT: return plain ? G_4ROT.class : G_4ROT_BE.class;
+            case GENERIC_16ROT: return plain ? G_16ROT.class : G_16ROT_BE.class;
+            case GENERIC_SIMPLE: return plain ? PlainBase.class : BlockBase.class;
+        }
+        return plain ? PlainBase.class : BlockBase.class;
+    }
+
+}

b77c02643fee736dc0a09b38a02e56a191a95208
Ferdinand Calo <fexcalo@gmail.com>
2025-06-21 22:37:49 +0200

    Update FvtmProperties.java

--- forge20/src/main/java/net/fexcraft/mod/fvtm/block/generated/FvtmProperties.java
+++ forge20/src/main/java/net/fexcraft/mod/fvtm/block/generated/FvtmProperties.java
@@ -1,8 +1,10 @@
 package net.fexcraft.mod.fvtm.block.generated;
 
 import net.minecraft.core.Direction;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -13,5 +15,13 @@ public class FvtmProperties {
 	public static final IntegerProperty PROP_VAR4 = IntegerProperty.create("variant", 0, 3);
 	public static final IntegerProperty PROP_ROT8 = IntegerProperty.create("rotation", 0, 7);
 	public static final IntegerProperty PROP_ROT16 = IntegerProperty.create("rotation", 0, 15);
+	//
+	public static final VoxelShape[] SHAPES = new VoxelShape[16];
+	static{
+		SHAPES[0] = Block.box(0.0, 0.0, 0.0, 16.0, 16.0, 16.0);
+		for(int i = 1; i < 16; i++){
+			SHAPES[i] = Block.box(0, 0, 0, 16, i, 16);
+		}
+	}
 
 }

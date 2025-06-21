960d266c83142919983865d28691857bbf8ce21f
Ferdinand Calo <fexcalo@gmail.com>
2025-06-21 22:37:44 +0200

    Update Asphalt.java

--- forge20/src/main/java/net/fexcraft/mod/fvtm/block/Asphalt.java
+++ forge20/src/main/java/net/fexcraft/mod/fvtm/block/Asphalt.java
@@ -1,8 +1,6 @@
 package net.fexcraft.mod.fvtm.block;
 
-import net.fexcraft.mod.fvtm.ui.UIKeys;
 import net.fexcraft.mod.fvtm.util.Resources20;
-import net.fexcraft.mod.uni.UniEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
@@ -16,26 +14,19 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
-import net.minecraftforge.common.ToolAction;
-import net.minecraftforge.common.ToolActions;
+
+import static net.fexcraft.mod.fvtm.block.generated.FvtmProperties.SHAPES;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public class Asphalt extends Block {
-
-	public static final VoxelShape[] SHAPES = new VoxelShape[16];
 	public final int height;
 
 	public Asphalt(int height){
 		super(Properties.of().noOcclusion().explosionResistance(2000).strength(height == 0 ? 3 : 3f - 3f / height));
 		this.height = height;
-		if(SHAPES[0] == null){
-			SHAPES[0] = Block.box(0.0, 0.0, 0.0, 16.0, 16.0, 16.0);
-			for(int i = 1; i < 16; i++){
-				SHAPES[i] = Block.box(0, 0, 0, 16, i, 16);
-			}
-		}
+
 	}
 
 	@Override

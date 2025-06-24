be03dd5fd16b691f1b6190d4dfb5374d7489f1d2
Ferdinand Calo <fexcalo@gmail.com>
2025-06-24 15:35:52 +0200

    Update Handler_SeatUpdate.java

--- neutral/src/net/fexcraft/mod/fvtm/packet/Handler_SeatUpdate.java
+++ neutral/src/net/fexcraft/mod/fvtm/packet/Handler_SeatUpdate.java
@@ -1,6 +1,6 @@
 package net.fexcraft.mod.fvtm.packet;
 
-import net.fexcraft.mod.fvtm.sys.uni.Passenger;
+import net.fexcraft.mod.fvtm.sys.uni.FvtmWorld;
 import net.fexcraft.mod.fvtm.sys.uni.SeatInstance;
 import net.fexcraft.mod.uni.packet.PacketHandler;
 import net.fexcraft.mod.uni.world.EntityW;
@@ -21,7 +21,7 @@ public class Handler_SeatUpdate implements PacketHandler<Packet_SeatUpdate> {
 	}
 
 	private void findAndApply(Packet_SeatUpdate packet, EntityW player, boolean send){
-		SeatInstance seat = ((Passenger)player).getFvtmWorld().getSeat(packet.entid, packet.seatid);
+		SeatInstance seat = ((FvtmWorld)player.getWorld()).getSeat(packet.entid, packet.seatid);
 		if(seat != null){
 			seat.pelook = seat.elook.copy();
 			seat.elook.set_rotation(packet.yaw, packet.pitch, 0F, true);

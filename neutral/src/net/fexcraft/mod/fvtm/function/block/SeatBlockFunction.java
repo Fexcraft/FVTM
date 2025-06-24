ecc91a31cfbedab524d388c6b609d20b763cd0b6
Ferdinand Calo <fexcalo@gmail.com>
2025-06-24 15:35:36 +0200

    Update SeatBlockFunction.java

--- neutral/src/net/fexcraft/mod/fvtm/function/block/SeatBlockFunction.java
+++ neutral/src/net/fexcraft/mod/fvtm/function/block/SeatBlockFunction.java
@@ -1,19 +1,19 @@
 package net.fexcraft.mod.fvtm.function.block;
 
-import java.util.List;
-
 import net.fexcraft.app.json.JsonValue;
 import net.fexcraft.lib.common.math.V3D;
 import net.fexcraft.lib.common.math.V3I;
 import net.fexcraft.lib.common.utils.Formatter;
 import net.fexcraft.mod.fvtm.data.block.BlockData;
 import net.fexcraft.mod.fvtm.data.block.BlockFunction;
-import net.fexcraft.mod.fvtm.sys.uni.Passenger;
 import net.fexcraft.mod.fvtm.util.ContentConfigUtil;
 import net.fexcraft.mod.uni.inv.StackWrapper;
 import net.fexcraft.mod.uni.world.CubeSide;
+import net.fexcraft.mod.uni.world.EntityW;
 import net.fexcraft.mod.uni.world.StateWrapper;
 import net.fexcraft.mod.uni.world.WorldW;
+
+import java.util.List;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -43,7 +43,7 @@ public class SeatBlockFunction extends BlockFunction.StaticBlockFunction {
     }
 
     @Override
-    public boolean onClick(WorldW world, V3I pos, V3D hit, StateWrapper state, CubeSide side, Passenger player, boolean main){
+    public boolean onClick(WorldW world, V3I pos, V3D hit, StateWrapper state, CubeSide side, EntityW player, boolean main){
         if(!main) return false;
         if(!player.isRiding()){
             world.spawnBlockSeat(offset.add(pos.x + 0.5, pos.y, pos.z + 0.5), player);

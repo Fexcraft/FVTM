d2631cdb620b5106e7cd7f02d2a0affa03a7a79a
Ferdinand Calo <fexcalo@gmail.com>
2025-06-27 00:02:19 +0200

    Update NoHotbar.java

--- forge12/src/main/java/net/fexcraft/mod/fvtm/util/NoHotbar.java
+++ forge12/src/main/java/net/fexcraft/mod/fvtm/util/NoHotbar.java
@@ -28,28 +28,27 @@ public class NoHotbar {
 			mc.fontRenderer.drawStringWithShadow("Fuel: " + ent.vehicle.data.getStoredFuel() + "/" + ent.vehicle.data.getFuelCapacity(), 10, 10, 0xffffff);
 			mc.fontRenderer.drawStringWithShadow("Throttle: " + RGB.df.format(ent.vehicle.throttle), 10, 18, 0xffffff);
 			mc.fontRenderer.drawStringWithShadow("Speed: " + RGB.df.format(ent.vehicle.speed), 10, 26, 0xffffff);
-			if(ent instanceof ULandVehicle){
-				int gear = ent.vehicle.data.getAttributeInteger("gear", 0);
-				if(lastgear != gear){
-					lastgear = gear;
-					boolean auto = ent.vehicle.transmission != null && ent.vehicle.transmission.isAutomatic();
-					gear_label = ent.vehicle.transmission != null && ent.vehicle.transmission.isAutomatic() && gear != 0 ? "A" : "";
-					if(gear < 0){
-						gear_label = (auto ? "A-" : "") + "R" + (ent.vehicle.transmission.getRGearAmount() == 1 ? "" : -gear);
-					}
-					else if(gear == 0){
-						gear_label = "N";
-					}
-					else{
-						gear_label += gear;
-					}
+			int gear = ent.vehicle.data.getAttributeInteger("gear", 0);
+			if(lastgear != gear){
+				lastgear = gear;
+				boolean auto = ent.vehicle.transmission != null && ent.vehicle.transmission.isAutomatic();
+				gear_label = ent.vehicle.transmission != null && ent.vehicle.transmission.isAutomatic() && gear != 0 ? "A" : "";
+				if(gear < 0){
+					gear_label = (auto ? "A-" : "") + "R" + (ent.vehicle.transmission.getRGearAmount() == 1 ? "" : -gear);
 				}
-				//mc.fontRenderer.drawStringWithShadow("RPM: " + (ent.crpm / 100 * 100), 10, 34, 0xffffff);
-				mc.fontRenderer.drawStringWithShadow("Gear: " + gear_label, 10, 42, 0xffffff);
-				mc.fontRenderer.drawStringWithShadow("P-Brake: " + (ent.vehicle.pbrake ? "ON" : "OFF"), 10, 58, 0xffffff);
+				else if(gear == 0){
+					gear_label = "N";
+				}
+				else{
+					gear_label += gear;
+				}
 			}
+			mc.fontRenderer.drawStringWithShadow("RPM: " + (ent.vehicle.crpm / 100 * 100), 10, 34, 0xffffff);
+			mc.fontRenderer.drawStringWithShadow("Gear: " + gear_label, 10, 42, 0xffffff);
 			mc.fontRenderer.drawStringWithShadow("Braking: " + (ent.vehicle.braking ? "yes" : "no"), 10, 50, 0xffffff);
+			mc.fontRenderer.drawStringWithShadow("P-Brake: " + (ent.vehicle.pbrake ? "ON" : "OFF"), 10, 58, 0xffffff);
 			mc.fontRenderer.drawStringWithShadow("Engine: " + (ent.vehicle.engine.isOn() ? "ON" : "OFF"), 10, 66, 0xffffff);
+			if(ent.vehicle.overloaded) mc.fontRenderer.drawStringWithShadow("Towing limit reached, vehicle is overloaded.", 10, 74, 0xffffff);
 		}
 		/*if(event.getType() == ElementType.HOTBAR){
 			if(mc.gameSettings.hideGUI || OVERLAY_ON_BOTTOM) event.setCanceled(true);

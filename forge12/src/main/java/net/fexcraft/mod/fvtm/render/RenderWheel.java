0cc4ec4087662534babd757e049c44384ad340be
Ferdinand Calo <fexcalo@gmail.com>
2025-07-01 13:36:46 +0200

    Update RenderWheel.java

--- forge12/src/main/java/net/fexcraft/mod/fvtm/render/RenderWheel.java
+++ forge12/src/main/java/net/fexcraft/mod/fvtm/render/RenderWheel.java
@@ -10,39 +10,39 @@ import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.entity.Entity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.client.registry.IRenderFactory;
+import org.lwjgl.opengl.GL11;
 
 import static net.fexcraft.mod.fvtm.render.EffectRenderer.drawString;
 
-public class RenderWheel extends Render<Entity> implements IRenderFactory<Entity> {
+public class RenderWheel extends Render<WheelEntity> implements IRenderFactory<WheelEntity> {
 
     public RenderWheel(RenderManager renderManager){
         super(renderManager); shadowSize = 0.125F;
     }
 
     @Override
-    public void doRender(Entity entity, double x, double y, double z, float yaw, float partialticks){
-    	if(Command.OTHER) shadowSize = 0.125f;
-    	else shadowSize = 0f;
-        if(entity instanceof WheelEntity && Minecraft.getMinecraft().getRenderManager().isDebugBoundingBox()){
-            drawString(((WheelEntity)entity).wheelid, x, y + 2, z, true, true, 0.8f, 0xb8bc38, null);
-            if(EnvInfo.DEV && Static.getServer().isSinglePlayer()){
-                Entity serv = Static.getServer().getWorld(entity.dimension).getEntityByID(entity.getEntityId());
-                if(serv == null) return;
-                drawString(serv.motionX + "", x, y + 2.2, z, true, true, 0.8f, 0xb8bc38, null);
-                drawString(serv.motionY + "", x, y + 2.4, z, true, true, 0.8f, 0xb8bc38, null);
-                drawString(serv.motionZ + "", x, y + 2.6, z, true, true, 0.8f, 0xb8bc38, null);
-                drawString(serv.rotationYaw + "", x, y + 2.8, z, true, true, 0.8f, 0xb8bc38, null);
-            }
+    public void doRender(WheelEntity entity, double x, double y, double z, float yaw, float partialticks){
+        if(Minecraft.getMinecraft().getRenderManager().isDebugBoundingBox()){
+            if(Command.OTHER) shadowSize = 0.125f;
+            else shadowSize = 0f;
+            GL11.glPushMatrix();
+            GL11.glRotatef(entity.rotationYaw, 0, 1, 0);
+            drawString(entity.wheelid, x, y + 2, z, true, true, 0.8f, 0xb8bc38, null);
+            drawString(entity.motionX + "", x, y + 2.2, z, true, true, 0.8f, 0xb8bc38, null);
+            drawString(entity.motionY + "", x, y + 2.4, z, true, true, 0.8f, 0xb8bc38, null);
+            drawString(entity.motionZ + "", x, y + 2.6, z, true, true, 0.8f, 0xb8bc38, null);
+            drawString(entity.rotationYaw + "", x, y + 2.8, z, true, true, 0.8f, 0xb8bc38, null);
+            GL11.glPopMatrix();
         }
     }
 
     @Override
-    protected ResourceLocation getEntityTexture(Entity entity){
+    protected ResourceLocation getEntityTexture(WheelEntity entity){
         return null;
     }
 
     @Override
-    public Render<Entity> createRenderFor(RenderManager manager){
+    public Render<WheelEntity> createRenderFor(RenderManager manager){
         return new RenderWheel(manager);
     }
 

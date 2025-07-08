2c43064030b48a3f89ad00c4196348ce4f5ca7b2
Ferdinand Calo <fexcalo@gmail.com>
2025-07-08 22:13:11 +0200

    Update Axle.java

--- neutral/src/net/fexcraft/mod/fvtm/sys/uni/Axle.java
+++ neutral/src/net/fexcraft/mod/fvtm/sys/uni/Axle.java
@@ -4,8 +4,10 @@ import static net.fexcraft.mod.fvtm.sys.uni.VehicleInstance.GRAVITY;
 
 import java.util.ArrayList;
 
+import net.fexcraft.lib.common.Static;
 import net.fexcraft.lib.common.math.V3D;
 import net.fexcraft.mod.fvtm.FvtmLogger;
+import net.fexcraft.mod.uni.world.EntityW;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -15,6 +17,7 @@ public class Axle {
 	public final ArrayList<WheelTireData> wheels = new ArrayList<>();
 	public final int id;
 	public boolean powered;
+	public boolean steering;
 	public double weight_ratio;
 	public double weight_on;
 	public double yaw_speed;
@@ -30,6 +33,7 @@ public class Axle {
 		wheels.forEach(wtd -> {
 			pos = pos.add(wtd.pos.x, 0, 0);
 			radius += wtd.radius;
+			if(wtd.slot.steering) steering = true;
 		});
 		pos = new V3D(pos.x / wheels.size(), pos.y, pos.z);
 		radius /= wheels.size();
@@ -41,4 +45,21 @@ public class Axle {
 		yaw_speed = pos.z * (yaw / mass);
 	}
 
+	public double getGrip(VehicleInstance vi){
+		double grip = 0;
+		for(WheelTireData wheel : wheels){
+			double gr = wheel.function.getGripFor(null, vi.entity.getWorld().isRainingAt(vi.wheels.get(wheel.id).pos()));
+			if(wheel.slot.braking && vi.pbrake) gr *= wheel.function.brake_grip;
+			grip += gr;
+		}
+		return grip / wheels.size();
+	}
+
+	public double getFric(VehicleInstance vi, double slip, double grip){
+		double stif = 0;
+		for(WheelTireData wheel : wheels){
+			stif += wheel.function.getCornerStiffnessFor(null, wheel.slot.steering);
+		}
+		return Static.clamp(-(stif / wheels.size()) * slip, -grip, grip) * weight_on;
+	}
 }

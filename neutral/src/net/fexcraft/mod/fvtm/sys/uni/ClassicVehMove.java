b926c24b953928f7a1edb70426ebed37498c77b9
Ferdinand Calo <fexcalo@gmail.com>
2025-07-12 00:08:18 +0200

    Create ClassicVehMove.java

---
+++ neutral/src/net/fexcraft/mod/fvtm/sys/uni/ClassicVehMove.java
@@ -0,0 +1,122 @@
+package net.fexcraft.mod.fvtm.sys.uni;
+
+import net.fexcraft.lib.common.math.V3D;
+import net.fexcraft.mod.fvtm.data.part.PartData;
+import net.fexcraft.mod.fvtm.function.part.TireFunction;
+import net.fexcraft.mod.fvtm.handler.TireInstallationHandler;
+import net.fexcraft.mod.fvtm.handler.WheelInstallationHandler;
+
+import java.util.Map;
+
+import static net.fexcraft.mod.fvtm.util.MathUtils.valRad;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class ClassicVehMove implements VehicleMovement {
+
+	protected VehicleInstance inst;
+
+	public ClassicVehMove(VehicleInstance vi){
+		inst = vi;
+	}
+
+	public void initWheels(){
+		inst.wheels.clear();
+		for(Map.Entry<String, V3D> entry : inst.data.getWheelPositions().entrySet()){
+			if(entry.getKey().endsWith(":tire")) continue;
+			WheelTireData wheel = new WheelTireData(entry.getKey());
+			wheel.pos = entry.getValue();
+			PartData part = inst.data.getPart(entry.getKey());
+			if(!((WheelInstallationHandler.WheelData)part.getType().getInstallHandlerData()).hasTire()){
+				part = inst.data.getPart(entry.getKey()+ ":tire");
+				wheel.radius = ((TireInstallationHandler.TireData)part.getType().getInstallHandlerData()).getOuterRadius();
+			}
+			else{
+				wheel.radius += ((WheelInstallationHandler.WheelData)part.getType().getInstallHandlerData()).getRadius();
+			}
+			wheel.function = part.getFunction(TireFunction.class, "fvtm:tire").getTireAttr(part);
+			wheel.slot = inst.data.getWheelSlots().get(entry.getKey());
+			wheel.mirror = inst.data.getWheelSlots().get(entry.getKey()).mirror;
+			inst.wheeldata.put(entry.getKey(), wheel);
+		}
+		assignWheels();
+		setupAxles();
+	}
+
+	@Override
+	public void move(boolean nocons){
+		if(inst.data.getType().isTrailer()) return;
+		inst.entity.setOnGround(true);
+		inst.moveto.set(0, 0, 0);
+		if(inst.type.isWaterVehicle()){
+			//TODO
+		}
+		else{
+			double steer = Math.toRadians(inst.steer_yaw);
+			double wyaw = valRad(inst.pivot().yaw());
+			double syaw = valRad(wyaw + steer);
+			double myaw = 0;
+			double ryaw = 0;
+			double scal = 0;
+			boolean cons = nocons || (inst.engine != null && inst.consumeFuel());
+			for(UniWheel wheel : inst.wheels.values()){
+				if(wheel.wtd() == null) continue;
+				wheel.prepare();
+				if(inst.engine != null && cons){
+					if(inst.data.getType().isTracked()){
+						//TODO
+					}
+					else{
+						scal = 0.05 * inst.throttle * (inst.throttle > 0 ? inst.spdata.max_throttle : inst.spdata.min_throttle) * inst.engine.getSphEngineSpeed();
+						ryaw = inst.pivot().deg_yaw();
+						myaw = wyaw;
+						if(wheel.wtd().slot.steering){
+							ryaw += inst.steer_yaw;
+							myaw = syaw;
+						}
+					}
+					wheel.addMotion(-Math.sin(-myaw) * scal, 0, -Math.cos(-myaw) * scal);
+				}
+				wheel.yaw((float)ryaw);
+				wheel.move();
+				inst.moveToWheel(wheel);
+			}
+		}
+		inst.moveFinish();
+	}
+
+	public void assignWheels(){
+		inst.w_front_l = inst.w_front_r = inst.w_rear_l = inst.w_rear_r = new WheelTireData();
+		for(WheelTireData wheel : inst.wheeldata.values()){
+			if(!inst.data.getType().isTrailer()){
+				if(wheel.pos.x <= inst.w_front_l.pos.x && wheel.pos.z <= inst.w_front_l.pos.z){
+					inst.w_front_l = wheel;
+					continue;
+				}
+				if(wheel.pos.x >= inst.w_front_r.pos.x && wheel.pos.z <= inst.w_front_r.pos.z){
+					inst.w_front_r = wheel;
+					continue;
+				}
+			}
+			if(wheel.pos.x <= inst.w_rear_l.pos.x && wheel.pos.z >= inst.w_rear_l.pos.z){
+				inst.w_rear_l = wheel;
+				continue;
+			}
+			if(wheel.pos.x >= inst.w_rear_r.pos.x && wheel.pos.z >= inst.w_rear_r.pos.z){
+				inst.w_rear_r = wheel;
+			}
+		}
+		if(inst.data.getType().isTrailer()){
+			inst.w_front_l = new WheelTireData("_" + inst.w_rear_l.id);
+			inst.w_front_l.asTrailerFront(inst.w_rear_l);
+			inst.wheeldata.put(inst.w_front_l.id, inst.w_front_l);
+			inst.w_front_r = new WheelTireData("_" + inst.w_rear_r.id);
+			inst.w_front_r.asTrailerFront(inst.w_rear_r);
+			inst.wheeldata.put(inst.w_front_r.id, inst.w_front_r);
+		}
+	}
+
+	public void setupAxles(){};
+
+}

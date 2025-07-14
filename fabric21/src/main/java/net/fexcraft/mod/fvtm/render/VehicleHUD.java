bfa70a638b1fc906f0353e8752ad0f6a11f7cf96
Ferdinand Calo <fexcalo@gmail.com>
2025-07-14 17:27:58 +0200

    Update VehicleHUD.java

--- fabric21/src/main/java/net/fexcraft/mod/fvtm/render/VehicleHUD.java
+++ fabric21/src/main/java/net/fexcraft/mod/fvtm/render/VehicleHUD.java
@@ -3,6 +3,7 @@ package net.fexcraft.mod.fvtm.render;
 import net.fabricmc.fabric.api.client.rendering.v1.IdentifiedLayer;
 import net.fexcraft.mod.fvtm.entity.RootVehicle;
 import net.fexcraft.mod.fvtm.sys.uni.VehicleInstance;
+import net.fexcraft.mod.fvtm.ui.VehicleOverlay;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
@@ -22,14 +23,11 @@ public class VehicleHUD implements IdentifiedLayer {
 	public void render(GuiGraphics gg, DeltaTracker delta){
 		if(Minecraft.getInstance().player.getVehicle() instanceof RootVehicle == false) return;
 		vehicle = ((RootVehicle)Minecraft.getInstance().player.getVehicle()).vehicle;
-		if(vehicle == null) return;
-		gg.drawString(Minecraft.getInstance().font, "Throttle: " + round(vehicle.throttle), 10, 10, 0xffffff);
-		gg.drawString(Minecraft.getInstance().font, "Steering: " + round(vehicle.steer_yaw), 10, 20, 0xffffff);
-		gg.drawString(Minecraft.getInstance().font, "Speed: " + round(vehicle.speed), 10, 30, 0xffffff);
-	}
-
-	private static double round(double var){
-		return (int)(var * 100) / 100D;
+		if(vehicle == null || vehicle.data == null) return;
+		VehicleOverlay.RS[] rs = VehicleOverlay.update(vehicle);
+		for(VehicleOverlay.RS r : rs){
+			gg.drawString(Minecraft.getInstance().font, r.str, r.x, r.y, 0xffffff);
+		}
 	}
 	
 }

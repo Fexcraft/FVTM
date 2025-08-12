b5e14663c1f14926502e498e7881651eff36fff0
Ferdinand Calo <fexcalo@gmail.com>
2025-08-12 21:22:30 +0200

    Update RoadToolCon.java

--- neutral/src/net/fexcraft/mod/fvtm/ui/road/RoadToolCon.java
+++ neutral/src/net/fexcraft/mod/fvtm/ui/road/RoadToolCon.java
@@ -9,6 +9,8 @@ import net.fexcraft.mod.uni.inv.UniInventory;
 import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.ui.ContainerInterface;
 
+import static net.fexcraft.mod.fvtm.sys.road.UniRoadTool.TAG_KEY;
+
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
@@ -17,19 +19,39 @@ public class RoadToolCon extends ContainerInterface {
 	public static final String[] fills = new String[]{ "RoadFill", "BottomFill", "SideLeftFill", "SideRightFill", "TopFill", "LinesFill", "SlabFill" };
 	protected StackWrapper stack;
 	protected boolean custom_road;
+	protected boolean custom_slab;
 	protected boolean custom_top;
 	protected boolean custom_lines;
+	//
+	protected int rt_width;
+	protected boolean bot_on;
+	protected boolean top_on;
+	protected boolean lin_on;
+	protected int rheight;
+	protected int lheight;
 
 	public RoadToolCon(JsonMap map, UniEntity player, V3I pos){
 		super(map, player, pos);
 		stack = player.entity.getHeldItem(true);
-		custom_road = stack.directTag().has("CustomRoadFill");
-		custom_top = stack.directTag().has("CustomTopFill");
-		custom_lines = stack.directTag().has("CustomLinesFill");
+		if(!stack.directTag().has(TAG_KEY)){
+			stack.updateTag(tag -> tag.set(TAG_KEY, TagCW.create()));
+		}
+		TagCW com = stack.directTag().getCompound(TAG_KEY);
+		custom_road = com.has("CustomRoadFill");
+		custom_slab = com.has("CustomSlabFill");
+		custom_top = com.has("CustomTopFill");
+		custom_lines = com.has("CustomLinesFill");
+		rt_width  = com.getInteger("Width");
+		bot_on = com.getBoolean("Ground");
+		top_on = com.getBoolean("Top");
+		lin_on = com.getBoolean("Lines");
+		rheight  = com.getInteger("RHeight");
+		lheight  = com.getInteger("LHeight");
+		//
 		inventory = UniInventory.create(fills.length).stacksize(1).name("Road Fill Inventory");
 		for(int i = 0; i < fills.length; i++){
-			if(stack.directTag().has(fills[i])){
-				inventory.set(i, stack.directTag().getCompound(fills[i]));
+			if(com.has(fills[i])){
+				inventory.set(i, com.getCompound(fills[i]));
 			}
 		}
 	}
@@ -43,8 +65,64 @@ public class RoadToolCon extends ContainerInterface {
 	@Override
 	public void packet(TagCW com, boolean client){
 		switch(com.getString("cargo")){
-			case "save":{
-				stack.updateTag(tag -> tag.set("RoadLayers", com.getIntArray("sizes")));
+			case "add_width":{
+				stack.updateTag(tag -> {
+					if(++rt_width > 64) rt_width = 64;
+					tag.getCompound(TAG_KEY).set("Width", rt_width);
+				});
+				break;
+			}
+			case "sub_width":{
+				stack.updateTag(tag -> {
+					if(--rt_width < 1) rt_width = 1;
+					tag.getCompound(TAG_KEY).set("Width", rt_width);
+				});
+				break;
+			}
+			case "ground":{
+				stack.updateTag(tag -> {
+					tag.getCompound(TAG_KEY).set("Ground", com.getBoolean("ground"));
+				});
+				break;
+			}
+			case "add_left":{
+				stack.updateTag(tag -> {
+					if(++lheight > 64) lheight = 64;
+					tag.getCompound(TAG_KEY).set("LHeight", lheight);
+				});
+				break;
+			}
+			case "sub_left":{
+				stack.updateTag(tag -> {
+					if(--lheight < 0) lheight = 0;
+					tag.getCompound(TAG_KEY).set("LHeight", lheight);
+				});
+				break;
+			}
+			case "add_right":{
+				stack.updateTag(tag -> {
+					if(++rheight > 64) rheight = 64;
+					tag.getCompound(TAG_KEY).set("RHeight", rheight);
+				});
+				break;
+			}
+			case "sub_right":{
+				stack.updateTag(tag -> {
+					if(--rheight < 0) rheight = 0;
+					tag.getCompound(TAG_KEY).set("RHeight", rheight);
+				});
+				break;
+			}
+			case "top":{
+				stack.updateTag(tag -> {
+					tag.getCompound(TAG_KEY).set("Top", com.getBoolean("top"));
+				});
+				break;
+			}
+			case "lines":{
+				stack.updateTag(tag -> {
+					tag.getCompound(TAG_KEY).set("Lines", com.getBoolean("lines"));
+				});
 				break;
 			}
 			case "custom":{
@@ -52,11 +130,13 @@ public class RoadToolCon extends ContainerInterface {
 				break;
 			}
 			case "remove":{
-				stack.updateTag(tag -> tag.rem("Custom" + fills[com.getInteger("layer")]));
-				custom_road = stack.directTag().has("CustomRoadFill");
-				custom_top = stack.directTag().has("CustomTopFill");
-				custom_lines = stack.directTag().has("CustomLinesFill");
-				if(!client) SEND_TO_CLIENT.accept(com, player);
+				stack.updateTag(tag -> tag.getCompound(TAG_KEY).rem("Custom" + fills[com.getInteger("layer")]));
+				TagCW rtg = stack.directTag().getCompound(TAG_KEY);
+				custom_road = rtg.has("CustomRoadFill");
+				custom_slab = rtg.has("CustomSlabFill");
+				custom_top = rtg.has("CustomTopFill");
+				custom_lines = rtg.has("CustomLinesFill");
+				if(!client) SEND_TO_CLIENT.accept(rtg, player);
 				break;
 			}
 		}
@@ -66,7 +146,8 @@ public class RoadToolCon extends ContainerInterface {
 	public void onClosed(){
 		super.onClosed();
 		if(player.entity.isOnClient()) return;
-		stack.updateTag(com -> {
+		stack.updateTag(ut -> {
+			TagCW com = ut.getCompound(TAG_KEY);
 			for(int i = 0; i < fills.length; i++){
 				if(!inventory.empty(i)){
 					TagCW tag = TagCW.create();

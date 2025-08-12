188cd7c25e1ad05d445e5616112f033f2cbc03ca
Ferdinand Calo <fexcalo@gmail.com>
2025-08-12 21:22:39 +0200

    Update RoadToolUI.java

--- neutral/src/net/fexcraft/mod/fvtm/ui/road/RoadToolUI.java
+++ neutral/src/net/fexcraft/mod/fvtm/ui/road/RoadToolUI.java
@@ -2,6 +2,7 @@ package net.fexcraft.mod.fvtm.ui.road;
 
 import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.lib.common.utils.Formatter;
+import net.fexcraft.mod.fvtm.sys.road.UniRoadTool;
 import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.ui.ContainerInterface;
 import net.fexcraft.mod.uni.ui.UIButton;
@@ -9,21 +10,18 @@ import net.fexcraft.mod.uni.ui.UserInterface;
 
 import java.util.List;
 
+import static net.fexcraft.mod.uni.ui.ContainerInterface.sendToServer;
+
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public class RoadToolUI extends UserInterface {
 
 	protected RoadToolCon rtc;
-	protected int[] size = new int[]{ 1, 0, 0, 0, 0, 0 };
 
 	public RoadToolUI(JsonMap map, ContainerInterface con) throws Exception{
 		super(map, con);
 		rtc = (RoadToolCon)con;
-		if(!rtc.stack.directTag().has("RoadLayers")){
-			rtc.stack.updateTag(tag -> tag.set("RoadLayers", size));
-		}
-		else size = rtc.stack.directTag().getIntArray("RoadLayers");
 	}
 
 	@Override
@@ -40,8 +38,22 @@ public class RoadToolUI extends UserInterface {
 			texts.get("road_fill").value(rtc.inventory.get(0).getName());
 		}
 		texts.get("road_width").value("ui.fvtm.road_tool.road_fill_width");
-		texts.get("road_width").translate(size[0]);
+		texts.get("road_width").translate(rtc.rt_width);
+		texts.get("slab_width").value("ui.fvtm.road_tool.slab_fill_width");
+		texts.get("slab_width").translate(rtc.rt_width);
 		//
+		if(rtc.custom_slab){
+			texts.get("slab_fill").value("ui.fvtm.road_tool.slab_fill_custom");
+			texts.get("slab_fill").translate();
+		}
+		else if(rtc.inventory.empty(6)){
+			texts.get("slab_fill").value("ui.fvtm.road_tool.no_fill_block");
+			texts.get("slab_fill").translate();
+		}
+		else{
+			texts.get("slab_fill").value(rtc.inventory.get(6).getName());
+		}
+ 		//
 		if(rtc.inventory.empty(1)){
 			texts.get("ground_fill").value("ui.fvtm.road_tool.no_fill_block");
 			texts.get("ground_fill").translate();
@@ -49,7 +61,7 @@ public class RoadToolUI extends UserInterface {
 		else{
 			texts.get("ground_fill").value(rtc.inventory.get(1).getName());
 		}
-		texts.get("ground_status").value("ui.fvtm.road_tool.ground_fill_" + (size[1] > 0 ? "enabled" : "disabled"));
+		texts.get("ground_status").value("ui.fvtm.road_tool.ground_fill_" + (rtc.bot_on ? "enabled" : "disabled"));
 		texts.get("ground_status").translate();
 		//
 		if(rtc.inventory.empty(2)){
@@ -59,9 +71,9 @@ public class RoadToolUI extends UserInterface {
 		else{
 			texts.get("left_fill").value(rtc.inventory.get(2).getName());
 		}
-		if(size[2] > 0){
+		if(rtc.lheight > 0){
 			texts.get("left_size").value("ui.fvtm.road_tool.left_fill_size");
-			texts.get("left_size").translate(size[2]);
+			texts.get("left_size").translate(rtc.lheight);
 		}
 		else{
 			texts.get("left_size").value("ui.fvtm.road_tool.left_fill_disabled");
@@ -75,9 +87,9 @@ public class RoadToolUI extends UserInterface {
 		else{
 			texts.get("right_fill").value(rtc.inventory.get(3).getName());
 		}
-		if(size[3] > 0){
+		if(rtc.rheight > 0){
 			texts.get("right_size").value("ui.fvtm.road_tool.right_fill_size");
-			texts.get("right_size").translate(size[3]);
+			texts.get("right_size").translate(rtc.rheight);
 		}
 		else{
 			texts.get("right_size").value("ui.fvtm.road_tool.right_fill_disabled");
@@ -95,7 +107,7 @@ public class RoadToolUI extends UserInterface {
 		else{
 			texts.get("top_fill").value(rtc.inventory.get(4).getName());
 		}
-		texts.get("top_status").value("ui.fvtm.road_tool.top_fill_" + (size[4] > 0 ? "enabled" : "disabled"));
+		texts.get("top_status").value("ui.fvtm.road_tool.top_fill_" + (rtc.top_on ? "enabled" : "disabled"));
 		texts.get("top_status").translate();
 		//
 		if(rtc.custom_lines){
@@ -109,65 +121,98 @@ public class RoadToolUI extends UserInterface {
 		else{
 			texts.get("lines_fill").value(rtc.inventory.get(5).getName());
 		}
-		texts.get("lines_status").value("ui.fvtm.road_tool.lines_fill_" + (size[5] > 0 ? "enabled" : "disabled"));
+		texts.get("lines_status").value("ui.fvtm.road_tool.lines_fill_" + (rtc.lin_on ? "enabled" : "disabled"));
 		texts.get("lines_status").translate();
 	}
 
 	@Override
 	public boolean onAction(UIButton button, String id, int x, int y, int b){
 		switch(id){
-			case "add_width":{
-				size(0, 1);
+			case "add_width0":
+			case "add_width1":{
+				if(++rtc.rt_width > 64) rtc.rt_width = 64;
+				sendToServer(com -> com.set("cargo", "add_width"));
 				return true;
 			}
-			case "sub_width":{
-				size(0, -1);
+			case "sub_width0":
+			case "sub_width1":{
+				if(--rtc.rt_width < 1) rtc.rt_width = 1;
+				sendToServer(com -> com.set("cargo", "sub_width"));
 				return true;
 			}
 			case "enable_ground":{
-				size(1, 1);
+				sendToServer(com -> {
+					com.set("cargo", "ground");
+					com.set("ground", rtc.bot_on = true);
+				});
 				return true;
 			}
 			case "disable_ground":{
-				size(1, -1);
+				sendToServer(com -> {
+					com.set("cargo", "ground");
+					com.set("ground", rtc.bot_on = false);
+				});
 				return true;
 			}
 			case "add_left":{
-				size(2, 1);
+				if(++rtc.lheight > 64) rtc.lheight = 64;
+				sendToServer(com -> com.set("cargo", "add_left"));
 				return true;
 			}
 			case "sub_left":{
-				size(2, -1);
+				if(--rtc.lheight < 0) rtc.lheight = 0;
+				sendToServer(com -> com.set("cargo", "sub_left"));
 				return true;
 			}
 			case "add_right":{
-				size(3, 1);
+				if(++rtc.rheight > 64) rtc.rheight = 64;
+				sendToServer(com -> com.set("cargo", "add_right"));
 				return true;
 			}
 			case "sub_right":{
-				size(3, -1);
+				if(--rtc.rheight < 0) rtc.rheight = 0;
+				sendToServer(com -> com.set("cargo", "sub_right"));
 				return true;
 			}
 			case "enable_top":{
-				size(4, 1);
+				sendToServer(com -> {
+					com.set("cargo", "top");
+					com.set("top", rtc.top_on = true);
+				});
 				return true;
 			}
 			case "disable_top":{
-				size(4, -1);
+				sendToServer(com -> {
+					com.set("cargo", "top");
+					com.set("top", rtc.top_on = false);
+				});
 				return true;
 			}
 			case "enable_lines":{
-				size(5, 1);
+				sendToServer(com -> {
+					com.set("cargo", "lines");
+					com.set("lines", rtc.lin_on = true);
+				});
 				return true;
 			}
 			case "disable_lines":{
-				size(5, -1);
+				sendToServer(com -> {
+					com.set("cargo", "lines");
+					com.set("lines", rtc.lin_on = false);
+				});
 				return true;
 			}
 			case "edit_road":{
 				TagCW compound = TagCW.create();
 				compound.set("cargo", "custom");
 				compound.set("layer", 0);
+				container.SEND_TO_SERVER.accept(compound);
+				return true;
+			}
+			case "edit_slab":{
+				TagCW compound = TagCW.create();
+				compound.set("cargo", "custom");
+				compound.set("layer", 6);
 				container.SEND_TO_SERVER.accept(compound);
 				return true;
 			}
@@ -192,6 +237,13 @@ public class RoadToolUI extends UserInterface {
 				container.SEND_TO_SERVER.accept(compound);
 				return true;
 			}
+			case "rem_slab":{
+				TagCW compound = TagCW.create();
+				compound.set("cargo", "remove");
+				compound.set("layer", 6);
+				container.SEND_TO_SERVER.accept(compound);
+				return true;
+			}
 			case "rem_top":{
 				TagCW compound = TagCW.create();
 				compound.set("cargo", "remove");
@@ -210,30 +262,16 @@ public class RoadToolUI extends UserInterface {
 		return false;
 	}
 
-	protected void size(int idx, int am){
-		switch(idx){
-			case 0:
-			case 2:
-			case 3:
-				if(size[idx] + am >= 0 && size[idx] + am <= 64) size[idx] += am;
-				break;
-			case 1:
-			case 4:
-			case 5:
-				if(size[idx] + am >= 0 && size[idx] + am < 2) size[idx] += am;
-				break;
-		}
-		TagCW compound = TagCW.create();
-		compound.set("sizes", size);
-		compound.set("cargo", "save");
-		container.SEND_TO_SERVER.accept(compound);
-	}
-
 	@Override
 	public void getTooltip(int mx, int my, List<String> list){
 		if(rtc.inventory.empty(0) && slots.get("road").hovered(gLeft, gTop, mx, my)){
 			for(int i = 0; i < 8; i++){
 				list.add(container.TRANSLATOR.apply("ui.fvtm.road_tool.road_slot_info" + i));
+			}
+		}
+		if(rtc.inventory.empty(6) && slots.get("slab").hovered(gLeft, gTop, mx, my)){
+			for(int i = 0; i < 4; i++){
+				list.add(container.TRANSLATOR.apply("ui.fvtm.road_tool.road_slab_info" + i));
 			}
 		}
 	}

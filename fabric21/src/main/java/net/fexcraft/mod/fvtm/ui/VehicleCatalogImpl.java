6d8a9e5bd8039d32794a741e7790a6685677b43b
Ferdinand Calo <fexcalo@gmail.com>
2025-08-30 15:30:32 +0200

    Update VehicleCatalogImpl.java

--- fabric21/src/main/java/net/fexcraft/mod/fvtm/ui/VehicleCatalogImpl.java
+++ fabric21/src/main/java/net/fexcraft/mod/fvtm/ui/VehicleCatalogImpl.java
@@ -1,38 +1,49 @@
 package net.fexcraft.mod.fvtm.ui;
 
 import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.lib.common.Static;
+import net.fexcraft.mod.fvtm.entity.RootVehicle;
+import net.fexcraft.mod.fvtm.render.FvtmRenderState;
 import net.fexcraft.mod.fvtm.ui.vehicle.VehicleCatalog;
+import net.fexcraft.mod.fvtm.util.Resources21;
 import net.fexcraft.mod.uni.ui.ContainerInterface;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.renderer.entity.EntityRenderDispatcher;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import org.joml.Quaternionf;
+import org.joml.Vector3f;
+
+import static net.fexcraft.mod.fvtm.render.Renderer21.*;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public class VehicleCatalogImpl extends VehicleCatalog {
 
+	private EntityRenderer<? super RootVehicle, ?> renderer;
+	private RootVehicle rv;
+
     public VehicleCatalogImpl(JsonMap map, ContainerInterface container) throws Exception{
         super(map, container);
+		EntityRenderDispatcher dis = Minecraft.getInstance().getEntityRenderDispatcher();
+		rv = new RootVehicle(Resources21.VEHICLE_ENTITY, Minecraft.getInstance().level);
+		renderer = dis.getRenderer(rv);
     }
 
     @Override
     public void postdraw(float ticks, int mx, int my){
         super.postdraw(ticks, mx, my);
-        /*GuiGraphics gg = root.matrix();
-        gg.pose().pushPose();
-        gg.pose().translate(gLeft + 67, gTop + 63, 50);
-        gg.pose().mulPose((new Matrix4f()).scaling(-preset.scale * 16, -preset.scale * 16, -preset.scale * 16));
-        gg.pose().mulPose(new Quaternionf()
-            .rotateAxis((float)Math.atan((mx - gLeft - 67) / 40f), AY)
-            .rotateAxis((float)-Math.atan((my - gTop - 63) / 40f), AX)
-        );
-        Lighting.setupForEntityInInventory();
-        drawer.applyWhite();
-        Renderer21.set(gg.pose(), gg.bufferSource, 15728880);
-        FvtmRenderTypes.setCutout(data.getCurrentTexture());
-        veh.getModel().render(DefaultModel.RENDERDATA.set(data, null, null, null, ticks));
-        RVRenderer.renderPoint(gg.pose(), data.getRotationPoint("vehicle"), null, data, null, ticks);
-        gg.flush();
-        gg.pose().popPose();
-        Lighting.setupFor3DItems();*/
+		if(data == null) return;
+		rv.initVD(data);
+        GuiGraphics gg = root.matrix();
+		gg.enableScissor(gLeft + 7, gTop + 23, gLeft + 127, gTop + 103);
+		Quaternionf qua = new Quaternionf()
+			.rotateAxis((float)Math.atan((mx - gLeft - 67) / 40f), AY)
+			.rotateAxis((float)-Math.atan((my - gTop - 63) / 40f), AX)
+			.rotateAxis(Static.rad180, AZ);
+		gg.submitEntityRenderState(renderer.createRenderState(rv, 1f), preset.scale * 16, new Vector3f(), qua, null, gLeft + 7, gTop + 23, gLeft + 127, gTop + 103);
+		gg.disableScissor();
     }
 
 }

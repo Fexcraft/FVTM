2d4eada57f5234c646f5d87851719436430d33f8
Ferdinand Calo <fexcalo@gmail.com>
2025-09-02 22:42:47 +0200

    Update RoadLinesModelLoader.java

--- forge20/src/main/java/net/fexcraft/mod/fvtm/util/RoadLinesModelLoader.java
+++ forge20/src/main/java/net/fexcraft/mod/fvtm/util/RoadLinesModelLoader.java
@@ -27,6 +27,7 @@ import java.util.function.Function;
 
 import static net.fexcraft.lib.common.Static.*;
 import static net.fexcraft.mod.fvtm.block.generated.FvtmProperties.*;
+import static net.fexcraft.mod.fvtm.model.block.BakedQuadData.*;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -55,70 +56,6 @@ public class RoadLinesModelLoader implements IGeometryLoader<RoadLinesModelLoade
 		public BakedGeo(IGeometryBakingContext context, Function<Material, TextureAtlasSprite> function){
 			sprite = function.apply(context.getMaterial("texture"));
 			particle = function.apply(context.getMaterial("particle"));
-		}
-
-		private static double[][] quad = {
-			{ 1, thirtysecondth, 0 },
-			{ 0, thirtysecondth, 0 },
-			{ 0, thirtysecondth, 1 },
-			{ 1, thirtysecondth, 1 }
-		};
-		private static int[][] uv_full = {
-			{ 16,  0 }, { 0,  0 }, { 0, 16 }, { 16, 16 }
-		};
-		private static int[][] uv_ls = {
-			{ 8,  0 }, { 0,  0 }, { 0, 8 }, { 8, 8 }
-		};
-		private static int[][] uv_lc = {
-			{ 16,  0 }, { 8,  0 }, { 8, 8 }, { 16, 8 }
-		};
-		private static int[][] uv_lt = {
-			{ 8,  8 }, { 0,  8 }, { 0, 16 }, { 8, 16 }
-		};
-		private static int[][] uv_lf = {
-			{ 16,  8 }, { 8,  8 }, { 8, 16 }, { 16, 16 }
-		};
-		private static int[][][] uv_ls2 = new int[2][][];
-		private static int[][][] uv_lc2 = new int[4][][];
-		private static int[][][] uv_l3 = new int[4][][];
-		static{
-			uv_ls2[0] = uv_ls;
-			uv_ls2[1] = rotateR(uv_ls);
-			uv_lc2[0] = uv_lc;
-			uv_lc2[1] = rotateL(uv_lc);
-			uv_lc2[2] = rotateU(uv_lc);
-			uv_lc2[3] = rotateR(uv_lc);
-			uv_l3[0] = uv_lt;
-			uv_l3[1] = rotateL(uv_lt);
-			uv_l3[2] = rotateU(uv_lt);
-			uv_l3[3] = rotateR(uv_lt);
-		}
-
-		private static int[][] rotateR(int[][] ar){
-			int[][] na = new int[ar.length][ar[0].length];
-			na[0] = ar[3];
-			na[1] = ar[0];
-			na[2] = ar[1];
-			na[3] = ar[2];
-			return na;
-		}
-
-		private static int[][] rotateL(int[][] ar){
-			int[][] na = new int[ar.length][ar[0].length];
-			na[0] = ar[1];
-			na[1] = ar[2];
-			na[2] = ar[3];
-			na[3] = ar[0];
-			return na;
-		}
-
-		private static int[][] rotateU(int[][] ar){
-			int[][] na = new int[ar.length][ar[0].length];
-			na[0] = ar[2];
-			na[1] = ar[3];
-			na[2] = ar[0];
-			na[3] = ar[1];
-			return na;
 		}
 
 		@Override

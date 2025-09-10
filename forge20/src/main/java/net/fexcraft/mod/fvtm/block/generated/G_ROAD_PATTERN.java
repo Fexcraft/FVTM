c47a4af74dcee19a9a7f9312760af74c5689243c
Ferdinand Calo <fexcalo@gmail.com>
2025-09-10 23:03:26 +0200

    Update G_ROAD_PATTERN.java

--- forge20/src/main/java/net/fexcraft/mod/fvtm/block/generated/G_ROAD_PATTERN.java
+++ forge20/src/main/java/net/fexcraft/mod/fvtm/block/generated/G_ROAD_PATTERN.java
@@ -15,6 +15,7 @@ import javax.annotation.Nullable;
 
 import static net.fexcraft.mod.fvtm.block.generated.FvtmProperties.FACING;
 import static net.fexcraft.mod.fvtm.block.generated.FvtmProperties.PROP_HEIGHT;
+import static net.fexcraft.mod.fvtm.data.block.Block.BLK_GETTER_CACHE;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -36,10 +37,10 @@ public class G_ROAD_PATTERN extends G_ROAD_MARKER {
 	protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> sd){
 		sd.add(PROP_HEIGHT);
 		sd.add(FACING);
-		JsonArray arr = type.getCustomStates().get("pattern_x").asArray();
+		JsonArray arr = BLK_GETTER_CACHE.getCustomStates().get("pattern_x").asArray();
 		prop_x = FvtmProperties.PROP_PATTERN_X.get(sizex = arr.get(0).integer_value());
 		defx = arr.get(1).integer_value();
-		arr = type.getCustomStates().get("pattern_z").asArray();
+		arr = BLK_GETTER_CACHE.getCustomStates().get("pattern_z").asArray();
 		prop_z = FvtmProperties.PROP_PATTERN_Z.get(sizez = arr.get(0).integer_value());
 		defz = arr.get(1).integer_value();
 		sd.add(prop_x);
@@ -55,7 +56,7 @@ public class G_ROAD_PATTERN extends G_ROAD_MARKER {
 		BlockState e = getPatternAt(context.getLevel(), context.getClickedPos().east());
 		Direction rot = Direction.NORTH;
 		if(type.isRandomRot()) rot = Direction.values()[Static.random.nextInt(4) + 2];
-		else if(context.getPlayer() != null) rot = context.getPlayer().getDirection().getOpposite();
+		else if(context.getPlayer() != null) rot = context.getPlayer().getDirection();
 		switch(rot){
 			case NORTH -> rot = setPatternPos(rot, xz, n, w, s, e);
 			case SOUTH -> rot = setPatternPos(rot, xz, s, e, n, w);
@@ -71,31 +72,35 @@ public class G_ROAD_PATTERN extends G_ROAD_MARKER {
 			xz[0] = n.getValue(prop_x);
 			xz[1] = n.getValue(prop_z) + 1;
 			if(xz[1] >= sizez) xz[1] = 0;
+			if(xz[1] < 0) xz[1] = sizez - 1;
 			return n.getValue(FACING);
  		}
 		if(w != null){
 			xz[0] = w.getValue(prop_x) + 1;
 			xz[1] = w.getValue(prop_z);
-			if(xz[1] >= sizex) xz[1] = 0;
+			if(xz[0] >= sizex) xz[0] = 0;
+			if(xz[0] < 0) xz[0] = sizex - 1;
 			return w.getValue(FACING);
 		}
 		if(s != null){
 			xz[0] = s.getValue(prop_x);
 			xz[1] = s.getValue(prop_z) - 1;
+			if(xz[1] >= sizez) xz[1] = 0;
 			if(xz[1] < 0) xz[1] = sizez - 1;
 			return s.getValue(FACING);
 		}
 		if(e != null){
 			xz[0] = e.getValue(prop_x) - 1;
 			xz[1] = e.getValue(prop_z);
-			if(xz[1] < 0) xz[1] = sizex - 1;
+			if(xz[0] >= sizex) xz[0] = 0;
+			if(xz[0] < 0) xz[0] = sizex - 1;
 			return e.getValue(FACING);
 		}
 		return rot;
 	}
 
 	private BlockState getSamePattern(BlockState state){
-		return ((PlainBase)state.getBlock()).type == type ? state : null;
+		return state != null && ((PlainBase)state.getBlock()).type == type ? state : null;
 	}
 
 	private BlockState getPatternAt(Level level, BlockPos pos){

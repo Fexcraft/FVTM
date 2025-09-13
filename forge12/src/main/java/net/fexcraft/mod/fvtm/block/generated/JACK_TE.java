054bcf140ac3a5097a53f4c0cc34bfbb40e9cee4
Ferdinand Calo <fexcalo@gmail.com>
2025-09-13 22:24:04 +0200

    Create JACK_TE.java

---
+++ forge12/src/main/java/net/fexcraft/mod/fvtm/block/generated/JACK_TE.java
@@ -0,0 +1,105 @@
+package net.fexcraft.mod.fvtm.block.generated;
+
+import net.fexcraft.lib.common.math.V3D;
+import net.fexcraft.mod.fvtm.FvtmResources;
+import net.fexcraft.mod.fvtm.data.block.BlockType;
+import net.fexcraft.mod.fvtm.data.block.JackEntity;
+import net.fexcraft.mod.fvtm.data.vehicle.SwivelPoint;
+import net.fexcraft.mod.fvtm.data.vehicle.VehicleData;
+import net.fexcraft.mod.fvtm.handler.InteractionHandler;
+import net.fexcraft.mod.fvtm.packet.Packet_TagListener;
+import net.fexcraft.mod.fvtm.packet.Packets;
+import net.fexcraft.mod.uni.packet.PacketTagListener;
+import net.fexcraft.mod.uni.tag.TagCW;
+import net.fexcraft.mod.uni.world.EntityW;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.nbt.NBTTagCompound;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class JACK_TE extends BlockTileEntity implements JackEntity, PacketTagListener, InteractionHandler.InteractRefHolder {
+
+	protected InteractionHandler.InteractRef ref = new InteractionHandler.InteractRef(this);
+	protected VehicleData vehicle;
+	protected float offset;
+
+	public JACK_TE(BlockBase type){
+		super(type);
+		offset = type.type.getCustomStates().get("height").asArray().get(1).float_value();
+	}
+
+	public JACK_TE(){}
+    
+    @Override
+    public void readFromNBT(NBTTagCompound compound){
+        super.readFromNBT(compound);
+        if(compound.hasKey("VehicleData")){
+			vehicle = FvtmResources.getVehicleData(compound.getCompoundTag("VehicleData"));
+			vehicle.getRotationPoint(SwivelPoint.DEFAULT).getPivot().set_yaw(-(float)BlockType.GENERIC_4ROT.getRotationFor(meta), true);
+		}
+    }
+
+    @Override
+    public NBTTagCompound writeToNBT(NBTTagCompound compound){
+        super.writeToNBT(compound);
+		if(vehicle != null) compound.setTag("VehicleData", vehicle.write(null).local());
+        return compound;
+    }
+
+	public void dropVehicle(boolean upd){
+		if(vehicle == null) return;
+		EntityItem item = new EntityItem(world);
+		item.setPosition(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5);
+		item.setItem(vehicle.newItemStack().local());
+		world.spawnEntity(item);
+		if(upd) sendVehUpdate();
+	}
+
+	public VehicleData getVehicle(){
+		return vehicle;
+	}
+
+	public InteractionHandler.InteractRef iref(){
+		return ref.set(getV3I(), pos.toLong(), getVehiclePos());
+	}
+
+	public V3D getVehiclePos(){
+		return new V3D(pos.getX() + 0.5, pos.getY() + offset, pos.getZ() + 0.5);
+	}
+
+	@Override
+	public void markChanged(){
+		markDirty();
+	}
+
+	public void sendVehUpdate(){
+		TagCW com = TagCW.create();
+		com.set("pos", pos.toLong());
+		if(vehicle != null) com.set("data", vehicle.write(TagCW.create()));
+		com.set("task", "update");
+		Packets.sendToAll(Packet_TagListener.class, "blockentity", com);
+	}
+
+	@Override
+	public void handle(TagCW packet, EntityW player){
+		if(world.isRemote){
+			switch(packet.getString("task")){
+				case "update":{
+					if(packet.has("data")){
+						vehicle = FvtmResources.getVehicleData(packet.getCompound("data"));
+						vehicle.getRotationPoint(SwivelPoint.DEFAULT).getPivot().set_yaw(-(float)BlockType.GENERIC_4ROT.getRotationFor(getBlockMetadata()), true);
+					}
+					else{
+						vehicle = null;
+					}
+					return;
+				}
+			}
+		}
+		else{
+
+		}
+	}
+
+}

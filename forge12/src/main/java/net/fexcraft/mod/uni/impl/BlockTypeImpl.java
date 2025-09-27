73440876a912a686b055fbf16dbbcbf6899e70c6
Ferdinand Calo <fexcalo@gmail.com>
2025-09-27 14:40:19 +0200

    Update BlockTypeImpl.java

--- forge12/src/main/java/net/fexcraft/mod/uni/impl/BlockTypeImpl.java
+++ forge12/src/main/java/net/fexcraft/mod/uni/impl/BlockTypeImpl.java
@@ -32,6 +32,7 @@ public class BlockTypeImpl {
 			case GENERIC_15VAR: return plain ? G_VAR.T15.class : G_VAR_TE.T15.class;
 			case GENERIC_16VAR: return plain ? G_VAR.T16.class : G_VAR_TE.T16.class;
 			case GENERIC_8LAYER: return G_8LAY.class;
+			case GENERIC_2x8LAYER: return G_2x8LAY.class;
 			case GENERIC_ROAD: return G_ROAD.class;
 			case GENERIC_ROAD_LINES: return G_ROAD_LINES.class;
 			case GENERIC_ROAD_MARKER: return G_ROAD_MARKER.class;

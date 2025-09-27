86942837643e6fa113a4fe19d89ebb431367313c
Ferdinand Calo <fexcalo@gmail.com>
2025-09-27 23:55:21 +0200

    Update RoadMarker.java

--- fabric21/src/main/java/net/fexcraft/mod/fvtm/entity/RoadMarker.java
+++ fabric21/src/main/java/net/fexcraft/mod/fvtm/entity/RoadMarker.java
@@ -1,14 +1,15 @@
 package net.fexcraft.mod.fvtm.entity;
 
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fexcraft.mod.fcl.util.FclCodecs;
 import net.fexcraft.mod.fvtm.item.RoadToolItem;
 import net.fexcraft.mod.fvtm.sys.road.RoadPlacingUtil;
+import net.fexcraft.mod.fvtm.util.ClientAddEntity;
 import net.fexcraft.mod.fvtm.util.QV3D;
-import net.fexcraft.mod.fvtm.util.SpawnPacket;
+import net.fexcraft.mod.fvtm.util.SpawnPacketEntity;
 import net.fexcraft.mod.uni.UniEntity;
 import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.world.EntityW;
+import net.minecraft.network.protocol.game.ClientboundAddEntityPacket;
 import net.minecraft.network.syncher.SynchedEntityData;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionHand;
@@ -26,14 +27,19 @@ import java.util.UUID;
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
-public class RoadMarker extends Entity implements SpawnPacket.PacketEntity {
+public class RoadMarker extends Entity implements SpawnPacketEntity {
 
 	public QV3D position;
 	public UUID queueid;
-	private boolean clsync;
 
 	public RoadMarker(EntityType<RoadMarker> type, Level level){
 		super(type, level);
+	}
+
+	@Override
+	public void recreateFromPacket(ClientboundAddEntityPacket packet){
+		super.recreateFromPacket(packet);
+		readSpawnData(((ClientAddEntity)packet).getFvtmData());
 	}
 
 	@Override
@@ -99,13 +105,6 @@ public class RoadMarker extends Entity implements SpawnPacket.PacketEntity {
 	@Override
 	public void baseTick(){
 		super.baseTick();
-		if(level().isClientSide){
-			if(!clsync){
-				ClientPlayNetworking.send(new SpawnPacket((Entity)this));
-				clsync = true;
-			}
-			return;
-		}
 		if(queueid == null || !RoadPlacingUtil.QUEUE.containsKey(queueid)) kill((ServerLevel)level());
 	}
 

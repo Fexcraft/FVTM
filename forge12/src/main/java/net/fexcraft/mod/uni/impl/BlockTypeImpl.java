106e9349161c7c2babe6899a407868f5d184fc35
Ferdinand Calo <fexcalo@gmail.com>
2025-11-07 20:58:39 +0100

    Update BlockTypeImpl.java

--- forge12/src/main/java/net/fexcraft/mod/uni/impl/BlockTypeImpl.java
+++ forge12/src/main/java/net/fexcraft/mod/uni/impl/BlockTypeImpl.java
@@ -38,7 +38,7 @@ public class BlockTypeImpl {
 			case GENERIC_ROAD_MARKER: return G_ROAD_MARKER.class;
 			case GENERIC_ROAD_MARKER_4ROT: return G_ROAD_MARKER4.class;
 			case GENERIC_ROAD_PATTERN: return G_ROAD_MARKER.class;
-			case MULTIBLOCK_4ROT: return plain ? M_4ROT.class : M_4ROT_TE.class;
+			//case MULTIBLOCK_4ROT: return plain ? M_4ROT.class : M_4ROT_TE.class;
 			case SIGNAL_4ROT: return SG_4ROT_TE.class;
 			case SIGNAL_16ROT: return SG_16ROT_TE.class;
 			case FORK2_SWITCH_4ROT: return F2SW_4ROT_TE.class;

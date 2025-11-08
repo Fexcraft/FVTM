7769d5fc32c4a00a987d7f37d1b6c4d943cd3c7f
Ferdinand Calo <fexcalo@gmail.com>
2025-11-08 21:53:46 +0100

    Update Condition.java

--- neutral/src/net/fexcraft/mod/fvtm/sys/condition/Condition.java
+++ neutral/src/net/fexcraft/mod/fvtm/sys/condition/Condition.java
@@ -1,7 +1,5 @@
 package net.fexcraft.mod.fvtm.sys.condition;
 
-import net.fexcraft.app.json.JsonArray;
-import net.fexcraft.app.json.JsonHandler;
 import net.fexcraft.app.json.JsonValue;
 
 /**
@@ -10,24 +8,12 @@ import net.fexcraft.app.json.JsonValue;
 public class Condition {
 
 	public CondKey key;
-	public JsonValue value;
 	public Conditional al;
-	
-	public Condition(){}
 
-	public Condition(CondKey ck, JsonValue val){
-		key = ck;
-		value = val == null ? new JsonValue(true) : val;
-	}
-
-	public Condition(String[] array){
-		key = ConditionRegistry.parseKey(array[0], array[1], array.length > 2 ? array[2] : CondMode.EQUAL.key);
-		value = array.length > 3 ? JsonHandler.parseValue(array[3]) : new JsonValue(true);
-	}
-
-	public Condition(JsonArray array){
-		key = ConditionRegistry.parseKey(array.get(0).string_value(), array.get(1).string_value(), array.size() > 2 ? array.get(2).string_value() : CondMode.EQUAL.key);
-		value = array.size() > 3 ? array.get(3) : new JsonValue(true);
+	public static Condition parse(JsonValue<?> val){
+		if(val == null) return ConditionRegistry.parse("true");
+		if(val.isArray()) return new MultiCondition(val.asArray());
+		else return ConditionRegistry.parse(val.string_value());
 	}
 
 	public Condition link(){
@@ -35,9 +21,12 @@ public class Condition {
 		return this;
 	}
 
-	@Override
-	public String toString(){
-		return key.type.key + " " + key.target + " " + key.mode.key + " " + value.string_value();
+	public JsonValue<?> value(){
+		return null;
+	}
+
+	public Condition[] conditions(){
+		return null;
 	}
 
 }

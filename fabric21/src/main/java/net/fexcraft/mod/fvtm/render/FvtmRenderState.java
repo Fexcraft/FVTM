625922f52c4776492e027cdf9b5009df96d734f8
Ferdinand Calo <fexcalo@gmail.com>
2025-12-08 19:51:18 +0100

    Update FvtmRenderState.java

--- fabric21/src/main/java/net/fexcraft/mod/fvtm/render/FvtmRenderState.java
+++ fabric21/src/main/java/net/fexcraft/mod/fvtm/render/FvtmRenderState.java
@@ -1,5 +1,6 @@
 package net.fexcraft.mod.fvtm.render;
 
+import net.fexcraft.mod.fvtm.entity.DecorationEntity;
 import net.fexcraft.mod.fvtm.entity.RailMarker;
 import net.fexcraft.mod.fvtm.entity.RoadMarker;
 import net.fexcraft.mod.fvtm.entity.RootVehicle;
@@ -14,6 +15,7 @@ public class FvtmRenderState extends EntityRenderState {
 	public RootVehicle entity;
 	public RoadMarker road_marker;
 	public RailMarker rail_marker;
+	public DecorationEntity decoration;
 	public VehicleInstance vehicle;
 	public float f;
 

7144921ffb5eb48a363c3ee0d732ce558bbf6bb1
Ferdinand Calo <fexcalo@gmail.com>
2025-12-13 20:54:14 +0100

    Update UniWheel.java

--- neutral/src/net/fexcraft/mod/fvtm/sys/uni/UniWheel.java
+++ neutral/src/net/fexcraft/mod/fvtm/sys/uni/UniWheel.java
@@ -2,37 +2,59 @@ package net.fexcraft.mod.fvtm.sys.uni;
 
 import net.fexcraft.lib.common.math.V3D;
 
+import java.util.function.Consumer;
+
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
-public interface UniWheel {
+public class UniWheel {
 
-	public void updatePrevPos();
+	public static Consumer<UniWheel> SET_STEP = uw -> {};
+	protected V3D pos = new V3D();
+	protected V3D prev = new V3D();
+	protected V3D move = new V3D();
+	public final VehicleInstance vehicle;
+	public final WheelTireData wdata;
 
-	public void remove();
+	public UniWheel(VehicleInstance inst, String id){
+		vehicle = inst;
+		wdata = vehicle.wheeldata.get(id);
+		pos.copy(vehicle.pivot().get_vector(wdata.pos).add(vehicle.entity.getPos()));
+		prev.copy(pos);
+	}
 
-	public boolean isAdded();
+	public void updatePrevPos(){
+		prev.copy(pos);
+	}
 
-	public V3D prev();
+	public void prepare(){
+		vehicle.entity.setOnGround(true);
+		vehicle.entity.setPrevPos(prev);
+		vehicle.entity.setPos(pos);
+		SET_STEP.accept(this);
+	}
 
-	public V3D pos();
+	public void move(){
+		vehicle.entity.move(move);
+		pos.copy(vehicle.entity.getPos());
+	}
 
-	public void pos(double x, double y, double z);
+	public WheelTireData wtd(){
+		return wdata;
+	}
 
-	public void yaw(float yaw);
+	public void addMotion(double x, double y, double z){
+		V3D.add(x, y, z, move);
+	}
 
-	public void prepare();
+	public void setMotion(double x, double y, double z){
+		move.set(x, y, z);
+	}
 
-	public void move();
+	public void mulMotion(double by){
+		V3D.mul(by, by, by, move);
+	}
 
-	public WheelTireData wtd();
-
-	public void addMotion(double x, double y, double z);
-
-	public void setMotion(double x, double y, double z);
-
-	public void mulMotion(double by);
-
-	public void fillMotion(V3D vec);
+	public void fillMotion(V3D vec){}
 
 }

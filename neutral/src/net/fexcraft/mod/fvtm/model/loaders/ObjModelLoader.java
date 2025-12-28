f6f3836e669693a9aba99bf216c1680966ec9bea
Ferdinand Calo <fexcalo@gmail.com>
2025-12-28 23:18:18 +0100

    Update ObjModelLoader.java

--- neutral/src/net/fexcraft/mod/fvtm/model/loaders/ObjModelLoader.java
+++ neutral/src/net/fexcraft/mod/fvtm/model/loaders/ObjModelLoader.java
@@ -1,10 +1,6 @@
 package net.fexcraft.mod.fvtm.model.loaders;
 
-import java.io.Closeable;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.*;
-import java.util.function.Supplier;
 
 import net.fexcraft.lib.frl.Polyhedron;
 import net.fexcraft.lib.frl.gen.FRLObjParser;
@@ -31,11 +27,10 @@ public class ObjModelLoader implements ModelLoader {
 	}
 
 	@Override
-	public Object[] load(String name, ModelData confdata, Supplier<Model> supplier) throws Exception {
-		String[] filter = name.split(";");
-		String id = filter.length > 1 ? filter[filter.length - 1] : name;
-		IDL loc = IDLManager.getIDLCached(id);
-		DefaultModel model = (DefaultModel)supplier.get();
+	public boolean load(String loc, ModelData confdata, DefaultModel model) throws Exception {
+		String[] filter = loc.split(";");
+		String id = filter.length > 1 ? filter[filter.length - 1] : loc;
+		IDL idl = IDLManager.getIDLCached(id);
 		ArrayList<String> groups = new ArrayList<>();
 		boolean exclude = false;
 		if(filter.length > 1){
@@ -46,21 +41,21 @@ public class ObjModelLoader implements ModelLoader {
 		boolean flip_u = confdata.getBoolean("FlipU", false);
 		boolean flip_v = confdata.getBoolean("FlipV", false);
 		boolean norm = !confdata.getBoolean("SkipNormals", false);
-		addObjGroups(model, loc, groups, exclude, flip_f, flip_u, flip_v, norm);
+		addObjGroups(model, idl, groups, exclude, flip_f, flip_u, flip_v, norm);
 		List<String> include = new ArrayList<>();
 		if(confdata.has(Model.OBJ_INCLUDE)) include.addAll(confdata.getArray(Model.OBJ_INCLUDE).toStringList());
 		for(String str : include){
 			filter = str.split(";");
-			loc = IDLManager.getIDL(filter.length > 1 ? filter[filter.length - 1] : str);
+			idl = IDLManager.getIDL(filter.length > 1 ? filter[filter.length - 1] : str);
 			exclude = false;
 			groups.clear();
 			if(filter.length > 1){
 				if(filter[0].equals("!") || filter[0].equals("exclude")) exclude = true;
 				if(!exclude || filter.length > 2) for(int i = exclude ? 1 : 0; i < filter.length - 1; i++) groups.add(filter[i]);
 			}
-			addObjGroups(model, loc, groups, exclude, flip_f, flip_u, flip_v, norm);
+			addObjGroups(model, idl, groups, exclude, flip_f, flip_u, flip_v, norm);
 		}
-		return new Object[]{ model, confdata };
+		return true;
 	}
 
 	private void addObjGroups(DefaultModel model, IDL loc, ArrayList<String> groups, boolean exclude, boolean flip_f, boolean flip_u, boolean flip_v, boolean norm){

69be5376f4cbbe65b96127deb44ed2b727428d53
Ferdinand Calo <fexcalo@gmail.com>
2025-12-28 23:18:11 +0100

    Update FMFModelLoader.java

--- neutral/src/net/fexcraft/mod/fvtm/model/loaders/FMFModelLoader.java
+++ neutral/src/net/fexcraft/mod/fvtm/model/loaders/FMFModelLoader.java
@@ -1,16 +1,10 @@
 package net.fexcraft.mod.fvtm.model.loaders;
 
-import java.io.Closeable;
-import java.io.InputStream;
-import java.util.HashMap;
-import java.util.function.Supplier;
-
 import net.fexcraft.app.json.JsonHandler;
 import net.fexcraft.mod.fvtm.FvtmResources;
 import net.fexcraft.mod.fvtm.FvtmResources.InputStreamWithFallback;
 import net.fexcraft.mod.fvtm.model.DefaultModel;
 import net.fexcraft.mod.fvtm.model.FMFParser;
-import net.fexcraft.mod.fvtm.model.Model;
 import net.fexcraft.mod.fvtm.model.ModelData;
 import net.fexcraft.mod.fvtm.model.ModelLoader;
 
@@ -25,12 +19,11 @@ public class FMFModelLoader implements ModelLoader {
 	}
 
 	@Override
-	public Object[] load(String name, ModelData confdata, Supplier<Model> supplier) throws Exception {
-		InputStreamWithFallback iswf = FvtmResources.getAssetInputStreamWithFallback(name);
-		Model model = supplier.get();
+	public boolean load(String loc, ModelData confdata, DefaultModel model) throws Exception {
+		InputStreamWithFallback iswf = FvtmResources.getAssetInputStreamWithFallback(loc);
 		JsonHandler.wrap(FMFParser.parse((DefaultModel)model, iswf.stream()), confdata);
 		iswf.close();
-		return new Object[]{ model, confdata };
+		return true;
 	}
 
 }

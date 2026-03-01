f76c8646fc7f1d6415562959d4fd3a0c1d382d74
Ferdinand Calo <fexcalo@gmail.com>
2026-03-01 14:59:44 +0100

    Update RailGaugeItem.java

--- fabric21/src/main/java/net/fexcraft/mod/fvtm/item/RailGaugeItem.java
+++ fabric21/src/main/java/net/fexcraft/mod/fvtm/item/RailGaugeItem.java
@@ -23,8 +23,6 @@ import net.minecraft.world.item.context.UseOnContext;
 
 import java.util.function.Consumer;
 
-import static net.fexcraft.mod.fvtm.Config.DISABLE_RAILS;
-
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
@@ -57,7 +55,7 @@ public class RailGaugeItem extends Item implements ContentItem<RailGauge>, Junct
 
 	@Override
 	public InteractionResult useOn(UseOnContext context){
-		if(context.getLevel().isClientSide || DISABLE_RAILS) return InteractionResult.PASS;
+		if(context.getLevel().isClientSide) return InteractionResult.PASS;
 		WorldW world = WrapperHolder.getWorld(context.getLevel());
 		RailSystem railsys = SystemManager.get(Systems.RAIL, world);
 		if(railsys == null){

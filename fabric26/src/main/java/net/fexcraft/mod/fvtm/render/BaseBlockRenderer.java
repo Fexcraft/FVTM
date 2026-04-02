3612990aae0dad9ce707bd42c0c6b5fbab2cba79
Ferdinand Calo <fexcalo@gmail.com>
2026-04-02 23:29:16 +0200

    Update BaseBlockRenderer.java

--- fabric26/src/main/java/net/fexcraft/mod/fvtm/render/BaseBlockRenderer.java
+++ fabric26/src/main/java/net/fexcraft/mod/fvtm/render/BaseBlockRenderer.java
@@ -2,12 +2,12 @@ package net.fexcraft.mod.fvtm.render;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.fexcraft.lib.common.Static;
-import net.fexcraft.mod.fcl.util.Renderer21;
 import net.fexcraft.mod.fvtm.block.generated.BaseBlockEntity;
-import net.fexcraft.mod.fvtm.data.block.BlockData;
 import net.fexcraft.mod.fvtm.model.DefaultModel;
-import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
+import net.minecraft.client.renderer.feature.ModelFeatureRenderer;
+import net.minecraft.client.renderer.state.level.CameraRenderState;
 import net.minecraft.world.phys.Vec3;
 import org.joml.Quaternionf;
 
@@ -17,26 +17,32 @@ import static net.fexcraft.mod.fvtm.util.BlockTypeImpl.getRot;
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
-public class BaseBlockRenderer implements BlockEntityRenderer<BaseBlockEntity> {
+public class BaseBlockRenderer implements BlockEntityRenderer<BaseBlockEntity, BERS> {
 
-	private BlockData data;
 	private double rot;
 
 	@Override
-	public void render(BaseBlockEntity tile, float ticks, PoseStack pose, MultiBufferSource buffer, int light, int overlay, Vec3 pos){
-		data = tile.getBlockData();
-		if(data == null) return;
-		Renderer21.pose = pose;
-		Renderer21.set(pose, buffer, light, overlay);
-		FvtmRenderTypes.setCutout(data.getCurrentTexture());
+	public BERS createRenderState(){
+		return new BERS();
+	}
+
+	@Override
+	public void extractRenderState(BaseBlockEntity ent, BERS state, float partialTicks, Vec3 cpos, ModelFeatureRenderer.CrumblingOverlay breakProgress){
+		BlockEntityRenderer.super.extractRenderState(ent, state, partialTicks, cpos, breakProgress);
+		state.fvtmData = ent.getBlockData();
+	}
+
+	@Override
+	public void submit(BERS state, PoseStack pose, SubmitNodeCollector nodecoll, CameraRenderState camera){
+		if(state.fvtmData == null) return;
 		pose.pushPose();
 		pose.translate(0.5, 0, 0.5);
 		//
-		rot = getRot(tile.getBlockState());
+		rot = getRot(state.blockState);
 		if(rot != 0d){
 			pose.mulPose(new Quaternionf().rotateAxis((float)Static.toRadians(rot), AY));
 		}
-		data.getType().getModel().render(DefaultModel.RENDERDATA.set(data, tile, null));
+		RenderUtil26.render(state.fvtmData.getType().getModel(), DefaultModel.RENDERDATA.set(state.fvtmData, state, null), pose, FvtmRenderTypes.getCutout(state.fvtmData.getCurrentTexture()), nodecoll, state.lightCoords);
  		//
 		pose.popPose();
 	}

d052172ffb8c3d6042adf7e4c71c8855fb5d5e53
Ferdinand Calo <fexcalo@gmail.com>
2026-04-02 23:29:30 +0200

    Update EmptyRenderer.java

--- fabric26/src/main/java/net/fexcraft/mod/fvtm/render/EmptyRenderer.java
+++ fabric26/src/main/java/net/fexcraft/mod/fvtm/render/EmptyRenderer.java
@@ -2,9 +2,11 @@ package net.fexcraft.mod.fvtm.render;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.entity.EntityRenderer;
 import net.minecraft.client.renderer.entity.EntityRendererProvider;
 import net.minecraft.client.renderer.entity.state.EntityRenderState;
+import net.minecraft.client.renderer.state.level.CameraRenderState;
 import net.minecraft.world.entity.Entity;
 
 /**
@@ -22,7 +24,7 @@ public class EmptyRenderer extends EntityRenderer<Entity, EntityRenderState> {
 	}
 
 	@Override
-	public void render(EntityRenderState state, PoseStack pose, MultiBufferSource buffer, int light){
+	public void submit(EntityRenderState state, PoseStack pose, SubmitNodeCollector nodecoll, CameraRenderState camera){
 		//
 	}
 

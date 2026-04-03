5340bcdad3ed4e2240def4f1c84f08b0af2792cc
Ferdinand Calo <fexcalo@gmail.com>
2026-04-04 00:11:24 +0200

    Update RenderUtil26.java

--- fabric26/src/main/java/net/fexcraft/mod/fvtm/render/RenderUtil26.java
+++ fabric26/src/main/java/net/fexcraft/mod/fvtm/render/RenderUtil26.java
@@ -2,48 +2,71 @@ package net.fexcraft.mod.fvtm.render;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.fexcraft.lib.frl.Polyhedron;
+import net.fexcraft.mod.fcl.util.Renderer21;
 import net.fexcraft.mod.fvtm.model.*;
+import net.fexcraft.mod.fvtm.util.DebugUtils;
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.rendertype.RenderType;
 
-import static net.fexcraft.mod.fcl.util.Renderer21.REN_IN;
+import static net.fexcraft.lib.frl.Renderer.RENDERER;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public class RenderUtil26 {
 
-	public static void render(Model model, ModelRenderData mdata, PoseStack pose, RenderType type, SubmitNodeCollector nodecoll, int lc){
-		REN_IN.stack = pose;
+	private static SubmitNodeCollector noco;
+
+	public static void set(PoseStack pose, SubmitNodeCollector nocoll, RenderType type, int lc){
+		Renderer21.stack = pose;
+		noco = nocoll;
+		Renderer21.type = type;
+		Renderer21.light = lc;
+	}
+
+	public static void type(RenderType type){
+		Renderer21.type = type;
+	}
+
+	public static void render(Model model, ModelRenderData mdata){
 		for(ModelGroup group : model.getGroups()){
-			render(group, mdata, pose, type, nodecoll, lc);
+			render(group, mdata);
 		}
 	}
 
-	public static void render(ModelGroup group, ModelRenderData mdata, PoseStack pose, RenderType type, SubmitNodeCollector nodecoll, int lc){
-		REN_IN.stack = pose;
-		REN_IN.type = type;
-		REN_IN.light = lc;
+	public static void render(ModelGroup group, ModelRenderData mdata){
 		group.pre(mdata);
-		nodecoll.submitCustomGeometry(pose, type, (last, cons) -> {
-			REN_IN.pose = last;
-			REN_IN.cons = cons;
+		noco.submitCustomGeometry(Renderer21.stack, Renderer21.type, (last, cons) -> {
+			Renderer21.pose = last;
+			Renderer21.cons = cons;
 			group.render();
-			REN_IN.pose = null;
+			Renderer21.pose = null;
 		});
 		group.post(mdata);
 	}
 
-	public static void render(Polyhedron poly, PoseStack pose, RenderType type, SubmitNodeCollector nodecoll, int lc){
-		REN_IN.stack = pose;
-		REN_IN.type = type;
-		REN_IN.light = lc;
-		nodecoll.submitCustomGeometry(pose, type, (last, cons) -> {
-			REN_IN.pose = last;
-			REN_IN.cons = cons;
-			REN_IN.render(poly);
-			REN_IN.pose = null;
+	public static void render(Polyhedron poly){
+		noco.submitCustomGeometry(Renderer21.stack, Renderer21.type, (last, cons) -> {
+			Renderer21.pose = last;
+			Renderer21.cons = cons;
+			RENDERER.render(poly);
+			Renderer21.pose = null;
 		});
+	}
+
+	public static void renderSphere(float scale, int col){
+		Renderer21.type = FvtmRenderTypes.white();
+		Renderer21.stack.pushPose();
+		Renderer21.stack.scale(scale, scale, scale);
+		noco.submitCustomGeometry(Renderer21.stack, FvtmRenderTypes.white(), (last, cons) -> {
+			RENDERER.color(col);
+			Renderer21.pose = last;
+			Renderer21.cons = cons;
+			RENDERER.render(DebugUtils.SPHERE);
+			Renderer21.pose = null;
+			Renderer21.resetColor();
+		});
+		Renderer21.stack.popPose();
 	}
 
 }

662441335dc11682b580b31ba2e948894f71b507
Ferdinand Calo <fexcalo@gmail.com>
2026-04-04 23:25:21 +0200

    Update BlockRS.java

--- fabric26/src/main/java/net/fexcraft/mod/fvtm/render/state/BlockRS.java
+++ fabric26/src/main/java/net/fexcraft/mod/fvtm/render/state/BlockRS.java
@@ -2,6 +2,7 @@ package net.fexcraft.mod.fvtm.render.state;
 
 import net.fexcraft.mod.fvtm.data.block.BlockData;
 import net.minecraft.client.renderer.blockentity.state.BlockEntityRenderState;
+import net.minecraft.world.level.block.entity.BlockEntity;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -9,5 +10,6 @@ import net.minecraft.client.renderer.blockentity.state.BlockEntityRenderState;
 public class BlockRS extends BlockEntityRenderState {
 
 	public BlockData fvtmData;
+	public BlockEntity blockEntity;
 
 }

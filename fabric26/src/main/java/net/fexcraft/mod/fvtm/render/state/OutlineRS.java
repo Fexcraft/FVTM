0b48ba1aed7a1f2cbd835b9c650e2af726431a93
Ferdinand Calo <fexcalo@gmail.com>
2026-04-05 00:17:36 +0200

    Create OutlineRS.java

---
+++ fabric26/src/main/java/net/fexcraft/mod/fvtm/render/state/OutlineRS.java
@@ -0,0 +1,14 @@
+package net.fexcraft.mod.fvtm.render.state;
+
+import net.fexcraft.mod.fvtm.util.QV3D;
+import net.minecraft.core.BlockPos;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class OutlineRS {
+
+	public QV3D result;
+	public BlockPos pos;
+
+}

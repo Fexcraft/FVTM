12655745e4d8cd369c7d2cb885e4d5d7d1ccbeb6
Ferdinand Calo <fexcalo@gmail.com>
2026-04-05 00:17:32 +0200

    Update LevelRS.java

--- fabric26/src/main/java/net/fexcraft/mod/fvtm/render/state/LevelRS.java
+++ fabric26/src/main/java/net/fexcraft/mod/fvtm/render/state/LevelRS.java
@@ -1,12 +1,10 @@
 package net.fexcraft.mod.fvtm.render.state;
 
-import net.minecraft.client.multiplayer.ClientLevel;
-
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public class LevelRS {
 
-	public ClientLevel level;
+	public String key;
 
 }

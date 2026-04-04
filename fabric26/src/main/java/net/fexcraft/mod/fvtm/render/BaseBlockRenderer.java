7cc402060c5d9eafa56e3346ce9e64666e2dd6ab
Ferdinand Calo <fexcalo@gmail.com>
2026-04-04 22:27:49 +0200

    Delete BERS.java

--- fabric26/src/main/java/net/fexcraft/mod/fvtm/render/BaseBlockRenderer.java
+++ fabric26/src/main/java/net/fexcraft/mod/fvtm/render/BaseBlockRenderer.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.vertex.PoseStack;
 import net.fexcraft.lib.common.Static;
 import net.fexcraft.mod.fvtm.block.generated.BaseBlockEntity;
 import net.fexcraft.mod.fvtm.model.DefaultModel;
+import net.fexcraft.mod.fvtm.render.state.BlockRS;
 import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderer;
 import net.minecraft.client.renderer.feature.ModelFeatureRenderer;
@@ -17,23 +18,23 @@ import static net.fexcraft.mod.fvtm.util.BlockTypeImpl.getRot;
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
-public class BaseBlockRenderer implements BlockEntityRenderer<BaseBlockEntity, BERS> {
+public class BaseBlockRenderer implements BlockEntityRenderer<BaseBlockEntity, BlockRS> {
 
 	private double rot;
 
 	@Override
-	public BERS createRenderState(){
-		return new BERS();
+	public BlockRS createRenderState(){
+		return new BlockRS();
 	}
 
 	@Override
-	public void extractRenderState(BaseBlockEntity ent, BERS state, float partialTicks, Vec3 cpos, ModelFeatureRenderer.CrumblingOverlay breakProgress){
+	public void extractRenderState(BaseBlockEntity ent, BlockRS state, float partialTicks, Vec3 cpos, ModelFeatureRenderer.CrumblingOverlay breakProgress){
 		BlockEntityRenderer.super.extractRenderState(ent, state, partialTicks, cpos, breakProgress);
 		state.fvtmData = ent.getBlockData();
 	}
 
 	@Override
-	public void submit(BERS state, PoseStack pose, SubmitNodeCollector nodecoll, CameraRenderState camera){
+	public void submit(BlockRS state, PoseStack pose, SubmitNodeCollector nodecoll, CameraRenderState camera){
 		if(state.fvtmData == null) return;
 		pose.pushPose();
 		pose.translate(0.5, 0, 0.5);

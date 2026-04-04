f326a58e2bd6acb7d39a7d330a7f76bd67f96092
Ferdinand Calo <fexcalo@gmail.com>
2026-04-04 22:29:39 +0200

    Create RailMarkerRS.java

---
+++ fabric26/src/main/java/net/fexcraft/mod/fvtm/render/state/RailMarkerRS.java
@@ -0,0 +1,13 @@
+package net.fexcraft.mod.fvtm.render.state;
+
+import net.fexcraft.mod.fvtm.entity.RailMarker;
+import net.minecraft.client.renderer.entity.state.EntityRenderState;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class RailMarkerRS extends EntityRenderState {
+
+	public RailMarker rail_marker;
+
+}

a4b23dee3bec1f2ce1318a6bfff453f007c6b292
Ferdinand Calo <fexcalo@gmail.com>
2026-04-04 22:29:35 +0200

    Create DecorationRS.java

---
+++ fabric26/src/main/java/net/fexcraft/mod/fvtm/render/state/DecorationRS.java
@@ -0,0 +1,14 @@
+package net.fexcraft.mod.fvtm.render.state;
+
+import net.fexcraft.mod.fvtm.entity.DecorationEntity;
+import net.fexcraft.mod.fvtm.entity.RoadMarker;
+import net.minecraft.client.renderer.entity.state.EntityRenderState;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class DecorationRS extends EntityRenderState {
+
+	public DecorationEntity decoration;
+
+}

31cf5fc611e314f9a28d9d1d5c3db2ba4e85aa0d
Ferdinand Calo <fexcalo@gmail.com>
2026-04-04 22:28:01 +0200

    Update RailMarkerRenderer.java

--- fabric26/src/main/java/net/fexcraft/mod/fvtm/render/RailMarkerRenderer.java
+++ fabric26/src/main/java/net/fexcraft/mod/fvtm/render/RailMarkerRenderer.java
@@ -5,6 +5,8 @@ import net.fexcraft.lib.common.math.RGB;
 import net.fexcraft.mod.fcl.util.Renderer26;
 import net.fexcraft.mod.fvtm.entity.RailMarker;
 import net.fexcraft.mod.fvtm.model.entity.RailMarkerModel;
+import net.fexcraft.mod.fvtm.render.state.RailMarkerRS;
+import net.fexcraft.mod.fvtm.render.state.VehicleRenderState;
 import net.fexcraft.mod.fvtm.sys.rail.RailPlacingUtil;
 import net.fexcraft.mod.uni.IDL;
 import net.fexcraft.mod.uni.IDLManager;
@@ -18,7 +20,7 @@ import static net.fexcraft.mod.fvtm.model.DefaultModel.RENDERDATA;
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
-public class RailMarkerRenderer extends EntityRenderer<RailMarker, FvtmRenderState> {
+public class RailMarkerRenderer extends EntityRenderer<RailMarker, RailMarkerRS> {
 
 	private static IDL texture = IDLManager.getIDLCached("fvtm:textures/entity/railmarker.png");
 	public static final RGB CYAN = new RGB(0, 255, 255);
@@ -29,19 +31,18 @@ public class RailMarkerRenderer extends EntityRenderer<RailMarker, FvtmRenderSta
 	}
 
 	@Override
-	public FvtmRenderState createRenderState(){
-		return new FvtmRenderState();
+	public RailMarkerRS createRenderState(){
+		return new RailMarkerRS();
 	}
 
 	@Override
-	public void extractRenderState(RailMarker entity, FvtmRenderState state, float f){
+	public void extractRenderState(RailMarker entity, RailMarkerRS state, float f){
 		super.extractRenderState(entity, state, f);
 		state.rail_marker = entity;
-		state.f = f;
 	}
 
 	@Override
-	public void submit(FvtmRenderState state, PoseStack pose, SubmitNodeCollector nodecoll, CameraRenderState camera){
+	public void submit(RailMarkerRS state, PoseStack pose, SubmitNodeCollector nodecoll, CameraRenderState camera){
 		pose.pushPose();
 		RenderUtil26.set(pose, nodecoll, FvtmRenderTypes.getCutout(texture), state.lightCoords);
 		RenderUtil26.render(RailMarkerModel.INST.base, RENDERDATA);

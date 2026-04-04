c0cb5c8e6d1f33b290c2206bea4782f93ea0019c
Ferdinand Calo <fexcalo@gmail.com>
2026-04-04 22:29:41 +0200

    Create RoadMarkerRS.java

---
+++ fabric26/src/main/java/net/fexcraft/mod/fvtm/render/state/RoadMarkerRS.java
@@ -0,0 +1,13 @@
+package net.fexcraft.mod.fvtm.render.state;
+
+import net.fexcraft.mod.fvtm.entity.RoadMarker;
+import net.minecraft.client.renderer.entity.state.EntityRenderState;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class RoadMarkerRS extends EntityRenderState {
+
+	public RoadMarker road_marker;
+
+}

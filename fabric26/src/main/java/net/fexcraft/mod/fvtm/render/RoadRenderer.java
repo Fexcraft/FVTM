01a9893ade60f0a268bc9d2428c0de7f249dd256
Ferdinand Calo <fexcalo@gmail.com>
2026-04-05 00:17:15 +0200

    Update RoadRenderer.java

--- fabric26/src/main/java/net/fexcraft/mod/fvtm/render/RoadRenderer.java
+++ fabric26/src/main/java/net/fexcraft/mod/fvtm/render/RoadRenderer.java
@@ -1,27 +1,34 @@
 package net.fexcraft.mod.fvtm.render;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.fabricmc.fabric.api.client.rendering.v1.level.LevelRenderContext;
-import net.minecraft.client.renderer.state.level.BlockOutlineRenderState;
+import net.fexcraft.mod.fvtm.sys.road.RoadPlacingUtil;
+import net.fexcraft.mod.fvtm.util.QV3D;
+
+import java.util.ArrayList;
+
+import static net.fexcraft.lib.common.Static.sixteenth;
+import static net.fexcraft.mod.fvtm.util.DebugUtils.*;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public class RoadRenderer {
 
-	public static boolean renderRoadPreview(LevelRenderContext event, BlockOutlineRenderState res){
-		/*if(RoadPlacingUtil.CL_CURRENT == null || RoadPlacingUtil.CL_CURRENT.points.size() < 2) return true;
-		double cx = event.camera().getPosition().x;
-		double cy = event.camera().getPosition().y;
-		double cz = event.camera().getPosition().z;
-		PoseStack pose = event.matrixStack();
-		Renderer26.set(pose, Minecraft.getInstance().renderBuffers().bufferSource(), 255);
-		FvtmRenderTypes.setLines();
+	public static boolean renderRoadPreview(LevelRenderContext context){
+		if(RoadPlacingUtil.CL_CURRENT == null || RoadPlacingUtil.CL_CURRENT.points.size() < 2) return true;
+		double cx = context.levelState().cameraRenderState.pos.x;
+		double cy = context.levelState().cameraRenderState.pos.y;
+		double cz = context.levelState().cameraRenderState.pos.z;
+		PoseStack pose = context.poseStack();
+		RenderUtil26.set(pose, context.submitNodeCollector(), null, 255);
+		//RenderUtil26.lines();
 		pose.pushPose();
 		pose.translate(-cx, -cy, -cz);
-		V3D vec0, vec1;
+		//V3D vec0, vec1;
 		RoadPlacingUtil.NewRoad nroad = RoadPlacingUtil.CL_CURRENT;
 		if(nroad.coords == null) nroad.genpreview();
-		Renderer26.setColor(COL_BLU);
+		/*Renderer26.setColor(COL_BLU);
 		for(int j = 0; j < nroad.road.vecpath.length - 1; j++){
 			vec0 = nroad.road.vecpath[j];
 			vec1 = nroad.road.vecpath[j + 1];
@@ -38,16 +45,16 @@ public class RoadRenderer {
 			LINE_POLY.vertices[0].pos(arr[0], arr[1] + 1.25, arr[2]);
 			LINE_POLY.vertices[1].pos(vec1.x, vec1.y + 1.25, vec1.z);
 			LINE.render();
-		}
+		}*/
 		for(ArrayList<QV3D> coords : nroad.coords){
 			for(QV3D coord : coords){
 				pose.pushPose();
 				pose.translate(coord.pos.x + 1, coord.pos.y + 1 + coord.y * sixteenth, coord.pos.z + 1);
-				DebugUtils.renderPane(0.5f, COL_CYN);
+				RenderUtil26.renderPane(0.5f, COL_CYN);
 				pose.popPose();
 			}
 		}
-		pose.popPose();*/
+		pose.popPose();
 		return true;
 	}
 

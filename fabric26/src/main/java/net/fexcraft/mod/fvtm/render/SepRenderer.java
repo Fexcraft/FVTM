4f53ae5c0bb7908e5e731f49fd604528830e2726
Ferdinand Calo <fexcalo@gmail.com>
2026-04-05 00:17:25 +0200

    Update SepRenderer.java

--- fabric26/src/main/java/net/fexcraft/mod/fvtm/render/SepRenderer.java
+++ fabric26/src/main/java/net/fexcraft/mod/fvtm/render/SepRenderer.java
@@ -1,17 +1,33 @@
 package net.fexcraft.mod.fvtm.render;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import net.fabricmc.fabric.api.client.rendering.v1.level.LevelRenderContext;
+import net.fexcraft.mod.fcl.util.Renderer26;
+import net.fexcraft.mod.fvtm.data.block.BlockData;
+import net.fexcraft.mod.fvtm.model.Model;
+import net.fexcraft.mod.fvtm.model.ModelGroupList;
+import net.fexcraft.mod.fvtm.model.RenderCacheI;
+import net.fexcraft.mod.fvtm.sys.uni.VehicleInstance;
+import net.minecraft.world.level.block.entity.BlockEntity;
+
+import static net.fexcraft.lib.frl.Renderer.RENDERER;
+import static net.fexcraft.mod.fvtm.model.DefaultModel.RENDERDATA;
+import static net.fexcraft.mod.fvtm.render.RVRenderer.renderPointSep;
+import static net.fexcraft.mod.fvtm.render.SeparateRenderCache.*;
+import static net.fexcraft.mod.fvtm.render.SeparateRenderCache.SepVehCache;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public class SepRenderer {
 
-	public static void renderSeparate(LevelRenderContext event){
-		/*if(VEHICLES.size() == 0 && SORTED_BLK_QUEUE.size() == 0) return;Camera camera = event.camera();
-		double cx = camera.getPosition().x;
-		double cy = camera.getPosition().y;
-		double cz = camera.getPosition().z;
+	public static void renderSeparate(LevelRenderContext context){
+		if(VEHICLES.size() == 0 && SORTED_BLK_QUEUE.size() == 0) return;
+		double cx = context.levelState().cameraRenderState.pos.x;
+		double cy = context.levelState().cameraRenderState.pos.y;
+		double cz = context.levelState().cameraRenderState.pos.z;
+		PoseStack pose = context.poseStack();
+		RenderUtil26.set(pose, context.submitNodeCollector(), FvtmRenderTypes.white(), 0);
 		Renderer26.resetColor();
 		if(VEHICLES.size() > 0){
 			pose.pushPose();
@@ -28,7 +44,7 @@ public class SepRenderer {
 				Model vehmod = inst.data.getType().getModel();
 				if(vehmod != null && vehmod.getSeparateGroups() != null){
 					pose.pushPose();
-					FvtmRenderTypes.setCutout(inst.data.getCurrentTexture());
+					RenderUtil26.type(FvtmRenderTypes.getCutout(inst.data.getCurrentTexture()));
 					vehmod.getSeparateGroups().render(RENDERDATA.set(inst, 1).rc(inst.cache).sep());
 					pose.popPose();
 				}
@@ -49,12 +65,12 @@ public class SepRenderer {
 				pose.pushPose();
 				pose.translate(tile.getBlockPos().getX() + 0.5, tile.getBlockPos().getY(), tile.getBlockPos().getZ() + 0.5);
 				//TODO rotate
-				sgroup.render(RENDERDATA.set(data, tile, null).rcs(null*//*TODO*//*));
+				sgroup.render(RENDERDATA.set(data, tile, null).rcs(null/*TODO*/));
 				pose.popPose();
 			}
 			pose.popPose();
 		}
-		clear();*/
+		clear();
 	}
 
 }

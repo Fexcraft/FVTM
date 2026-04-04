9f3bc81b4880ef9bf3bba1df77325d3630c6fb58
Ferdinand Calo <fexcalo@gmail.com>
2026-04-04 22:29:32 +0200

    Create BlockRS.java

---
+++ fabric26/src/main/java/net/fexcraft/mod/fvtm/render/state/BlockRS.java
@@ -0,0 +1,13 @@
+package net.fexcraft.mod.fvtm.render.state;
+
+import net.fexcraft.mod.fvtm.data.block.BlockData;
+import net.minecraft.client.renderer.blockentity.state.BlockEntityRenderState;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class BlockRS extends BlockEntityRenderState {
+
+	public BlockData fvtmData;
+
+}

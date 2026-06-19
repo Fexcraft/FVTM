aea6c3247aa2bc742d99d89000b96d3cf63c4407
Ferdinand Calo <fexcalo@gmail.com>
2026-06-19 13:31:19 +0200

    Update BlockBase.java

--- fabric26/src/main/java/net/fexcraft/mod/fvtm/block/generated/BlockBase.java
+++ fabric26/src/main/java/net/fexcraft/mod/fvtm/block/generated/BlockBase.java
@@ -1,21 +1,31 @@
 package net.fexcraft.mod.fvtm.block.generated;
 
+import net.fexcraft.lib.common.math.V3D;
 import net.fexcraft.mod.fvtm.data.block.Block;
+import net.fexcraft.mod.fvtm.data.block.BlockFunction;
 import net.fexcraft.mod.fvtm.item.BlockItem21;
 import net.fexcraft.mod.fvtm.sys.uni.SystemManager;
 import net.fexcraft.mod.fvtm.sys.wire.WireSystem;
 import net.fexcraft.mod.fvtm.util.Resources21;
+import net.fexcraft.mod.uni.UniEntity;
 import net.fexcraft.mod.uni.inv.UniStack;
+import net.fexcraft.mod.uni.world.StateWrapper;
 import net.fexcraft.mod.uni.world.WrapperHolder;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.Nullable;
+
+import static net.fexcraft.mod.uni.world.WrapperHolder.*;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -51,6 +61,18 @@ public class BlockBase extends PlainBase implements EntityBlock {
 	@Override
 	public RenderShape getRenderShape(BlockState state){
 		return type.isInvisible() || !type.hasPlainModel() ? RenderShape.INVISIBLE : RenderShape.MODEL;
+	}
+
+	@Override
+	public InteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult res){
+		if(level.isClientSide() || hand == InteractionHand.OFF_HAND) return InteractionResult.PASS;
+		if(!player.isCrouching() && type.getFunctions().size() > 0){
+			BaseBlockEntity tile = (BaseBlockEntity)level.getBlockEntity(pos);
+			for(BlockFunction func : tile.data.getFunctions()){
+				if(func.onClick(getWorld(level), getPos(pos), new V3D(0, 0, 0), StateWrapper.of(state), getSide(res.getDirection()), UniEntity.getEntity(player), hand == InteractionHand.MAIN_HAND)) return InteractionResult.SUCCESS;
+			}
+		}
+		return InteractionResult.PASS;
 	}
 
 }

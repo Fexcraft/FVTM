79618ae9472e659ee0bb7405bc911704e9da590f
Ferdinand Calo <fexcalo@gmail.com>
2026-06-22 23:45:51 +0200

    Update Transforms120.java

--- forge20/src/main/java/net/fexcraft/mod/fvtm/render/Transforms120.java
+++ forge20/src/main/java/net/fexcraft/mod/fvtm/render/Transforms120.java
@@ -14,79 +14,74 @@ public class Transforms120 {
 
 	public static final TF_RescaleNormal TF_RESCALE_NORMAL = new TF_RescaleNormal();
 
-	public static class TF_Translate implements Transforms.Transformer {
-
-		private float x;
-		private float y;
-		private float z;
+	public static class TF_Translate extends Transforms.TF_Translate {
 
 		public TF_Translate(float xx, float yy, float zz){
-			this.x = xx;
-			this.y = yy;
-			this.z = zz;
+			super(xx, yy, zz);
 		}
 
+		@Override
 		public void apply(){
-			Renderer20.pose.translate(this.x, this.y, this.z);
+			Renderer20.pose.translate(x, y, z);
 		}
 
+		@Override
 		public void deapply(){
-			Renderer20.pose.translate(-this.x, -this.y, -this.z);
+			Renderer20.pose.translate(-x, -y, -z);
 		}
 
 	}
 
-	public static class TF_Rotate implements Transforms.Transformer {
+	public static class TF_Rotate extends Transforms.TF_Rotate {
 
 		private int axe;
-		private float angle;
+		private float ang;
 
-		public TF_Rotate(float xx, float yy, float zz, float ang){
+		public TF_Rotate(float xx, float yy, float zz, float a){
+			super(xx, yy, zz, a);
 			axe = xx > 0 ? 0 : yy > 0 ? 1 : 2;
-			angle = Static.toRadians(ang);
+			ang = Static.toRadians(a);
 		}
 
+		@Override
 		public void apply(){
 			Renderer20.pose.pushPose();
 			switch(axe){
 				case 0: {
-					Renderer20.pose.mulPose(new Quaternionf().rotateAxis(angle, AX));
+					Renderer20.pose.mulPose(new Quaternionf().rotateAxis(ang, AX));
 					break;
 				}
 				case 1: {
-					Renderer20.pose.mulPose(new Quaternionf().rotateAxis(angle, AY));
+					Renderer20.pose.mulPose(new Quaternionf().rotateAxis(ang, AY));
 					break;
 				}
 				case 2: {
-					Renderer20.pose.mulPose(new Quaternionf().rotateAxis(angle, AZ));
+					Renderer20.pose.mulPose(new Quaternionf().rotateAxis(ang, AZ));
 					break;
 				}
 			}
 		}
 
+		@Override
 		public void deapply(){
 			Renderer20.pose.popPose();
 		}
 
 	}
 
-	public static class TF_Scale implements Transforms.Transformer {
-
-		private float x;
-		private float y;
-		private float z;
+	public static class TF_Scale extends Transforms.TF_Scale {
 
 		public TF_Scale(float xx, float yy, float zz){
-			this.x = xx;
-			this.y = yy;
-			this.z = zz;
+			super(xx, yy, zz);
 		}
 
+		@Override
 		public void apply(){
 			Renderer20.pose.pushPose();
-			Renderer20.pose.scale(this.x, this.y, this.z);
+			Renderer20.pose.scale(x, y, z);
 		}
 
+		@Override
 		public void deapply(){
 			Renderer20.pose.popPose();
 		}

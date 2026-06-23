9d4706d8e03e2be224a4b3c99c2390482481d49d
Ferdinand Calo <fexcalo@gmail.com>
2026-06-23 23:42:02 +0200

    Update G_16ROT.java

--- forge20/src/main/java/net/fexcraft/mod/fvtm/block/generated/G_16ROT.java
+++ forge20/src/main/java/net/fexcraft/mod/fvtm/block/generated/G_16ROT.java
@@ -1,9 +1,15 @@
 package net.fexcraft.mod.fvtm.block.generated;
 
 import net.fexcraft.lib.common.Static;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 import javax.annotation.Nullable;
 
@@ -27,6 +33,25 @@ public class G_16ROT extends PlainBase {
 	@Nullable
 	public BlockState getStateForPlacement(BlockPlaceContext context) {
 		return defaultBlockState().setValue(PROP_ROT16, type.isRandomRot() ? Static.random.nextInt(16) : (int)Math.floor((double)(context.getPlayer().yRotO * 16.0F / 360.0F) + 0.5D) & 15);
+	}
+
+	@Override
+	protected void fillVoxelShapes(){
+		super.fillVoxelShapes();
+		for(Integer rot : PROP_ROT16.getPossibleValues()){
+			vshapes.put("rotation=", Shapes.create(type.getAABB("default", "rotation=" + rot).get(0)));
+			collision.put("rotation=", Shapes.create(type.getAABB("collision", "rotation=" + rot).get(0)));
+		}
+	}
+
+	@Override
+	public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext ctx){
+		return vshapes.getOrDefault("rotation=" + state.getValue(PROP_ROT16), vshapes.get("normal"));
+	}
+
+	@Override
+	public VoxelShape getCollisionShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext ctx){
+		return collision.getOrDefault("rotation=" + state.getValue(PROP_ROT16), collision.get("normal"));
 	}
 
 

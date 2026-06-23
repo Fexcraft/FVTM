1e1a334da0c64ec996ecc1b52b86db25fb1db273
Ferdinand Calo <fexcalo@gmail.com>
2026-06-23 23:59:05 +0200

    Update BakedModelLoader.java

--- forge12/src/main/java/net/fexcraft/mod/fvtm/render/block/BakedModelLoader.java
+++ forge12/src/main/java/net/fexcraft/mod/fvtm/render/block/BakedModelLoader.java
@@ -3,6 +3,7 @@ package net.fexcraft.mod.fvtm.render.block;
 import net.fexcraft.lib.common.math.AxisRotator;
 import net.fexcraft.lib.common.math.Vec3f;
 import net.fexcraft.lib.mc.utils.Axis3DL;
+import net.fexcraft.mod.fvtm.data.block.Block;
 import net.fexcraft.mod.fvtm.model.DefaultModel;
 import net.fexcraft.mod.fvtm.model.ModelGroup;
 import net.fexcraft.mod.fvtm.model.Program;
@@ -39,36 +40,34 @@ public class BakedModelLoader {
                 for(Program program : group.getPstPrograms()) program.post(group, DefaultModel.RENDERDATA.set(null, null, state));
             }
         }
-        model.bk = null;
     }
 
-    public static void convertTransforms(BlockModel model, net.fexcraft.mod.fvtm.data.block.Block block, IBlockState state){
-        model.bk = new BlockModel.BakedTransformData();
-        model.bk.rot_poly = new Axis3DL();
-        model.bk.rot_meta = new Axis3DL();
+    public static void convertTransforms(BlockModel model, BlockModel.BakedTransformData bk, Block block, IBlockState state){
+        bk.rot_poly = new Axis3DL();
+        bk.rot_meta = new Axis3DL();
         if(state != null){
-            model.bk.rot_meta.setAngles(-(float)block.getBlockType().getRotationFor(state.getBlock().getMetaFromState(state)), 0, 0);
+            bk.rot_meta.setAngles(-(float)block.getBlockType().getRotationFor(state.getBlock().getMetaFromState(state)), 0, 0);
         }
         if(model.transforms.hasRotate()){
             ArrayList<Transforms.TF_Rotate> list = model.transforms.getBakedRotate();
-            model.bk.rot_tf = new AxisRotator[list.size()];
+            bk.rot_tf = new AxisRotator[list.size()];
             for(int i = 0; i < list.size(); i++){
                 Transforms.TF_Rotate rot = list.get(i);
-                model.bk.rot_tf[i] = AxisRotator.newDefInstance();
-                model.bk.rot_tf[i].setAngles(rot.y * -rot.angle, rot.z * -rot.angle, rot.x * -rot.angle);
+                bk.rot_tf[i] = AxisRotator.newDefInstance();
+                bk.rot_tf[i].setAngles(rot.y * -rot.angle, rot.z * -rot.angle, rot.x * -rot.angle);
             }
         }
         if(model.transforms.hasTranslate()){
-            model.bk.translate = model.transforms.getBakedTranslate();
+            bk.translate = model.transforms.getBakedTranslate();
         }
-        else model.bk.translate = new Vec3f();
+        else bk.translate = new Vec3f();
         if(model.transforms.hasScale()){
-            model.bk.scale = model.transforms.getBakedScale();
+            bk.scale = model.transforms.getBakedScale();
         }
-        else model.bk.scale = new Vec3f(1, 1, 1);
+        else bk.scale = new Vec3f(1, 1, 1);
         //
         for(ArrayList<BlockModel> val : model.state_models.values()){
-            for(BlockModel v : val) convertTransforms(v, block, state);
+            for(BlockModel v : val) convertTransforms(v, bk, block, state);
         }
     }
 

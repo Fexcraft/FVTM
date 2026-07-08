bf9b5ceb83fcc41d5861282131113090bcf674b2
Ferdinand Calo <fexcalo@gmail.com>
2026-07-08 23:50:38 +0200

    Update FVTM.java

--- fabric26/src/main/java/net/fexcraft/mod/fvtm/FVTM.java
+++ fabric26/src/main/java/net/fexcraft/mod/fvtm/FVTM.java
@@ -125,6 +125,7 @@ public class FVTM implements ModInitializer {
 		StackWrapper.ITEM_TYPES.put(ContentType.TOOLBOX.item_type, item -> item instanceof ToolboxItem);
 		StackWrapper.ITEM_TYPES.put(ContentType.WIRE.item_type, item -> item instanceof WireItem);
 		StackWrapper.ITEM_TYPES.put(ContentType.WIREDECO.item_type, item -> item instanceof WireDecoItem);
+		StackWrapper.ITEM_TYPES.put(ContentType.DECORATION.item_type, item -> item instanceof DecorationItem);
 		StackWrapper.ITEM_TYPES.put(ContentType.SIGN.item_type, item -> item instanceof SignItem);
 		StackWrapper.CONTENT_TYPES.put(ContentType.PART.item_type, stack -> ((PartItem)stack.getItem().direct()).getData(stack));
 		StackWrapper.CONTENT_TYPES.put(ContentType.VEHICLE.item_type, stack -> ((VehicleItem)stack.getItem().direct()).getData(stack));
@@ -136,6 +137,7 @@ public class FVTM implements ModInitializer {
 		StackWrapper.CONTENT_TYPES.put(ContentType.RAILGAUGE.item_type, stack -> ((RailGaugeItem)stack.getItem().direct()).getContent());
 		StackWrapper.CONTENT_TYPES.put(ContentType.WIRE.item_type, stack -> ((WireItem)stack.getItem().direct()).getContent());
 		StackWrapper.CONTENT_TYPES.put(ContentType.WIREDECO.item_type, stack -> ((WireDecoItem)stack.getItem().direct()).getContent());
+		StackWrapper.CONTENT_TYPES.put(ContentType.DECORATION.item_type, stack -> ((DecorationItem)stack.getItem().direct()).getContent());
 		StackWrapper.CONTENT_TYPES.put(ContentType.SIGN.item_type, stack -> ((SignItem)stack.getItem().direct()).getContent());
 		StackWrapper.CONTENT_TYPES.put(ContentType.TOOLBOX.item_type, stack -> ((ToolboxItem)stack.getItem().direct()).var);
 		AABB.SUPPLIER = () -> new AABBI();
@@ -180,12 +182,6 @@ public class FVTM implements ModInitializer {
 				MobCategory.MISC, true, false, true, true,
 				ImmutableSet.of(), EntityDimensions.fixed(1f, 1f),
 				0, 256, 1, "fvtm.rail_vehicle", Optional.empty(), FeatureFlagSet.of(), true));
-		}
-		if(Config.MD_DECORATION){
-			Resources21.DECO_ENTITY = Registry.register(BuiltInRegistries.ENTITY_TYPE, "fvtm:decoration", new EntityType<>(DecorationEntity::new,
-				MobCategory.MISC, true, false, true, true,
-				ImmutableSet.of(), EntityDimensions.fixed(0.25f, 0.25f),
-				0, 256, 1, "fvtm.decoration", Optional.empty(), FeatureFlagSet.of(), true));
 		}
 		if(Config.MD_RAIL){
 			Resources21.RAIL_MARKER_ENTITY = Registry.register(BuiltInRegistries.ENTITY_TYPE, "fvtm:rail_marker", new EntityType<>(RailMarker::new,

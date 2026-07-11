068cabd19395d7a4b1a8fe45a43c2ac9962b19bc
Ferdinand Calo <fexcalo@gmail.com>
2026-07-11 19:21:38 +0200

    Update DecorationItem.java

--- forge12/src/main/java/net/fexcraft/mod/fvtm/item/DecorationItem.java
+++ forge12/src/main/java/net/fexcraft/mod/fvtm/item/DecorationItem.java
@@ -9,15 +9,25 @@ import net.fexcraft.lib.common.utils.Formatter;
 import net.fexcraft.mod.fvtm.FvtmResources;
 import net.fexcraft.mod.fvtm.data.*;
 import net.fexcraft.mod.fvtm.data.root.ItemTextureable;
+import net.fexcraft.mod.fvtm.sys.deco.DecoSystem;
+import net.fexcraft.mod.fvtm.sys.uni.SystemManager;
+import net.fexcraft.mod.fvtm.util.QV3D;
 import net.fexcraft.mod.uni.EnvInfo;
+import net.fexcraft.mod.uni.UniEntity;
 import net.fexcraft.mod.uni.inv.StackWrapper;
 import net.fexcraft.mod.uni.inv.UniStack;
 import net.fexcraft.mod.uni.tag.TagCW;
+import net.fexcraft.mod.uni.world.EntityW;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
@@ -92,6 +102,16 @@ public class DecorationItem extends Item implements ContentItem.ContentDataItem<
 	@Override
 	public DecorationData getData(TagCW compound){
 		return new DecorationData(deco).read(compound);
+	}
+
+	@Override
+	public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ){
+		if(world.isRemote || player.isSneaking()) return EnumActionResult.PASS;
+		EntityW ply = UniEntity.getEntity(player);
+		SystemManager.run(SystemManager.Systems.DECO, ply.getWorld(), DecoSystem.class, sys -> {
+			sys.addNewDeco(ply, UniStack.getStack(player.getHeldItem(hand)), new QV3D(pos.getX() + hitX, pos.getY() + hitY, pos.getZ() + hitZ));
+		});
+		return EnumActionResult.SUCCESS;
 	}
 
 }

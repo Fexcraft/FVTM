b98a816f5e1b11126dd0021311d95a3d0b0d294d
Ferdinand Calo <fexcalo@gmail.com>
2026-07-23 00:08:52 +0200

    Update WireRenderer.java

--- fabric26/src/main/java/net/fexcraft/mod/fvtm/render/WireRenderer.java
+++ fabric26/src/main/java/net/fexcraft/mod/fvtm/render/WireRenderer.java
@@ -14,8 +14,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.world.item.ItemStack;
 
 import static net.fexcraft.mod.fvtm.FVTMC.LEVEL_RS_KEY;
-import static net.fexcraft.mod.fvtm.data.ToolboxType.WIRE_REMOVAL;
-import static net.fexcraft.mod.fvtm.data.ToolboxType.WIRE_SLACK;
+import static net.fexcraft.mod.fvtm.data.ToolboxType.*;
 import static net.fexcraft.mod.fvtm.item.ToolboxItem.getToolboxType;
 import static net.fexcraft.mod.fvtm.util.DebugUtils.COL_CYN;
 import static net.fexcraft.mod.fvtm.util.DebugUtils.COL_ORG;
@@ -29,8 +28,9 @@ public class WireRenderer {
 	private static ItemStack held;
 	private static boolean holding_wire;
 	private static boolean holding_slack;
-	private static boolean holding_relaydeco;
-	private static boolean holding_deco;
+	private static boolean holding_comp_relay;
+	private static boolean holding_comp;
+	private static boolean holding_comp_rem;
 
 	public static void renderWires(LevelRenderContext context){
 		wiredata = SystemManager.get(SystemManager.Systems.WIRE, context.levelState().getData(LEVEL_RS_KEY).key);
@@ -38,12 +38,13 @@ public class WireRenderer {
 		held = Minecraft.getInstance().player.getMainHandItem();
 		holding_wire = Config.DEBUG_ACTIVE || held.getItem() instanceof WireItem || (held.getItem() instanceof ToolboxItem && WIRE_REMOVAL.eq(getToolboxType(held)));
 		holding_slack = Config.DEBUG_ACTIVE || held.getItem() instanceof ToolboxItem && WIRE_SLACK.eq(getToolboxType(held));
+		holding_comp_rem = held.getItem() instanceof ToolboxItem && WIRE_COMPONENT.eq(getToolboxType(held));
 		if(held.getItem() instanceof WireCompItem){
 			WireCompItem item = (WireCompItem)held.getItem();
-			holding_relaydeco = item.getContent().getType().equals("relay");
-			holding_deco = !holding_relaydeco;
+			holding_comp_relay = item.getContent().getType().equals("relay");
+			holding_comp = !holding_comp_relay;
 		}
-		else holding_relaydeco = holding_deco = false;
+		else holding_comp_relay = holding_comp = false;
 		//
 		double cx = context.levelState().cameraRenderState.pos.x;
 		double cy = context.levelState().cameraRenderState.pos.y;
@@ -69,14 +70,14 @@ public class WireRenderer {
 						}
 					}
 					if(relay.wires.size() > 0){
-						if(holding_slack || holding_deco){
+						if(holding_slack || holding_comp || holding_comp_rem){
 							for(Wire wire : relay.wires){
 								if(wire.copy) continue;
 								RenderUtil26.renderBB(wire.getVectorPosition(wire.length * 0.5, false),
 									holder.hasRef() ? holder.ref().getSize(relay.getKey()) * 2 : 0.25f, COL_ORG);
 							}
 						}
-						if(holding_relaydeco){
+						if(holding_comp_relay || holding_comp_rem){
 							for(Wire wire : relay.wires){
 								RenderUtil26.renderBB(wire.getVectorPosition(holder.hasRef() ? holder.ref().getSize(relay.getKey()) * 2 : 0.25f, false),
 									holder.hasRef() ? holder.ref().getSize(relay.getKey()) * 2 : 0.25f, COL_CYN);

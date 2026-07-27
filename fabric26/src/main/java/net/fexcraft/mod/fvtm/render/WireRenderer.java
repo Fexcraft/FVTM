d853f5bc8b4e296c85eca455215ede725fbde4a2
Ferdinand Calo <fexcalo@gmail.com>
2026-07-27 18:10:41 +0200

    Update WireRenderer.java

--- fabric26/src/main/java/net/fexcraft/mod/fvtm/render/WireRenderer.java
+++ fabric26/src/main/java/net/fexcraft/mod/fvtm/render/WireRenderer.java
@@ -3,6 +3,7 @@ package net.fexcraft.mod.fvtm.render;
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.fabricmc.fabric.api.client.rendering.v1.level.LevelRenderContext;
 import net.fexcraft.mod.fvtm.Config;
+import net.fexcraft.mod.fvtm.data.WireComponent;
 import net.fexcraft.mod.fvtm.item.ToolboxItem;
 import net.fexcraft.mod.fvtm.item.WireCompItem;
 import net.fexcraft.mod.fvtm.item.WireItem;
@@ -16,8 +17,6 @@ import net.minecraft.world.item.ItemStack;
 import static net.fexcraft.mod.fvtm.FVTMC.LEVEL_RS_KEY;
 import static net.fexcraft.mod.fvtm.data.ToolboxType.*;
 import static net.fexcraft.mod.fvtm.item.ToolboxItem.getToolboxType;
-import static net.fexcraft.mod.fvtm.util.DebugUtils.COL_CYN;
-import static net.fexcraft.mod.fvtm.util.DebugUtils.COL_ORG;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -25,8 +24,10 @@ import static net.fexcraft.mod.fvtm.util.DebugUtils.COL_ORG;
 public class WireRenderer {
 
 	private static WireSystem wiredata;
+	private static WireComponent comp;
 	private static ItemStack held;
 	private static boolean holding_wire;
+	private static boolean holding_rem;
 	private static boolean holding_slack;
 	private static boolean holding_comp_relay;
 	private static boolean holding_comp;
@@ -36,12 +37,13 @@ public class WireRenderer {
 		wiredata = SystemManager.get(SystemManager.Systems.WIRE, context.levelState().getData(LEVEL_RS_KEY).key);
 		if(wiredata == null || wiredata.getRegions() == null) return;
 		held = Minecraft.getInstance().player.getMainHandItem();
-		holding_wire = Config.DEBUG_ACTIVE || held.getItem() instanceof WireItem || (held.getItem() instanceof ToolboxItem && WIRE_REMOVAL.eq(getToolboxType(held)));
+		holding_wire = Config.DEBUG_ACTIVE || held.getItem() instanceof WireItem;
+		holding_rem = Config.DEBUG_ACTIVE || (held.getItem() instanceof ToolboxItem && WIRE_REMOVAL.eq(getToolboxType(held)));
 		holding_slack = Config.DEBUG_ACTIVE || held.getItem() instanceof ToolboxItem && WIRE_SLACK.eq(getToolboxType(held));
 		holding_comp_rem = held.getItem() instanceof ToolboxItem && WIRE_COMPONENT.eq(getToolboxType(held));
 		if(held.getItem() instanceof WireCompItem){
-			WireCompItem item = (WireCompItem)held.getItem();
-			holding_comp_relay = item.getContent().getType().equals("relay");
+			WireComponent comp = ((WireCompItem)held.getItem()).getContent();
+			holding_comp_relay = comp.getType().equals("relay");
 			holding_comp = !holding_comp_relay;
 		}
 		else holding_comp_relay = holding_comp = false;
@@ -59,32 +61,7 @@ public class WireRenderer {
 				for(WireRelay relay : holder.relays.values()){
 					//TODO Renderer26.light = LevelRenderer.getLightColor(camera.getEntity().level(), pos.set(relay.pos.x, relay.pos.y + 0.1, relay.pos.z));
 					//TODO frustum check
-					if(Config.DEBUG_ACTIVE || holding_wire){
-						RenderUtil26.renderBB(relay.pos, holder.hasRef() ? holder.ref().getSize(relay.getKey()) * 2 : 0.25f, COL_CYN);
-					}
-					if((Config.DEBUG_ACTIVE || holding_slack) && relay.wires.size() > 0){
-						for(Wire wire : relay.wires){
-							if(wire.copy) continue;
-							RenderUtil26.renderBB(wire.getVectorPosition(wire.length * 0.5, false),
-								holder.hasRef() ? holder.ref().getSize(relay.getKey()) * 2 : 0.25f, COL_ORG);
-						}
-					}
-					if(relay.wires.size() > 0){
-						if(holding_slack || holding_comp || holding_comp_rem){
-							for(Wire wire : relay.wires){
-								if(wire.copy) continue;
-								RenderUtil26.renderBB(wire.getVectorPosition(wire.length * 0.5, false),
-									holder.hasRef() ? holder.ref().getSize(relay.getKey()) * 2 : 0.25f, COL_ORG);
-							}
-						}
-						if(holding_comp_relay || holding_comp_rem){
-							for(Wire wire : relay.wires){
-								RenderUtil26.renderBB(wire.getVectorPosition(holder.hasRef() ? holder.ref().getSize(relay.getKey()) * 2 : 0.25f, false),
-									holder.hasRef() ? holder.ref().getSize(relay.getKey()) * 2 : 0.25f, COL_CYN);
-							}
-						}
-					}
-					UniWireRenderer.renderRelay(relay, 0, 0, 0);
+					UniWireRenderer.renderRelay(holder, relay, 0, 0, 0, holding_wire, holding_rem, holding_slack, holding_comp, holding_comp_relay, holding_comp_rem, comp);
 				}
 			}
 		}

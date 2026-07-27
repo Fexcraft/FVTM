78f9054fccf89e93e5a720b6fbff108b41bafec3
Ferdinand Calo <fexcalo@gmail.com>
2026-07-27 18:50:13 +0200

    Update WireRenderer.java

--- forge20/src/main/java/net/fexcraft/mod/fvtm/render/WireRenderer.java
+++ forge20/src/main/java/net/fexcraft/mod/fvtm/render/WireRenderer.java
@@ -24,6 +24,7 @@ import net.minecraftforge.fml.common.Mod;
 import static net.fexcraft.mod.fvtm.FvtmResources.WHITE_TEXTURE;
 import static net.fexcraft.mod.fvtm.data.ToolboxType.*;
 import static net.fexcraft.mod.fvtm.item.ToolboxItem.getToolboxType;
+import static net.fexcraft.mod.fvtm.render.UniWireRenderer.DATA;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -32,14 +33,7 @@ import static net.fexcraft.mod.fvtm.item.ToolboxItem.getToolboxType;
 public class WireRenderer {
 
 	private static WireSystem wiredata;
-	private static WireComponent comp;
 	private static ItemStack held;
-	private static boolean holding_wire;
-	private static boolean holding_rem;
-	private static boolean holding_slack;
-	private static boolean holding_comp_relay;
-	private static boolean holding_comp;
-	private static boolean holding_comp_rem;
 
 	@SubscribeEvent
 	public static void renderWires(RenderLevelStageEvent event){
@@ -48,16 +42,17 @@ public class WireRenderer {
 		wiredata = SystemManager.get(SystemManager.Systems.WIRE, WrapperHolder.getWorld(event.getCamera().getEntity().level()), WireSystem.class);
 		if(wiredata == null || wiredata.getRegions() == null) return;
 		held = Minecraft.getInstance().player.getMainHandItem();
-		holding_wire = Config.DEBUG_ACTIVE || held.getItem() instanceof WireItem;
-		holding_rem = Config.DEBUG_ACTIVE || (held.getItem() instanceof ToolboxItem && WIRE_REMOVAL.eq(getToolboxType(held)));
-		holding_slack = Config.DEBUG_ACTIVE || held.getItem() instanceof ToolboxItem && WIRE_SLACK.eq(getToolboxType(held));
-		holding_comp_rem = held.getItem() instanceof ToolboxItem && WIRE_COMPONENT.eq(getToolboxType(held));
+		DATA.ih_wire = held.getItem() instanceof WireItem;
+		if(DATA.ih_wire) DATA.wiretype = ((WireItem)held.getItem()).getContent();
+		DATA.ih_tb_rem = held.getItem() instanceof ToolboxItem && WIRE_REMOVAL.eq(getToolboxType(held));
+		DATA.ih_tb_slack = held.getItem() instanceof ToolboxItem && WIRE_SLACK.eq(getToolboxType(held));
+		DATA.ih_tb_comp = held.getItem() instanceof ToolboxItem && WIRE_COMPONENT.eq(getToolboxType(held));
 		if(held.getItem() instanceof WireCompItem){
-			comp = ((WireCompItem)held.getItem()).getContent();
-			holding_comp_relay = comp.getType().equals("relay");
-			holding_comp = !holding_comp_relay;
+			DATA.comp = ((WireCompItem)held.getItem()).getContent();
+			DATA.ih_comp_relay = DATA.comp.getType().equals("relay");
+			DATA.ih_comp = !DATA.ih_comp_relay;
 		}
-		else holding_comp_relay = holding_comp = false;
+		else DATA.ih_comp_relay = DATA.ih_comp = false;
 		//
 		Camera camera = event.getCamera();
 		double cx = camera.getPosition().x;
@@ -74,7 +69,7 @@ public class WireRenderer {
 				for(WireRelay relay : holder.relays.values()){
 					Renderer20.light = LevelRenderer.getLightColor(camera.getEntity().level(), pos.set(relay.pos.x, relay.pos.y + 0.1, relay.pos.z));
 					//TODO frustum check
-					UniWireRenderer.renderRelay(holder, relay, 0, 0, 0, holding_wire, holding_rem, holding_slack, holding_comp, holding_comp_relay, holding_comp_rem, comp);
+					UniWireRenderer.renderRelay(holder, relay, 0, 0, 0, DATA);
 				}
 			}
 		}

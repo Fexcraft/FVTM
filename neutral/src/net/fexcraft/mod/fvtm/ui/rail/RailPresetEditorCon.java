136eadb08aa5ebde35ae5ce52a66dc9f33ae281c
Ferdinand Calo <fexcalo@gmail.com>
2026-08-17 11:58:54 +0200

    Update RailPresetEditorCon.java

--- neutral/src/net/fexcraft/mod/fvtm/ui/rail/RailPresetEditorCon.java
+++ neutral/src/net/fexcraft/mod/fvtm/ui/rail/RailPresetEditorCon.java
@@ -2,12 +2,15 @@ package net.fexcraft.mod.fvtm.ui.rail;
 
 import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.lib.common.math.V3I;
+import net.fexcraft.mod.fvtm.sys.rail.RailPlacingUtil;
 import net.fexcraft.mod.fvtm.sys.rail.RailSystem;
 import net.fexcraft.mod.fvtm.sys.uni.SystemManager;
 import net.fexcraft.mod.uni.UniEntity;
 import net.fexcraft.mod.uni.inv.StackWrapper;
 import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.ui.ContainerInterface;
+
+import java.util.UUID;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -38,7 +41,7 @@ public class RailPresetEditorCon extends ContainerInterface {
 			case "on":{
 				stack.updateTag(tag -> {
 					tag.set("fvtm:preset_mode", true);
-					tag.set("fvtm:rail_preset", com.getInteger("sel"));
+					tag.set("fvtm:rail_preset", com.getString("sel"));
 				});
 				return;
 			}
@@ -50,9 +53,9 @@ public class RailPresetEditorCon extends ContainerInterface {
 				return;
 			}
 			case "sel":{
-				int sel = com.getInteger("sel");
+				String sel = com.getBoolean("nosel") ? null : com.getString("sel");
 				stack.updateTag(tag -> {
-					if(sel >= 0){
+					if(sel != null){
 						tag.set("fvtm:preset_mode", true);
 						tag.set("fvtm:rail_preset", sel);
 					}
@@ -61,6 +64,13 @@ public class RailPresetEditorCon extends ContainerInterface {
 						//tag.rem("fvtm:rail_preset");
 					}
 				});
+				return;
+			}
+			case "reset":{
+				UUID uuid = RailPlacingUtil.CURRENT.get(player.entity.getUUID());
+				if(uuid == null) return;
+				RailPlacingUtil.NewTrack track = RailPlacingUtil.QUEUE.get(uuid);
+				if(track != null) track.reset();
 				return;
 			}
 		}

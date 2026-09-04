f8510b83e83d12b27d1d02b129e10ac7f030126d
Ferdinand Calo <fexcalo@gmail.com>
2026-09-04 21:17:15 +0200

    Update FvtmBlockModelLoader.java

--- forge12/src/main/java/net/fexcraft/mod/fvtm/render/block/FvtmBlockModelLoader.java
+++ forge12/src/main/java/net/fexcraft/mod/fvtm/render/block/FvtmBlockModelLoader.java
@@ -2,8 +2,14 @@ package net.fexcraft.mod.fvtm.render.block;
 
 import net.fexcraft.mod.fvtm.FvtmResources;
 import net.fexcraft.mod.fvtm.data.block.Block;
+import net.fexcraft.mod.fvtm.model.Model;
+import net.fexcraft.mod.fvtm.model.ModelGroup;
+import net.fexcraft.mod.fvtm.model.program.BakedPrograms;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.color.IBlockColor;
 import net.minecraft.client.resources.IResourceManager;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.biome.Biome;
 import net.minecraftforge.client.model.ICustomModelLoader;
 import net.minecraftforge.client.model.IModel;
 
@@ -48,6 +54,37 @@ public class FvtmBlockModelLoader implements ICustomModelLoader {
 
 	public static FvtmBlockModelLoader getInstance(){
 		return INSTANCE;
+	}
+
+	public static void registerTint(){
+		IBlockColor ibc_g = (state, world, pos, t) -> {
+			Biome bio = world.getBiome(pos);
+			return bio.getGrassColorAtPos(pos);
+		};
+		IBlockColor ibc_f = (state, world, pos, t) -> {
+			Biome bio = world.getBiome(pos);
+			return bio.getFoliageColorAtPos(pos);
+		};
+		HashSet<Block> tint_g = new HashSet<>();
+		HashSet<Block> tint_f = new HashSet<>();
+		BakedPrograms.Tint tint;
+		for(Block block : FvtmBlockModelLoader.BLOCKS.values()){
+			Model model = block.getModel();
+			if(model == null) return;
+			for(ModelGroup group : model.getGroups()){
+				tint = group.getProgram("fvtm:baked_tint");
+				if(tint == null) continue;
+				if(tint.grass) tint_g.add(block);
+				else tint_f.add(block);
+				break;
+			}
+		}
+		for(Block block : tint_g){
+			Minecraft.getMinecraft().getBlockColors().registerBlockColorHandler(ibc_g, (net.minecraft.block.Block)block.getBlock());
+		}
+		for(Block block : tint_f){
+			Minecraft.getMinecraft().getBlockColors().registerBlockColorHandler(ibc_f, (net.minecraft.block.Block)block.getBlock());
+		}
 	}
 
 }

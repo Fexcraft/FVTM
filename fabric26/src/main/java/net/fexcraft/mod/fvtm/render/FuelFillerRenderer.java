f5cc95e4d3691c21046ce4968ef2d85e48f6d621
Ferdinand Calo <fexcalo@gmail.com>
2026-09-12 21:08:08 +0200

    Update FuelFillerRenderer.java

--- fabric26/src/main/java/net/fexcraft/mod/fvtm/render/FuelFillerRenderer.java
+++ fabric26/src/main/java/net/fexcraft/mod/fvtm/render/FuelFillerRenderer.java
@@ -2,12 +2,9 @@ package net.fexcraft.mod.fvtm.render;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import net.fexcraft.lib.common.Static;
-import net.fexcraft.mod.fvtm.FvtmResources;
 import net.fexcraft.mod.fvtm.block.FuelFillerEntity;
 import net.fexcraft.mod.fvtm.model.DefaultModel;
-import net.fexcraft.mod.fvtm.model.ModelData;
-import net.fexcraft.mod.fvtm.model.content.BlockModel;
-import net.fexcraft.mod.fvtm.util.DebugUtils;
+import net.fexcraft.mod.fvtm.model.InternalModels;
 import net.fexcraft.mod.uni.IDL;
 import net.fexcraft.mod.uni.IDLManager;
 import net.minecraft.client.renderer.SubmitNodeCollector;
@@ -27,7 +24,6 @@ import static net.fexcraft.mod.fvtm.render.RenderUtil.RENDER_UTIL;
 public class FuelFillerRenderer implements BlockEntityRenderer<FuelFillerEntity, BlockEntityRenderState> {
 
 	public static final IDL TEXTURE = IDLManager.getIDLCached("fvtm:textures/block/fuelfiller.png");
-	private static BlockModel MODEL;
 
 	@Override
 	public BlockEntityRenderState createRenderState(){
@@ -41,9 +37,7 @@ public class FuelFillerRenderer implements BlockEntityRenderer<FuelFillerEntity,
 		pose.translate(0.5, 0, 0.5);
 		Direction dir = state.blockState.getValue(FACING);
 		pose.mulPose(new Quaternionf().rotateAxis(Static.toRadians(dir.getAxis() == Direction.Axis.Z ? dir.toYRot() + 90 : dir.toYRot() - 90), AY));
-		if(MODEL == null) MODEL = (BlockModel)FvtmResources.getModel("fvtm:models/block/fuelfiller.fmf", new ModelData(), BlockModel.class);
-		if(MODEL != null) RENDER_UTIL.render(MODEL, DefaultModel.RENDERDATA_BLANK);
-		else RENDER_UTIL.render(DebugUtils.SPHERE);
+		RENDER_UTIL.render(InternalModels.FUEL_FILLER, DefaultModel.RENDERDATA_BLANK);
 		pose.popPose();
 	}
 
